c86821fcbb4c621a0106edebe525873da26cbef2
Joachim Zeelmaekers <joachimzeelmaekers@MacBook-Pro-4.local>
2019-01-20 18:07:16 +0100

    implemented the ability to open and close the sidemenu.

--- app/controllers/sessions/session/agendas.js
+++ app/controllers/sessions/session/agendas.js
@@ -1,9 +1,18 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+	agendaMenuOpened: true,
+
 	actions: {
 		navigateToSubCases() {
 			this.transitionToRoute('sessions.session.agendas.agenda.subcases');
+		},
+
+		compareAgendas() {
+			
+		},
+		collapseSideMenu() {
+			this.set('agendaMenuOpened', !this.get('agendaMenuOpened'));
 		}
 	}
 });
--- app/routes/sessions.js
+++ app/routes/sessions.js
@@ -4,11 +4,12 @@ export default Route.extend({
 	model() {
 		let date = new Date();
 		date.setHours(0, 0, 0, 0);
-
 		return this.store.query('session', {
 			filter: {
+				':gt:plannedstart': "",
 				':gte:plannedstart': date.toISOString()
-			}
+			},
+			sort: "number"
 		});
 	},
 
