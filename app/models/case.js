8a510ea23d5ad80210e0a411723efe7ca2f08278
Joachim Zeelmaekers <joachimzeelmaekers@macbook-pro-4.iconos.be>
2019-02-20 14:29:56 +0100

    added mandatee titles

--- app/models/case.js
+++ app/models/case.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
 
 const { Model, attr, hasMany, belongsTo } = DS;
 
@@ -14,5 +15,15 @@ export default Model.extend({
   themes: hasMany('theme'),
   subcases: hasMany('subcase'),
   related: hasMany('case'),
-  creators: hasMany('person')
+  creators: hasMany('person'),
+
+  latestSubcase: computed('subcases', function() {
+    const subcases = this.get('subcases');
+    if(subcases && subcases.length > 0) {
+      const currentSubcase = subcases.sortBy('created').get('lastObject');
+      return currentSubcase;
+    } else {
+      return {title:"Nog geen procedurestap."}
+    }
+  })
 });
