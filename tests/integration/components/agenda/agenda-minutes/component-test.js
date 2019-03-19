1c9b70bb49184d11156ed61ad0cf4ea59b424b37
Joachim Zeelmaekers <joachimzeelmaekers@Macbook-van-Joachim.local>
2019-03-19 13:10:30 +0100

    added printable minutes for the agenda

--- tests/integration/components/agenda/agenda-minutes/component-test.js
+++ tests/integration/components/agenda/agenda-minutes/component-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | agenda/agendaitem/agendaitem-notes', function(hooks) {
+module('Integration | Component | agenda/agenda-minutes', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{agenda/agendaitem/agendaitem-notes}}`);
+    await render(hbs`{{agenda/agenda-minutes}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#agenda/agendaitem/agendaitem-notes}}
+      {{#agenda/agenda-minutes}}
         template block text
-      {{/agenda/agendaitem/agendaitem-notes}}
+      {{/agenda/agenda-minutes}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
