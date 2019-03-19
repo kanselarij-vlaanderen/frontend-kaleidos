f3fbc83dfc4df5ea29bb925d5965a5529c1cb6d9
Joachim Zeelmaekers <joachimzeelmaekers@Macbook-van-Joachim.local>
2019-03-19 17:05:56 +0100

    implemented editing function for themes

--- app/components/agenda/agendaitem/agendaitem-case/subcase-themes/component.js
+++ app/components/agenda/agendaitem/agendaitem-case/subcase-themes/component.js
@@ -1,5 +1,37 @@
 import Component from '@ember/component';
+import { inject } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Component.extend({
-	classNames: ["vl-u-spacer--large"]
+	store: inject(),
+	classNames: ["vl-u-spacer--large"],
+	isEditing: false,
+	subcase: null,
+
+	selectedThemes: computed('subcase', function() {
+		return this.get('subcase.themes');
+	}),
+
+	actions: {
+		toggleIsEditing() {
+			this.toggleProperty('isEditing');
+		},
+
+		cancelEditing() {
+			this.set('selectedThemes', this.get('subcase.themes'));
+			this.toggleProperty('isEditing');
+		},
+
+		chooseTheme(themes) {
+			this.set('selectedThemes', themes);
+		},
+
+		saveChanges(subcase) {
+			const subcaseModel = this.store.peekRecord('subcase', subcase.get('id'));
+			subcaseModel.set('themes', this.get('selectedThemes'));
+			subcaseModel.save().then(() => {
+				this.toggleProperty('isEditing');
+			});
+		}
+	}
 });
