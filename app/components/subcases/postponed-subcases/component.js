761615e1ab86845e23453dda5cd2b409e9a804d2
Joachim Zeelmaekers <joachimzeelmaekers@Macbook-van-Joachim.local>
2019-04-01 12:06:52 +0200

    fixed issue where the subcases were not set to an empty array

--- app/components/subcases/postponed-subcases/component.js
+++ app/components/subcases/postponed-subcases/component.js
@@ -1,30 +1,11 @@
 import Component from '@ember/component';
-import { inject } from '@ember/service';
-import DefaultQueryParamsMixin from 'ember-data-table/mixins/default-query-params';
 
-export default Component.extend(DefaultQueryParamsMixin, {
-  subcasesService: inject(),
-  store: inject(),
+export default Component.extend({
+  
   actions: {
-    selectAllSubCases(subcases) {
-      //this.typeChanged(type);
-    },
     selectPostponedSubcase(subcase) {
       this.selectSubcase(subcase);
     }
   },
-  async didInsertElement() {
-    this._super(...arguments);
-    const ids = await this.get('subcasesService').getPostPonedSubcaseIds();
-    let subcases = [];
-
-    if (ids && ids.length > 0) {
-      subcases = await this.store.query('subcase', {
-        filter: {
-          "id": ids.toString()
-        }
-      });
-    }
-    this.set('subcases', subcases);
-  }
+  
 });
--- app/routes/subcases.js
+++ app/routes/subcases.js
@@ -1,4 +1,31 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
+import { inject } from '@ember/service';
 
 export default Route.extend({
+	subcasesService: inject(),
+
+	async model() {
+		const subcases = await this.store.query('subcase', {
+			filter: {
+				":has-no:agendaitems": "yes"
+			},
+			include: ['agendaitems']
+		});
+
+		subcases.map(subcase => subcase.set('selected', false));
+
+		const ids = await this.get('subcasesService').getPostPonedSubcaseIds();
+		let postPonedSubcases = [];
+
+		if (ids && ids.length > 0) {
+			postPonedSubcases = await this.store.query('subcase', {
+				filter: {
+					"id": ids.toString()
+				}
+			});
+		}
+
+		return hash({ subcases: subcases, postPonedSubcases: postPonedSubcases })
+	}
 });
