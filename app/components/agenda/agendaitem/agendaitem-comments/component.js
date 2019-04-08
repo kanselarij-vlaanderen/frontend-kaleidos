35ba80da5553b54bcad43622674f27499d6394e7
Joachim Zeelmaekers <joachimzeelmaekers@Macbook-van-Joachim.local>
2019-04-08 16:32:52 +0200

    added multiple remarks functionality

--- app/components/agenda/agendaitem/agendaitem-comments/component.js
+++ app/components/agenda/agendaitem/agendaitem-comments/component.js
@@ -1,17 +1,35 @@
 import Component from '@ember/component';
 import { inject } from '@ember/service';
+import { sort } from '@ember/object/computed';
 
 export default Component.extend({
   classNames: ['vl-u-spacer-extended-l'],
   store: inject('store'),
 
+  sortedRemarks: sort('agendaitem.remarks', function(a, b) {
+      if (a.created < b.created) {
+        return 1;
+      } else if (a.created > b.created) {
+        return -1;
+      }
+      return 0;
+    }),
+
   actions: {
     async addComment(event){
       const agendaitem = await this.get('agendaitem');
 
-      let comment = this.store.createRecord('remark', { text:event, created: new Date(), agendaitem: agendaitem });
+      const comment = this.store.createRecord('remark', { text:event, created: new Date(), agendaitem: agendaitem });
       comment.save().then(() => {
         this.set('text', "");
+      });
+    },
+
+    async addAnswer(comment) {
+      const newComment = this.store.createRecord('remark', { text:comment.get('answer'), created: new Date()});
+      newComment.save().then(savedComment => {
+        comment.get('answers').addObject(savedComment);
+        comment.save();
       });
     }
   }
