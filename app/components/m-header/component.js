251ccd265b2ce2123655d254b84210f7be07a626
Joachim Zeelmaekers <joachimzeelmaekers@Macbook-van-Joachim.local>
2019-04-18 14:24:08 +0200

    fixed link-to issues

--- app/components/m-header/component.js
+++ app/components/m-header/component.js
@@ -3,5 +3,6 @@ import isAuthenticatedMixin from 'fe-redpencil/mixins/is-authenticated-mixin';
 
 export default Component.extend(isAuthenticatedMixin, {
 	classNames: ['vlc-navbar','vlc-navbar--bordered-bottom', 'vlc-navbar--no-padding', 'vlc-navbar--bg-alt', 'vlc-navbar--auto', 'c-main-nav'],
-	elementId:'c-main-nav'
+	elementId:'c-main-nav',
+
 });
