059a534ae1974417e0b463aa0ace3df393b4d622
Joachim Zeelmaekers <joachimzeelmaekers@Macbook-van-Joachim.local>
2019-04-26 08:33:24 +0200

    implemented the finished state of newsletter-info, removed unused scss and refactored some scss

--- app/components/news-item/edit-item/component.js
+++ app/components/news-item/edit-item/component.js
@@ -1,11 +1,16 @@
 import Component from '@ember/component';
 import { inject } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Component.extend({
 	store: inject(),
 	classNames: ["vl-form__group vl-u-bg-porcelain"],
 	isEditing: false,
 	documentsSelected: null,
+
+	finished: computed('agendaitem.newsletterInfo', function() {
+		return this.get('agendaitem.newsletterInfo').get('finished');
+	}),
 
 	actions: {
 		toggleIsEditing() {
@@ -18,20 +23,24 @@ export default Component.extend({
 			const newsletterToEdit = await agendaitem.get('newsletterInfo');
 			const newsletterToEditDocuments = await newsletterToEdit.get('documents');
 			const documents = this.get('documentsSelected');
-			await Promise.all(documents.map((document) => {
-				if (document.get('selected')) {
-					newsletterToEdit.get('documents').addObject(document);
-					newsletterToEdit.save();
-				} else {
-					const foundDocument = newsletterToEditDocuments.find((item) => item.get('id') == document.get('id'));
-					if (foundDocument) {
-						newsletterToEdit.get('documents').removeObject(document);
+			if(documents) {
+				await Promise.all(documents.map((document) => {
+					if (document.get('selected')) {
+						newsletterToEdit.get('documents').addObject(document);
 						newsletterToEdit.save();
+					} else {
+						const foundDocument = newsletterToEditDocuments.find((item) => item.get('id') == document.get('id'));
+						if (foundDocument) {
+							newsletterToEdit.get('documents').removeObject(document);
+							newsletterToEdit.save();
+						}
 					}
-				}
-			}))
+				}))
+			}
+			newsletterToEdit.set('finished', this.get('finished'));
 			await newsletterToEdit.save().then((item) => {
 				item.hasMany('documents').reload();
+				item.reload();
 				this.toggleProperty('isEditing');
 			});
 		},
