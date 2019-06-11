a9e7d48ff1a6df877d6f73e129b91464b131056f
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-06-11 13:01:12 +0200

    commit before adding new-data-table

--- app/routes/print-overviews/decisions.js
+++ app/routes/print-overviews/decisions.js
@@ -4,7 +4,7 @@ export default Route.extend({
 	model(params) {
 		const agenda_id = params.agenda_id
 		if (agenda_id) {
-			const agenda = this.store.findRecord('agenda', agenda_id)
+			const agenda = this.store.findRecord('agenda', agenda_id);
 			return agenda;
 		}
 	}
