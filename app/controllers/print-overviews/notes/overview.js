1624c1574719fa659a37ae0bd771425c89ad7227
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-06-13 14:23:55 +0200

    fixed the definite and the design view for the notes

--- app/controllers/print-overviews/notes/overview.js
+++ app/controllers/print-overviews/notes/overview.js
@@ -1,5 +1,14 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import moment from 'moment';
 
+import { inject } from '@ember/service';
 export default Controller.extend({
+	intl: inject(),
+	queryParams: ['definite'],
 
+	title: computed('model.currentAgenda.createdFor', function () {
+		const date = this.get('model.currentAgenda.createdFor.plannedStart');
+		return `${this.intl.t('notes-of-session')} ${moment(date).format('dddd DD-MM-YYYY')}`;
+	}),
 });
