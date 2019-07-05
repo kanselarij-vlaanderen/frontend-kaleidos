536252e1ad1be7742527f4bafeaed186d866f168
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-07-05 15:03:53 +0200

    added decision-approval KAS-426

--- app/models/decision.js
+++ app/models/decision.js
@@ -1,8 +1,11 @@
 import DS from 'ember-data';
+import { computed } from '@ember/object';
+import { inject } from '@ember/service';
 
 let { Model, attr, belongsTo, hasMany } = DS;
 
 export default Model.extend({
+  intl: inject(),
   richtext: attr("string"),
   shortTitle: attr("string"),
   approved: attr('boolean'),
@@ -16,4 +19,8 @@ export default Model.extend({
   documentType: belongsTo('document-type'),
   documentVersions: hasMany('document-version', { inverse: null }),
   signedDocument: belongsTo('document'),
+
+  decisionApproval: computed('signedDocument', function () {
+    return this.intl.t('signed-document-decision', { name: this.get('signedDocument.numberVr') })
+  })
 });
