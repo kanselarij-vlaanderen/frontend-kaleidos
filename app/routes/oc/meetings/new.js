ae0c337d4d71e48e060434355e54dc39fb5f257a
Michaël Dierick <michael@dierick.io>
2019-07-23 15:33:47 +0200

    remove unnecessary custom render hook for new meeting route

--- app/routes/oc/meetings/new.js
+++ app/routes/oc/meetings/new.js
@@ -2,11 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   modelName: "oc-meeting",
-  renderTemplate(controller, model) {
-    this.render('oc.meetings.new', {
-      into: 'oc.meetings',
-      controller: 'oc.meetings.new',
-      model: this.store.createRecord('oc-meeting', {})
-    });
-  }
+  model() {
+    return this.store.createRecord('oc-meeting', {});
+  },
 });
