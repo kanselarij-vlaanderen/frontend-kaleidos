8fb15e1419eddc63bd9d8ad3eebd8deb8fc8596d
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-07-24 13:04:12 +0200

    added a check for the subcases, removed the banner where not necessary, refactored some scss.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,6 +4,7 @@ import { alias } from '@ember/object/computed';
 import { on } from '@ember/object/evented';
 import { inject } from '@ember/service';
 import isAuthenticatedMixin from 'fe-redpencil/mixins/is-authenticated-mixin';
+import CONFIG from 'fe-redpencil/utils/config';
 
 export default Controller.extend(isAuthenticatedMixin, {
 	currentSession: inject(),
@@ -23,6 +24,12 @@ export default Controller.extend(isAuthenticatedMixin, {
 	},
 
 	shouldNavigateObserver: on('init', observer('router.currentRouteName', 'currentSession.userRole', async function () {
+		const currentRouteName = this.router.get('currentRouteName');
+		if(CONFIG.routesAllowed.includes(currentRouteName)) {
+			document.getElementById('vlaanderen-header').style = "display:none;";
+		} else {
+			document.getElementById('vlaanderen-header').style = "display:block;";
+		}
 		const router = this.get('router');
 		const role = await this.get('currentSession.userRole');
 		const user = await this.get('session.isAuthenticated');
