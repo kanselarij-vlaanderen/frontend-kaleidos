9ddbc9b2862ea280348b809fc7e744bef041cbde
Michaël Dierick <michael@dierick.io>
2019-07-25 11:36:07 +0200

    Agenda-items overview header

---
+++ app/components/oc/agenda-header/component.js
@@ -0,0 +1,24 @@
+import Component from '@ember/component';
+import { inject } from '@ember/service';
+import { alias, filter } from '@ember/object/computed';
+import isAuthenticatedMixin from 'fe-redpencil/mixins/is-authenticated-mixin';
+
+export default Component.extend(isAuthenticatedMixin, {
+	store: inject(),
+	classNames: ["vlc-page-header"],
+
+	meeting: undefined,
+	selectedItem: undefined,
+	
+	dropdownOpen: false,
+
+	actions: {
+		toggleDropdown() {
+			this.toggleProperty('dropdownOpen');
+		},
+	},
+
+	changeLoading() {
+		this.loading();
+	},
+});
