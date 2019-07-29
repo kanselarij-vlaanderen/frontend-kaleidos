dc8c50b9a78953d37277aeeb8d38a66e2d095f3f
Michaël Dierick <michael@dierick.io>
2019-07-29 10:07:44 +0200

    Linting

--- app/models/oc-agendaitem.js
+++ app/models/oc-agendaitem.js
@@ -1,5 +1,4 @@
 import DS from 'ember-data';
-import { computed } from '@ember/object';
 let { Model, attr, belongsTo, hasMany } = DS;
 
 export default Model.extend({
--- app/models/oc-case.js
+++ app/models/oc-case.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
-import { computed } from '@ember/object';
-let { Model, attr, belongsTo, hasMany } = DS;
+let { Model, attr, hasMany } = DS;
 
 export default Model.extend({
   identifier: attr('string'),
