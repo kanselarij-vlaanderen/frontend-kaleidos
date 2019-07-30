530cca23b75b230287bf01a66b913cd8e8416e25
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-07-30 11:55:05 +0200

    refactored the views of the agendaitems and removed some lint errors

--- app/routes/cases/overview.js
+++ app/routes/cases/overview.js
@@ -1,9 +1,8 @@
-import Route from '@ember/routing/route';
-import DataTableRouteMixin from 'ember-data-table/mixins/route';
-import CONFIG from 'fe-redpencil/utils/config';
-import $ from 'jquery';
-import { isEmpty } from '@ember/utils';
-import iseCode from '../../models/ise-code';
+import Route from "@ember/routing/route";
+import DataTableRouteMixin from "ember-data-table/mixins/route";
+import CONFIG from "fe-redpencil/utils/config";
+import $ from "jquery";
+import { isEmpty } from "@ember/utils";
 
 export default Route.extend(DataTableRouteMixin, {
   modelName: "case",
@@ -32,7 +31,7 @@ export default Route.extend(DataTableRouteMixin, {
     }
   },
 
-  textSearchFields: ['title', 'data', 'subcaseTitle', 'subcaseSubTitle'],
+  textSearchFields: ["title", "data", "subcaseTitle", "subcaseSubTitle"],
 
   mergeQueryOptions(params) {
     let filter = {};
@@ -52,46 +51,51 @@ export default Route.extend(DataTableRouteMixin, {
     };
   },
 
-  wantsFilteredResults(params){
-    return !isEmpty(params.searchText) || !isEmpty(params.dateFrom) || !isEmpty(params.dateTo) ||
-    !isEmpty(params.mandatees) || !isEmpty(params.decisionsOnly);
+  wantsFilteredResults(params) {
+    return (
+      !isEmpty(params.searchText) ||
+      !isEmpty(params.dateFrom) ||
+      !isEmpty(params.dateTo) ||
+      !isEmpty(params.mandatees) ||
+      !isEmpty(params.decisionsOnly)
+    );
   },
 
   async model(params) {
-    if(! this.wantsFilteredResults(params)){
+    if (!this.wantsFilteredResults(params)) {
       return this._super(...arguments);
     }
     let filterString = [];
-    let type = 'cases';
-    if(!isEmpty(params.decisionsOnly)){
-      type = 'casesByDecisionText';
+    let type = "cases";
+    if (!isEmpty(params.decisionsOnly)) {
+      type = "casesByDecisionText";
     }
-    if(!isEmpty(params.searchText)){
-      filterString.push(`filter[${this.textSearchFields.join(',')}]=${params.searchText || ''}`);
+    if (!isEmpty(params.searchText)) {
+      filterString.push(
+        `filter[${this.textSearchFields.join(",")}]=${params.searchText || ""}`
+      );
     }
-    if(!isEmpty(params.mandatees)){
+    if (!isEmpty(params.mandatees)) {
       filterString.push(`filter[creators,mandatees]=${params.mandatees}`);
     }
-    if(!isEmpty(params.dateFrom)){
+    if (!isEmpty(params.dateFrom)) {
       filterString.push(`filter[:gte:sessionDates]=${params.dateFrom}`);
     }
-    if(!isEmpty(params.dateTo)){
+    if (!isEmpty(params.dateTo)) {
       filterString.push(`filter[:lte:sessionDates]=${params.dateTo}`);
     }
-    let searchResults = await $.ajax(
-      {
-        method: "GET",
-        url: `/${type}/search?${filterString.join("&")}`
-      }
-    );
+    let searchResults = await $.ajax({
+      method: "GET",
+      url: `/${type}/search?${filterString.join("&")}`
+    });
 
-    if( !searchResults.data || searchResults.data.length < 1) {
+    if (!searchResults.data || searchResults.data.length < 1) {
       return [];
     }
 
-    return this.store.query(this.get('modelName'), {
+    return this.store.query(this.get("modelName"), {
       filter: {
-        id: searchResults.data.map((item) => item.id).join(",")
+        id: searchResults.data.map(item => item.id).join(",")
       }
     });
   },
