17c663aba608bfd19d9656da38ff9e40c1f33e6d
Michaël Dierick <michael.dierick@redpencil.io>
2019-08-22 14:13:57 +0200

    Supplementary document sorting by name

--- app/models/subcase.js
+++ app/models/subcase.js
@@ -97,7 +97,7 @@ export default Model.extend({
             // Sorting is done in the frontend to work around a Virtuoso issue, where
             // FROM-statements for multiple graphs, combined with GROUP BY, ORDER BY results in
             // some items not being returned. By not having a sort parameter, this doesn't occur.
-            return documents.sortBy('type.priority');
+            return documents.sortBy('type.priority', 'numberVr');
           });
         }
       })
