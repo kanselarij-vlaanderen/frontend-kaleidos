3f68c0c8b030d2c7679b8ded478daaeb44e194ca
Michaël Dierick <michael.dierick@redpencil.io>
2019-08-22 16:34:54 +0200

    File uploading: document name without extension + refactoring

--- app/models/file.js
+++ app/models/file.js
@@ -2,12 +2,26 @@ import DS from 'ember-data';
 
 const { Model, attr, belongsTo } = DS;
 import { alias } from '@ember/object/computed';
+import { computed } from '@ember/object';
 
 export default Model.extend({
 	documentVersion: belongsTo('document-version'),
 	signature: belongsTo('signature', { inverse: null }),
 
 	filename: attr('string'),
+  filenameWithoutExtension: computed('filename', {
+    get() {
+      const ext = this.get('extension');
+      const regex = new RegExp('\\.' + ext + '$');
+      return this.get('filename').replace(regex, '');
+    },
+    set(key, value) {
+      const filename = `${value}.${this.get('extension')}`;
+      this.set('filename', filename);
+      return value;
+    }
+  }),
+
 	format: attr('string'),
 	size: attr('number'),
 	extension: attr('string'),
