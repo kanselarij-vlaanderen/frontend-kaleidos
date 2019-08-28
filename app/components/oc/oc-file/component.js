1841e9a72e38fb9ae83ebe273324c0afce8a7fb7
Michaël Dierick <michael.dierick@redpencil.io>
2019-08-28 12:32:19 +0200

    Integrate vl-uploaded-document for oc-file. Refactors out Filesavermixin

--- app/components/oc/oc-file/component.js
+++ app/components/oc/oc-file/component.js
@@ -1,11 +1,8 @@
 import Component from "@ember/component";
 import { computed } from "@ember/object";
 import { inject as service } from "@ember/service";
-import FileSaverMixin from 'ember-cli-file-saver/mixins/file-saver';
-import $ from 'jquery';
 
-export default Component.extend(FileSaverMixin, {
-  classNames: ["vl-uploaded-document", "vlc-document-card"],
+export default Component.extend({
   showDeleteWarning: false,
   isLoading: false,
   globalError: service(),
@@ -17,16 +14,6 @@ export default Component.extend(FileSaverMixin, {
   }),
 
   actions: {
-    download() {
-      const file = this.get('model')
-      return $.ajax(file.get('downloadLink'), {
-        method: 'GET',
-        dataType: 'blob',
-        processData: false
-      })
-        .then((content) => this.saveFileAs(file.get('filename'), content, file.get('contentType')));
-    },
-
     promptDelete() {
       this.set("showDeleteWarning", true);
       this.get("model").deleteRecord();
