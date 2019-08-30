e03f3850816ff5bb01703afa2d7ea30a8c8f31b0
Michaël Dierick <michael.dierick@redpencil.io>
2019-08-30 12:08:38 +0200

    linting

--- app/services/mu-search.js
+++ app/services/mu-search.js
@@ -52,7 +52,7 @@ export default Service.extend({
             }
           }
           return resourceResults;
-        })
+        });
       } else {
         return Promise.resolve(A([]));
       }
