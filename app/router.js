5d0e7a706d948a57696d3c1bb234acd8b1ae78ac
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-09-03 07:59:05 +0200

    bugfix for KAS-729

--- app/router.js
+++ app/router.js
@@ -30,6 +30,7 @@ Router.map(function() {
   this.route('settings', { path: '/instellingen' }, function() {
     this.route('ministers', { path: '/ministers' });
     this.route('overview', { path: '/overzicht' });
+    this.route('users', { path:"/gebruikers"});
   });
   this.route('loading', { path: '/laden' });
   this.route('mock-login');
