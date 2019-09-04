ea37829c2ee522519bfa713702f8078c2ef3497c
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-09-04 10:54:45 +0200

    fixed the functionality to save documentversions to agendaitems and it's subcases.

--- app/components/agenda/agendaitem/agendaitem-case/subcase-documents/component.js
+++ app/components/agenda/agendaitem/agendaitem-case/subcase-documents/component.js
@@ -40,11 +40,13 @@ export default Component.extend(
       },
 
       async deleteAll() {
-        await Promise.all(this.get('documentsInCreation').map((document) => {
-          return this.deleteDocument(document).then(() => {
-            this.get('documentsInCreation').removeObject(document);
+        await Promise.all(
+          this.get('documentsInCreation').map((document) => {
+            return this.deleteDocument(document).then(() => {
+              this.get('documentsInCreation').removeObject(document);
+            });
           })
-        }))
+        );
         this.set('isAddingNewDocument', false);
       },
 
@@ -68,16 +70,36 @@ export default Component.extend(
         const documents = await this.saveDocuments(null);
         const item = await this.get('item');
 
+        const subcase = await item.get('subcase');
+        const agendaitemsOnDesignAgenda = await item.get('agendaitemsOnDesignAgendaToEdit');
         await Promise.all(
           documents.map(async (document) => {
             const documentVersions = await document.get('documentVersions');
+            if (subcase) {
+              await this.attachDocumentVersionsToModel(documentVersions, subcase);
+            } else if (agendaitemsOnDesignAgenda && agendaitemsOnDesignAgenda.length > 0) {
+              await this.addDocumentVersionsToAgendaitems(
+                documentVersions,
+                agendaitemsOnDesignAgenda
+              );
+            }
             return this.attachDocumentVersionsToModel(documentVersions, item);
           })
         );
+        if (subcase) await subcase.save();
         item.save().then(() => {
           this.set('isAddingNewDocument', false);
         });
       },
+    },
+
+    async addDocumentVersionsToAgendaitems(documentVersions, agendaitems) {
+      return Promise.all(
+        agendaitems.map(async (agendaitem) => {
+          await this.attachDocumentVersionsToModel(documentVersions, agendaitem);
+          return agendaitem.save();
+        })
+      );
     },
   }
 );
