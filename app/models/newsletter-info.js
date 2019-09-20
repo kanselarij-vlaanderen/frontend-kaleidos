3ef0d8d5abd4e9c825af05f1838ced7c9982b864
Roel <roel@wheelhouse.be>
2019-09-20 11:37:43 +0200

    KAS-691 added remark

--- app/models/newsletter-info.js
+++ app/models/newsletter-info.js
@@ -1,11 +1,13 @@
 import DS from 'ember-data';
+import { alias } from '@ember/object/computed';
+import { computed } from '@ember/object';
+import { inject } from '@ember/service';
 
 const { Model, attr, hasMany, belongsTo } = DS;
-import { alias } from '@ember/object/computed';
 
 export default Model.extend({
   modelName: alias('constructor.modelName'),
-
+  intl: inject(),
   text: attr('string'),
   subtitle: attr('string'),
   title: attr('string'),
@@ -15,8 +17,16 @@ export default Model.extend({
   publicationDate: attr('date'),
   publicationDocDate: attr('date'),
   remark: attr('string'),
-
   subcase: belongsTo('subcase'),
+
   meeting: belongsTo('meeting', { inverse: null }),
   documentVersions: hasMany('document-version', { inverse: null }),
+  displayRemark: computed('remark', 'intl', function() {
+    const remark = this.get('remark');
+    if (remark && remark != '') {
+      return `${this.intl.t('remark')}: ${this.get('remark')}`;
+    } else {
+      return '';
+    }
+  }),
 });
