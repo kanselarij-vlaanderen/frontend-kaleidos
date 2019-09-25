c8339d74ea4643180e77df4d7660191ae32456ce
Karel Kremer <karel.kremer@gmail.com>
2019-09-25 11:14:06 +0200

    KAS-837 don't show empty tabs if not editing

--- app/components/agenda/agendaitem/agendaitem-nav/component.js
+++ app/components/agenda/agendaitem/agendaitem-nav/component.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 import isAuthenticatedMixin from 'fe-redpencil/mixins/is-authenticated-mixin';
 import { computed } from '@ember/object';
+import { hash } from 'rsvp';
 
 export default Component.extend(isAuthenticatedMixin, {
   classNames: ['vl-tabs', 'vl-u-reset-margin'],
@@ -17,6 +18,73 @@ export default Component.extend(isAuthenticatedMixin, {
     }
     const meeting = await currentAgenda.get('createdFor');
     return meeting.isFinal;
+  }),
+
+  defaultTabs: [
+    {
+      name: "details",
+      label: "agendaitem-case"
+    },
+    {
+      name: "documents",
+      label: "documents"
+    }
+  ],
+
+  activeTabs: computed('isEditor', 'agendaitem.remarks.length', 'shouldShowFinishedDetails',
+  'agendaitem.subcase', async function(){
+    const activeTabs = [];
+    activeTabs.push(...this.defaultTabs);
+
+    let promises = await hash({
+      commentCount: this.get('agendaitem.remarks.length'),
+      shouldShowFinishedDetails: this.get('shouldShowFinishedDetails'),
+      subcase: this.get('agendaitem.subcase'),
+      decisions: this.get('agendaitem.subcase.decisions'),
+      minutes: this.get('agendaitem.meetingRecord'),
+      newsItems: this.get('agendaitem.subcase.newsletterInfo'),
+    });
+
+    const isEditor = this.isEditor;
+
+    if(isEditor){
+      activeTabs.push({
+        name: "comments",
+        label: "agendaitem-comment",
+        pillText: promises.commentCount
+      });
+    }
+    if(!promises.shouldShowFinishedDetails || !promises.subcase) {
+      return activeTabs;
+    }
+
+    if(isEditor || promises.decisions){
+      activeTabs.push({
+        name: "decision",
+        label: "agendaitem-decision"
+      })
+    }
+    if(isEditor || promises.minutes){
+      activeTabs.push({
+        name: "minutes",
+        label: "agendaitem-notes"
+      })
+    }
+    if(isEditor || promises.newsItems){
+      activeTabs.push({
+        name: "news-item",
+        label: "agendaitem-bestek"
+      });
+    }
+
+    if(isEditor || this.get('agendaitem.titlePress') && this.get('agendaitem.textPress')){
+      activeTabs.push({
+        name: "press-agenda",
+        label: "agendaitem-press-agenda"
+      });
+    }
+
+    return activeTabs;
   }),
 
   actions: {
