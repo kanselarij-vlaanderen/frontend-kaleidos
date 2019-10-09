ed0c6ae9adb73feb4994a4af8e7fce671383710a
ValenberghsSven <sven.valenberghs1@gmail.com>
2019-10-09 15:32:20 +0200

    KAS-817 fix modal closing faster and disabled button when processing

--- app/components/subcases/assign-to-meeting-form/component.js
+++ app/components/subcases/assign-to-meeting-form/component.js
@@ -16,24 +16,25 @@ export default Component.extend({
 				'is-final': false
 			}, 
 			sort: 'planned-start' });
-	}),
+  }),
 
 	actions: {
 		selectStartDate(val) {
 			this.set('startDate', moment(val).format());
-		},
+    },
 
 		async assignToMeeting(subcase) {
 			const { startDate, dateObjectsToEnable } = this;
 			const meetings = dateObjectsToEnable;
 
-      const meetingToAssignTo = meetings.find(meeting =>
-        // Using format('L') (DD/MM/YYYY) to only compare the dates since the startDate time will always be 0:00
-				moment(meeting.get('plannedStart')).utc().format('L') == moment(startDate).format('L'));
+      // Using format('L') (DD/MM/YYYY) to only compare the dates since the startDate time will always be 0:00
+      const meetingToAssignTo = meetings.find(meeting => 
+        moment(meeting.get('plannedStart')).utc().format('L') == moment(startDate).format('L'));
 			const selectedSubcase = await this.store.findRecord('subcase', subcase.get('id'));
 			if (selectedSubcase && meetingToAssignTo) {
+        // Closing modal happens first because a user is able to click assign button multiple times during proposing
+				this.closeModal();
 				await this.proposeForAgenda(selectedSubcase, meetingToAssignTo);
-				this.cancel();
 			}else {
 				this.globalError.showToast.perform(
 					EmberObject.create({
@@ -47,7 +48,9 @@ export default Component.extend({
 
 		cancel() {
 			this.cancel();
-		}
+    },
+    closeModal() {
+      this.cancel();
+    },
 	},
-
 });
