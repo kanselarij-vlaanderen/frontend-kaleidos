50a07e228c9c493a1e07c6ee0c5b314f715796fa
Roel <roel@wheelhouse.be>
2019-10-23 09:42:20 +0200

    KAS-1024: removed set on deprecated property

--- app/components/newsletter/newsletter-header-overview/component.js
+++ app/components/newsletter/newsletter-header-overview/component.js
@@ -56,15 +56,14 @@ export default Component.extend(isAuthenticatedMixin, {
 
       if(!mailCampaign || !mailCampaign.id || mailCampaign.sent) {
         return;
-      } 
+      }
        await this.newsletterService.sendCampaign(mailCampaign.campaignId).catch(() => {
         this.globalError.showToast.perform(EmberObject.create({
           title: this.intl.t('warning-title'),
           message: this.intl.t('error-send-newsletter'),
           type: 'error'
         }));
-      });        
-      mailCampaign.set('sent', true);
+      });
       mailCampaign.set('sentAt', moment().utc().toDate());
       await mailCampaign.save();
       await meeting.belongsTo('mailCampaign').reload();
