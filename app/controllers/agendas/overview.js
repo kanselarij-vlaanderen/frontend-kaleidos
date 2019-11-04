a67a422db8a382024f4f0017f90c2b51db321614
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-11-04 17:15:39 +0100

    refactored the overview of the agendas so that all active agenda's are shown which are not final, this is combined with a change in the session-number-service.

--- app/controllers/agendas/overview.js
+++ app/controllers/agendas/overview.js
@@ -1,56 +1,28 @@
-import Controller from '@ember/controller';
-import { computed } from '@ember/object';
-import moment from 'moment';
-import isAuthenticatedMixin from 'fe-redpencil/mixins/is-authenticated-mixin';
-import { inject } from '@ember/service';
-export default Controller.extend(isAuthenticatedMixin,{
-	agendaService:inject(),
-	sessionService:inject(),
+import Controller from "@ember/controller";
+import { computed } from "@ember/object";
+import moment from "moment";
+import isAuthenticatedMixin from "fe-redpencil/mixins/is-authenticated-mixin";
+import { inject } from "@ember/service";
 
-	nearestMeeting: computed('model', async function() {
-    const nearestMeeting = await this.agendaService.getClosestMeetingAndAgendaIdInTheFuture(
-      moment()
-        .utc()
-        .toDate()
-    );
-    const meetings = this.get('model');
+export default Controller.extend(isAuthenticatedMixin, {
+  agendaService: inject(),
 
-    const nearestMeetingInModel = meetings.find(
-      item => item.get('id') === nearestMeeting.meeting_id
-    );
-    if (nearestMeetingInModel) {
-      nearestMeetingInModel.set('isNearest', true);
-      return [nearestMeetingInModel];
-    } else {
-      return [this.store.findRecord('meeting', nearestMeeting.meeting_id)];
-    }
-	}),
-
-	futureMeetings: computed('model', 'nearestMeeting', function() {
-    const meetings = this.get('model');
-    const nearestMeetingDate = moment(this.get('nearestMeeting.plannedStart'))
+  activeAgendas: computed("model", async function() {
+    const meetings = this.get("model");
+    const dateOfToday = moment()
       .utc()
       .format();
-    return meetings
-      .filter(meeting => {
-        let date = moment(meeting.plannedStart)
-          .utc()
-          .format();
+    const activeAgendas = await this.agendaService.getActiveAgendas(dateOfToday);
 
-        if (date > nearestMeetingDate) {
-          if (this.page === 0) {
-            const found = meetings.find(
-              meetingToCheck => meetingToCheck.get('id') === meeting.get('id')
-            );
-            if (found) {
-              found.set('alreadyShown', true);
-            }
-          }
-          if (!meeting.get('isNearest')) {
-            return meeting;
-          }
-        }
-      })
-      .sortBy('plannedStart');
-  })
+    return meetings.filter((meeting) => this.checkIfAgendaIsPresent(activeAgendas, meeting));
+  }),
+
+  checkIfAgendaIsPresent(activeAgendas, meeting) {
+    const foundItem = activeAgendas.find((activeAgenda) => activeAgenda.meeting_id === meeting.id);
+
+    if (!foundItem) {
+      return false;
+    }
+    return true;
+  }
 });
