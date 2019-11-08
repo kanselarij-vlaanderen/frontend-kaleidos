107c71fad262224238ec349eef6984eae4cd1678
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-11-08 09:41:31 +0100

    fixed formatting

--- app/controllers/agendas/overview.js
+++ app/controllers/agendas/overview.js
@@ -1,14 +1,17 @@
-import Controller from "@ember/controller";
-import { computed } from "@ember/object";
-import moment from "moment";
-import isAuthenticatedMixin from "fe-redpencil/mixins/is-authenticated-mixin";
-import { inject } from "@ember/service";
+import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import moment from 'moment';
+import isAuthenticatedMixin from 'fe-redpencil/mixins/is-authenticated-mixin';
+import { inject } from '@ember/service';
 export default Controller.extend(isAuthenticatedMixin, {
   agendaService: inject(),
 
-  activeAgendas: computed("model", async function() {
-    const meetings = this.get("model");
-    const dateOfToday = moment().utc().subtract(5, 'days').format();
+  activeAgendas: computed('model', async function() {
+    const meetings = this.get('model');
+    const dateOfToday = moment()
+      .utc()
+      .subtract(5, 'days')
+      .format();
     const activeAgendas = await this.agendaService.getActiveAgendas(dateOfToday);
 
     return meetings.filter((meeting) => this.checkIfAgendaIsPresent(activeAgendas, meeting));
