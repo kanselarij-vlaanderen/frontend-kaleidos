8002828d240c5ab93fe2e2d1b55f4900d5c32ea6
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-11-27 13:07:22 +0100

    KAS-1127 & KAS-934: implemented the feature to postpone items and removed all unused code & refactored the templates + added a new subcase-phase 'Uitgesteld'

--- app/components/agenda/agendaitem/agendaitem-controls/component.js
+++ app/components/agenda/agendaitem/agendaitem-controls/component.js
@@ -1,8 +1,9 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import moment from 'moment';
-import { inject } from '@ember/service';
-import isAuthenticatedMixin from 'fe-redpencil/mixins/is-authenticated-mixin';
+import Component from "@ember/component";
+import { computed } from "@ember/object";
+import moment from "moment";
+import { inject } from "@ember/service";
+import isAuthenticatedMixin from "fe-redpencil/mixins/is-authenticated-mixin";
+import CONFIG from "fe-redpencil/utils/config";
 
 export default Component.extend(isAuthenticatedMixin, {
   store: inject(),
@@ -12,120 +13,129 @@ export default Component.extend(isAuthenticatedMixin, {
   agendaitem: null,
   lastDefiniteAgenda: null,
 
-  currentMeeting: computed('currentAgenda.createdFor', function() {
-    return this.currentAgenda.get('createdFor');
+  currentMeeting: computed("currentAgenda.createdFor", function() {
+    return this.currentAgenda.get("createdFor");
   }),
 
-  meetings: computed('currentMeeting', function() {
-    const currentMeetingDate = this.currentMeeting.get('plannedStart')
-    const dateOfToday = moment(currentMeetingDate).utc().format();
-    const dateInTwoWeeks = moment().utc().add(6, 'weeks').format();
-
-    return this.store.query('meeting', {
-      filter: {
-        ':gt:planned-start': dateOfToday,
-        ':lte:planned-start': dateInTwoWeeks,
-        'is-final': false
-      },
-      sort: 'planned-start'
-    })
-  }),
-
-  isPostPonable: computed('sessionService.agendas.@each', function() {
-    return this.get('sessionService.agendas').then(agendas => {
-      if (agendas && agendas.get('length') > 1) {
+  isPostPonable: computed("sessionService.agendas.@each", function() {
+    return this.get("sessionService.agendas").then(agendas => {
+      if (agendas && agendas.get("length") > 1) {
         return true;
       } else {
         return false;
       }
-    })
+    });
   }),
 
   isDeletable: computed(
-    'agendaitem.{subcase,subcase.agendaitems}',
-    'currentAgenda.name',
-    async function() {
-      const currentAgendaName = await this.get('currentAgenda.name');
-      const agendaitemSubcase = await this.get('agendaitem.subcase');
-      const agendaitems = await this.get('agendaitem.subcase.agendaitems');
+    "agendaitem.{subcase,subcase.agendaitems}", "currentAgenda.name", async function() {
+      const currentAgendaName = await this.get("currentAgenda.name");
+      const agendaitemSubcase = await this.get("agendaitem.subcase");
+      const agendaitems = await this.get("agendaitem.subcase.agendaitems");
       if (currentAgendaName && currentAgendaName !== "Ontwerpagenda") {
         return false;
       } else if (agendaitemSubcase) {
         if (agendaitems && agendaitems.length > 1) {
-          return false
+          return false;
         } else {
           return true;
         }
       } else {
         return true;
       }
-    }),
+    }
+  ),
 
   async deleteItem(agendaitem) {
-    const id = agendaitem.get('id')
+    const id = agendaitem.get("id");
     await this.agendaService.deleteAgendaitemFromAgenda(agendaitem);
-    this.set('sessionService.selectedAgendaItem', null);
+    this.set("sessionService.selectedAgendaItem", null);
     this.refreshRoute(id);
   },
 
   actions: {
     showOptions() {
-      this.toggleProperty('showOptions');
+      this.toggleProperty("showOptions");
     },
 
-    async postponeAgendaItem(agendaitem, meetingToPostponeTo) {
-      agendaitem.set('retracted', true);
-
-      const postPonedObject = this.store.createRecord('postponed', {
-        agendaitem: agendaitem,
-        meeting: meetingToPostponeTo
+    async postponeAgendaItem(agendaitem) {
+      agendaitem.set("retracted", true);
+      const postPonedObject = this.store.createRecord("postponed", {
+        agendaitem: agendaitem
       });
-
-      if (meetingToPostponeTo) {
-        const subcase = await agendaitem.get('subcase');
-        const agenda = await meetingToPostponeTo.get('latestAgenda');
-        if (agenda.get('name') == 'Ontwerpagenda' && subcase) {
-          await this.agendaService.createNewAgendaItem(agenda, subcase);
-        }
-      }
+      const subcase = await agendaitem.get("subcase");
+      await this.createPostponedPhase(subcase);
 
       postPonedObject.save().then(postponedTo => {
-        agendaitem.set('postponed', postponedTo);
+        agendaitem.set("postponed", postponedTo);
       });
-
+      await subcase.notifyPropertyChange('isPostponed');
+      await subcase.hasMany("phases").reload();
       await agendaitem.save();
       await agendaitem.reload();
-      await agendaitem.subcase.reload();
+      await subcase.reload();
     },
 
     async advanceAgendaitem() {
-      const agendaitem = await this.store.findRecord('agendaitem', this.agendaitem.get('id'));
+      const agendaitem = await this.store.findRecord(
+        "agendaitem",
+        this.agendaitem.get("id")
+      );
+      const postponedTo = await agendaitem.get("postponedTo");
+      const subcase = await agendaitem.get("subcase");
+      await this.deletePostponedPhases(subcase);
       if (agendaitem && agendaitem.retracted) {
-        agendaitem.set('retracted', false);
+        agendaitem.set("retracted", false);
       }
-      const postponedTo = await agendaitem.get('postponedTo');
+
       if (agendaitem && postponedTo) {
         await postponedTo.destroyRecord();
       }
       await agendaitem.save();
       await agendaitem.reload();
-      await agendaitem.subcase.reload();
+      await subcase.hasMany("phases").reload();
+      await subcase.notifyPropertyChange('isPostponed');
+      await subcase.reload();
     },
 
     toggleIsVerifying() {
-      this.toggleProperty('isVerifying');
+      this.toggleProperty("isVerifying");
     },
 
     async tryToDeleteItem(agendaitem) {
       if (await this.isDeletable) {
         this.deleteItem(agendaitem);
       } else if (this.isAdmin) {
-        this.toggleProperty('isVerifying');
+        this.toggleProperty("isVerifying");
       }
     },
 
     verifyDelete(agendaitem) {
       this.deleteItem(agendaitem);
     }
+  },
+
+  async deletePostponedPhases(subcase) {
+    const postponedPhases = await subcase.get("postponedPhases");
+    if (postponedPhases && postponedPhases.length) {
+      await Promise.all(
+        postponedPhases.map(phase => {
+          phase.destroyRecord();
+        })
+      );
+    }
+    return subcase;
+  },
+
+  async createPostponedPhase(subcase) {
+    const postponedCode = await this.store.findRecord("subcase-phase-code", CONFIG.postponedCodeId);
+    const newDecisionPhase = this.store.createRecord("subcase-phase", {
+      date: moment()
+        .utc()
+        .toDate(),
+      code: postponedCode,
+      subcase: subcase
+    });
+    return newDecisionPhase.save();
   }
 });
--- app/components/subcases/subcases-overview-header/component.js
+++ app/components/subcases/subcases-overview-header/component.js
@@ -1,44 +1,61 @@
-import Component from '@ember/component';
-import isAuthenticatedMixin from 'fe-redpencil/mixins/is-authenticated-mixin';
-import { computed } from '@ember/object';
+import Component from "@ember/component";
+import isAuthenticatedMixin from "fe-redpencil/mixins/is-authenticated-mixin";
+import { computed } from "@ember/object";
 
 export default Component.extend(isAuthenticatedMixin, {
-	classNames: ["vl-u-bg-alt"],
-	isAddingSubcase: false,
+  classNames: ["vl-u-bg-alt"],
+  isAddingSubcase: false,
+  title: null,
+  shortTitle: null,
 
-	activeProcess: computed('isShowingProcess', function () {
-		if (this.get('isShowingProcess')) {
-			return 'vlc-tabs-reverse__link--active';
-		}
-	}),
+  activeProcess: computed("isShowingProcess", function() {
+    if (this.get("isShowingProcess")) {
+      return "vlc-tabs-reverse__link--active";
+    }
+  }),
 
-	activeOverview: computed('isShowingOverview', function () {
-		if (this.get('isShowingOverview')) {
-			return 'vlc-tabs-reverse__link--active';
-		}
-	}),
+  activeOverview: computed("isShowingOverview", function() {
+    if (this.get("isShowingOverview")) {
+      return "vlc-tabs-reverse__link--active";
+    }
+  }),
 
-	actions: {
-		toggleIsAddingSubcase() {
-			this.toggleProperty('isAddingSubcase');
-		},
+	// This is needed to give the input-helpers a proper string instead of 
+	// a promise object based on the previous subcase or known case
+  async setKnownPropertiesOfCase() {
+    const caze = await this.get("model.case");
+    const latestSubcase = await caze.get("latestSubcase");
+    if (latestSubcase) {
+      this.set("title", latestSubcase.get("title"));
+      this.set("shortTitle", latestSubcase.get("shortTitle"));
+    } else {
+      this.set("title", caze.title);
+      this.set("shortTitle", caze.shortTitle);
+    }
+  },
 
-		refresh() {
-			this.refresh();
-		},
+  actions: {
+    async toggleIsAddingSubcase() {
+      await this.setKnownPropertiesOfCase();
+      this.toggleProperty("isAddingSubcase");
+    },
 
-		close() {
-			this.toggleProperty('isAddingSubcase');
-		},
+    refresh() {
+      this.refresh();
+    },
 
-		toggleIsShowingProcess() {
-			this.set('isShowingProcess', true);
-			this.set('isShowingOverview', false);
-		},
+    close() {
+      this.toggleProperty("isAddingSubcase");
+    },
 
-		toggleIsShowingOverview() {
-			this.set('isShowingProcess', false);
-			this.set('isShowingOverview', true);
-		},
-	}
+    toggleIsShowingProcess() {
+      this.set("isShowingProcess", true);
+      this.set("isShowingOverview", false);
+    },
+
+    toggleIsShowingOverview() {
+      this.set("isShowingProcess", false);
+      this.set("isShowingOverview", true);
+    }
+  }
 });
