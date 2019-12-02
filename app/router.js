10d6b274d1b8bd50a0c7c55541e4b68125589bdd
Joachimzeelmaekers <joa.zeelmaekers@hotmail.com>
2019-12-02 09:53:10 +0100

    added a component to show editing modus

--- app/router.js
+++ app/router.js
@@ -8,7 +8,6 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('agendas', { path: '/' }, function() {
-    this.route('history', { path: '/historiek' });
     this.route('overview', { path: '/overzicht' });
   });
   this.route('agenda', { path: '/agenda/:id' }, function() {
