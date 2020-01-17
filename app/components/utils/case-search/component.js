bba39fab600351f16acd9df80aa2b3cc1257d6d7
rafael-mindreau <rafael.mindreau@gmail.com>
2020-01-17 13:19:37 +0100

    Switch from computed property with side effects to observer model
    The computed property model introduced side-effects for changing the loading state. So a search with an observer that triggers an action would be more appropriate. Added in some debounce for good measure!

--- app/components/utils/case-search/component.js
+++ app/components/utils/case-search/component.js
@@ -3,11 +3,11 @@ import {inject} from "@ember/service";
 import DataTableRouteMixin from 'ember-data-table/mixins/route';
 import {isEmpty} from '@ember/utils';
 import DS from 'ember-data';
-import {computed } from "@ember/object";
+import { get, set, observer } from "@ember/object";
 import moment from "moment";
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import { debounce } from '@ember/runloop';
 import _ from "lodash";
-
 
 export default Component.extend(DataTableRouteMixin, AuthenticatedRouteMixin, {
   store: inject(),
@@ -25,7 +25,15 @@ export default Component.extend(DataTableRouteMixin, AuthenticatedRouteMixin, {
   dateTo: null,
   decisionsOnly: false,
   page: 0,
+  results: null,
 
+  didReceiveAttrs() {
+    this.send('performSearch');
+  },
+
+  searchChanged: observer('searchText', function () {
+    debounce(this, this.debouncedSearch, 500);
+  }),
 
   postProcessDates: function (_case) {
     const sessionDates = _case.get('sessionDates');
@@ -38,66 +46,75 @@ export default Component.extend(DataTableRouteMixin, AuthenticatedRouteMixin, {
       }
     }
   },
-  model: computed("searchText", function () {
-    return DS.PromiseArray.create({
-      promise: (() => {
-        // let params = {};
-        const searchModifier = ':sqs:';
 
-        const textSearchKey = this.textSearchFields.join(',');
-        let queryParams = {
-          filter: {
-            // 'is-archived': params.isArchived // Cannot post-filter on mu-cl-resources. field MUST be included in search-object keys
-          },
-          page: {
-            size: this.size,
-            number: this.page
-          },
-          sort: this.sort // Currently only "sessionDates available in search config"
-        };
-        if(!this.searchText) {
-          _.merge(queryParams, this.mergeQueryOptions(queryParams));
-          return this.get('store').query("case",queryParams);
-        }
-        queryParams.filter[searchModifier + textSearchKey] = this.searchText;
-
-
-        let searchDocumentType = this.decisionsOnly ? 'casesByDecisionText' : 'cases';
-        if (!isEmpty(this.mandatees)) {
-          queryParams.filter['creators,mandatees'] = this.mandatees;
-        }
-        if (!isEmpty(this.dateFrom)) {
-          queryParams.filter[':gte:sessionDates'] = this.dateFrom;
-        }
-        if (!isEmpty(this.dateTo)) {
-          queryParams.filter[':lte:sessionDates'] = this.dateTo;
-        }
-        const postProcessDates = this.postProcessDates;
-        return this.muSearch.query(searchDocumentType,
-          queryParams,
-          "case",
-          {'session-dates': 'sessionDates'})
-          .then((res) => {
-            this.set('isLoading', false);
-            res.forEach(postProcessDates);
-            return res;
-          }).catch((error) => {
-            console.log(error);
-            this.set('isLoading', false);
-            return [];
-          })
-      })()
-    })
-
-  }),
+  debouncedSearch: function() {
+    this.send('performSearch', get(this, 'searchText'));
+  },
 
   actions: {
+    async performSearch(searchTerm) {
+      set(this, 'isLoading', true);
+
+      const results = await DS.PromiseArray.create({
+        promise: (() => {
+          // let params = {};
+          const searchModifier = ':sqs:';
+
+          const textSearchKey = this.textSearchFields.join(',');
+          let queryParams = {
+            filter: {
+              // 'is-archived': params.isArchived // Cannot post-filter on mu-cl-resources. field MUST be included in search-object keys
+            },
+            page: {
+              size: this.size,
+              number: this.page
+            },
+            sort: this.sort // Currently only "sessionDates available in search config"
+          };
+          if(!searchTerm) {
+            _.merge(queryParams, this.mergeQueryOptions(queryParams));
+            return this.get('store').query("case",queryParams);
+          }
+          queryParams.filter[searchModifier + textSearchKey] = searchTerm;
+
+
+          let searchDocumentType = this.decisionsOnly ? 'casesByDecisionText' : 'cases';
+          if (!isEmpty(this.mandatees)) {
+            queryParams.filter['creators,mandatees'] = this.mandatees;
+          }
+          if (!isEmpty(this.dateFrom)) {
+            queryParams.filter[':gte:sessionDates'] = this.dateFrom;
+          }
+          if (!isEmpty(this.dateTo)) {
+            queryParams.filter[':lte:sessionDates'] = this.dateTo;
+          }
+          const postProcessDates = this.postProcessDates;
+          return this.muSearch.query(searchDocumentType,
+            queryParams,
+            "case",
+            {'session-dates': 'sessionDates'})
+            .then((res) => {
+              this.set('isLoading', false);
+              res.forEach(postProcessDates);
+              return res;
+            }).catch((error) => {
+              console.log(error);
+              this.set('isLoading', false);
+              return [];
+            })
+        })()
+      });
+
+      set(this, 'results', results);
+    },
+
     async selectCase(caseItem, event) {
       if (event) {
         event.stopPropagation();
       }
       this.onSelect(caseItem);
     },
+
     selectSize(size) {
       this.set('size', size);
     }
