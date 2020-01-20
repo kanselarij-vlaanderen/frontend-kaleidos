f635dc2c744d57bb3f3690828b4a66d20861ce46
BenjaminDendas <benjamindendas@msn.com>
2020-01-20 16:50:07 +0100

    KAS-1240: add new case tests

---
+++ cypress/integration/unit/case/shouldCreateNewCases.spec.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef */
+/// <reference types="Cypress" />
+
+import {casesHeaderAddCaseSelector, metadataForm} from "../../../selectors/cases/caseSelectors";
+import {formVlToggle} from "../../../selectors/formSelectors/formSelectors";
+
+context('Create case as Admin user', () => {
+
+  beforeEach(() => {
+    cy.server();
+    cy.login('Admin');
+  });
+
+  it('Create a case with empty values', () => {
+      cy.visit('/dossiers');
+      cy.get(casesHeaderAddCaseSelector).click();
+      cy.get(metadataForm).type("Dit is de korte titel van het dossier", {delay: 50});
+      cy.get('button').contains('Dossier aanmaken').click();
+  });
+
+  it('Create a case with confidentiality', () => {
+    cy.visit('/dossiers');
+    cy.get(casesHeaderAddCaseSelector).click();
+    cy.get(formVlToggle).eq(0).click();
+    cy.get(metadataForm).type("Dit is een dossier met confidentiality");
+    cy.get('button').contains('Dossier aanmaken').click();
+  });
+
+  it('Create a case with confidentiality and short title', () => {
+    cy.visit('/dossiers');
+    cy.get(casesHeaderAddCaseSelector).click();
+    cy.get(formVlToggle).eq(0).click();
+    cy.get(metadataForm).type("Dit is een dossier met confidentiality en een korte titel");
+    cy.get('button').contains('Dossier aanmaken').click();
+  });
+
+  it('Create a case with short title', () => {
+    cy.visit('/dossiers');
+    cy.get(casesHeaderAddCaseSelector).click();
+    cy.get(metadataForm).type("Dit is een dossier met een korte titel");
+    cy.get('button').contains('Dossier aanmaken').click();
+  });
+});
