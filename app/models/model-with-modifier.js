f884b8cb13ed042db270d187c2cc9a8b7b507c9a
Frederik Wouters <wouters_frederik@721548.no-reply.drupal.org>
2020-02-13 11:20:05 +0100

    KAS-1109: locking for subcase items

--- app/models/model-with-modifier.js
+++ app/models/model-with-modifier.js
@@ -2,11 +2,12 @@ import DS from 'ember-data';
 import {inject} from '@ember/service';
 import moment from "moment";
 import EmberObject from '@ember/object';
+import ModelWithToasts from 'fe-redpencil/models/model-with-toasts';
 import fetch from 'fetch';
 
-let {Model, attr, belongsTo} = DS;
+let {attr, belongsTo} = DS;
 
-export default Model.extend({
+export default ModelWithToasts.extend({
   currentSession: inject(),
   intl: inject(),
   globalError: inject(),
@@ -15,6 +16,8 @@ export default Model.extend({
 
   async save() {
     const parentSave = this._super;
+    const modified = this.get('modified');
+
     const current_modified = moment.utc(this.get('modified'));
     const oldModelData = await this.store.adapterFor(this.get('constructor.modelName'))
       .queryRecord(this.store, this.get('constructor'),
@@ -24,11 +27,17 @@ export default Model.extend({
         });
 
     const old_model_modified = moment.utc(oldModelData.data[0].attributes.modified);
-    if (current_modified.toString() == old_model_modified.toString()) {
+
+    if (typeof modified != 'undefined' && current_modified.toString() == old_model_modified.toString() || typeof modified == 'undefined') {
       this.set('modified', moment().utc().toDate());
       await this.currentSession.get('user').then((user) => {
         this.set('modifiedBy', user);
       });
+      this.globalError.showToast.perform(EmberObject.create({
+        title: this.intl.t('successfully-created-title'),
+        message: this.intl.t('successfully-saved'),
+        type: 'success'
+      }));
       return parentSave.call(this, ...arguments);
     } else {
       const userId = oldModelData.data[0]['relationships']['modified-by']['links']['self'];
