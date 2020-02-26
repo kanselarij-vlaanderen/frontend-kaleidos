695de6722eb3e330eb3257fd15922233d51f88dd
Frederik Wouters <wouters_frederik@721548.no-reply.drupal.org>
2020-02-26 14:00:47 +0100

    KAS-1109 fixes

--- app/models/model-with-modifier.js
+++ app/models/model-with-modifier.js
@@ -19,45 +19,66 @@ export default ModelWithToasts.extend({
     const modified = this.get('modified');
 
     const currentModifiedModel = moment.utc(this.get('modified'));
-    const oldModelData = await this.store.adapterFor(this.get('constructor.modelName'))
-      .queryRecord(this.store, this.get('constructor'),
-        {
-          filter:
-            {id: this.get('id')}
-        });
-    const oldModelModifiedMoment = moment.utc(oldModelData.data[0].attributes.modified);
 
-    if (typeof modified == 'undefined' ||
-      (
-        typeof modified != 'undefined'
-        && currentModifiedModel.isSame(oldModelModifiedMoment)
-        && typeof oldModelData.data[0]['relationships']['modified-by'] != 'undefined')
-    ) {
-      this.set('modified', moment().utc().toDate());
-      await this.currentSession.get('user').then((user) => {
-        this.set('modifiedBy', user);
-      });
-      this.globalError.showToast.perform(EmberObject.create({
-        title: this.intl.t('successfully-created-title'),
-        message: this.intl.t('successfully-saved'),
-        type: 'success'
-      }));
-      return parentSave.call(this, ...arguments);
-    } else {
-      const userId = oldModelData.data[0]['relationships']['modified-by']['links']['self'];
-      const userData = await fetch(userId);
-      const userDataFields = await userData.json();
-      const vals = userDataFields.data.attributes;
-      this.globalError.showToast.perform(EmberObject.create({
-        title: this.intl.t('changes-could-not-be-saved-title'),
-        message: this.intl.t('changes-could-not-be-saved-message', {
-          firstname: vals['first-name'],
-          lastname: vals['last-name'],
-          time: oldModelModifiedMoment.locale("nl").fromNow()
-        }),
-        type: 'error'
-      }), 600000);
-      throw('The content you were trying to save has already been updated.');
+    const dirtyType = this.get('dirtyType');
+    switch (dirtyType) {
+      case 'created': {
+        this.set('modified', moment().utc().toDate());
+        await this.currentSession.get('user').then((user) => {
+          this.set('modifiedBy', user);
+        });
+        break;
+      }
+
+      case 'deleted': {
+        break;
+      }
+      case 'updated': {
+        const oldModelData = await this.store.adapterFor(this.get('constructor.modelName'))
+          .queryRecord(this.store, this.get('constructor'),
+            {
+              filter:
+                {id: this.get('id')}
+            });
+        const oldModelModifiedMoment = moment.utc(oldModelData.data[0].attributes.modified);
+
+        if (typeof modified == 'undefined' ||
+          (
+            typeof modified != 'undefined'
+            && currentModifiedModel.isSame(oldModelModifiedMoment)
+            && typeof oldModelData.data[0]['relationships']['modified-by'] != 'undefined')
+        ) {
+          this.set('modified', moment().utc().toDate());
+          await this.currentSession.get('user').then((user) => {
+            this.set('modifiedBy', user);
+          });
+          this.globalError.showToast.perform(EmberObject.create({
+            title: this.intl.t('successfully-created-title'),
+            message: this.intl.t('successfully-saved'),
+            type: 'success'
+          }));
+          return parentSave.call(this, ...arguments);
+        } else {
+          const userId = oldModelData.data[0]['relationships']['modified-by']['links']['self'];
+          const userData = await fetch(userId);
+          const userDataFields = await userData.json();
+          const vals = userDataFields.data.attributes;
+          this.globalError.showToast.perform(EmberObject.create({
+            title: this.intl.t('changes-could-not-be-saved-title'),
+            message: this.intl.t('changes-could-not-be-saved-message', {
+              firstname: vals['first-name'],
+              lastname: vals['last-name'],
+              time: oldModelModifiedMoment.locale("nl").fromNow()
+            }),
+            type: 'error'
+          }), 600000);
+          throw('The content you were trying to save has already been updated.');
+        }
+        break;
+      }
+
+
     }
+
   }
 });
