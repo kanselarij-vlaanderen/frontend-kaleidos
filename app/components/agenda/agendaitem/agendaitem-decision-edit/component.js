ab5edde71c508631574efef7834443856c22cd85
Frederik Wouters <wouters_frederik@721548.no-reply.drupal.org>
2020-02-27 11:01:43 +0100

    KAS-1109: auto reformat file

--- app/components/agenda/agendaitem/agendaitem-decision-edit/component.js
+++ app/components/agenda/agendaitem/agendaitem-decision-edit/component.js
@@ -1,108 +1,108 @@
 import Component from '@ember/component';
 import DocumentsSelectorMixin from 'fe-redpencil/mixins/documents-selector-mixin';
 import RdfaEditorMixin from 'fe-redpencil/mixins/rdfa-editor-mixin';
-import { getCachedProperty } from 'fe-redpencil/mixins/edit-agendaitem-or-subcase';
+import {getCachedProperty} from 'fe-redpencil/mixins/edit-agendaitem-or-subcase';
 import CONFIG from 'fe-redpencil/utils/config';
 import moment from 'moment';
 
 export default Component.extend(DocumentsSelectorMixin, RdfaEditorMixin, {
-	classNames: ["vl-form__group vl-u-bg-porcelain"],
-	propertiesToSet: Object.freeze(['approved', 'richtext']),
-	approved: getCachedProperty('approved'),
-	initValue: getCachedProperty('richtext'),
+  classNames: ["vl-form__group vl-u-bg-porcelain"],
+  propertiesToSet: Object.freeze(['approved', 'richtext']),
+  approved: getCachedProperty('approved'),
+  initValue: getCachedProperty('richtext'),
 
-	async setDecisionPhaseToSubcase() {
-		const approved = await this.get('approved');
-		const subcase = await this.get('subcase')
+  async setDecisionPhaseToSubcase() {
+    const approved = await this.get('approved');
+    const subcase = await this.get('subcase')
 
-		const foundDecidedPhases = await this.store.query('subcase-phase', {
-			filter: { code: { id: CONFIG.decidedCodeId }, subcase: { id: subcase.get('id') } }
-		});
+    const foundDecidedPhases = await this.store.query('subcase-phase', {
+      filter: {code: {id: CONFIG.decidedCodeId}, subcase: {id: subcase.get('id')}}
+    });
 
-		if (foundDecidedPhases && foundDecidedPhases.length > 0) {
-			await Promise.all(foundDecidedPhases.map((phase) => phase.destroyRecord()));
-		}
-		if (approved) {
-			const decidedCode = await this.store.findRecord('subcase-phase-code', CONFIG.decidedCodeId);
-			const newDecisionPhase = this.store.createRecord('subcase-phase', {
-				date: moment().utc().toDate(),
-				code: decidedCode,
-				subcase: subcase
-			});
-			return newDecisionPhase.save();
-		}
-	},
+    if (foundDecidedPhases && foundDecidedPhases.length > 0) {
+      await Promise.all(foundDecidedPhases.map((phase) => phase.destroyRecord()));
+    }
+    if (approved) {
+      const decidedCode = await this.store.findRecord('subcase-phase-code', CONFIG.decidedCodeId);
+      const newDecisionPhase = this.store.createRecord('subcase-phase', {
+        date: moment().utc().toDate(),
+        code: decidedCode,
+        subcase: subcase
+      });
+      return newDecisionPhase.save();
+    }
+  },
 
-	actions: {
-		async saveChanges() {
+  actions: {
+    async saveChanges() {
       this._super.call(this);
       this.set('isLoading', true);
-			const { isAgendaItem } = this;
-			const item = await this.get('item');
-			item.set('modified', moment().utc().toDate());
+      const {isAgendaItem} = this;
+      const item = await this.get('item');
+      item.set('modified', moment().utc().toDate());
 
-			if (isAgendaItem && !item.showAsRemark) {
-				const isDesignAgenda = await item.get('isDesignAgenda');
-				if (isDesignAgenda) {
-					const agendaitemSubcase = await item.get('subcase');
-					agendaitemSubcase.set('modified', moment().utc().toDate());
-					await this.setNewPropertiesToModel(agendaitemSubcase).catch((e) => {
+      if (isAgendaItem && !item.showAsRemark) {
+        const isDesignAgenda = await item.get('isDesignAgenda');
+        if (isDesignAgenda) {
+          const agendaitemSubcase = await item.get('subcase');
+          agendaitemSubcase.set('modified', moment().utc().toDate());
+          await this.setNewPropertiesToModel(agendaitemSubcase).catch((e) => {
             this.set('isLoading', false);
             throw(e);
           });
-					agendaitemSubcase.reload();
-				}
-				await this.setNewPropertiesToModel(item).then(async () => {
-					const agenda = await item.get('agenda');
-					this.updateModifiedProperty(agenda);
-					item.reload();
-				}).catch((e) => {
+          agendaitemSubcase.reload();
+        }
+        await this.setNewPropertiesToModel(item).then(async () => {
+          const agenda = await item.get('agenda');
+          this.updateModifiedProperty(agenda);
+          item.reload();
+        }).catch((e) => {
           this.set('isLoading', false);
           throw(e);
         });
-			} else {
-				await this.setNewPropertiesToModel(item).catch((e) => {
+      } else {
+        await this.setNewPropertiesToModel(item).catch((e) => {
           this.set('isLoading', false);
           throw(e);
         });
-				const agendaitemsOnDesignAgendaToEdit = await item.get('agendaitemsOnDesignAgendaToEdit');
-				if (agendaitemsOnDesignAgendaToEdit && agendaitemsOnDesignAgendaToEdit.get('length') > 0) {
-					await Promise.all(agendaitemsOnDesignAgendaToEdit.map(async (agendaitem) => {
-						await this.setNewPropertiesToModel(agendaitem).then(async () => {
-							const agenda = await item.get('agenda');
-							this.updateModifiedProperty(agenda).then((agenda) => {
-								agenda.reload();
-							});
-							await item.reload();
-							item.notifyPropertyChanged('decisions');
-						}).catch((e) => {
+        const agendaitemsOnDesignAgendaToEdit = await item.get('agendaitemsOnDesignAgendaToEdit');
+        if (agendaitemsOnDesignAgendaToEdit && agendaitemsOnDesignAgendaToEdit.get('length') > 0) {
+          await Promise.all(agendaitemsOnDesignAgendaToEdit.map(async (agendaitem) => {
+            await this.setNewPropertiesToModel(agendaitem).then(async () => {
+              const agenda = await item.get('agenda');
+              this.updateModifiedProperty(agenda).then((agenda) => {
+                agenda.reload();
+              });
+              await item.reload();
+              item.notifyPropertyChanged('decisions');
+            }).catch((e) => {
               this.set('isLoading', false);
               throw(e);
             });
-					}));
-				}
-			}
-			await this.setDecisionPhaseToSubcase();
+          }));
+        }
+      }
+      await this.setDecisionPhaseToSubcase();
 
-			if (!this.get('isDestroyed')) {
-				let agendaitemToUpdate;
-				if (this.isTableRow) {
-					const subcase = await this.agendaitem.get('subcase');
-					(await subcase.get('decisions')).reload();
-					agendaitemToUpdate = await this.agendaitem.content;
-				} else {
-					agendaitemToUpdate = await this.agendaitem;
-				}
-				agendaitemToUpdate.set('modified', moment().utc().toDate())
+      if (!this.get('isDestroyed')) {
+        let agendaitemToUpdate;
+        if (this.isTableRow) {
+          const subcase = await this.agendaitem.get('subcase');
+          (await subcase.get('decisions')).reload();
+          agendaitemToUpdate = await this.agendaitem.content;
+        } else {
+          agendaitemToUpdate = await this.agendaitem;
+        }
+        agendaitemToUpdate.set('modified', moment().utc().toDate())
 
-				await agendaitemToUpdate.save();
-			}
-			this.set('isLoading', false);
-			this.toggleProperty('isEditing');
-		},
+        await agendaitemToUpdate.save();
+      }
+      this.set('isLoading', false);
+      this.toggleProperty('isEditing');
+    },
 
-		descriptionUpdated(val) {
-			this.set('initValue', this.richtext + val);
-		}
-	}
+    descriptionUpdated(val) {
+      this.set('initValue', this.richtext + val);
+    }
+  }
 });
