117807db5d7086bdf63d3a9ef2927b6be2c24b3c
Michaël Dierick <michael.dierick@redpencil.io>
2020-03-07 12:40:10 +0100

    Adapted frontend calls to changed backend approach

--- app/utils/zip-agenda-files.js
+++ app/utils/zip-agenda-files.js
@@ -16,45 +16,17 @@ async function constructArchiveName (agenda) {
   return `VR_zitting_${formattedDate}_${agendaName}_alle_punten.zip`
 }
 
-async function fetchFilesOfAgenda (agenda) {
-  const url = `/agendas/${agenda.id}/agendaitems/documents`;
-  const documents = (await (await fetch(url, {
-    headers: { 'Content-type': 'application/vnd.api+json' }
-  })).json());
-  return documents.data.map((doc) => {
-    // Match 'files' included in the JSONAPI payload to their 'documents'
-    const file = documents.included.filter((file) => {
-      return file.type === 'files' &&
-        file.id === doc.relationships.file.data.id;
-    })[0];
-    // JSONAPI objects with constructed filenames for the files in the archive
-    return {
-      type: 'files',
-      id: file.id,
-      attributes: {
-        name: doc.attributes.name + '.' + file.attributes.extension
-      }
-    }
-  })
-}
-
-async function fetchArchivingJob (files, archiveName) {
-  const url = `/files/archive?name=${archiveName}`;
+async function fetchArchivingJob (agenda) {
+  const url = `/agendas/${agenda.id}/agendaitems/documents/files/archive`;
   let job = (await fetch(url, {
     method: 'post',
-    headers: { 'Content-type': 'application/vnd.api+json' },
-    body: JSON.stringify({
-      data: files
-    })
+    headers: { 'Content-type': 'application/vnd.api+json' }
   })).json();
   return job;
 }
 
 async function fetchArchivingJobForAgenda (agenda, store) {
-  const namePromise = await constructArchiveName(agenda);
-  const filesPromise = await fetchFilesOfAgenda(agenda);
-  const [name, files] = await Promise.all([namePromise, filesPromise]);
-  let job = await fetchArchivingJob(files, name);
+  let job = await fetchArchivingJob(agenda);
   job = registerJobToStore(job, store);
   return job;
 }
