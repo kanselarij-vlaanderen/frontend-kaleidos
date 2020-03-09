10f61d3665924cfa99bc2fccf09249c4e30bdddb
BenjaminDendas <benjamindendas@msn.com>
2020-03-09 09:26:50 +0100

    KAS-1306: solve formatting issues

--- app/transforms/unix-to-datetime.js
+++ app/transforms/unix-to-datetime.js
@@ -4,8 +4,7 @@ import moment from 'moment';
 
 export default DS.Transform.extend({
   deserialize(serialized) {
-    console.log('serializing: ', serialized);
-      return moment.unix(serialized).format("HH:MM:SS DD-MM-YYYY");
+    return moment.unix(serialized).format("HH:MM:SS DD-MM-YYYY");
   },
 
   serialize(deserialized) {
