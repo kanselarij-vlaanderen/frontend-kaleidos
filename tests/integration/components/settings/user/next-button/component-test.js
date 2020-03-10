15cc94ac26e745a0bd1db64d75d3f9f3e4d4da3f
BenjaminDendas <benjamindendas@msn.com>
2020-03-10 09:02:31 +0100

    KAS-1227: move next-button component from general util to specific component

--- tests/integration/components/settings/user/next-button/component-test.js
+++ tests/integration/components/settings/user/next-button/component-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | utils/next-button', function(hooks) {
+module('Integration | Component | settings/user/next-button', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{utils/next-button}}`);
+    await render(hbs`{{settings/user/next-button}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#utils/next-button}}
+      {{#settings/user/next-button}}
         template block text
-      {{/utils/next-button}}
+      {{/settings/user/next-button}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
