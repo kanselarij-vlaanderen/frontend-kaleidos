02e580083176732ae4a94111d773505b5e9f0c44
BenjaminDendas <benjamindendas@msn.com>
2020-03-12 16:54:42 +0100

    KAS-1348: formatting frontend

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -5,14 +5,14 @@ module.exports = {
 
   rules: {
     "no-bare-strings": true,
-    "attribute-indentation": false,
+    "attribute-indentation": true,
     "no-inline-styles": false,
     "block-indentation": false,
     "quotes": false,
     "no-unnecessary-concat": false,
     "no-nested-interactive":false,
     "self-closing-void-elements":false,
-    "no-invalid-interactive":false
+    "no-invalid-interactive":false,
   },
 
   ignore: [
