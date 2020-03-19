5e52778bdccf0fab78680636aabf1ea49949cb02
rafael-mindreau <rafael.mindreau@gmail.com>
2020-03-19 17:02:15 +0100

    Update file download ajax

--- app/utils/file-utils.js
+++ app/utils/file-utils.js
@@ -1,14 +1,11 @@
 import { ajax } from 'fe-redpencil/utils/ajax';
-import $ from 'jquery';
 
 export const downloadFilePrompt = function (context, file, filename) {
   filename = filename || file.get('filename');
-  return $.ajax(`/files/${file.id}/download`, {
+  return ajax(`/files/${file.id}/download`, {
     method: 'GET',
     dataType: 'blob',
-    processData: false
-  })
-    .then((content) => context.saveFileAs(filename, content, file.get('contentType')));
+  }).then((content) => context.saveFileAs(filename, content, file.get('contentType')));
 };
 
 export const removeFile = function (file) {
