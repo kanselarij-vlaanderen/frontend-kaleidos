5fc7be3287b35bd5b7aeb84dc56d533e7727c395
Michaël Dierick <michael.dierick@redpencil.io>
2020-03-20 10:30:16 +0100

    Rework class assignment for overloading (WIP)

--- app/components/web-components/vl-alert/component.js
+++ app/components/web-components/vl-alert/component.js
@@ -1,6 +1,12 @@
 import Component from '@glimmer/component';
 
 export default class extends Component {
+  classNames = ['vl-alert'];
+  classNameBindings = [
+    'typeClass',
+    'args.small:vl-alert--small'
+  ];
+
   get typeClass() {
     const t = this.args.type;
     if (['success', 'error', 'warning'].includes(t)) {
@@ -8,6 +14,19 @@ export default class extends Component {
     } else {
       return null;
     }
+  }
+
+  get iconClass() {
+    const classes = ['vl-vi'];
+    const t = this.args.type;
+    if (['warning', 'error'].includes(t)) {
+      classes.push('vl-vi-alert-triangle');
+    } else if (t === 'success') {
+      classes.push('vl-vi-check');
+    } else {
+      classes.push('vl-vi-alert-circle');
+    }
+    return classes.join(' ');
   }
 
   close() {
