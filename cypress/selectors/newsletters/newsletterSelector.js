f82e2e5b9d510f87afd42109fd2df4664e9d0544
ValenberghsSven <sven.valenberghs1@gmail.com>
2020-03-23 16:04:58 +0100

    KAS-1353 add cypress tests

--- cypress/selectors/newsletters/newsletterSelector.js
+++ cypress/selectors/newsletters/newsletterSelector.js
@@ -1,1 +1,3 @@
 export const newslettersTitleSelector = '[data-test-newsletter-header-title]';
+export const newslettersMeetingTitleSelector = '[data-test-newsletter-meeting-header-title]';
+export const newslettersoverviewTitleSelector = '[data-test-newsletter-overview-header-title]';
