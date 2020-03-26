3c43fbb312586bd389565a27de468ff0ce5cc5d7
BenjaminDendas <benjamindendas@msn.com>
2020-03-26 17:03:18 +0100

    KAS-1218: full KB flow tests

--- cypress/selectors/newsletters/newsletterSelector.js
+++ cypress/selectors/newsletters/newsletterSelector.js
@@ -1,1 +1,4 @@
 export const newslettersTitleSelector = '[data-test-newsletter-header-title]';
+export const newsletterEditSelector = '[data-test-newsletter-edit]';
+export const newsletterRdfaEditorSelector = '[data-test-rdfa-editor]';
+export const newsletterEditSave = '[data-test-newsletter-edit-save]';
