025b01a5700928817c172284c3062d88753a1551
Michaël Dierick <michael.dierick@redpencil.io>
2020-03-26 16:07:08 +0100

    Fix typo

--- app/components/cases/cases-filter/component.js
+++ app/components/cases/cases-filter/component.js
@@ -8,7 +8,7 @@ export default Component.extend({
   ministerName: null,
   dateFrom: null,
   dateTo: null,
-  so: false,
+  searchInDecisionsOnly: false,
   popoverShown: false,
 
   searchTask: task(function* () {
