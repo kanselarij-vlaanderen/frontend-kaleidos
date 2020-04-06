62cf285cf96f3237a052e10cd31688c190cdd4d5
rafael-mindreau <rafael.mindreau@gmail.com>
2020-04-06 12:03:22 +0200

    Phase out no reload mixin and access level adapter

--- app/adapters/access-level.js
+++
@@ -1,4 +0,0 @@
-import ApplicationAdapter from './application';
-import NoReloadAdapterMixin from  'fe-redpencil/mixins/adapters/no-reload-adapter-mixin';
-
-export default ApplicationAdapter.extend(NoReloadAdapterMixin, {});
