678a87e802832da36472c9d8695bba73e140191d
BenjaminDendas <benjamindendas@msn.com>
2020-04-07 10:24:09 +0200

    KAS-1218: apply coding standards

--- app/services/agenda-service.js
+++ app/services/agenda-service.js
@@ -224,7 +224,7 @@ export default Service.extend(ModifiedMixin, isAuthenticatedMixin, {
   },
 
   async deleteAgendaitemFromMeeting(agendaitem) {
-    let itemToDelete = await this.store.findRecord('agendaitem', agendaitem.get('id'), {reload: true});
+    let itemToDelete = await this.store.findRecord('agendaitem', agendaitem.get('id'), { reload: true });
     const currentAgenda = await itemToDelete.get('agenda');
     const currentMeeting = await currentAgenda.get('createdFor');
     const currentMeetingId = await currentMeeting.get('id');
@@ -264,14 +264,14 @@ export default Service.extend(ModifiedMixin, isAuthenticatedMixin, {
   async retrieveModifiedDateFromNota(agendaItem) {
     const newsletterInfoForSubcase = await agendaItem.get('subcase.newsletterInfo');
     const nota = await agendaItem.get('nota');
-    if(!nota) {
+    if (!nota) {
       return null;
     }
 
     const documentVersion = await nota.get('lastDocumentVersion');
     const modifiedDateFromMostRecentlyAddedNotaDocumentVersion = documentVersion.created;
     const notaDocumentVersions = await nota.get('documentVersions');
-    if(notaDocumentVersions.length > 1) {
+    if (notaDocumentVersions.length > 1) {
       const newsletterInfoOnSubcaseLastModifiedTime = newsletterInfoForSubcase.modified;
       if (newsletterInfoOnSubcaseLastModifiedTime) {
         if (moment(newsletterInfoOnSubcaseLastModifiedTime).isBefore(moment(modifiedDateFromMostRecentlyAddedNotaDocumentVersion))) {
