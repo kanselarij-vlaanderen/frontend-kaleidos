cf38ccf66e59bf0c15e337b3735818c30ac364c0
rafael-mindreau <rafael.mindreau@gmail.com>
2020-04-09 17:35:24 +0200

    Convert to a class-based component

--- app/components/agenda/agendaitem/agendaitem-case/subcase-titles-edit/component.js
+++ app/components/agenda/agendaitem/agendaitem-case/subcase-titles-edit/component.js
@@ -1,43 +1,45 @@
 import Component from '@ember/component';
-import { inject } from '@ember/service';
+import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
-import { computed, get, set } from '@ember/object';
-import { getCachedProperty } from 'fe-redpencil/mixins/edit-agendaitem-or-subcase';
+import { computed, action, get, set } from '@ember/object';
+import { cached } from 'fe-redpencil/decorators/cached';
 import { saveChanges, cancelEdit } from 'fe-redpencil/utils/agenda-item-utils';
 
-export default Component.extend({
-  store: inject(),
-  classNames: ['vl-form__group', 'vl-u-bg-porcelain'],
-  propertiesToSet: Object.freeze(['title', 'shortTitle', 'accessLevel', 'confidential', 'showInNewsletter']),
+export default class SubcaseTitlesEdit extends Component {
+  @service store;
+  classNames = ['vl-form__group', 'vl-u-bg-porcelain'];
+  propertiesToSet = Object.freeze(['title', 'shortTitle', 'accessLevel', 'confidential', 'showInNewsletter']);
 
-  isRemark: alias('item.showAsRemark'),
+  isRemark = alias('item.showAsRemark');
 
-  isAgendaItem: computed('item.modelName', function () {
+  @computed('item.modelName')
+  get isAgendaItem() {
     return 'agendaitem' == this.get('item.modelName');
-  }),
-
-  title: getCachedProperty('title'),
-  accessLevel: getCachedProperty('accessLevel'),
-  shortTitle: getCachedProperty('shortTitle'),
-  confidential: getCachedProperty('confidential'),
-  showInNewsletter: getCachedProperty('showInNewsletter'),
-
-  actions: {
-    async cancelEditing() {
-      const item = await this.get('item');
-      cancelEdit(item, get(this, 'propertiesToSet'));
-      this.toggleProperty('isEditing');
-    },
-
-    setAccessLevel(accessLevel) {
-      this.set('accessLevel', accessLevel);
-    },
-
-    async saveChanges() {
-      set(this, 'isLoading', true);
-      await saveChanges(get(this, 'item'), get(this, 'propertiesToSet'), get(this, 'propertiesToSet'), true);
-      set(this, 'isLoading', false);
-      this.toggleProperty('isEditing');
-    }
   }
-});
+
+  @cached('title') title;
+  @cached('accessLevel') accessLevel;
+  @cached('shortTitle') shortTitle;
+  @cached('confidential') confidential;
+  @cached('showInNewsletter') showInNewsletter;
+
+  @action
+  async cancelEditing() {
+    const item = await this.get('item');
+    cancelEdit(item, get(this, 'propertiesToSet'));
+    this.toggleProperty('isEditing');
+  }
+
+  @action
+  setAccessLevel(accessLevel) {
+    this.set('accessLevel', accessLevel);
+  }
+
+  @action
+  async saveChanges() {
+    set(this, 'isLoading', true);
+    await saveChanges(get(this, 'item'), get(this, 'propertiesToSet'), get(this, 'propertiesToSet'), true);
+    set(this, 'isLoading', false);
+    this.toggleProperty('isEditing');
+  }
+}
