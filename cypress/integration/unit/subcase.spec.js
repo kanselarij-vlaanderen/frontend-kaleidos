56adf9fb5fda4fb210869125fc766eb27d137c03
ValenberghsSven <sven.valenberghs1@gmail.com>
2020-04-09 18:57:15 +0200

    cypress tests flaky fix

--- cypress/integration/unit/subcase.spec.js
+++ cypress/integration/unit/subcase.spec.js
@@ -36,7 +36,6 @@ context('Subcase tests', () => {
     cy.openSubcase(0);
 
     cy.changeSubcaseAccessLevel(false, SubcaseTitleShort, true, 'Intern Overheid', SubcaseTitleShort, 'Cypress test nieuwere lange titel');
-    cy.addSubcaseThemes([0, 5 , 10]);
     cy.addSubcaseMandatee(1, 0, 0);
     cy.addSubcaseMandatee(2, 0, 0);
 
