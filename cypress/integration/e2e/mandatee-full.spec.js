bdb1e49cd0b379d589ef77b33301482ddb9d1c4b
BenjaminDendas <benjamindendas@msn.com>
2020-04-22 11:59:16 +0200

    KAS-1322: update overgang minister test

--- cypress/integration/e2e/mandatee-full.spec.js
+++ cypress/integration/e2e/mandatee-full.spec.js
@@ -5,15 +5,32 @@ import toolbar from '../../selectors/toolbar.selectors';
 import settings from '../../selectors/settings.selectors';
 import form from '../../selectors/form.selectors';
 import mandatee from '../../selectors/mandatees/mandateeSelectors';
+import modal from '../../selectors/modal.selectors';
 
 context('Full test', () => {
   before(() => {
     cy.server();
     // cy.resetCache();
+    cy.resetSearch();
     cy.login('Admin');
   });
 
   it('should Add new minister', () => {
+    const testId = 'testId=' + currentTimestamp() + ': ';
+
+    const PLACE = 'Brussel';
+    const KIND = 'Ministerraad';
+    const JANUARI = 'maart';
+    const YEAR = '2020';
+    const DAY = '3';
+
+    const caseTitle_3_Short= testId + 'Cypress test dossier 3';
+    const type_3= 'Mededeling';
+    const newSubcase_3_TitleShort= caseTitle_3_Short + ' procedure';
+    const subcase_3_TitleLong= testId + 'Cypress test voor het aanmaken van een dossier (3) en procedurestap';
+    const subcase_3_Type='In voorbereiding';
+    const subcase_3_Name='Principiële goedkeuring m.h.o. op adviesaanvraag';
+
     cy.route('/');
     cy.get(toolbar.settings).click();
     cy.get(settings.manageMinisters).click();
@@ -21,20 +38,50 @@ context('Full test', () => {
     cy.get(settings.addMinister).should('exist').should('be.visible').click();
     cy.get(mandatee.addMandateeTitleContainer).should('exist').should('be.visible').within(() => {
       cy.get(form.formInput).should('exist').should('be.visible').type('Eerste minister van onderhoud');
-    }).then(() => {
-      cy.get(mandatee.addMandateeNicknameContainer).should('exist').should('be.visible').within(() => {
-        cy.get(form.formInput).should('exist').should('be.visible').type('Eerste minister');
-      })
-    }).then(() => {
-      cy.get(mandatee.addMandateeDropdownContainer).should('exist').should('be.visible').within(() => {
-        cy.get('.ember-power-select-trigger').click().then(() => {
-          cy.get('.ember-power-select-option').eq(0).click();
-        })
-      })
-    })
-  })
+    });
+    cy.get(mandatee.addMandateeNicknameContainer).should('exist').should('be.visible').within(() => {
+      cy.get(form.formInput).should('exist').should('be.visible').type('Eerste minister');
+    });
+    cy.get(mandatee.addMandateeDropdownContainer).should('exist').should('be.visible').within(() => {
+      cy.get('.ember-power-select-trigger').click();
+    });
+    cy.get('.ember-power-select-option').should('exist').then(() => {
+      cy.contains('Liesbeth Homans').click();
+    });
+
+    cy.get(mandatee.addMandateeIseCodeDropdownContainer).should('exist').should('be.visible').within(() => {
+      cy.get('.ember-power-select-trigger').click();
+    });
+    cy.get('.ember-power-select-option').should('exist').then(() => {
+      cy.contains('Aanvullend net').click();
+      cy.get(mandatee.addMandateeIseCodeDropdownContainer).should('exist').should('be.visible').within(() => {
+        cy.get('.ember-power-select-trigger').click();
+      });
+    });
+    cy.selectDate('2020','maart','1');
+    cy.get(form.formSave).should('exist').should('be.visible').click();
+    cy.visit('/').then(()=> {
+      cy.createDefaultAgenda(KIND,YEAR,JANUARI,DAY,PLACE).then(() => {
+        const agendaDate = Cypress.moment("2020-03-03").set({"hour": 10, "minute": 10});
+        cy.openAgendaForDate(agendaDate);
+      });
+    });
+
+    cy.createCase(false, caseTitle_3_Short);
+    cy.addSubcase(type_3,newSubcase_3_TitleShort,subcase_3_TitleLong, subcase_3_Type, subcase_3_Name);
+
+  });
 
 
-
+  /**
+   * @description returns the current time in unix timestamp
+   * @name currentTimestamp
+   * @memberOf Cypress.Chainable#
+   * @function
+   * @returns {number} The current time in unix timestamp
+   */
+  function currentTimestamp() {
+    return Cypress.moment().unix();
+  }
 
 });
