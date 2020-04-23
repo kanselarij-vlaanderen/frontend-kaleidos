694b5a6118957e91327e99d2be3f7efdcb96696d
rafael-mindreau <rafael.mindreau@gmail.com>
2020-04-23 19:38:01 +0200

    Refactor agendaitem minutes + Octane + Angle brackets

--- app/components/agenda/agendaitem/agendaitem-minutes/component.js
+++ app/components/agenda/agendaitem/agendaitem-minutes/component.js
@@ -1,41 +1,43 @@
 import Component from '@ember/component';
-import { inject } from '@ember/service';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
-import isAuthenticatedMixin from 'fe-redpencil/mixins/is-authenticated-mixin';
 import moment from 'moment';
 
-export default Component.extend(isAuthenticatedMixin, {
-  store: inject(),
-  sessionService: inject(),
-  classNames: ['vlc-padding-bottom--large'],
-  isEditing: false,
+export default class AgendaitenMinutes extends Component {
+  @service store;
+  @service sessionService;
+  @service currentSession;
 
-  currentSession: alias('sessionService.currentSession'),
+  classNames = ['vlc-padding-bottom--large'];
+  isEditing = false;
 
-  actions: {
-    async toggleIsEditing() {
-      const agendaitemNotes = await this.get('agendaitem.meetingRecord');
-      const date = moment().utc().toDate();
-      if (!agendaitemNotes) {
-        const meetingRecord = this.store.createRecord('meeting-record', {
-          created: date,
-          modified: date,
-          announcements: null,
-          others: null,
-          description: '',
-          attendees: [],
-          agendaitem: await this.get('agendaitem'),
-          meeting: null
-        })
-        await meetingRecord.save()
-      }
-      this.toggleProperty('isEditing');
-    },
+  @alias('sessionService.currentSession') currentSession;
 
-    async saveChanges(meetingRecord) {
-      const recordToSave = this.store.peekRecord('meeting-record', meetingRecord.get('id'));
-      await recordToSave.save();
-      this.toggleProperty('isEditing');
+  @action
+  async toggleIsEditing() {
+    const agendaitemNotes = await this.get('agendaitem.meetingRecord');
+    const date = moment().utc().toDate();
+    if (!agendaitemNotes) {
+      const meetingRecord = this.store.createRecord('meeting-record', {
+        created: date,
+        modified: date,
+        announcements: null,
+        others: null,
+        description: '',
+        attendees: [],
+        agendaitem: await this.get('agendaitem'),
+        meeting: null
+      })
+      await meetingRecord.save()
     }
+    this.toggleProperty('isEditing');
   }
-});
+
+  @action
+  async saveChanges(meetingRecord) {
+    const recordToSave = this.store.peekRecord('meeting-record', meetingRecord.get('id'));
+    await recordToSave.save();
+    this.toggleProperty('isEditing');
+  }
+}
