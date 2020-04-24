02d54509720cfb8e225191f84896b6102fe58c28
rafael-mindreau <rafael.mindreau@gmail.com>
2020-04-24 16:25:22 +0200

    Unlink a mixin from a component that does not use it

--- app/components/subcases/subcases-process/component.js
+++ app/components/subcases/subcases-process/component.js
@@ -1,8 +1,7 @@
 import Component from '@ember/component';
 import UploadDocumentMixin from 'fe-redpencil/mixins/upload-document-mixin';
-import isAuthenticatedMixin from 'fe-redpencil/mixins/is-authenticated-mixin';
 
-export default Component.extend(UploadDocumentMixin, isAuthenticatedMixin, {
+export default Component.extend(UploadDocumentMixin, {
   classNames: ['vl-u-spacer-extended-l'],
   actions: {
     toggleIsShowingDocuments(subcase) {
