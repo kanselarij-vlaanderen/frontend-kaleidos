df07008712cfbffd4f5a51ace246769fd44170dd
rafael-mindreau <rafael.mindreau@gmail.com>
2020-04-27 10:30:12 +0200

    Unlink authentication mixin from agendas overview since it isn't used

--- app/controllers/agendas/overview.js
+++ app/controllers/agendas/overview.js
@@ -1,11 +1,10 @@
 import Controller from '@ember/controller';
 import moment from 'moment';
-import isAuthenticatedMixin from 'fe-redpencil/mixins/is-authenticated-mixin';
 import DefaultQueryParamsMixin from 'ember-data-table/mixins/default-query-params';
 import { inject } from '@ember/service';
 import { computed } from '@ember/object';
 
-export default Controller.extend(isAuthenticatedMixin, DefaultQueryParamsMixin, {
+export default Controller.extend(DefaultQueryParamsMixin, {
   sessionService: inject(),
   intl: inject(),
   agendaService: inject(),
