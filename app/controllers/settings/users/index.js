aa572aa44244128844eac04a557fb454f4df03f8
BenjaminDendas <benjamindendas@msn.com>
2020-04-29 17:25:26 +0200

    KAS-1497: switch light-table with data-table part 1

--- app/controllers/settings/users/index.js
+++ app/controllers/settings/users/index.js
@@ -9,66 +9,15 @@ export default Controller.extend({
   toaster: service(),
   isUploadingFile: null,
   shouldRefreshTableModel: null,
-  filterText: null,
+  filterText: '',
 
-  columns: computed(function () {
-    return [
-      {
-        label: this.intl.t('first-name'),
-        classNames: ['vl-data-table-col-2 vl-data-table__header-title'],
-        cellClassNames: ['vl-data-table-col-2'],
-        sortable: true,
-        breakpoints: ['mobile', 'tablet', 'desktop'],
-        valuePath: 'firstName',
-      },
-      {
-        label: this.intl.t('last-name'),
-        classNames: ['vl-data-table-col-2 vl-data-table__header-title'],
-        cellClassNames: ['vl-data-table-col-2'],
-        sortable: true,
-        breakpoints: ['mobile', 'tablet', 'desktop'],
-        valuePath: 'lastName',
-      },
-      {
-        label: this.intl.t('email'),
-        classNames: ['vl-data-table-col-2 vl-data-table__header-title'],
-        cellClassNames: ['vl-data-table-col-2'],
-        breakpoints: ['mobile', 'tablet', 'desktop'],
-        sortable: false, // Should be able to display 'email' and sort on 'emailLink'. This doesn't seem possible with ember-light-table. Sort disabled for now.
-        valuePath: 'email',
-      },
-      {
-        label: this.intl.t('ovo-code'),
-        classNames: ['vl-data-table-col-2 vl-data-table__header-title'],
-        cellClassNames: ['vl-data-table-col-2'],
-        breakpoints: ['mobile', 'tablet', 'desktop'],
-        valuePath: 'organization.identifier',
-      },
-      {
-        label: this.intl.t('group'),
-        classNames: ['vl-data-table-col-3 vl-data-table__header-title'],
-        cellClassNames: ['vl-data-table-col-3'],
-        breakpoints: ['mobile', 'tablet', 'desktop'],
-        valuePath: 'group.name',
-        sortable: true,
-      },
-      {
-        classNames: ['vl-data-table-col-1'],
-        cellClassNames: ['vl-data-table-col-1'],
-        breakpoints: ['mobile', 'tablet', 'desktop'],
-        sortable: false,
-        cellComponent: 'web-components/light-table/vlc-user-table-actionbar',
-      },
-    ];
-  }),
-
-  filter: computed('filterText', function () {
-    const searchText = this.get('filterText');
-    if (isEmpty(searchText)) {
-      return null;
-    }
-    return searchText;
-  }),
+  // filter: computed('filterText', function () {
+  //   const searchText = this.get('filterText');
+  //   if (isEmpty(searchText)) {
+  //     return null;
+  //   }
+  //   return searchText;
+  // }),
 
   actions: {
     showFileUploader() {
--- app/routes/settings/users/index.js
+++ app/routes/settings/users/index.js
@@ -1,14 +1,28 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import DataTableRouteMixin from 'ember-data-table/mixins/route';
 
-export default Route.extend({
+export default Route.extend(DataTableRouteMixin, {
+  modelName: 'user',
   router: service(),
+
+  queryParams: {
+    firstname: {
+      refreshModel: true,
+    },
+  },
 
   model() {
     return this.store.query('user', {
       include: 'group,organization',
       sort: 'first-name'
     }).then(users => users.toArray());
+  },
+
+  mergeQueryOptions(params) {
+    return {
+      'filter[first-name]': params.firstname
+    };
   },
 
   actions: {
