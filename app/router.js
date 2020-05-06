543a4795942e09a200a324151d92d673535dc541
rafael-mindreau <rafael.mindreau@gmail.com>
2020-05-06 14:35:23 +0200

    Add a new route for the icons to live in

--- app/router.js
+++ app/router.js
@@ -18,7 +18,7 @@ Router.map(function () {
     this.route('compare', { path: '/vergelijken' });
     this.route('documents', { path: '/documenten' });
   });
-  this.route('agendaitems', { path: '/agendaitems' }, function () {
+  this.route('agendaitems', function () {
 
   });
   this.route('cases', { path: '/dossiers' }, function () {
@@ -35,7 +35,7 @@ Router.map(function () {
     this.route('overview', { path: '' });
   });
   this.route('settings', { path: '/instellingen' }, function () {
-    this.route('ministers', { path: '/ministers' });
+    this.route('ministers');
     this.route('overview', { path: '/overzicht' });
     this.route('users', { path:"/gebruikers"}, function() {
       this.route('user', { path: "/:id"});
@@ -81,6 +81,7 @@ Router.map(function () {
   this.route('not-supported');
   this.route('help');
   this.route('manual', { path: '/handleiding' });
+  this.route('icons');
 });
 
 export default Router;
