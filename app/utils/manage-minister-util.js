aff9ef45f593e146c53acf3a0c7bad3f58786a30
BenjaminDendas <benjamindendas@msn.com>
2020-05-14 16:32:57 +0200

    KAS-1402: improve naming of method

--- app/utils/manage-minister-util.js
+++ app/utils/manage-minister-util.js
@@ -5,17 +5,17 @@ export const refreshData = async(mandatee, mandateeRows) => {
   const fields = (await Promise.all(iseCodes.map((iseCode) => iseCode.get('field')))).filter((item) => item);
   const domains = await Promise.all(fields.map((field) => field.get('domain')));
 
-  const rowToShow = EmberObject.create({
+  const mandateeRow = EmberObject.create({
     domains: [...new Set(domains)],
     fields: [...new Set(fields)]
   });
-  rowToShow.get('domains').map((domain) => domain.set('selected', false));
-  rowToShow.get('fields').map((domain) => domain.set('selected', false));
+  mandateeRow.get('domains').map((domain) => domain.set('selected', false));
+  mandateeRow.get('fields').map((domain) => domain.set('selected', false));
 
   if (!mandateeRows) {
-    rowToShow.set('isSubmitter', true);
+    mandateeRow.set('isSubmitter', true);
   }
-  return rowToShow;
+  return mandateeRow;
 };
 
 export const selectDomain = async (rowToShowFields, domain, value) => {
