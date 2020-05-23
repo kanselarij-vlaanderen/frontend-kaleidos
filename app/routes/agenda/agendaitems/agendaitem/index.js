9799a99835d73005d38270b3dd1e515309851a7f
Michaël Dierick <michael.dierick@redpencil.io>
2020-05-23 13:16:24 +0200

    Make agenda available to controller

--- app/routes/agenda/agendaitems/agendaitem/index.js
+++ app/routes/agenda/agendaitems/agendaitem/index.js
@@ -4,4 +4,11 @@ export default class DetailAgendaitemAgendaitemsAgendaRoute extends Route {
   model() {
     return this.modelFor('agenda.agendaitems.agendaitem');
   }
+
+  setupController(controller, model) {
+    super.setupController(...arguments);
+    const agenda = this.modelFor('agenda').agenda;
+    controller.set('agenda', agenda);
+    controller.set('model', model);
+  }
 }
