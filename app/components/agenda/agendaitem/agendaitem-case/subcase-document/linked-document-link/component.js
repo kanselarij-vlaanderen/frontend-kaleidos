091f854450ce06844056496729f54442e68a0de5
BenjaminDendas <benjamindendas@msn.com>
2020-05-29 12:18:24 +0200

    KAS-1405: refactor mydocumentversions mixin in linked-document-link

--- app/components/agenda/agendaitem/agendaitem-case/subcase-document/linked-document-link/component.js
+++ app/components/agenda/agendaitem/agendaitem-case/subcase-document/linked-document-link/component.js
@@ -1,11 +1,11 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { inject } from '@ember/service';
-import MyDocumentVersions from 'fe-redpencil/mixins/my-document-versions';
 import { A } from '@ember/array';
 import { deprecatingAlias } from '@ember/object/computed';
+import DS from 'ember-data';
 
-export default Component.extend(MyDocumentVersions, {
+export default Component.extend({
   currentSession: inject(),
   classNames: ['vl-u-spacer-extended-bottom-s'],
   classNameBindings: ['aboutToDelete'],
@@ -24,6 +24,48 @@ export default Component.extend(MyDocumentVersions, {
   }),
 
   myDocumentVersions: computed.alias('item.linkedDocumentVersions'),
+
+  lastDocumentVersion: computed('mySortedDocumentVersions.@each', function () {
+    const sortedVersions = this.get('mySortedDocumentVersions');
+    return sortedVersions.lastObject;
+  }),
+
+
+  lastDocumentVersionName: computed('lastDocumentVersion.name', function () {
+    return this.get('lastDocumentVersion.name');
+  }),
+
+  mySortedDocumentVersions: computed('myDocumentVersions.@each', 'document.sortedDocumentVersions.@each', function () {
+    return DS.PromiseArray.create({
+      promise: (async () => {
+        const itemVersionIds = {};
+        const versions = await this.get('myDocumentVersions');
+        if (versions) {
+          versions.map((item) => {
+            itemVersionIds[item.get('id')] = true;
+          });
+        }
+        const documentVersions = await this.get('document.sortedDocumentVersions');
+        if (documentVersions) {
+          const matchingVersions = await documentVersions.filter((item) => {
+            return itemVersionIds[item.id];
+          });
+          return matchingVersions;
+        }
+
+        return;
+      })()
+    });
+  }),
+
+  myReverseSortedVersions: computed('mySortedDocumentVersions.@each', function () {
+    const reversed = [];
+    this.get('mySortedDocumentVersions').map((item) => {
+      reversed.push(item);
+    });
+    reversed.reverse();
+    return reversed;
+  }),
 
   // TODO: refactor model/code in function of "reeds aangeleverde documenten"
   async unlinkDocumentVersions(documentVersions, model) {
