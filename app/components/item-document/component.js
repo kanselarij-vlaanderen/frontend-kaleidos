6a85d84c4ccc6e4b76d0c8b9054c60621f266436
BenjaminDendas <benjamindendas@msn.com>
2020-05-29 12:14:32 +0200

    KAS-1405: refactor mydocumentversions mixin uit item-document

--- app/components/item-document/component.js
+++ app/components/item-document/component.js
@@ -1,13 +1,47 @@
 import Component from '@ember/component';
-import MyDocumentVersions from 'fe-redpencil/mixins/my-document-versions';
 import { deprecatingAlias } from '@ember/object/computed';
+import { computed } from '@ember/object';
+import DS from 'ember-data';
 
-export default Component.extend(MyDocumentVersions, {
+export default Component.extend({
   isClickable: null,
+  myDocumentVersions: computed.alias('item.documentVersions'),
 
   document: deprecatingAlias('documentContainer', {
     id: 'model-refactor.documents',
     until: '?'
+  }),
+
+  lastDocumentVersion: computed('mySortedDocumentVersions.@each', function () {
+    const sortedVersions = this.get('mySortedDocumentVersions');
+    return sortedVersions.lastObject;
+  }),
+
+  lastDocumentVersionName: computed('lastDocumentVersion.name', function () {
+    return this.get('lastDocumentVersion.name');
+  }),
+
+  mySortedDocumentVersions: computed('myDocumentVersions.@each', 'document.sortedDocumentVersions.@each', function () {
+    return DS.PromiseArray.create({
+      promise: (async () => {
+        const itemVersionIds = {};
+        const versions = await this.get('myDocumentVersions');
+        if (versions) {
+          versions.map((item) => {
+            itemVersionIds[item.get('id')] = true;
+          });
+        }
+        const documentVersions = await this.get('document.sortedDocumentVersions');
+        if (documentVersions) {
+          const matchingVersions = await documentVersions.filter((item) => {
+            return itemVersionIds[item.id];
+          });
+          return matchingVersions;
+        }
+
+        return;
+      })()
+    });
   }),
 
   actions: {
