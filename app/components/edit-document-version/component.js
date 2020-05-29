df63117d2aa1694ee233b8a14e33dc4021b4ec64
BenjaminDendas <benjamindendas@msn.com>
2020-05-29 12:12:51 +0200

    KAS-1405: refactor mydocumentversions mixin uit edit-document-version

--- app/components/edit-document-version/component.js
+++ app/components/edit-document-version/component.js
@@ -1,8 +1,43 @@
 import Component from '@ember/component';
-import MyDocumentVersions from 'fe-redpencil/mixins/my-document-versions';
+import { computed } from '@ember/object';
+import DS from 'ember-data';
 
-export default Component.extend(MyDocumentVersions, {
+export default Component.extend({
   tagName: 'tr',
+  myDocumentVersions: computed.alias('item.documentVersions'),
+
+  lastDocumentVersion: computed('mySortedDocumentVersions.@each', function () {
+    const sortedVersions = this.get('mySortedDocumentVersions');
+    return sortedVersions.lastObject;
+  }),
+
+  lastDocumentVersionName: computed('lastDocumentVersion.name', function () {
+    return this.get('lastDocumentVersion.name');
+  }),
+
+  mySortedDocumentVersions: computed('myDocumentVersions.@each', 'document.sortedDocumentVersions.@each', function () {
+    return DS.PromiseArray.create({
+      promise: (async () => {
+        const itemVersionIds = {};
+        const versions = await this.get('myDocumentVersions');
+        if (versions) {
+          versions.map((item) => {
+            itemVersionIds[item.get('id')] = true;
+          });
+        }
+        const documentVersions = await this.get('document.sortedDocumentVersions');
+        if (documentVersions) {
+          const matchingVersions = await documentVersions.filter((item) => {
+            return itemVersionIds[item.id];
+          });
+          return matchingVersions;
+        }
+
+        return;
+      })()
+    });
+  }),
+
   actions: {
 
     deleteRow(document) {
