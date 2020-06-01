c3488fb8987382f8e9d177c95257f0e6a1ccb0b9
Michaël Dierick <michael.dierick@redpencil.io>
2020-06-01 16:09:00 +0200

    Search is an authenticated route

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default class SearchRoute extends Route {
+export default class SearchRoute extends Route.extend(AuthenticatedRouteMixin) {
   queryParams = {
     searchText: {
       refreshModel: true,
