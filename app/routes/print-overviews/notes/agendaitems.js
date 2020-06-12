7226192af34ec2a464d885641a14bd9100c2110c
BenjaminDendas <benjamindendas@msn.com>
2020-06-12 17:09:02 +0200

    Revert "KAS-1408: move mixins"
    This reverts commit bbe8247a8b609ae52105b381ccb93d2bed058af3.

--- app/routes/print-overviews/notes/agendaitems.js
+++ app/routes/print-overviews/notes/agendaitems.js
@@ -1,63 +1,10 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import { inject as service } from '@ember/service';
+import AgendaitemTableRouteMixin from 'fe-redpencil/mixins/light-table/agendaitem-table-route-mixin';
 
-export default Route.extend(AuthenticatedRouteMixin, {
+export default Route.extend(AuthenticatedRouteMixin, AgendaitemTableRouteMixin, {
   authenticationRoute: 'login',
   sort: 'priority',
   routeNamePrefix: 'notes',
-  include: 'subcase,meeting-record',
-  currentSession: service(),
-  modelName: 'agendaitem',
-  filter: null,
-  page: 0,
-  size: 9999,
-  shouldFilterRemarks: true,
-
-  model() {
-    const filter = {
-      agenda: {
-        id: this.modelFor(`print-overviews.${this.routeNamePrefix}`).get('id'),
-      },
-    };
-    if (this.shouldFilterRemarks) {
-      filter['show-as-remark'] = false;
-    }
-    this.set('filter', filter);
-    return this.store
-      .query(this.get('modelName'), {
-        filter,
-        include: this.get('include'),
-        sort: this.get('sort'),
-        page: {
-          size: this.get('size'),
-          number: this.get('page'),
-        },
-      })
-      .then((items) => {
-        return items.toArray();
-      });
-  },
-
-  setupController(controller) {
-    this._super(...arguments);
-    controller.set('filter', this.filter);
-    controller.set('sort', this.sort);
-    controller.set('include', this.include);
-  },
-
-  redirect() {
-    if (!this.currentSession.isEditor) {
-      this.transitionTo(`print-overviews.${this.routeNamePrefix}.overview`, {
-        queryParams: { definite: true },
-      });
-    }
-  },
-
-  actions: {
-    refresh() {
-      this._super(...arguments);
-      this.refresh();
-    },
-  }
+  include: 'subcase,meeting-record'
 });
