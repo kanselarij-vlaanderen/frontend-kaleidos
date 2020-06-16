fb7791b5a8cb9d890145cf559d72eba545b0baf3
Fred Kroket <wouters.f@gmail.com>
2020-06-16 14:21:14 +0200

    bugfix navigation on agendaitem

--- app/routes/agenda/agendaitems/agendaitem/news-item.js
+++ app/routes/agenda/agendaitems/agendaitem/news-item.js
@@ -4,6 +4,13 @@ import { inject as service } from '@ember/service';
 export default class NewsitemAgendaitemAgendaitemsAgendaRoute extends Route {
   @service agendaService;
 
+  async beforeModel() {
+    const agendaItem = this.modelFor('agenda.agendaitems.agendaitem');
+    const subcase = await agendaItem.subcase;
+    if (!subcase) {
+      this.transitionTo('agenda.agendaitems.agendaitem.index')
+    }
+  }
   async setupController(controller, model) {
     super.setupController(...arguments);
     const agendaItem = this.modelFor('agenda.agendaitems.agendaitem');
