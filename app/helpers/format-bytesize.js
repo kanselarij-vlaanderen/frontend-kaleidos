732ee2d6ebc95a001546ce926bfc4aa404586f66
BenjaminDendas <benjamindendas@msn.com>
2020-06-22 15:41:27 +0200

    KAS-1674: linter fixes - tripple equals + radix

--- app/helpers/format-bytesize.js
+++ app/helpers/format-bytesize.js
@@ -2,8 +2,8 @@ import { helper } from '@ember/component/helper';
 
 export function formatBytesize([bytes]) {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes == 0) return '0 Byte';
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+  if (bytes === 0) return '0 Byte';
+  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
   return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`;
 }
 
