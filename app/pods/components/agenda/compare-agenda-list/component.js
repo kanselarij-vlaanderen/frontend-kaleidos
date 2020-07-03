e02db531b6c7b4e1849ac223a33fe08e2a14cd9f
brecht philips <brecht_philips@hotmail.com>
2020-07-03 13:54:04 +0200

    KAS-1634: removed the observer and refactored the component to octane

--- app/pods/components/agenda/compare-agenda-list/component.js
+++ app/pods/components/agenda/compare-agenda-list/component.js
@@ -1,79 +1,85 @@
-import Component from '@ember/component';
-import { computed, observer, get } from '@ember/object';
-import { inject } from '@ember/service';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import EmberObject from '@ember/object';
 import { alias } from '@ember/object/computed';
+import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  store: inject(),
-  sessionService: inject(),
-  agendaService: inject(),
-  addedAgendaitems: alias('agendaService.addedAgendaitems'),
+export default class CompareAgendaList extends Component {
+  /**
+   * INFO arguments from parent.
+   * @agendaToCompare=undefined
+   * @currentAgenda type agenda
+   * @isShowingChanges=undefined
+   */
 
-  classNames: ['vlc-scroll-wrapper__body'],
-  agendaToCompare: null,
-  currentAgenda: null,
-  currentAgendaGroups: null,
-  agendaToCompareGroups: null,
-  agendaOne: null,
-  agendaTwo: null,
+  @service store;
+  @service sessionService;
+  @service agendaService;
+  @alias('agendaService.addedAgendaitems') addedAgendaitems;
 
-  isLoadingAgendaitems: computed(
-    'isLoadingAgendaOne',
-    'isLoadingAgendaTwo',
-    'isLoadingComparison',
-    function () {
-      if (this.isLoadingAgendaOne || this.isLoadingAgendaTwo || this.isLoadingComparison) {
-        return true;
-      }
-      return false;
-    }
-  ),
+  classNames = ['vlc-scroll-wrapper__body'];
 
-  bothAgendasSelectedObserver: observer('agendaOne.id', 'agendaTwo.id', async function () {
-    const { agendaOne, agendaTwo, agendaitemsLeft, agendaitemsRight } = this;
-    const bothAgendasSelected = agendaOne && agendaTwo;
+  agendaToCompare = null;
+  currentAgenda = null;
+  currentAgendaGroups = null;
+  agendaToCompareGroups = null;
 
-    if (bothAgendasSelected) {
-      this.set('isLoadingComparison', true);
-      this.set('combinedItems', []);
+  @tracked agendaOne = null;
+  @tracked agendaTwo = null;
+  @tracked isLoadingAgendaOne = null;
+  @tracked isLoadingAgendaTwo = null;
+  @tracked isLoadingComparison = null;
+
+  @tracked agendaitemsLeft = null;
+  @tracked agendaitemsRight = null;
+  @tracked combinedItems = [] ;
+
+
+  async bothAgendasSelected() {
+    if (this.agendaOne && this.agendaTwo) {
+      this.isLoadingComparison = true;
 
       const sortedAgendas = await this.sessionService.currentSession.sortedAgendas;
-      let agendaOneIndex = sortedAgendas.indexOf(agendaOne);
-      let agendaTwoIndex = sortedAgendas.indexOf(agendaTwo);
+      let agendaOneIndex = sortedAgendas.indexOf(this.agendaOne);
+      let agendaTwoIndex = sortedAgendas.indexOf(this.agendaTwo);
 
       if (agendaOneIndex < agendaTwoIndex) {
-        await this.agendaService.agendaWithChanges(agendaOne.get('id'), agendaTwo.get('id'));
+        await this.agendaService.agendaWithChanges(this.agendaOne.get('id'), this.agendaTwo.get('id'));
       } else {
-        await this.agendaService.agendaWithChanges(agendaTwo.get('id'), agendaOne.get('id'));
+        await this.agendaService.agendaWithChanges(this.agendaTwo.get('id'), this.agendaOne.get('id'));
       }
 
-      const newItems = await this.createComparisonList(agendaitemsLeft, agendaitemsRight);
-      this.set('combinedItems', newItems);
-      this.set('isLoadingComparison', false);
+      this.combinedItems = await this.createComparisonList(this.agendaitemsLeft, this.agendaitemsRight);
+      this.isLoadingComparison = false;
     }
-  }),
+  }
 
-  actions: {
-    async chooseAgendaOne(agenda) {
-      this.set('isLoadingAgendaOne', true);
-      const agendaitems = await this.getAgendaitemsFromAgenda(agenda.get('id'));
-      await this.agendaService.groupAgendaItemsOnGroupName(agendaitems);
 
-      this.set('agendaitemsLeft', agendaitems);
-      this.set('agendaOne', agenda);
-      this.set('isLoadingAgendaOne', false);
-    },
-    async chooseAgendaTwo(agenda) {
-      this.set('isLoadingAgendaTwo', true);
-      const agendaitems = await this.getAgendaitemsFromAgenda(agenda.get('id'));
-      await this.agendaService.groupAgendaItemsOnGroupName(agendaitems);
+  @action
+  async chooseAgendaOne(agenda) {
+    this.isLoadingAgendaOne = true;
+    const agendaitems = await this.getAgendaitemsFromAgenda(agenda.get('id'));
+    await this.agendaService.groupAgendaItemsOnGroupName(agendaitems);
 
-      this.set('agendaitemsRight', agendaitems);
-      this.set('agendaTwo', agenda);
-      this.set('isLoadingAgendaTwo', false);
-    },
-  },
+    this.agendaitemsLeft = agendaitems;
+    this.agendaOne = agenda;
+    this.isLoadingAgendaOne = false;
+    await this.bothAgendasSelected();
+  }
+
+  @action
+  async chooseAgendaTwo(agenda) {
+    this.isLoadingAgendaTwo = true;
+    const agendaitems = await this.getAgendaitemsFromAgenda(agenda.get('id'));
+    await this.agendaService.groupAgendaItemsOnGroupName(agendaitems);
+
+    this.agendaitemsRight = agendaitems;
+    this.agendaTwo = agenda;
+    this.isLoadingAgendaTwo = false;
+    await this.bothAgendasSelected();
+  }
+
 
   getAgendaitemsFromAgenda(id) {
     return this.store.query('agendaitem', {
@@ -84,16 +90,15 @@ export default Component.extend({
       sort: 'priority',
       include: 'agenda,subcase,mandatees',
     });
-  },
+  }
 
   async compareSubcase(left, right) {
     const leftSubcaseId = await left.get('subcase.id');
     const rightSubcaseId = await right.get('subcase.id');
     return leftSubcaseId == rightSubcaseId;
-  },
+  }
 
   async createComparisonList(leftAgendaitems, rightAgendaitems) {
-    const addedAgendaitems = this.get('addedAgendaitems');
     leftAgendaitems = [].concat(leftAgendaitems.toArray());
     rightAgendaitems = [].concat(rightAgendaitems.toArray());
 
@@ -115,7 +120,7 @@ export default Component.extend({
         continue;
       }
 
-      if (addedAgendaitems.indexOf(currentRight.id) >= 0) {
+      if (this.addedAgendaitems.indexOf(currentRight.id) >= 0) {
         combinedItems.push(EmberObject.create({ left: null, right: currentRight }));
         currentRight = null;
         continue;
@@ -141,7 +146,7 @@ export default Component.extend({
       currentRight = null;
     }
     return this.setCombinedGroupNames(combinedItems);
-  },
+  }
 
   setCombinedGroupNames(list) {
     list.map((combinedItem) => {
@@ -154,9 +159,9 @@ export default Component.extend({
       combinedItem.set('groupName', leftGroupName || rightGroupName);
     });
     return list;
-  },
+  }
 
   findItemBySubcase(item, list) {
-    return list.find((possibleMatch) => possibleMatch.get('subcase.id') == item.get('subcase.id'));
-  },
-});
+    return list.find((possibleMatch) => possibleMatch.get('subcase.id') === item.get('subcase.id'));
+  }
+}
