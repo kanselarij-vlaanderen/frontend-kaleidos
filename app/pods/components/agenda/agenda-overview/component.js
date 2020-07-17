2e500f1d7502a8251470b1269290e0b28c7c6945
BenjaminDendas <benjamindendas@msn.com>
2020-07-17 12:15:32 +0200

    KAS-1699: refactor agendalist naar agenda overview

--- app/pods/components/agenda/agenda-overview/component.js
+++ app/pods/components/agenda/agenda-overview/component.js
@@ -5,7 +5,7 @@ import { inject as service } from '@ember/service';
 import { restartableTask } from 'ember-concurrency-decorators';
 import { isPresent } from '@ember/utils';
 
-export default class AgendaList extends Component {
+export default class AgendaOverview extends Component {
   @service sessionService;
   @service agendaService;
   @service('current-session') currentSessionService;
