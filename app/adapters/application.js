6538e55aaba17bae53452af3d3c6e0b02bedfcfd
BenjaminDendas <benjamindendas@msn.com>
2020-07-22 08:05:52 +0200

    KAS-1674: fix spacing

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -14,41 +14,43 @@ export default DS.JSONAPIAdapter.extend({
   handleResponse(status, headers, payload, requestData) {
     if (!this.isSuccess(status, headers, payload)) {
       switch (status) {
-        case 400:
-          this.toaster.error(this.intl.t('error-bad-request'), this.intl.t('warning-title'));
-          break;
-        case 404:
-          this.toaster.error(this.intl.t('error-not-found'), this.intl.t('warning-title'));
-          break;
-        case 500:
-          this.toaster.error(this.intl.t('error'), this.intl.t('warning-title'));
-          break;
-        default:
-          return new DS.InvalidError(status);
+      case 400:
+        this.toaster.error(this.intl.t('error-bad-request'), this.intl.t('warning-title'));
+        break;
+      case 404:
+        this.toaster.error(this.intl.t('error-not-found'), this.intl.t('warning-title'));
+        break;
+      case 500:
+        this.toaster.error(this.intl.t('error'), this.intl.t('warning-title'));
+        break;
+      default:
+        return new DS.InvalidError(status);
       }
     } else {
       switch (status) {
-        case 201:
-          if (payload && payload.data && this.checkIfNotificationShouldBeShown(payload.data.type)) {
-            this.toaster.success(this.intl.t('successfully-created', { type: this.translateAndParseSuccesType(payload.data.type) }),
-              this.intl.t('successfully-created-title'));
-          }
-          break;
-        case 204:
-          if (requestData && requestData.method === 'DELETE') {
-            this.toaster.success(this.intl.t('successfully-deleted'), this.intl.t('successfully-created-title'));
-          } else if (requestData && requestData.method === 'PATCH') {
-            // Toast for newer models (with modifier locking) are thrown from the model.
-            // Not form here.
-            if (!this.checkIfNotificationShouldBeShownInModel(requestData.url)) {
-              this.toaster.success(this.intl.t('successfully-saved'), this.intl.t('successfully-created-title'));
-            }
-          } else {
+      case 201:
+        if (payload && payload.data && this.checkIfNotificationShouldBeShown(payload.data.type)) {
+          this.toaster.success(this.intl.t('successfully-created', {
+            type: this.translateAndParseSuccesType(payload.data.type),
+          }),
+          this.intl.t('successfully-created-title'));
+        }
+        break;
+      case 204:
+        if (requestData && requestData.method === 'DELETE') {
+          this.toaster.success(this.intl.t('successfully-deleted'), this.intl.t('successfully-created-title'));
+        } else if (requestData && requestData.method === 'PATCH') {
+          // Toast for newer models (with modifier locking) are thrown from the model.
+          // Not form here.
+          if (!this.checkIfNotificationShouldBeShownInModel(requestData.url)) {
             this.toaster.success(this.intl.t('successfully-saved'), this.intl.t('successfully-created-title'));
           }
-          break;
-        default:
-          return this._super(...arguments);
+        } else {
+          this.toaster.success(this.intl.t('successfully-saved'), this.intl.t('successfully-created-title'));
+        }
+        break;
+      default:
+        return this._super(...arguments);
       }
       return this._super(...arguments);
     }
@@ -59,16 +61,16 @@ export default DS.JSONAPIAdapter.extend({
   },
 
   checkIfNotificationShouldBeShown(type) {
-    const modelListToNotShowNotificationFor = ['genders', 'formally-oks', 'confidentialities', 'approvals', 'alert-types', 'agenda-activities', 'meetings'];
+    const modelListToNotShowNotificationFor = ['genders', 'formally-oks', 'confidentialities', 'approvals', 'alert-types', 'agenda-activities', 'meetings', ];
     return !(modelListToNotShowNotificationFor.includes(type));
   },
 
   ajax() {
     const args = [].slice.call(arguments);
-    if (args[1] === 'DELETE') {
+    if (args[ 1 ] === 'DELETE') {
       return this._super.apply(this, args);
     }
-    const originalData = args[2] && args[2].data;
+    const originalData = args[ 2 ] && args[ 2 ].data;
 
     const original = this._super;
     let retries = 0;
@@ -76,9 +78,9 @@ export default DS.JSONAPIAdapter.extend({
       if (retries < 3) {
         retries += 1;
         const originalResult = original.apply(this, args);
-        return originalResult.catch((catchError) => {
+        return originalResult[ 'catch' ]((catchError) => {
           if (originalData) {
-            args[2].data = typeof originalData === 'string' ? JSON.parse(originalData) : originalData;
+            args[ 2 ].data = typeof originalData === 'string' ? JSON.parse(originalData) : originalData;
           }
           return retry(catchError);
         });
