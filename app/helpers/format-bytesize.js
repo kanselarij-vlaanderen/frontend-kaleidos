fd87692eb0ba4fd839c9ffcd10404e2cdbb807ef
BenjaminDendas <benjamindendas@msn.com>
2020-07-22 08:25:14 +0200

    KAS-1674: rename i variable

--- app/helpers/format-bytesize.js
+++ app/helpers/format-bytesize.js
@@ -2,9 +2,11 @@ import { helper } from '@ember/component/helper';
 
 export function formatBytesize([bytes]) {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes === 0) return '0 Byte';
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-  return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`;
+  if (bytes === 0) {
+    return '0 Byte';
+  }
+  const sizesIndex = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
+  return `${Math.round(bytes / Math.pow(1024, sizesIndex), 2)} ${sizes[sizesIndex]}`;
 }
 
 export default helper(formatBytesize);
