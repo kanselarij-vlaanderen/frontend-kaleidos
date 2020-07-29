96dd1e48e6db27a656de0a11649ce825d7bce5f6
brecht philips <brecht_philips@hotmail.com>
2020-07-29 14:50:46 +0200

    octane refactor agenda title edit

--- app/pods/components/agenda/agendaitem/agendaitem-case/agendaitem-titles-edit/component.js
+++ app/pods/components/agenda/agendaitem/agendaitem-case/agendaitem-titles-edit/component.js
@@ -1,4 +1,4 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import {
   action, get, set
@@ -7,49 +7,52 @@ import {
   saveChanges as saveSubcaseTitles, cancelEdit
 } from 'fe-redpencil/utils/agenda-item-utils';
 import { trimText } from 'fe-redpencil/utils/trim-util';
+import { tracked } from '@glimmer/tracking';
 
 export default class SubcaseTitlesEdit extends Component {
   @service store;
+  @tracked isLoading= false;
   classNames = ['vl-form__group', 'vl-u-bg-porcelain'];
   propertiesToSet = Object.freeze(['title', 'shortTitle', 'explanation', 'showInNewsletter']);
   subcase = null;
 
+
   @action
   async cancelEditing() {
-    cancelEdit(this.agendaitem, get(this, 'propertiesToSet'));
-    this.toggleProperty('isEditing');
+    cancelEdit(this.args.agendaitem, get(this, 'propertiesToSet'));
+    this.args.toggleIsEditing();
   }
 
   @action
   async saveChanges() {
-    set(this, 'isLoading', true);
+    this.isLoading = true;
     let shouldResetFormallyOk = false;
 
-    if (this.agendaitem.get('hasDirtyAttributes')) {
+    if (this.args.agendaitem.get('hasDirtyAttributes')) {
       shouldResetFormallyOk = true;
       // If only the showInNewsletter attribute has changed, the formally ok should not be reset
-      if ((Object.keys(this.agendaitem.changedAttributes()).length === 1) && this.agendaitem.changedAttributes().showInNewsletter) {
+      if ((Object.keys(this.args.agendaitem.changedAttributes()).length === 1) && this.args.agendaitem.changedAttributes().showInNewsletter) {
         shouldResetFormallyOk = false;
       }
     }
 
     const propertiesToSetOnAgendaitem = {
-      title: trimText(this.agendaitem.title),
-      shortTitle: trimText(this.agendaitem.shortTitle),
+      title: trimText(this.args.agendaitem.title),
+      shortTitle: trimText(this.args.agendaitem.shortTitle),
       // explanation and showInNewsletter are set directly on the agendaitem, no need to have them in here
     };
     const propertiesToSetOnSubcase = {
-      title: trimText(this.agendaitem.title),
-      shortTitle: trimText(this.agendaitem.shortTitle),
+      title: trimText(this.args.agendaitem.title),
+      shortTitle: trimText(this.args.agendaitem.shortTitle),
     };
-    propertiesToSetOnSubcase.confidential = await this.subcase.get('confidential');
+    propertiesToSetOnSubcase.confidential = await this.args.subcase.confidential;
 
     try {
-      await saveSubcaseTitles(this.agendaitem, propertiesToSetOnAgendaitem, propertiesToSetOnSubcase, shouldResetFormallyOk);
-      set(this, 'isLoading', false);
-      this.toggleProperty('isEditing');
+      await saveSubcaseTitles(this.args.agendaitem, propertiesToSetOnAgendaitem, propertiesToSetOnSubcase, shouldResetFormallyOk);
+      this.isLoading = false;
+      this.args.toggleIsEditing();
     } catch (exception) {
-      set(this, 'isLoading', false);
+      this.isLoading = false;
       throw (exception);
     }
   }
