7dcd94d297b451892f8d75fcb1ccfe03748d49e1
brecht philips <brecht_philips@hotmail.com>
2020-07-29 14:51:23 +0200

    octane refactor agenda title

--- app/pods/components/agenda/agendaitem/agendaitem-case/agendaitem-titles/component.js
+++ app/pods/components/agenda/agendaitem/agendaitem-case/agendaitem-titles/component.js
@@ -1,28 +1,15 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import {
-  action, computed
-} from '@ember/object';
+import { action } from '@ember/object';
 
 export default class AgendaitemTitles extends Component {
   classNames = ['vl-u-spacer-extended-bottom-l'];
   @service currentSession;
-  agendaitem = null;
-  subcase = null;
-  shouldShowDetails = false;
 
-  // eslint-disable-next-line ember/use-brace-expansion
-  @computed('subcase.subcaseName', 'subcase.approved')
   get pillClass() {
-    return this.getPillClass();
-  }
-
-  async getPillClass() {
     const baseClass = 'vl-pill vl-u-text--capitalize';
-    const subcase = await this.subcase;
-    if (subcase) {
-      const approved = await subcase.get('approved');
-      if (approved) {
+    if (this.args.subcase) {
+      if (this.args.subcase.approved) {
         return `${baseClass} vl-pill--success`;
       }
     }
@@ -31,6 +18,6 @@ export default class AgendaitemTitles extends Component {
 
   @action
   toggleIsEditingAction() {
-    this.toggleIsEditing();
+    this.args.toggleIsEditing();
   }
 }
