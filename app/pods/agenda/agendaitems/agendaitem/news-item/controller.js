d97e3c0817523360b6f8ca482d2358a41bb73dd6
Michaël Dierick <michael.dierick@redpencil.io>
2020-08-13 15:52:23 +0200

    Restructured newsletter detail route, top level comp. + state in route

---
+++ app/pods/agenda/agendaitems/agendaitem/news-item/controller.js
@@ -0,0 +1,52 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import moment from 'moment';
+
+export default class NewsItemAgendaitemAgendaitemsAgendaController extends Controller {
+  @service store;
+  @service newsletterService;
+  @service currentSession;
+
+  @tracked notaModifiedTime = null;
+
+  @tracked isEditing = false;
+  @tracked notaModifiedWarningConfirmed = false;
+
+  // Nota changed since last newsitem edit
+  get notaHasChanged() {
+    const modifiedNliTime = this.model.modified;
+    if (modifiedNliTime && this.notaModifiedTime) {
+      return moment(modifiedNliTime).isBefore(moment(this.notaModifiedTime));
+    }
+    return false;
+  }
+
+  @action
+  confirmNotaModifiedWarning() {
+    this.notaModifiedWarningConfirmed = true;
+  }
+
+  @action
+  startEditing() {
+    this.isEditing = true;
+  }
+
+  @action
+  stopEditing() {
+    this.isEditing = false;
+  }
+
+  @action
+  async createAndStartEditing() {
+    await this.newsletterService.createNewsItemForAgendaItem(this.agendaItem);
+    this.refresh();
+    this.isEditing = true;
+  }
+
+  @action
+  refresh() {
+    this.send('reloadModel');
+  }
+}
--- app/pods/agenda/agendaitems/agendaitem/news-item/route.js
+++ app/pods/agenda/agendaitems/agendaitem/news-item/route.js
@@ -1,10 +1,12 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class NewsitemAgendaitemAgendaitemsAgendaRoute extends Route {
   @service agendaService;
 
   async beforeModel() {
+    // Check if a treatment exists, otherwise redirect gracefully. This should only happen with corrupt data.
     const agendaItem = this.modelFor('agenda.agendaitems.agendaitem');
     const agendaActivity = await agendaItem.get('agendaActivity');
     if (!agendaActivity) {
@@ -28,6 +30,22 @@ export default class NewsitemAgendaitemAgendaitemsAgendaRoute extends Route {
     const agendaItemTreatment = await agendaItem.get('agenda-item-treatment');
     controller.set('agendaItemTreatment', agendaItemTreatment);
 
+    controller.set('notaModifiedTime',
+      await this.agendaService.retrieveModifiedDateFromNota(controller.get('agendaItem')));
+
     controller.set('model', model);
+  }
+
+  resetController(controller, _, transition) {
+    if (!transition.data.isRefresh) {
+      controller.isEditing = false;
+    }
+    controller.notaModifiedWarningConfirmed = false;
+  }
+
+  @action
+  reloadModel() {
+    const transition = this.refresh();
+    transition.data.isRefresh = true;
   }
 }
