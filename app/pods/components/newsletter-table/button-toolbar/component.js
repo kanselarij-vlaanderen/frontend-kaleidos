38791ac249b572ced8bb48f51c0a0c44708d116f
Fred Kroket <wouters.f@gmail.com>
2020-09-01 12:12:03 +0200

    KAS-1679: rework with new development branch

--- app/pods/components/newsletter-table/button-toolbar/component.js
+++ app/pods/components/newsletter-table/button-toolbar/component.js
@@ -1,7 +1,25 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
+import {
+  action,
+  computed
+} from '@ember/object';
 
 export default class ButtonToolbarNewsletterTable extends Component {
+  @computed('this.args.agendaItem')
+  get latestMeetingId() {
+    return  this.args.agendaItem.get('agenda').then((agenda) => agenda.get('createdFor').then((meeting) => meeting.id));
+  }
+
+  @computed('this.args.agendaItem')
+  get latestAgendaId() {
+    return this.args.agendaItem.get('agenda').then((agenda) => agenda.id);
+  }
+
+  @computed('this.args.agendaItem')
+  get latestAgendaItemId() {
+    return this.args.agendaItem.get('id');
+  }
+
   @action
   async openNota() {
     const nota = await this.args.agendaItem.get('nota');
