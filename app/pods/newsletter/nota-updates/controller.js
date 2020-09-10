99861f041cb0ba9fad1b9ba7c48931011d52cb22
BenjaminDendas <benjamindendas@msn.com>
2020-09-10 15:48:49 +0200

    KAS-1709: pagina weergeven + cleanup code

--- app/pods/newsletter/nota-updates/controller.js
+++ app/pods/newsletter/nota-updates/controller.js
@@ -1,26 +1,20 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-export default class NewsletterUpdatesController extends Controller {
-  sizeOptions = Object.freeze([5, 10, 20, 50, 100, 200]);
+export default class NewsletterNotaUpdatesController extends Controller {
   queryParams = ['sort'];
 
   @tracked page;
-  @tracked size;
   @tracked sort = '-modified';
 
   constructor() {
     super(...arguments);
     this.page = 0;
-    this.size = this.sizeOptions[2];
   }
 
   @action
-  selectSize(size) {
-    this.size = size;
-  }
-
-  showDocumentVersionViewer = (documentId) => {
+  // eslint-disable-next-line class-methods-use-this
+  showDocumentVersionViewer(documentId) {
     window.open(`/document/${documentId}`);
-  };
+  }
 }
