4ceaf5aeda4cc435f8ac21e732eda16f30eceec5
rafael-mindreau <rafael.mindreau@gmail.com>
2020-09-11 17:21:24 +0200

    Make the header navbar and toolbar meet the design requirements

--- app/pods/components/toolbar/component.js
+++ app/pods/components/toolbar/component.js
@@ -1,5 +1,10 @@
 import Component from '@glimmer/component';
 
 export default class Toolbar extends Component {
-
+  get auto() {
+    if (this.args.auto) {
+      return 'vlc-toolbar--auto';
+    }
+    return '';
+  }
 }
