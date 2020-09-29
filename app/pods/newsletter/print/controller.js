5b36fb9c2fc4c940c4c0bbf8cd19850cf35e7e9a
Michaël Dierick <michael.dierick@redpencil.io>
2020-09-29 13:28:22 +0200

    Native class syntax

--- app/pods/newsletter/print/controller.js
+++ app/pods/newsletter/print/controller.js
@@ -1,15 +1,9 @@
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
-import moment from 'moment';
-import { inject } from '@ember/service';
 
-export default Controller.extend({
-  intl: inject(),
-  queryParams: {
+export default class PrintNewsletterController extends Controller {
+  queryParams = {
     showDraft: {
       type: 'boolean',
     },
-  },
-
-
-});
+  };
+}
