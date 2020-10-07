60de10e0ffa1d0607d7cda02b0a500494571e3fe
Michaël Dierick <michael.dierick@redpencil.io>
2020-10-07 11:00:41 +0200

    Keep uri property removal

---
+++ app/serializers/application.js
@@ -0,0 +1,12 @@
+import DS from 'ember-data';
+import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
+
+export default DS.JSONAPISerializer.extend(DataTableSerializerMixin, {
+  serialize() {
+    const payload = this._super(...arguments);
+    if (payload && payload.data && payload.data.attributes) {
+      delete payload.data.attributes.uri;
+    }
+    return payload;
+  },
+});
