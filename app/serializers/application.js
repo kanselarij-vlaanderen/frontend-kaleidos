3151114cce93992a2a88734eaa1b45686815ed64
ValenberghsSven <sven.valenberghs1@gmail.com>
2020-10-07 16:20:32 +0200

    return false

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,7 +1,21 @@
+/* eslint-disable */
 import DS from 'ember-data';
 import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
 
 export default DS.JSONAPISerializer.extend(DataTableSerializerMixin, {
+
+  // eslint-disable-next-line no-unused-vars
+  shouldSerializeHasMany(snapshot, key, relationshipType) {
+    return false;
+    const shouldSerialize = this._super(...arguments);
+    const serializeOption = relationshipType.options || {
+      serialize: true,
+    };
+    if (typeof serializeOption.serialize !== 'undefined') {
+      return shouldSerialize && serializeOption.serialize;
+    }
+    return shouldSerialize;
+  },
 
   serialize() {
     const payload = this._super(...arguments);
