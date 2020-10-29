b7a6abc02fd1dcd5ca5abda5fd49e7b31d6d14dd
Michaël Dierick <michael.dierick@redpencil.io>
2020-10-29 19:00:02 +0100

    KAS-1884: Improved search history ergonomics + added doc on assumptions

--- app/pods/search/index/route.js
+++ app/pods/search/index/route.js
@@ -2,6 +2,24 @@ import Route from '@ember/routing/route';
 
 export default class IndexSearchRoute extends Route {
   beforeModel() {
-    this.transitionTo('search.agenda-items');
+    /* It is assumed here that explicitly navigating to the index-route
+     * expresses a desire to "reset", to "start a new search-session".
+     * Therefore all parameters except filters -such as "type" of agenda-item- are cleared.
+     * We assume that those (checkbox-like) filters are specific to the user/session
+     * and therefore are desired to be persisted. */
+    /* TODO: If you go through this transition when already in a search (sub-) route, the queryParams
+     * in the url get reset as requested, but you don't go through the model/setupController-hook
+     * of the "search" route, even though the queryParams are marked `refreshModel: true`.
+     * As a result "searchTextBuffer" doesn't get cleared.
+     */
+    this.transitionTo('search.agenda-items', {
+      queryParams: {
+        searchText: null,
+        mandatees: null,
+        dateFrom: null,
+        dateTo: null,
+        page: 0,
+      },
+    });
   }
 }
