d384c226486ade90b703af9df94c43b1cdca4047
BenjaminDendas <benjamindendas@msn.com>
2020-10-29 09:02:06 +0100

    KAS-1944: include reference to base package

--- app/pods/components/number-pagination/component.js
+++ app/pods/components/number-pagination/component.js
@@ -1,3 +1,6 @@
+// BRON: https://github.com/mu-semtech/ember-data-table/blob/master/addon/components/number-pagination.js
+// BRON: https://github.com/mu-semtech/ember-data-table/blob/master/addon/templates/components/number-pagination.hbs
+
 import { computed } from '@ember/object';
 import Component from '@ember/component';
 
