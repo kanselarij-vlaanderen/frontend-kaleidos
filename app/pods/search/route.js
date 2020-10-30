3160935523b44de53028fab73aea6e30280426c4
BenjaminDendas <benjamindendas@msn.com>
2020-10-30 14:31:13 +0100

    Fix linter

--- app/pods/search/route.js
+++ app/pods/search/route.js
@@ -19,25 +19,22 @@ export default class SearchRoute extends Route.extend(AuthenticatedRouteMixin) {
       refreshModel: true,
       as: 'tot',
     },
-    page: {
-      as: 'page',
-    },
+  };
 
-    /* There is no reset of query parameters here by means of "resetController".
-     * It is assumed that -unless users explicitly click the main "search" button-
-     * search state (term, page number, ...) should be remembered, expecially with
-     * a trial-and-error search-session in mind, where users navigate to a detail item,
-     * realize it's not what they're looking for and go back in history.
-     */
+  /* There is no reset of query parameters here by means of "resetController".
+   * It is assumed that -unless users explicitly click the main "search" button-
+   * search state (term, page number, ...) should be remembered, expecially with
+   * a trial-and-error search-session in mind, where users navigate to a detail item,
+   * realize it's not what they're looking for and go back in history.
+   */
 
-    setupController(controller, model) {
-      super.setupController(controller, model);
-      const params = this.paramsFor('search');
-      controller.searchTextBuffer = params.searchText;
-      controller.mandateesBuffer = params.mandatees;
-      controller.page = params.page;
-      controller.dateFromBuffer = controller.deserializeDate(params.dateFrom);
-      controller.dateToBuffer = controller.deserializeDate(params.dateTo);
-    },
+  setupController(controller, model) {
+    super.setupController(controller, model);
+    const params = this.paramsFor('search');
+    controller.searchTextBuffer = params.searchText;
+    controller.mandateesBuffer = params.mandatees;
+    controller.page = params.page;
+    controller.dateFromBuffer = controller.deserializeDate(params.dateFrom);
+    controller.dateToBuffer = controller.deserializeDate(params.dateTo);
   }
 }
