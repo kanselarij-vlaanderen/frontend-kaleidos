a1db486d3a5c29e489c1e89698f7312f80c5286f
Fred Kroket <wouters.f@gmail.com>
2020-11-02 18:15:34 +0100

    linter now has 0 warnings

--- app/adapters/agendaitem.js
+++ app/adapters/agendaitem.js
@@ -2,17 +2,18 @@ import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
   findHasMany(store, snapshot, url, relationship) {
+    let newUrl = url;
     // Workaround for Ember Data not taking paging into account when fetching a hasMany
     const namesOnly = snapshot.adapterOptions && snapshot.adapterOptions.namesOnly;
     if (relationship.meta.key === 'pieces') {
-      url += '?page[size]=200';
+      newUrl += '?page[size]=200';
       if (namesOnly) {
-        url += '&fields[pieces]=name';
+        newUrl += '&fields[pieces]=name';
       }
     }
     if (relationship.meta.key === 'linkedPieces') {
-      url += '?page[size]=200';
+      newUrl += '?page[size]=200';
     }
-    return this._super(store, snapshot, url, relationship);
+    return this._super(store, snapshot, newUrl, relationship);
   },
 });
--- app/utils/file-utils.js
+++ app/utils/file-utils.js
@@ -1,11 +1,11 @@
 import { ajax } from 'fe-redpencil/utils/ajax';
 
 export const downloadFilePrompt = function(context, file, filename) {
-  filename = filename || file.get('filename');
+  const newfilename = filename || file.get('filename');
   return ajax(`/files/${file.id}/download`, {
     method: 'GET',
     dataType: 'blob',
-  }).then((content) => context.saveFileAs(filename, content, file.get('contentType')));
+  }).then((content) => context.saveFileAs(newfilename, content, file.get('contentType')));
 };
 
 export const removeFile = function(file) {
