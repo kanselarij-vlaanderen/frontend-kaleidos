bf7a3612951c402c0d364b21384cbbe35222dddd
brecht <brecht@tests-MacBook-Pro.local>
2020-11-02 12:01:22 +0100

    integrated new components in to the style guide

---
+++ app/pods/components/web-components/au-button-link/component.js
@@ -0,0 +1,27 @@
+import Component from '@glimmer/component';
+
+export default class ButtonLink extends Component {
+  get skin() {
+    if (this.args.skin) {
+      return `auk-button-link--${this.args.skin}`;
+    }
+    return '';
+  }
+
+  get block() {
+    if (this.args.block) {
+      return 'auk-button-link--block';
+    }
+    return '';
+  }
+
+  get padded() {
+    if (this.args.padded === 'padded') {
+      return 'auk-button-link--padded';
+    }
+    if (this.args.padded === 'padded-y') {
+      return 'auk-button-link--padded-y';
+    }
+    return '';
+  }
+}
