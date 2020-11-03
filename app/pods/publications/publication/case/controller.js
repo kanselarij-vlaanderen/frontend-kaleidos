de76a2e0efd0135bc8e3de0077104f33989935cf
BenjaminDendas <benjamindendas@msn.com>
2020-11-03 13:10:42 +0100

    KAS-1955: popup

--- app/pods/publications/publication/case/controller.js
+++ app/pods/publications/publication/case/controller.js
@@ -1,6 +1,10 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
+
+import { action } from '@ember/object';
 
 export default class CaseController extends Controller {
+  @tracked isShowingPersonModal = false;
   get getShortTitle() {
     const caze = this.model.get('case');
     return caze.get('shortTitle');
@@ -9,5 +13,17 @@ export default class CaseController extends Controller {
   get getLongTitle() {
     const caze = this.model.get('case');
     return caze.get('title');
+  }
+
+  @action
+  showContactPersonModal() {
+    this.isShowingPersonModal = true;
+    console.log('show person modal');
+  }
+
+  @action
+  closeContactPersonModal() {
+    this.isShowingPersonModal = false;
+    console.log('close clicked');
   }
 }
