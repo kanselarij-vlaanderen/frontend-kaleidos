3903bc2460a14668c93c195ef9829a4cfc7f2354
Fred Kroket <wouters.f@gmail.com>
2020-11-04 12:20:40 +0100

    KAS-1952: ES6 syntax

--- app/errors/sending-old-campaign-error.js
+++ app/errors/sending-old-campaign-error.js
@@ -1,8 +1,7 @@
 import EmberError from '@ember/error';
 
-const SendingOldCampaignError = function(errors, message = 'This error is result of my custom logic.') {
+const SendingOldCampaignError = (errors, message = 'This error is result of my custom logic.') => {
   EmberError.call(this, message);
-
   this.errors = errors || [
     {
       title: 'You tried to send an older mailchimp campaign.',
