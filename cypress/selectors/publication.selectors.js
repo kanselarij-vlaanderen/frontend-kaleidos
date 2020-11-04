e9c0e208802b31c0ed0efdeeac05c72c416d15b9
Kevin <kevinleyssens@hotmail.com>
2020-11-04 12:03:31 +0100

    KAS-1955: wrote tests for publication flow

--- cypress/selectors/publication.selectors.js
+++ cypress/selectors/publication.selectors.js
@@ -2,5 +2,12 @@ const selectors = {
   newPublicationButton: '[data-test-publication-header-button-new]',
   publicationDetailHeaderShortTitle: '[data-test-publication-detail-menu-short-title]',
   publicationDetailHeaderPublicationNumber: '[data-test-publication-detail-menu-publication-number]',
+  goToPublication: '[data-test-publications-button-go-to-publication]',
+  notViaMinisterTab: '[data-test-publication-in-progress-not-via-minister-tab]',
+  editInscriptionButton: '[data-test-publication-edit-inscription-button]',
+  inscriptionShortTitleTextarea: '[data-test-publication-inscription-short-title-textarea]',
+  inscriptionLongTitleTextarea: '[data-test-publication-inscription-long-title-textarea]',
+  inscriptionSaveButton: '[data-test-publication-save-inscription]',
+  goBackToOverviewButton: '[data-test-publication-detail-menu-go-back-to-overview]',
 };
 export default selectors;
