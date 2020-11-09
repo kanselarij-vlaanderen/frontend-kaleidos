7e97b5e377007d3a5cb324bfb955215e08832242
Michaël Dierick <michael.dierick@redpencil.io>
2020-11-09 17:59:40 +0100

    KAS-1978: load case amount of documents

--- app/pods/publications/to-treat/route.js
+++ app/pods/publications/to-treat/route.js
@@ -1,7 +1,25 @@
 import Route from '@ember/routing/route';
-import { action } from '@ember/object';
+import EmberObject, { action } from '@ember/object';
 import moment from 'moment';
 import search from 'fe-redpencil/utils/mu-search';
+import { task } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
+
+class Case extends EmberObject {
+  @service store;
+
+  @task({
+    maxConcurrency: 2,
+    restartable: false,
+  })
+  *loadDocumentsCount() {
+    const res = yield this.store.query('piece', {
+      'filter[cases][:id:]': this.id,
+      'page[size]': 0,
+    });
+    return this.set('documentsCount', res.meta.count);
+  }
+}
 
 export default class ToTreatRoute extends Route {
   postProcessDates(_case) {
@@ -27,11 +45,16 @@ export default class ToTreatRoute extends Route {
     const size = 50;
     const page = 0;
     filter[':sqs:title'] = '*'; // search without filter
-
     return await search('cases', page, size, sort, filter, (item) => {
-      const entry = item.attributes;
+      const entry = Case.create(item.attributes); // EmberObject for tracking
       entry.id = item.id;
       return entry;
+    });
+  }
+
+  afterModel(model) {
+    model.forEach((_case) => {
+      _case.loadDocumentsCount.perform();
     });
   }
 
