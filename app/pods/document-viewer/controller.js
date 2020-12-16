2dcb2427c38d8a34f95ba07d7d4cf72d4021648a
Michaël Dierick <michael.dierick@redpencil.io>
2020-12-16 13:44:08 +0100

    style: ignore linting error

--- app/pods/document-viewer/controller.js
+++ app/pods/document-viewer/controller.js
@@ -5,7 +5,7 @@ export default class DocumentViewerController extends Controller {
     const pdfMime =	'application/pdf';
     const pdfExtension = 'pdf';
     const file = this.model.get('file');
-    return file.get('format').toLowerCase().includes(pdfMime)
+    return file.get('format').toLowerCase().includes(pdfMime) // eslint-disable-line
       || file.get('extension').toLowerCase() === pdfExtension;
   }
 }
