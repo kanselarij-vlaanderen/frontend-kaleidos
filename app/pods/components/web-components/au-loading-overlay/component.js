85a8a6a1c1cfe8d1780709d58d956cfe1e507a50
Fred Kroket <wouters.f@gmail.com>
2020-12-22 08:11:16 +0100

    allow title and message as params

--- app/pods/components/web-components/au-loading-overlay/component.js
+++ app/pods/components/web-components/au-loading-overlay/component.js
@@ -1,2 +1,20 @@
-import Component from '@ember/component';
-export default Component.extend();
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+
+export default class LoadingOverlay extends Component {
+  @service() intl;
+
+  get getTitle() {
+    if (this.args.title) {
+      return `${this.args.title}`;
+    }
+    return this.intl.t('loading-text');
+  }
+
+  get getMessage() {
+    if (this.args.message) {
+      return `${this.args.message}`;
+    }
+    return `${this.intl.t('saving-change-message')} ${this.intl.t('please-be-patient')}`;
+  }
+}
