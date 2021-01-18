108b3250da0ace4852e8820b1373190c1e05a725
Michaël Dierick <michael.dierick@redpencil.io>
2021-01-18 22:48:58 +0100

    refactor: move meeting documents listing to route instead of component

--- app/pods/agenda/documents/route.js
+++ app/pods/agenda/documents/route.js
@@ -1,8 +1,39 @@
 import Route from '@ember/routing/route';
+import config from 'fe-redpencil/utils/config';
+import { sortPieces } from 'fe-redpencil/utils/documents';
+import { action } from '@ember/object';
 
 export default class AgendaDocumentsRoute extends Route {
-  model() {
-    const parentModel = this.modelFor('agenda');
-    return parentModel.meeting;
+  async model() {
+    const meeting = this.modelFor('agenda').meeting;
+    let pieces = await this.store.query('piece', {
+      'filter[meeting][:id:]': meeting.id,
+      'page[size]': 500, // TODO add pagination when sorting is done in the backend
+    });
+    pieces = pieces.toArray();
+    return sortPieces(pieces);
+  }
+
+  async afterModel() {
+    this.defaultAccessLevel = this.store.peekRecord('access-level', config.internRegeringAccessLevelId);
+    if (!this.defaultAccessLevel) {
+      const accessLevels = await this.store.query('access-level', {
+        'page[size]': 1,
+        'filter[:id:]': config.internRegeringAccessLevelId,
+      });
+      this.defaultAccessLevel = accessLevels.firstObject;
+    }
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    const meeting = this.modelFor('agenda').meeting;
+    controller.set('meeting', meeting);
+    controller.set('defaultAccessLevel', this.defaultAccessLevel);
+  }
+
+  @action
+  reloadModel() {
+    this.refresh();
   }
 }
