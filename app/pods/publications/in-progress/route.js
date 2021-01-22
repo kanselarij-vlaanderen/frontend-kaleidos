d6d952baa36a485a36e6dca7ddef3f23d4134626
brecht <brecht@tests-MacBook-Pro.local>
2021-01-22 13:56:06 +0100

    KAS-2187: deleted in-progress route

--- app/pods/publications/in-progress/route.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class InProgressRoute extends Route {
-  model() {
-    // Normally we would query store here, but for now, we get the mocks
-    return null;
-  }
-}
--- app/router.js
+++ app/router.js
@@ -160,10 +160,6 @@ Router.map(function() {
       this.route('signatures', { path: '/handtekeningen', });
     });
     // this.route('to-treat', { path: '/te-behandelen', });
-    this.route('in-progress', { path: '/in-behandeling', }, function() {
-      this.route('in-progress-minister', { path: '/via-ministerraad', });
-      this.route('in-progress-not-minister', { path: '/niet-via-ministerraad', });
-    });
     this.route('done', { path: '/behandeld', });
     this.route('paused', { path: '/gepauzeerd', }, function() {
       this.route('paused-minister', { path: '/via-ministerraad', });
