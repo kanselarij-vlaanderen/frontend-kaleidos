1d18bda834bff8a117eb8e0672081b3ff7cd444b
Michaël Dierick <michael.dierick@redpencil.io>
2021-01-27 16:55:39 +0100

    refactor: move toggle availability logic to subcase item + angle bracket

--- app/pods/components/subcases/subcase-item/component.js
+++ app/pods/components/subcases/subcase-item/component.js
@@ -1,5 +1,31 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { task } from 'ember-concurrency-decorators';
+import { inject as service } from '@ember/service';
 
 export default class SubcaseItemSubcasesComponent extends Component {
+  @service store;
 
+  @tracked isShowingDocuments = false;
+  @tracked hasDocumentsToShow = false;
+
+  constructor() {
+    super(...arguments);
+    this.updateHasDocumentsToShow.perform();
+  }
+
+  @task
+  *updateHasDocumentsToShow() {
+    const doc = yield this.store.queryOne('submission-activity', {
+      'filter[subcase][:id:]': this.args.subcase.id,
+      'filter[:has:pieces]': 'true',
+    });
+    this.hasDocumentsToShow = doc !== null;
+  }
+
+  @action
+  toggleIsShowingDocuments() {
+    this.isShowingDocuments = !this.isShowingDocuments;
+  }
 }
--- app/pods/components/subcases/subcases-process/component.js
+++ app/pods/components/subcases/subcases-process/component.js
@@ -1,9 +1,4 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 
 export default class SubcaseProcessSubcases extends Component {
-  @action
-  toggleIsShowingDocuments(subcase) {
-    subcase.toggleProperty('isShowingDocuments');
-  }
 }
