3f88952e8c2514da95e10ec618610b7afa819168
Fred Kroket <wouters.f@gmail.com>
2021-01-28 09:20:20 +0100

    KAS-2159: select, link and unlink ministers

--- app/pods/publications/publication/case/route.js
+++ app/pods/publications/publication/case/route.js
@@ -1,12 +1,17 @@
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default class CaseRoute extends Route.extend(AuthenticatedRouteMixin) {
   async model() {
     const parentHash = this.modelFor('publications.publication');
     const publicationFlow = parentHash.publicationFlow;
     const _case = await publicationFlow.get('case');
-    return _case;
+    return hash({
+      publicationFlow,
+      case: _case,
+      refreshAction: parentHash.refreshAction,
+    });
   }
 
   async setupController(controller, model) {
