7230f9a758cae89137948b4a18235d4bf4acbda2
Michaël Dierick <michael.dierick@redpencil.io>
2021-01-29 18:13:08 +0100

    refactor: load subcase through task

--- app/pods/components/agenda/agenda-overview/agenda-overview-item/component.js
+++ app/pods/components/agenda/agenda-overview/agenda-overview-item/component.js
@@ -4,6 +4,7 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
 import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency-decorators';
 
 export default class AgendaOverviewItem extends Component {
   /**
@@ -20,18 +21,24 @@ export default class AgendaOverviewItem extends Component {
 
   @service('current-session') currentSessionService;
   @alias('sessionService.currentSession') currentSession;
-  @alias('args.agendaitem.agendaActivity.subcase') subcase;
   @alias('args.agendaitem.treatments.firstObject.newsletterInfo') newsletterInfo;
 
   hideLabel = true;
 
   isShowingChanges = null;
 
+  @tracked subcase;
+
   @tracked showLoader = false;
 
   @tracked retracted = this.args.agendaitem.retracted || false;
   @tracked aboutToDelete = this.args.agendaitem.aboutToDelete || null;
   @tracked formallyOk = this.args.agendaitem.formallyOk || null;
+
+  constructor() {
+    super(...arguments);
+    this.loadSubcase.perform();
+  }
 
   get classNameBindings() {
     return `
@@ -59,5 +66,13 @@ export default class AgendaOverviewItem extends Component {
   async setAction(agendaitem) {
     const uri = agendaitem.get('uri');
     this.args.setFormallyOkAction(uri);
+  }
+
+  @task
+  *loadSubcase() {
+    const agendaActivity = yield this.args.agendaitem.agendaActivity;
+    if (agendaActivity) { // the approval agenda-item doesn't have agenda activity
+      this.subcase = yield agendaActivity.subcase;
+    }
   }
 }
