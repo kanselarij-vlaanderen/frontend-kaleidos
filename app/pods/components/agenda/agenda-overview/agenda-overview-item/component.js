7609da3f6d0679f91d9f8943f15f8cbfa3511bb2
Michaël Dierick <michael.dierick@redpencil.io>
2021-02-01 14:47:26 +0100

    refactor: implement document list rendering with new components

--- app/pods/components/agenda/agenda-overview/agenda-overview-item/component.js
+++ app/pods/components/agenda/agenda-overview/agenda-overview-item/component.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency-decorators';
+import { sortPieces } from 'fe-redpencil/utils/documents';
 
 export default class AgendaOverviewItem extends Component {
   /**
@@ -14,14 +15,18 @@ export default class AgendaOverviewItem extends Component {
   @service sessionService;
   @service publicationService;
   @service router;
-
   @service('current-session') currentSessionService;
 
+  @tracked agendaitemDocuments
   @tracked subcase;
   @tracked newsletterIsVisible;
 
+  @tracked isShowingAllDocuments = false;
+
   constructor() {
     super(...arguments);
+    this.agendaitemDocuments = [];
+    this.loadDocuments.perform();
     this.loadSubcase.perform();
     this.loadNewsletterVisibility.perform();
   }
@@ -29,6 +34,22 @@ export default class AgendaOverviewItem extends Component {
   get documentsAreReleased() {
     return this.sessionService.currentSession.releasedDocuments > new Date();
   }
+
+  get documentListSize() {
+    return 20;
+  }
+
+  get limitedAgendaitemDocuments() {
+    if (this.isShowingAllDocuments) {
+      return this.agendaitemDocuments;
+    }
+    return this.agendaitemDocuments.slice(0, this.documentListSize);
+  }
+
+  get enableShowMore() {
+    return this.agendaitemDocuments.length > this.documentListSize;
+  }
+
 
   @task
   *startPublication() {
@@ -48,6 +69,14 @@ export default class AgendaOverviewItem extends Component {
     if (agendaActivity) { // the approval agenda-item doesn't have agenda activity
       this.subcase = yield agendaActivity.subcase;
     }
+  }
+
+  @task
+  *loadDocuments() {
+    let pieces = yield this.args.agendaitem.pieces;
+    pieces = pieces.toArray();
+    const sortedPieces = sortPieces(pieces);
+    this.agendaitemDocuments = sortedPieces;
   }
 
   @task
