d941c61f664c719349d7a8d4f1000108401eb639
Michaël Dierick <michael.dierick@redpencil.io>
2021-02-01 13:49:12 +0100

    refactor: simplify document visibility logic

--- app/pods/components/agenda/agenda-overview/agenda-overview-item/component.js
+++ app/pods/components/agenda/agenda-overview/agenda-overview-item/component.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { alias } from '@ember/object/computed';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency-decorators';
 
@@ -18,7 +17,6 @@ export default class AgendaOverviewItem extends Component {
   @service router;
 
   @service('current-session') currentSessionService;
-  @alias('sessionService.currentSession') currentSession;
 
   isShowingChanges = null;
 
@@ -42,11 +40,8 @@ export default class AgendaOverviewItem extends Component {
     `;
   }
 
-  get overheidCanViewDocuments() {
-    const isOverheid = this.currentSessionService.isOverheid;
-    const documentsAreReleased = this.currentSession.releasedDocuments;
-
-    return !(isOverheid && !documentsAreReleased);
+  get documentsAreReleased() {
+    return this.sessionService.currentSession.releasedDocuments > new Date();
   }
 
   @action
