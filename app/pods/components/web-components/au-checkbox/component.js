940ff429eaa53f718c292648608befc096ca9ac2
brecht <brecht@tests-MacBook-Pro.local>
2021-02-04 21:34:53 +0100

    changed it back to working code

--- app/pods/components/web-components/au-checkbox/component.js
+++ app/pods/components/web-components/au-checkbox/component.js
@@ -1,10 +1,5 @@
 import Component from '@glimmer/component';
 
 export default class Checkbox extends Component {
-  get mode() {
-    if (this.args.mode === 'table') {
-      return 'auk-checkbox__table';
-    }
-    return '"auk-checkbox"';
-  }
+
 }
