907500bda221e7fb48641047e33e09eeca68a81d
brecht <brecht@tests-MacBook-Pro.local>
2021-02-04 21:21:06 +0100

    added a custom css class for a checkbox in a table

--- app/pods/components/web-components/au-checkbox/component.js
+++ app/pods/components/web-components/au-checkbox/component.js
@@ -1,5 +1,10 @@
 import Component from '@glimmer/component';
 
 export default class Checkbox extends Component {
-
+  get mode() {
+    if (this.args.mode === 'table') {
+      return 'auk-checkbox__table';
+    }
+    return '"auk-checkbox"';
+  }
 }
