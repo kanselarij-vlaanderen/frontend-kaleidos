a2a5f1a8a0c2752d88666be1df42b17a7fe7b4f1
brecht <brecht@tests-MacBook-Pro.local>
2021-02-08 11:39:12 +0100

    implemented first version of progress bar

--- app/pods/components/web-components/au-file-uploader/component.js
+++ app/pods/components/web-components/au-file-uploader/component.js
@@ -20,7 +20,6 @@ export default class FileUploader extends Component {
   @tracked isLoading = null;
 
   @tracked filesInQueue = alias('fileQueue.files');
-
   uploadedFileAction = this.args.uploadedFileAction;
 
   @action
@@ -28,6 +27,7 @@ export default class FileUploader extends Component {
     this.isLoading = false;
     this.uploadedFileLength = 0;
     this.filesInQueue = A([]);
+    this.totalInsertedFiles = 0;
   }
 
   @enqueueTask({
@@ -51,6 +51,7 @@ export default class FileUploader extends Component {
 
   @action
   uploadFile(file) {
+    this.uploadedFileLength = 0;
     this.uploadFileTask.perform(file);
   }
 
