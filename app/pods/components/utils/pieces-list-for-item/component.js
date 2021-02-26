b839ea1d2753b140465081240c6360e6f8af08eb
ValenberghsSven <sven.valenberghs1@gmail.com>
2021-02-26 14:06:03 +0100

    KAS-2090 document list not showing for subcase, make component more reusable

--- app/pods/components/utils/pieces-list-for-item/component.js
+++ app/pods/components/utils/pieces-list-for-item/component.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { sortPieces } from 'frontend-kaleidos/utils/documents';
 
 export default class PiecesListForItem extends Component {
   @tracked isClickable = this.args.isClickable;
@@ -8,24 +9,22 @@ export default class PiecesListForItem extends Component {
   @tracked isShowingAll = false;
 
   get moreThan20() {
-    if (this.args.agendaitemOrSubcase) {
-      if (this.args.agendaitemOrSubcase.sortedPieces) {
-        if (this.args.agendaitemOrSubcase.sortedPieces.length > 20) {
-          return true;
-        }
-      }
+    if (this.args.pieces && this.args.pieces.length > 20) {
+      return true;
     }
     return false;
   }
 
   get piecesLimited() {
-    if (this.args.agendaitemOrSubcase) {
-      if (this.args.agendaitemOrSubcase.sortedPieces) {
-        if (this.args.agendaitemOrSubcase.sortedPieces.length > 20) {
-          const sortedPieces = this.args.agendaitemOrSubcase.sortedPieces;
-          return sortedPieces.slice(0, 20);
-        }
-      }
+    if (this.args.pieces && this.args.pieces.length > 20) {
+      return this.sortedPieces.slice(0, 20);
+    }
+    return null;
+  }
+
+  get sortedPieces() {
+    if (this.args.pieces && this.args.pieces.length > 0) {
+      return sortPieces(this.args.pieces.toArray());
     }
     return null;
   }
