54c185ab58b0f9a2fd83daea983a1f9976119ded
ValenberghsSven <sven.valenberghs1@gmail.com>
2021-03-01 15:06:26 +0100

    css refactor cleanup classnames, fix compare agenda double scrollbalk

--- app/pods/components/agenda/agendaitem/agendaitem-case/agendaitem-titles/component.js
+++ app/pods/components/agenda/agendaitem/agendaitem-case/agendaitem-titles/component.js
@@ -5,8 +5,6 @@ import { tracked } from '@glimmer/tracking';
 import { alias } from '@ember/object/computed';
 
 export default class AgendaitemTitles extends Component {
-  classNames = ['auk-u-mb-8']; // @brenner-company: since this is a glimmer component, this can be removed?
-
   @alias('args.agendaitem.agendaActivity.subcase') subcase;
 
   @tracked showLoader = false;
