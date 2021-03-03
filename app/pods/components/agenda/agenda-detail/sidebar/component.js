25e3cf5520e2523349679b3a29e788935231ff42
Michaël Dierick <michael.dierick@redpencil.io>
2021-03-03 21:17:28 +0100

    fix: always show border-bottom for annoucement title

--- app/pods/components/agenda/agenda-detail/sidebar/component.js
+++ app/pods/components/agenda/agenda-detail/sidebar/component.js
@@ -1,5 +1,4 @@
 import Component from '@glimmer/component';
-import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency-decorators';
@@ -44,7 +43,6 @@ export default class AgendaSidebar extends Component {
   @service sessionService;
   @service('current-session') currentSessionService;
   @service agendaService;
-  @alias('sessionService.selectedAgendaitem') selectedAgendaitem;
 
   @tracked isShowingChanges = false;
   @tracked groupedNotas;
