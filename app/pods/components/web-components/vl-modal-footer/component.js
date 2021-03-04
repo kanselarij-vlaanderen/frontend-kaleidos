5cb342ee49316e4f7e51230801b7326e203d17b3
Karel Persoons <karel@brenner.company>
2021-03-04 12:05:07 +0100

    Fix lint error and move wrapping class

--- app/pods/components/web-components/vl-modal-footer/component.js
+++ app/pods/components/web-components/vl-modal-footer/component.js
@@ -1,6 +1,7 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  classNames: ['auk-modal__footer auk-modal__footer--bordered'],
   isLoading: null,
   disableSave: false,
 
