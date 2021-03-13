c87335e60b7830f513c41df64aab991dc26099b9
Michaël Dierick <michael.dierick@redpencil.io>
2021-03-13 12:54:59 +0100

    refactor: mandatees-domains-selector to octane

--- app/components/mandatees/mandatees-domains-selector-modal.js
+++ app/components/mandatees/mandatees-domains-selector-modal.js
@@ -1,60 +1,73 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import {
   refreshData,
   selectDomain,
   selectField,
   prepareMandateeRowAfterEdit
 } from 'frontend-kaleidos/utils/manage-minister-util';
-import { inject } from '@ember/service';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
-export default Component.extend({
-  intl: inject(),
-  rowToShow: null,
-  selectedMandatee: null,
-  @tracked title: '',
+export default class MandateesMandateesDomainsSelectorModalComponent extends Component {
+  @service intl;
 
-  willRender() {
+  @tracked selectedMandatee;
+  @tracked rowToShow;
+
+  @tracked isLoading;
+
+  get isAddingMinister() {
+    return !this.args.mandatee;
+  }
+
+  get title() {
     if (this.isAddingMinister) {
-      this.title = this.intl.t('add-minister');
-    } else {
-      this.title = this.intl.t('edit-minister');
+      return this.intl.t('add-minister');
     }
-  },
+    return this.intl.t('edit-minister');
+  }
 
-  actions: {
+  consttructor() {
+    super(...arguments);
+    this.selectedMandatee = this.args.mandatee;
+  }
 
-    async saveChanges() {
-      this.set('isLoading', true);
-      const {
-        selectedMandatee, rowToShow,
-      } = this;
-      const newMinisterRow = await prepareMandateeRowAfterEdit(selectedMandatee, rowToShow);
-      this.saveChanges(selectedMandatee, newMinisterRow);
-      this.set('isLoading', false);
-      this.cancel();
-    },
+  @action
+  async saveChanges() {
+    this.isLoading = true;
+    const {
+      selectedMandatee, rowToShow,
+    } = this;
+    const newMinisterRow = await prepareMandateeRowAfterEdit(selectedMandatee, rowToShow);
+    this.args.saveChanges(selectedMandatee, newMinisterRow);
+    this.isLoading = false;
+    this.cancel();
+  }
 
-    async selectField(domain, value) {
-      const foundDomain = await this.get('rowToShow.domains');
-      await selectField(foundDomain, domain, value);
-    },
+  @action
+  async selectField(domain, value) {
+    const foundDomain = await this.rowToShow.domains;
+    await selectField(foundDomain, domain, value);
+  }
 
-    async mandateeSelected(mandatee) {
-      this.set('selectedMandatee', mandatee);
-      this.set('isLoading', true);
-      const rowsToShow = await refreshData(mandatee, await this.get('mandateeRows'));
-      this.set('rowToShow', rowsToShow);
-      this.set('isLoading', false);
-    },
+  @action
+  async mandateeSelected(mandatee) {
+    this.selectedMandatee = mandatee;
+    this.isLoading = true;
+    const rowsToShow = await refreshData(mandatee, await this.get('mandateeRows'));
+    this.rowToShow = rowsToShow;
+    this.isLoading = false;
+  }
 
-    async selectDomain(domain, value) {
-      const rowToShowFields = await this.get('rowToShow.fields');
-      await selectDomain(rowToShowFields, domain, value);
-    },
+  @action
+  async selectDomain(domain, value) {
+    const rowToShowFields = await this.rowToShow.fields;
+    await selectDomain(rowToShowFields, domain, value);
+  }
 
-    cancel() {
-      this.cancel();
-    },
-  },
-});
+  @action
+  cancel() {
+    this.args.cancel();
+  }
+}
