90c39f4178586a76a320fa61867fc81be9f69889
Michaël Dierick <michael.dierick@redpencil.io>
2021-03-16 18:34:50 +0100

    fix: load fields in agenda-item index route

--- app/pods/agenda/agendaitems/agendaitem/index/route.js
+++ app/pods/agenda/agendaitems/agendaitem/index/route.js
@@ -14,6 +14,14 @@ export default class DetailAgendaitemAgendaitemsAgendaRoute extends Route {
     const agendaActivity = await model.agendaActivity;
     if (agendaActivity) {
       controller.subcase = await agendaActivity.subcase;
+      if (controller.subcase) {
+        // TODO: below query doesn't return the expected results. (returns none) To be investigated
+        let governmentFields = this.store.query('government-field', {
+          'filter[ise-code][subcases][:id:]': controller.subcase.id,
+        });
+        governmentFields = governmentFields.toArray();
+        controller.governmentFields = governmentFields;
+      }
     }
     const agendaItemTreatment = await model.treatments;
     const anyTreatment = agendaItemTreatment.firstObject;
