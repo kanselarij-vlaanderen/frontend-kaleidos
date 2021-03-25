1d1b49870e8e02e77d4f0d1275239f7512816d9e
Michaël Dierick <michael.dierick@redpencil.io>
2021-03-25 15:17:41 +0100

    feat: redirect to one of visible items when item dissapears after filtering

--- app/pods/agenda/agendaitems/agendaitem/route.js
+++ app/pods/agenda/agendaitems/agendaitem/route.js
@@ -10,6 +10,13 @@ export default class AgendaitemAgendaitemsAgendaRoute extends Route {
     });
   }
 
+  afterModel(model) {
+    const arrayToSearch = model.showAsRemark ? this.modelFor('agenda.agendaitems').announcements : this.modelFor('agenda.agendaitems').notas;
+    if (!arrayToSearch.includes(model) && arrayToSearch.length) { // This can happen when the selected item no longer is visible in the sidebar after filtering
+      this.transitionTo('agenda.agendaitems.agendaitem', arrayToSearch[0]);
+    }
+  }
+
   setupController(controller) {
     super.setupController(...arguments);
     const {
