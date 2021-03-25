da8b25d84bf8a86b82d023518b8ac8b961f2b8a2
Michaël Dierick <michael.dierick@redpencil.io>
2021-03-25 21:54:12 +0100

    fix: handle case where no fields are passed to domains-fields-for-mandatee

--- app/components/mandatees/domains-fields-for-mandatee.js
+++ app/components/mandatees/domains-fields-for-mandatee.js
@@ -19,13 +19,17 @@ export default class MandateesDomainsFieldsForMandateeComponent extends Componen
 
   @task
   *loadData() {
-    let allMandateeFields = yield this.store.query('government-field', {
-      'filter[ise-code][mandatees][:id:]': this.args.mandatee.id,
-    });
-    allMandateeFields = allMandateeFields.toArray();
-    const selectedMandateeFields = allMandateeFields.filter((field) => this.args.fields.includes(field));
-    let selectedMandateeDomains = yield Promise.all(selectedMandateeFields.map((field) => field.domain));
-    selectedMandateeDomains = [...new Set(selectedMandateeDomains)]; // uniquify
+    let selectedMandateeFields = [];
+    let selectedMandateeDomains = [];
+    if (this.args.fields && this.args.fields.length) {
+      let allMandateeFields = yield this.store.query('government-field', {
+        'filter[ise-code][mandatees][:id:]': this.args.mandatee.id,
+      });
+      allMandateeFields = allMandateeFields.toArray();
+      selectedMandateeFields = allMandateeFields.filter((field) => this.args.fields.includes(field));
+      selectedMandateeDomains = yield Promise.all(selectedMandateeFields.map((field) => field.domain));
+      selectedMandateeDomains = [...new Set(selectedMandateeDomains)]; // uniquify
+    }
     return {
       mandatee: this.args.mandatee,
       domains: selectedMandateeDomains,
