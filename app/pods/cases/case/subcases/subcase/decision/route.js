c8ed38751c5e3b51e6eb9e62245c4e57d0eccabe
Michaël Dierick <michael.dierick@redpencil.io>
2021-03-30 13:13:12 +0200

    fix: order decision-documents by creation date descending

--- app/pods/cases/case/subcases/subcase/decision/route.js
+++ app/pods/cases/case/subcases/subcase/decision/route.js
@@ -5,6 +5,7 @@ export default class CasesCaseSubcasesSubcaseDecisionRoute extends Route {
     const subcase = this.modelFor('cases.case.subcases.subcase');
     const pieces = await this.store.query('piece', {
       'filter[agenda-item-treatment][subcase][:id:]': subcase.id,
+      sort: '-created',
     });
     return pieces.toArray();
   }
