5c89a1c8723bec4a37a7f6e071adb806b440fdca
Erika Pauwels <erika.pauwels@gmail.com>
2021-04-07 08:38:49 +0200

    Convert application serializer to native ES6 class

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,16 +1,15 @@
-import DS from 'ember-data';
+import JSONAPISerializer from '@ember-data/serializer/json-api';
 import DataTableSerializerMixin from 'ember-data-table/mixins/serializer';
 
-export default DS.JSONAPISerializer.extend(DataTableSerializerMixin, {
-
+export default class ApplicationSerializer extends JSONAPISerializer.extend(DataTableSerializerMixin) {
   // eslint-disable-next-line no-unused-vars
   shouldSerializeHasMany(snapshot, key, relationshipType) {
     // If serialization option specified, use that. Otherwise use Ember Data defaults
     if (relationshipType.options && (typeof relationshipType.options.serialize !== 'undefined')) {
       return relationshipType.options.serialize;
     }
-    return this._super(...arguments);
-  },
+    return super.shouldSerializeHasMany(...arguments);
+  }
 
   // eslint-disable-next-line no-unused-vars
   serializeBelongsTo(snapshot, json, relationship) {
@@ -19,14 +18,14 @@ export default DS.JSONAPISerializer.extend(DataTableSerializerMixin, {
     if (relationship.options && (typeof relationship.options.serialize !== 'undefined')) {
       return;
     }
-    this._super(...arguments);
-  },
+    super.serializeBelongsTo(...arguments);
+  }
 
   serialize() {
-    const payload = this._super(...arguments);
+    const payload = super.serialize(...arguments);
     if (payload && payload.data && payload.data.attributes) {
       delete payload.data.attributes.uri;
     }
     return payload;
-  },
-});
+  }
+}
