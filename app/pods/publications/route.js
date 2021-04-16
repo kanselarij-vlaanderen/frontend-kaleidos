8f1255a7078504105fed7258f1cda25236f4393f
Marnik De Vos <mdv.werk@outlook.com>
2021-04-16 12:56:17 +0200

    feature/KAS-2436-publicatie-flow-publicatie-wijze/PR-180-use afterModel()-hook

--- app/pods/publications/route.js
+++ app/pods/publications/route.js
@@ -2,20 +2,13 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
 import { inject } from '@ember/service';
-import { hash } from 'rsvp';
 
 export default class PublicationsRoute extends Route.extend(AuthenticatedRouteMixin) {
   @inject store;
 
   @action
-  async model() {
+  async afterModel() {
     // caching for use in publication detail
-    const publicationModes = this.store.findAll('publication-mode');
-
-    const model = hash({
-      publicationModes: publicationModes,
-    });
-
-    return model;
+    await this.store.findAll('publication-mode');
   }
 }
