01b8fab5f4c3bad374eda3655b092dbb18170689
Tanee Grade <tanee.grade@cereus.be>
2021-04-19 12:15:58 +0200

    Stepped away from AUPowerSelect and used the regular PowerSelect

--- app/components/publications/publication-status-selector.js
+++ app/components/publications/publication-status-selector.js
@@ -2,59 +2,18 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-
 export default class PublicationStatusSelector extends Component {
   @service store;
-
   @tracked publicationStatusses
-  @tracked statusOptions = [];
 
   constructor() {
     super(...arguments);
     this.publicationStatusses = this.store.peekAll('publication-status').sortBy('position');
-    this.fillStatusOptions();
-  }
-
-  fillStatusOptions() {
-    for (const publicationStatus of this.publicationStatusses) {
-      let icon;
-      if (publicationStatus.isPending) {
-        icon =  {
-          svg: 'clock',
-          color: 'warning',
-        };
-      } else if (publicationStatus.isPublished) {
-        icon =   {
-          svg: 'circle-check',
-          color: 'success',
-        };
-      } else if (publicationStatus.isPaused) {
-        icon =  {
-          svg: 'circle-pause',
-          color: 'muted',
-        };
-      } else if (publicationStatus.isWithdrawn) {
-        icon =   {
-          svg: 'circle-error',
-          color: 'danger',
-        };
-      } else {
-        icon =  {
-          color: 'muted',
-        };
-      }
-      const option = {
-        id: publicationStatus.id,
-        label: publicationStatus.label,
-        icon: icon,
-      };
-      this.statusOptions.push(option);
-    }
   }
 
   get publicationStatus() {
-    return this.statusOptions.find(
-      (statusOption) => statusOption.id === this.args.publicationStatus.id);
+    return this.publicationStatusses.find(
+      (status) => status.uri === this.args.publicationStatus.uri);
   }
 
   @action
