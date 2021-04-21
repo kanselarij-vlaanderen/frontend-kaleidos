72d338165dbd0993712bdf866ed47f8277542c6d
Karel Persoons <karel@brenner.company>
2021-04-21 16:20:31 +0200

    Refactor vlc-input-field-group-wrapper occurrence and adjust tests

--- cypress/selectors/agenda-overview.selectors.js
+++ cypress/selectors/agenda-overview.selectors.js
@@ -1,4 +1,5 @@
 const selectors = {
+  agendaFilter: '[data-test-agendas-filter]',
   agendaFilterInput: '[data-test-agendas-filter-input]',
   agendaFilterButton: '[data-test-agendas-filter-button]',
   agendaEditFormallyOkButton: '[data-test-agenda-edit-formally-ok-button]',
