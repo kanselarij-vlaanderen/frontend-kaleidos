390d4a6a336826f047d29f85c9527822f2dd93fa
Michaël Dierick <michael.dierick@redpencil.io>
2021-04-22 17:36:37 +0200

    feat: inlude anchor qp in link to overview

--- app/components/sessions/session-agenda-nav.js
+++ app/components/sessions/session-agenda-nav.js
@@ -16,4 +16,16 @@ export default class SessionsSessionAgendaNavComponent extends Component {
   get isInAgendaItemDetailRoute() {
     return this.router.currentRouteName.startsWith('agenda.agendaitems.agendaitem');
   }
+
+  get currentAgendaItemId() {
+    const currentRoute = this.router.currentRoute;
+    let agendaItemsRoute = currentRoute;
+    if (currentRoute && currentRoute.name.startsWith('agenda.agendaitems.agendaitem')) {
+      while (agendaItemsRoute.name !== 'agenda.agendaitems.agendaitem') {
+        agendaItemsRoute = agendaItemsRoute.parent;
+      }
+      return agendaItemsRoute.params.agendaitem_id;
+    }
+    return null;
+  }
 }
