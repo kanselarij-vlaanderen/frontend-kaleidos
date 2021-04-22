61c3ea095064095101231a4b4e24b334b25b9f25
Tanee Grade <tanee.grade@cereus.be>
2021-04-22 09:27:47 +0200

    Deleted too much of counts & Filter in loaddata wasn't filtering correctly

--- app/models/publication-flow.js
+++ app/models/publication-flow.js
@@ -3,6 +3,7 @@ import Model, {
 } from '@ember-data/model';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
+import CONFIG from 'frontend-kaleidos/utils/config';
 
 export default class PublicationFlow extends Model {
   // Attributes.
@@ -44,5 +45,56 @@ export default class PublicationFlow extends Model {
       return `${this.publicationNumber} ${this.publicationSuffix}`;
     }
     return `${this.publicationNumber}`;
+  }
+
+  get translationRequestsTotal() {
+    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+      const openVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.open.url);
+      const closedVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+      const withdrawnVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+      return openVertalingen + closedVertalingen + withdrawnVertalingen;
+    });
+  }
+
+  get publishpreviewRequestsTotal() {
+    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+      const openDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.open.url);
+      const closedDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+      const withdrawnDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+      return openDrukproeven + closedDrukproeven + withdrawnDrukproeven;
+    });
+  }
+
+  get translationRequestsFinished() {
+    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+      const closedVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+      const withdrawnVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+      return closedVertalingen + withdrawnVertalingen;
+    });
+  }
+
+  get publishpreviewRequestsFinished() {
+    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+      const closedDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+      const withdrawnDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+      return withdrawnDrukproeven + closedDrukproeven;
+    });
+  }
+
+  get publicationRequestsTotal() {
+    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+      const openPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.open.url);
+      const closedPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+      const withdrawnPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+      return openPublications + closedPublications + withdrawnPublications;
+    });
+  }
+
+  get publicationRequestsFinished() {
+    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+      const closedPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals,  CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+      const withdrawnPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+      return closedPublications + withdrawnPublications;
+    });
   }
 }
