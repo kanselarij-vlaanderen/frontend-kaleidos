a9fabeb07cc888b37d2a0a556fadcf1c31772f00
Marnik De Vos <mdv.werk@outlook.com>
2021-04-22 11:30:21 +0200

    refactor model (feature/kas-2473-publication-flow--publication-documents-model)

--- app/pods/publications/publication/documents/route.js
+++ app/pods/publications/publication/documents/route.js
@@ -4,7 +4,7 @@ export default class PublicationDocumentsRoute extends Route {
   async model() {
     const parentHash = this.modelFor('publications.publication');
     const _case = parentHash.case;
-    const piecesData = this.store.query('piece', {
+    const modelData = await this.store.query('piece', {
       include: 'cases,document-container,document-container.type',
       reload: true,
       filter: {
@@ -13,17 +13,22 @@ export default class PublicationDocumentsRoute extends Route {
         },
       },
     });
-
-    const model = {
-      case: _case,
-      pieces: (await piecesData).toArray(),
-    };
-
-    return model;
+    return modelData.toArray();
   }
 
-  /* eslint-disable id-length,no-unused-vars */
-  resetController(controller, _, transition) {
+  async afterModel() {
+    const parentHash = this.modelFor('publications.publication');
+    this.case = parentHash.case;
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    controller.setup({
+      _case: this.case,
+    });
+  }
+
+  resetController(controller) {
     controller.reset();
   }
 }
