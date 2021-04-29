d5baaaed787df8f1a630a44ae10a84cf35f89fd3
Michaël Dierick <michael.dierick@redpencil.io>
2021-04-29 16:45:30 +0200

    refactor: remove unnecessary action handler wrapper

--- app/components/publications/publication-status-selector.js
+++ app/components/publications/publication-status-selector.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 export default class PublicationStatusSelector extends Component {
   @service store;
@@ -9,11 +8,5 @@ export default class PublicationStatusSelector extends Component {
   constructor() {
     super(...arguments);
     this.publicationStatusses = this.store.peekAll('publication-status').sortBy('position');
-  }
-
-  @action
-  setPublicationStatus(event) {
-    const publicationStatus = this.store.peekRecord('publication-status', event.id);
-    this.args.onChange(publicationStatus);
   }
 }
