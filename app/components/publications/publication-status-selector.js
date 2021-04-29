e6b3570ac8c9a583d37245c074c961448c524a9d
Michaël Dierick <michael.dierick@redpencil.io>
2021-04-29 16:46:00 +0200

    style: semi, newline

--- app/components/publications/publication-status-selector.js
+++ app/components/publications/publication-status-selector.js
@@ -1,9 +1,10 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+
 export default class PublicationStatusSelector extends Component {
   @service store;
-  @tracked publicationStatusses
+  @tracked publicationStatusses;
 
   constructor() {
     super(...arguments);
