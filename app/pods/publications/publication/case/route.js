c820d87959ebf71393cb2d5954862fb1ac8023ed
Marnik D <m@rn.k>
2021-04-30 18:04:37 +0200

    KAS-2415: single request for subcase

--- app/pods/publications/publication/case/route.js
+++ app/pods/publications/publication/case/route.js
@@ -10,18 +10,6 @@ export default class CaseRoute extends Route {
     this.publicationFlow = this.modelFor('publications.publication');
     this.contactPersons = this.publicationFlow.contactPersons;
 
-    const latestSubcaseOnMeetingPromise = this.store.query('subcase', {
-      filter: {
-        case: {
-          // cannot access yet without get(...)
-          id: model.get('id'),
-        },
-        ':has:agenda-activities': 'yes',
-      },
-      sort: '-created',
-      include: 'mandatees',
-    }).then((subcase) => subcase.firstObject);
-
     // TODO This is not ideal, there are currently +- 60 organizations that come from ACM-IDM, they don't have a name
     // TODO need a better filter, add a boolean to model maybe ?
     const organizationsPromise = this.store.query('organization', {
@@ -30,17 +18,17 @@ export default class CaseRoute extends Route {
       },
     }).then((organizations) => organizations.filter((org) => org.name));
 
-    const [organizations, latestSubcaseOnMeeting] = await RSVP.all([organizationsPromise, latestSubcaseOnMeetingPromise]);
-    this.organizations = organizations;
-    this.latestSubcaseOnMeeting = latestSubcaseOnMeeting;
+    this.organizations = await organizationsPromise;
   }
 
   async setupController(controller) {
     super.setupController(...arguments);
 
+    const parentController = this.controllerFor('publications.publication');
+
     controller.publicationFlow = this.publicationFlow;
     controller.contactPersons = this.contactPersons;
-    controller.latestSubcaseOnMeeting = this.latestSubcaseOnMeeting;
+    controller.latestSubcaseOnMeeting = parentController.latestSubcaseOnMeeting;
     controller.organizations = this.organizations;
   }
 }
