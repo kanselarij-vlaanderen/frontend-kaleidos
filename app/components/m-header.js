4047a8e73aa875f60a471c849efca1e2efd5e09d
Tanee Grade <tanee.grade@cereus.be>
2021-05-07 13:39:43 +0200

    KAS-2494 Added glitter component & edited translations to english

--- app/components/m-header.js
+++ app/components/m-header.js
@@ -1,4 +1,4 @@
-import Component from '@ember/component';
+
 import ENV from 'frontend-kaleidos/config/environment';
 import { inject as service } from '@ember/service';
 import {
@@ -6,9 +6,10 @@ import {
 } from '@ember/object';
 import { isEmpty } from '@ember/utils';
 import { tracked } from '@glimmer/tracking';
+import Component from '@glimmer/component';
 
 
-export default class UrgencyLevelCheckboxComponent extends Component {
+export default class MHeader extends Component {
   @service currentSession;
   @service router;
 
@@ -20,24 +21,24 @@ export default class UrgencyLevelCheckboxComponent extends Component {
   constructor() {
     super(...arguments);
     if (window.location.href.indexOf('http://localhost') === 0) {
-      this.set('environmentName', 'LOCAL');
-      this.set('environmentClass', 'vlc-environment-pill--local');
+      this.environmentName = 'LOCAL';
+      this.environmentClass = 'vlc-environment-pill--local';
     }
 
     if (window.location.href.indexOf('https://kaleidos-dev.vlaanderen.be') === 0) {
-      this.set('environmentName', 'DEV');
-      this.set('environmentClass', 'vlc-environment-pill--dev');
+      this.environmentName = 'DEV';
+      this.environmentClass = 'vlc-environment-pill--dev';
     }
 
     if (window.location.href.indexOf('https://kaleidos-test.vlaanderen.be') === 0) {
-      this.set('environmentName', 'TEST');
-      this.set('environmentClass', 'vlc-environment-pill--test');
+      this.environmentName = 'TEST';
+      this.environmentClass = 'vlc-environment-pill--test';
     }
 
     if (window.location.href.indexOf('https://kaleidos.vlaanderen.be') === 0
       && this.currentSession.checkIsDeveloper()) {
-      this.set('environmentName', 'PROD');
-      this.set('environmentClass', 'vlc-environment-pill--prod');
+      this.environmentName = 'PROD';
+      this.environmentClass = 'vlc-environment-pill--prod';
     }
   }
 
