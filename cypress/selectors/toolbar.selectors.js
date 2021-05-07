fc344dc593951391ea6be0547fc4e15f6dda31e2
ValenberghsSven <sven.valenberghs1@gmail.com>
2021-05-07 13:59:50 +0200

    KAS-2494 logoutFlow using new dropdown

--- cypress/selectors/toolbar.selectors.js
+++ cypress/selectors/toolbar.selectors.js
@@ -3,5 +3,11 @@ const selectors = {
   cases: '[data-test-m-header-cases]',
   newsletters: '[data-test-m-header-newsletters]',
   settings: '[data-test-m-header-settings]',
+  mHeader: {
+    userActions: '[data-test-m-header-user-actions]',
+    userAction: {
+      logout: '[data-test-m-header-user-action-logout]',
+    },
+  },
 };
 export default selectors;
