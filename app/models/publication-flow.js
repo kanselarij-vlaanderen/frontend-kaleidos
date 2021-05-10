d3cd40ffe79e00bac1a41716d8e90311702927c6
mdv-professional <mdv.werk@outlook.com>
2021-05-10 16:53:54 +0200

    KAS 2546 busy

--- app/models/publication-flow.js
+++ app/models/publication-flow.js
@@ -57,54 +57,54 @@ export default class PublicationFlow extends Model {
       && this.translateBefore < new Date();
   }
 
-  get translationRequestsTotal() {
-    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
-      const openVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.open.url);
-      const closedVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
-      const withdrawnVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
-      return openVertalingen + closedVertalingen + withdrawnVertalingen;
-    });
-  }
+  // get translationRequestsTotal() {
+  //   return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+  //     const openVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.open.url);
+  //     const closedVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+  //     const withdrawnVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+  //     return openVertalingen + closedVertalingen + withdrawnVertalingen;
+  //   });
+  // }
 
-  get publishpreviewRequestsTotal() {
-    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
-      const openDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.open.url);
-      const closedDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
-      const withdrawnDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
-      return openDrukproeven + closedDrukproeven + withdrawnDrukproeven;
-    });
-  }
+  // get publishpreviewRequestsTotal() {
+  //   return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+  //     const openDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.open.url);
+  //     const closedDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+  //     const withdrawnDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+  //     return openDrukproeven + closedDrukproeven + withdrawnDrukproeven;
+  //   });
+  // }
 
-  get translationRequestsFinished() {
-    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
-      const closedVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
-      const withdrawnVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
-      return closedVertalingen + withdrawnVertalingen;
-    });
-  }
+  // get translationRequestsFinished() {
+  //   return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+  //     const closedVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+  //     const withdrawnVertalingen = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.vertalen.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+  //     return closedVertalingen + withdrawnVertalingen;
+  //   });
+  // }
 
-  get publishpreviewRequestsFinished() {
-    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
-      const closedDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
-      const withdrawnDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
-      return withdrawnDrukproeven + closedDrukproeven;
-    });
-  }
+  // get publishpreviewRequestsFinished() {
+  //   return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+  //     const closedDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+  //     const withdrawnDrukproeven = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.drukproeven.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+  //     return withdrawnDrukproeven + closedDrukproeven;
+  //   });
+  // }
 
-  get publicationRequestsTotal() {
-    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
-      const openPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.open.url);
-      const closedPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
-      const withdrawnPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
-      return openPublications + closedPublications + withdrawnPublications;
-    });
-  }
+  // get publicationRequestsTotal() {
+  //   return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+  //     const openPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.open.url);
+  //     const closedPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+  //     const withdrawnPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+  //     return openPublications + closedPublications + withdrawnPublications;
+  //   });
+  // }
 
-  get publicationRequestsFinished() {
-    return this.publicationService.getPublicationCounts(this.id).then((totals) => {
-      const closedPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals,  CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
-      const withdrawnPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
-      return closedPublications + withdrawnPublications;
-    });
-  }
+  // get publicationRequestsFinished() {
+  //   return this.publicationService.getPublicationCounts(this.id).then((totals) => {
+  //     const closedPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals,  CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.closed.url);
+  //     const withdrawnPublications = this.publicationService.getPublicationCountsPerTypePerStatus(totals, CONFIG.ACTIVITY_TYPES.publiceren.url, CONFIG.ACTIVITY_STATUSSES.withdrawn.url);
+  //     return closedPublications + withdrawnPublications;
+  //   });
+  // }
 }
