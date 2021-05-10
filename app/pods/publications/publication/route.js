3ce75881a6bfc96825ad94d973d129f4d78f2519
mdv-professional <mdv.werk@outlook.com>
2021-05-10 14:39:38 +0200

    KAS 2529 clean up pt. 2 (very little remaining) | cleanup publication [publication-flow]

--- app/pods/publications/publication/route.js
+++ app/pods/publications/publication/route.js
@@ -1,6 +1,5 @@
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import Route from '@ember/routing/route';
-import { action } from '@ember/object';
 import RSVP from 'rsvp';
 
 export default class PublicationRoute extends Route.extend(AuthenticatedRouteMixin) {
