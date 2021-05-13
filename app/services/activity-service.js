1d4ffe259533b4e22815d55d831292c8d68e3fe0
Michaël Dierick <michael.dierick@redpencil.io>
2021-05-13 10:25:42 +0200

    remove no longer used activities

--- app/services/activity-service.js
+++ app/services/activity-service.js
@@ -5,9 +5,7 @@ import moment from 'moment';
 
 export default class activityService extends Service {
   @service store;
-  @service toaster;
   @service configService;
-  @service publicationService;
   @service intl;
 
   /**
@@ -40,80 +38,6 @@ export default class activityService extends Service {
     outputString = outputString.replace('%%footer%%', await this.configService.get('email:footer', CONFIG.mail.defaultFooter));
     outputString = outputString.replace('%%kaleidosenvironment%%', window.location.origin);
     return outputString;
-  }
-
-  /**
-   * Create a new Translation Activity.
-   *
-   * @param finalTranslationDate
-   * @param mailContent
-   * @param mailSubject
-   * @param pieces
-   * @param subcase
-   * @returns {Promise<Model|any|Promise>}
-   */
-  async createNewTranslationActivity(finalTranslationDate, mailContent, mailSubject, pieces, subcase) {
-    const creationDatetime = moment()
-      .utc()
-      .toDate();
-
-    // TODO new logic needed with request-activity and email
-
-    // Create activity.
-    const translateActivity = this.store.createRecord('translation-activity', {
-      startDate: creationDatetime,
-      finalTranslationDate,
-      mailContent,
-      mailSubject,
-      subcase,
-      usedPieces: pieces,
-    });
-
-    // Persist to db.
-    await translateActivity.save();
-
-    // Reload relation.
-    await subcase.hasMany('translationActivities')
-      .reload();
-
-
-    return translateActivity;
-  }
-
-  /**
-   * Create a new Publish preview Activity.
-   *
-   * @param mailContent
-   * @param mailSubject
-   * @param pieces
-   * @param subcase
-   * @returns {Promise<Model|any|Promise>}
-   */
-  async createNewPublishPreviewActivity(mailContent, mailSubject, pieces, subcase) {
-    const creationDatetime = moment()
-      .utc()
-      .toDate();
-
-    // TODO new logic needed with request-activity and email
-    // publishPreviewActivityType.
-
-    // Create activity.
-    const PublishPreviewActivity = this.store.createRecord('proofing-activity', {
-      startDate: creationDatetime,
-      mailContent,
-      mailSubject,
-      subcase,
-      usedPieces: pieces,
-    });
-
-    // Persist to db.
-    await PublishPreviewActivity.save();
-
-    // Reload relation.
-    await subcase.hasMany('proofingActivities')
-      .reload();
-
-    return PublishPreviewActivity;
   }
 
   /**
