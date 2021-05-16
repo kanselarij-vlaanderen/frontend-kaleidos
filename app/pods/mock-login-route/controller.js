ced9cd1c2257a2d056d46b07333f5c0fc3535bdc
Erika Pauwels <erika.pauwels@gmail.com>
2021-05-16 11:58:44 +0200

    Refactor mock login route
    To be more in line with prototype

--- app/pods/mock-login-route/controller.js
+++ app/pods/mock-login-route/controller.js
@@ -1,51 +1,5 @@
 import Controller from '@ember/controller';
-import { timeout } from 'ember-concurrency';
-import {
-  lastValue,
-  restartableTask,
-  task
-} from 'ember-concurrency-decorators';
-import CONFIG from 'frontend-kaleidos/utils/config';
 
 export default class MockLoginController extends Controller {
-  role = '';
-  page = 0;
-  size = 10;
 
-  @lastValue('queryStore') accounts;
-
-  init() {
-    super.init(...arguments);
-    this.queryStore.perform();
-  }
-
-  @task
-  *queryStore() {
-    const filter = {
-      provider: CONFIG.mockLoginServiceProvider,
-    };
-    if (this.role) {
-      filter.user = {
-        'first-name': this.role,
-      };
-    }
-    const accounts = yield this.store.query('account', {
-      include: 'user,user.group',
-      filter,
-      sort: 'user.last-name,user.first-name',
-      page: {
-        size: this.size,
-        number: this.page,
-      },
-    });
-    return accounts;
-  }
-
-  @restartableTask
-  *updateSearch(value) {
-    yield timeout(500);
-    this.set('page', 0);
-    this.set('role', value);
-    yield this.queryStore.perform();
-  }
 }
--- app/pods/mock-login-route/route.js
+++ app/pods/mock-login-route/route.js
@@ -1,10 +1,25 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import CONFIG from 'frontend-kaleidos/utils/config';
 
 export default class MockLoginRoute extends Route {
   @service('session') simpleAuthSession;
 
   beforeModel() {
     this.simpleAuthSession.prohibitAuthentication('agendas');
+  }
+
+  model() {
+    return this.store.query('account', {
+      include: 'user,user.group',
+      filter: {
+        provider: CONFIG.mockLoginServiceProvider,
+      },
+      sort: 'user.last-name,user.first-name',
+      page: {
+        size: 100,
+        number: 0,
+      },
+    });
   }
 }
