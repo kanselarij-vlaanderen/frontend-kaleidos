20ad183060e80d542de6fa5ebb924740008c05dd
Karel Persoons <karel@brenner.company>
2021-05-18 18:35:15 +0200

    Replace '.auk-u-flex' instances & replace pre tag with span within '.auk-content'

--- app/utils/config.js
+++ app/utils/config.js
@@ -94,7 +94,7 @@ export default EmberObject.create({
       label: 'Formeel OK',
       uri:
         'http://kanselarij.vo.data.gift/id/concept/goedkeurings-statussen/CC12A7DB-A73A-4589-9D53-F3C2F4A40636',
-      classNames: 'vlc-agenda-items__status vlc-agenda-items__status--positive auk-u-flex',
+      classNames: 'vlc-agenda-items__status vlc-agenda-items__status--positive auk-o-flex',
       approved: true,
       pillClassNames: 'auk-pill auk-pill--success',
       iconClassNames: 'ki-check formally-ok-icon',
@@ -107,7 +107,7 @@ export default EmberObject.create({
       label: 'Formeel niet OK',
       uri:
         'http://kanselarij.vo.data.gift/id/concept/goedkeurings-statussen/92705106-4A61-4C30-971A-55532633A9D6',
-      classNames: 'vlc-agenda-items__status auk-u-text-error auk-u-text-bold auk-u-flex',
+      classNames: 'vlc-agenda-items__status auk-u-text-error auk-u-text-bold auk-o-flex',
       pillClassNames: 'auk-pill auk-pill--danger',
       iconClassNames: 'ki-cross formally-ok-icon',
       svg: {
@@ -119,7 +119,7 @@ export default EmberObject.create({
       label: 'Nog niet formeel OK',
       uri:
         'http://kanselarij.vo.data.gift/id/concept/goedkeurings-statussen/B72D1561-8172-466B-B3B6-FCC372C287D0',
-      classNames: 'vlc-agenda-items__status auk-u-text-bold auk-u-flex',
+      classNames: 'vlc-agenda-items__status auk-u-text-bold auk-o-flex',
       pillClassNames: 'auk-pill',
       iconClassNames: 'ki-question-mark formally-ok-icon',
       svg: {
