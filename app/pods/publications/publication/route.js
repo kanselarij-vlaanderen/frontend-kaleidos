7680c47ee3967a1b525c44e3c133ed5287922e5d
Erika Pauwels <erika.pauwels@gmail.com>
2021-05-22 12:35:09 +0200

    Cleanup publications.publication route

--- app/pods/publications/publication/route.js
+++ app/pods/publications/publication/route.js
@@ -3,38 +3,18 @@ import RSVP from 'rsvp';
 
 export default class PublicationRoute extends Route {
   async model(params) {
-    const publicationFlow = await this.store.findRecord('publication-flow', params.publication_id, {
+    return this.store.findRecord('publication-flow', params.publication_id, {
       include: 'case,status,mode,regulation-type,contact-persons,numac-numbers,identification',
       reload: true,
     });
-
-    return publicationFlow;
   }
 
-  async afterModel(model) {
-    const latestSubcaseOnMeetingPromise = this.store.queryOne('subcase', {
-      filter: {
-        case: {
-          // cannot access yet without get(...)
-          [':id:']: model.case.get('id'),
-        },
-        ':has:agenda-activities': 'yes',
-      },
-      sort: '-created',
-      include: 'mandatees',
-    });
-
+  async afterModel() {
     const publicationStatusPromise = this.store.query('publication-status', {});
     const regulationTypePromise = this.store.query('regulation-type', {});
-
-    const [latestSubcaseOnMeeting] = await RSVP.all([latestSubcaseOnMeetingPromise, publicationStatusPromise, regulationTypePromise]);
-
-    this.latestSubcaseOnMeeting = latestSubcaseOnMeeting;
-  }
-
-  setupController(controller) {
-    super.setupController(...arguments);
-
-    controller.latestSubcaseOnMeeting = this.latestSubcaseOnMeeting;
+    return RSVP.all([
+      publicationStatusPromise,
+      regulationTypePromise
+    ]);
   }
 }
