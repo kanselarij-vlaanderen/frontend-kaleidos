cb1ad736e1eeb9c471074831601c0ce7c6f4aaa4
Karel Persoons <karel@brenner.company>
2021-05-26 20:50:29 +0200

    Fix model/models passed parameters & adjust indentation

--- app/components/web-components/au-button-link.js
+++ app/components/web-components/au-button-link.js
@@ -1,6 +1,26 @@
 import Component from '@glimmer/component';
+import { computed } from '@ember/object';
+import { assert } from '@ember/debug';
 
 export default class ButtonLink extends Component {
+  @computed('args.{model,models}')
+  get models() {
+    const hasModel = 'model' in this.args;
+    const hasModels = 'models' in this.args;
+
+    assert(
+      'You cannot provide both the `@model` and `@models` arguments.',
+      !hasModel || !hasModels
+    );
+
+    if (hasModel) {
+      return [this.args.model];
+    } else if (hasModels) {
+      return this.args.models;
+    }
+    return [];
+  }
+
   /**
    * skin can be
    * (default, no skin)
@@ -8,7 +28,7 @@ export default class ButtonLink extends Component {
    */
   get skin() {
     if (this.args.skin) {
-      return `auk-button-link--${this.args.skin}`;
+      return `auk-button--${this.args.skin}`;
     }
     return '';
   }
