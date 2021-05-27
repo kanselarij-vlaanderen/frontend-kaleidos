98e4217bcfbcca90e9076e71b094ae9f9da91399
MathiasZabot <mathias.zabot@gmail.com>
2021-05-27 11:01:43 +0200

    KAS-2613 create new selector file and replace all occurences of css selectors

--- cypress/integration/unit/settings/general/shouldTestSettingsOverviewPage.spec.js
+++ cypress/integration/unit/settings/general/shouldTestSettingsOverviewPage.spec.js
@@ -4,8 +4,8 @@
 import settings from '../../../../selectors/settings.selectors';
 import toolbar from '../../../../selectors/toolbar.selectors';
 import modal from '../../../../selectors/modal.selectors';
-import agenda from '../../../../selectors/agenda.selectors';
 import utils from '../../../../selectors/utils.selectors';
+import dependency from '../../../../selectors/dependency.selectors';
 
 context('Settings overview page tests', () => {
   beforeEach(() => {
@@ -184,8 +184,8 @@ context('Settings overview page tests', () => {
       cy.get(settings.goToUserDetail).click();
       cy.contains('Gebruiker: Minister Test');
       cy.contains('Algemene informatie');
-      cy.get(settings.emberPowerSelectTrigger).click();
-      cy.get(agenda.emberPowerSelectOption).contains('kabinet')
+      cy.get(dependency.emberPowerSelect.trigger).click();
+      cy.get(dependency.emberPowerSelect.option).contains('kabinet')
         .click();
       cy.wait(5000); // TODO await PATCH call instead
       cy.get(utils.generalBackButton).should('exist')
