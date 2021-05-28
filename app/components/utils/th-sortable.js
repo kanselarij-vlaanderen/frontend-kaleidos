180a961f990a711bc1986e70919e627a8312d422
Erika Pauwels <erika.pauwels@gmail.com>
2021-05-28 12:23:16 +0200

    Remove unnecessary conversion between camelCase and kebab-case

--- app/components/utils/th-sortable.js
+++ app/components/utils/th-sortable.js
@@ -2,27 +2,24 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
 export default class ThSortable extends Component {
-  get field() {
-    return this.args.field.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
-  }
-
   get isAscendingOrder() {
-    return this.args.currentSorting === this.field;
+    return this.args.currentSorting === this.args.field;
   }
 
   get isDescendingOrder() {
-    return this.args.currentSorting === `-${this.field}`;
+    return this.args.currentSorting === `-${this.args.field}`;
+  }
   }
 
   @action
   changeSort() {
     let nextSort;
     if (this.isAscendingOrder) { // from ascending to descending
-      nextSort = `-${this.field}`;
+      nextSort = `-${this.args.field}`;
     } else if (this.isDescendingOrder) { // from descending to no-sort
       nextSort = '';
     } else { // from no-sort to ascending
-      nextSort = this.field;
+      nextSort = this.args.field;
     }
     this.args.onChange(nextSort);
   }
