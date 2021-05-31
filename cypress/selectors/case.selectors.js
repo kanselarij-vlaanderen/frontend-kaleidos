ed5f70114c311861bfcc457cf727638ddb944946
ValenberghsSven <sven.valenberghs1@gmail.com>
2021-05-31 11:33:24 +0200

    KAS-2614 reorder selectors, add comments

--- cypress/selectors/case.selectors.js
+++ cypress/selectors/case.selectors.js
@@ -3,6 +3,21 @@ const selectors = {
   casesHeaderAddCase: '[data-test-cases-header-add-case]',
   metadataForm: '[data-test-metadata-form]',
 
+  createSubcaseButton: '[data-test-case-create-subcase-button]',
+  clonePreviousSubcaseButton: '[data-test-clone-previous-subcase]',
+  subcaseModified: '[data-test-subcase-modified]',
+  subcaseDecidedOn: '[data-test-subcase-decided-on]',
+  subcaseRequestedBy: '[data-test-subcase-requested-by]',
+  subcaseMeetingNumber: '[data-test-meeting-number]',
+  subcaseMeetingPlannedStart: '[data-test-meeting-plannedStart]',
+
+  // subcase-item
+  // TODO better names
+  overviewSubcaseInfo: {
+    approved: '[data-test-case-overview-subcase-approved]',
+    notApproved: '[data-test-case-overview-subcase-not-approved]',
+  },
+
   // subcase-header
   subcaseHeader: {
     actionsDropdown: '[data-test-subcase-header-actions-dropdown]',
@@ -14,11 +29,13 @@ const selectors = {
     },
   },
 
+  // subcase-titles
   subcaseTitlesView: {
     type: '[data-test-subcase-titles-type]',
     edit: '[data-test-subcase-titles-edit]',
   },
 
+  // subcase-titles-edit
   subcaseTitlesEdit: {
     title: '[data-test-subcase-titles-edit-title]',
     shorttitle: '[data-test-subcase-titles-edit-shorttitle]',
@@ -29,26 +46,6 @@ const selectors = {
       // TODO unused selector
       cancel: '[data-test-subcase-titles-edit-cancel]',
     },
-  },
-
-  // from agenda
-  subcaseTitlesEditTitle: '[data-test-subcase-titles-edit-title]',
-  subcaseTitlesEditShorttitle: '[data-test-subcase-titles-edit-shorttitle]',
-  subcaseTitlesEditAccessLevel: '[data-test-subcase-titles-edit-accessLevel]',
-  subcaseTitlesEditConfidential: '[data-test-subcase-titles-edit-confidential ]',
-  subcaseTitlesEditSave: '[data-test-subcase-titles-edit-save]',
-
-  createSubcaseButton: '[data-test-case-create-subcase-button]',
-  clonePreviousSubcaseButton: '[data-test-clone-previous-subcase]',
-  subcaseModified: '[data-test-subcase-modified]',
-  subcaseDecidedOn: '[data-test-subcase-decided-on]',
-  subcaseRequestedBy: '[data-test-subcase-requested-by]',
-  subcaseMeetingNumber: '[data-test-meeting-number]',
-  subcaseMeetingPlannedStart: '[data-test-meeting-plannedStart]',
-
-  overviewSubcaseInfo: {
-    approved: '[data-test-case-overview-subcase-approved]',
-    notApproved: '[data-test-case-overview-subcase-not-approved]',
   },
 };
 export default selectors;
