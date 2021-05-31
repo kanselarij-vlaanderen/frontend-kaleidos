3ef400fd96dbeb80dc38ee38f9f4d2862b4e516e
ValenberghsSven <sven.valenberghs1@gmail.com>
2021-05-31 13:34:37 +0200

    KAS-2617 add comment

--- cypress/selectors/agenda.selectors.js
+++ cypress/selectors/agenda.selectors.js
@@ -49,6 +49,7 @@ const selectors = {
 
   // agendaitem-nav
   // TODO [data-test-agenda-agendaitem] bestaat ook in deze template, nog geen selector maar wel gebruikt in testen
+  // TODO rename selectors data-test-agendaitem-nav-tab....
   agendaitemKortBestekTab: '[data-test-agenda-agendaitem-tab="agendaitem-bestek"]',
   agendaitemDocumentsTab: '[data-test-agenda-agendaitem-tab="documents"]',
   agendaitemDossierTab: '[data-test-agenda-agendaitem-tab="agendaitem-case"]',
