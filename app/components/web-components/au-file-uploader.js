e50377a18fb1c6d2b439b0647b4e4d7a9993f326
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-01 14:12:23 +0200

    implement new file uploader closer to prototype

--- app/components/web-components/au-file-uploader.js
+++ app/components/web-components/au-file-uploader.js
@@ -1,19 +1,49 @@
 import Component from '@glimmer/component';
 import { enqueueTask } from 'ember-concurrency-decorators';
 import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
+import {
+  action,
+  setProperties
+} from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { guidFor } from '@ember/object/internals';
 
+/**
+ * @argument multiple <Boolean> (optional) allow uploading multiple files
+ * @argument accept <String> (optional)
+ * @argument fileQueueName <String> (optional) Name of the file queue to use.
+ *   Setting this name will allow you to access this queue from the file-queue service outside of this component.
+ * @argument onUpload: action fired for each file that gets uploaded. Passes a semantic.works File as an argument,
+ */
 export default class FileUploader extends Component {
   @service store;
+  @service('file-queue') fileQueueService;
 
-  @service fileQueue;
+  @tracked uploadedFileLength; // The amount of files that have been uploaded successfully
 
-  @tracked uploadedFileLength = null;
-
-  @action
-  insertElementInDom() {
+  constructor() {
+    super(...arguments);
     this.uploadedFileLength = 0;
+    const queue = this.fileQueueService.create(this.fileQueueName);
+    setProperties(queue, { // https://github.com/adopted-ember-addons/ember-file-upload/blob/888273b997d0336841daa1fb24287b5f5c5c9d62/addon/components/base-component.js#L13
+      accept: this.args.accept,
+      multiple: this.args.multiple,
+      // disabled,
+      onfileadd: this.uploadFileTaskAction,
+    });
+  }
+
+  get fileQueueName() {
+    return this.args.fileQueueName || `${guidFor(this)}-file-queue`;
+  }
+
+  get fileQueue() {
+    return this.fileQueueService.find(this.fileQueueName);
+  }
+
+  get uploadIsCompleted() {
+    return this.uploadedFileLength > 0
+      && (!this.fileQueue.files.length); // "queue automatically gets flushed when all the files in the queue have settled"
   }
 
   @enqueueTask({
@@ -22,23 +52,40 @@ export default class FileUploader extends Component {
     try {
       const response = yield file.upload('/files');
       const fileFromStore = yield this.store.findRecord('file', response.body.data.id);
-      this.args.uploadedFileAction(fileFromStore);
+      if (this.args.onUpload) {
+        this.args.onUpload(fileFromStore);
+      }
       this.uploadedFileLength += 1;
     } catch (exception) {
       console.warn('An exception occurred', exception);
     }
   }
 
+  // Wrapper for task invocation, since direct task perform doesn't work because of ember-file-upload internals
   @action
-  uploadFile(file) {
-    this.uploadedFileLength = 0;
-    this.uploadFileTask.perform(file);
+  uploadFileTaskAction() {
+    return this.uploadFileTask.perform(...arguments);
+  }
+
+  // Replacement for https://github.com/adopted-ember-addons/ember-file-upload/blob/888273b997d0336841daa1fb24287b5f5c5c9d62/addon/components/file-upload/component.js#L149
+  @action
+  addFilesToQueue(event) {
+    const files = event.target.files;
+    this.fileQueue._addFiles(files, 'browse');
+    event.target.value = null;
   }
 
   @action
-  handleButtonKeyUp(event) {
+  clickInputOnEnter(event) {
     if (event.key === 'Enter') {
-      event.target.click();
+      this.clickInput(event);
     }
+  }
+
+  @action
+  clickInput(event) {
+    event.target
+      .closest('.auk-file-upload').querySelector('input')
+      .click();
   }
 }
