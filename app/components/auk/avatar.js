f193045b3ad16d5fc5101d3aeb78423bc3daea6f
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-02 14:18:38 +0200

    detail avatar name argument

--- app/components/auk/avatar.js
+++ app/components/auk/avatar.js
@@ -4,6 +4,6 @@ import Component from '@glimmer/component';
  * @argument size {String}
  * @argument icon {String}
  * @argument image {String}
- * @argument name {String}
+ * @argument name {String}: Determines the name of the individual depicted (concerning accessibility)
  */
 export default class Avatar extends Component {}
