ca77c1460b7f944e8691c4699156520538f58f09
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-03 17:18:14 +0200

    DDAU checkbox usage

--- app/components/auk/checkbox.js
+++ app/components/auk/checkbox.js
@@ -2,9 +2,19 @@ import Component from '@glimmer/component';
 
 /**
  *
+ * Example usage:
+ * ```
+ * <Auk:Checkbox @label="A checkbox" {{on "input" this.toggleCheckbox}}
+ * ```
+ * ```
+ * @action
+ * toggleCheckbox(event) {
+ *   console.log('New checkbox value=', event.target.checked);
+ * }
+ * ```
+
  * @argument name {String}
  * @argument checked {Boolean}
- * @argument toggle {Function}: callback function fired when the checkbox gets toggled
  * @argument label {String}
  */
 export default class Checkbox extends Component {}
