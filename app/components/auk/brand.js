fb1ec3683112b95adc803bd7d3bfc9b6bdb408e6
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-03 12:48:22 +0200

    clarify todo

--- app/components/auk/brand.js
+++ app/components/auk/brand.js
@@ -2,6 +2,6 @@ import Component from '@glimmer/component';
 
 /**
  *
- * @argument logoType {String}: ? TODO
+ * @argument logoType {String}: ? TODO: presumably something like `brandName` would be clearer here?
  */
 export default class Brand extends Component {}
