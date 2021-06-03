902eb445a75d1fbbe4bc616340bfa74a69f4f1a8
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-03 12:22:59 +0200

    harden for @text being null

--- app/components/auk/abbreviated-text.js
+++ app/components/auk/abbreviated-text.js
@@ -1,4 +1,6 @@
 import Component from '@glimmer/component';
+import { warn } from '@ember/debug';
+import { isNone } from '@ember/utils';
 
 /**
  *
@@ -6,17 +8,30 @@ import Component from '@glimmer/component';
  * @argument maxLength {Number}
  */
 export default class AbbreviatedText extends Component {
+  constructor() {
+    super(...arguments);
+    warn(`Using ${this.constructor.name} while providing a none-like value to @text is pointless.`,
+      !isNone(this.args.text),
+      {
+        id: 'auk.pointless-argument-value',
+      }
+    );
+  }
+
   get maxLength() {
     // The amount of characters to show in the title.
     return this.args.maxLength || 20;
   }
 
   get textTooLong() {
-    return this.args.text.length > this.maxLength;
+    if (this.args.text) {
+      return this.args.text.length > this.maxLength;
+    }
+    return false;
   }
 
   // Abbreviated version of title.
   get abbreviatedText() {
-    return this.args.text.substring(0, this.maxLength);
+    return (this.args.text || '').substring(0, this.maxLength);
   }
 }
