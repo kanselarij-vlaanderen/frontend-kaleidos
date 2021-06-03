dfc77a8a6278681e14f4ccbfa3378a29da001685
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-03 15:58:59 +0200

    document arguments

--- app/components/auk/checkbox.js
+++ app/components/auk/checkbox.js
@@ -1,5 +1,10 @@
 import Component from '@glimmer/component';
 
-export default class Checkbox extends Component {
-
-}
+/**
+ *
+ * @argument name {String}
+ * @argument checked {Boolean}
+ * @argument toggle {Function}: callback function fired when the checkbox gets toggled
+ * @argument label {String}
+ */
+export default class Checkbox extends Component {}
