a0de0533627e40e73bb52af76fe4b120829db8bd
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-04 14:59:44 +0200

    document list-item component

--- app/components/auk/list/item.js
+++ app/components/auk/list/item.js
@@ -1,5 +1,10 @@
 import Component from '@glimmer/component';
 
+/**
+ *
+ * @argument {Boolean} flex
+ * @argument {String} skin
+ */
 export default class AuListItem extends Component {
   get skin() {
     if (this.args.skin) {
