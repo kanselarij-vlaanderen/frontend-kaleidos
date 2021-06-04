604f927b077dc6f2ac16e47062ccf38a29dca6d4
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-04 18:54:16 +0200

    get grab util out of generic icon component

--- app/components/auk/icon.js
+++ app/components/auk/icon.js
@@ -4,7 +4,6 @@ import Component from '@glimmer/component';
  * @argument {String} name: au-kaleidos-icons name (without prefix)
  * @argument {String} size: Possible values are (empty, default), "small", "large"
  * @argument {String} skin: Possible values are "muted", "muted-dark", "warning", "primary", "success", "success-dark", "danger"
- * @argument {Boolean} grab: Make the grab-hand appear on hover
  */
 export default class Icon extends Component {
   baseClass = 'auk-icon';
@@ -12,13 +11,6 @@ export default class Icon extends Component {
   get size() {
     const modifier = this.args.size || 'default';
     return `${this.baseClass}--${modifier}`;
-  }
-
-  get grab() {
-    if (this.args.grab) {
-      return 'auk-u-cursor-grab';
-    }
-    return null;
   }
 
   get skin() {
