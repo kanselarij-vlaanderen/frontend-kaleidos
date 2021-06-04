f2c65c9cf031b9e8ec4e6d8f61a83a1ac10053ed
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-04 11:59:51 +0200

    rename icon skin argument

--- app/components/auk/icon.js
+++ app/components/auk/icon.js
@@ -3,7 +3,7 @@ import Component from '@glimmer/component';
 /**
  * @argument {String} name: au-kaleidos-icons name (without prefix)
  * @argument {String} size: Possible values are 'small', 'large'
- * @argument {String} iconSkinColor: Possible values are 'muted', 'warning', 'primary', 'success', 'danger'
+ * @argument {String} skin: Possible values are 'muted', 'warning', 'primary', 'success', 'danger'
  * @argument {Boolean} grab: Make the grab-hand appear on hover
  */
 export default class Icon extends Component {
@@ -22,8 +22,8 @@ export default class Icon extends Component {
   }
 
   get skin() {
-    if (this.args.iconSkinColor) {
-      return `${this.baseClass}--${this.args.iconSkinColor}`;
+    if (this.args.skin) {
+      return `${this.baseClass}--${this.args.skin}`;
     }
     return null;
   }
