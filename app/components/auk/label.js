60caf747cd064cfb20902e4383c78ea74e8cb690
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-04 14:32:56 +0200

    simplify required arg

--- app/components/auk/label.js
+++ app/components/auk/label.js
@@ -1,7 +1,3 @@
 import Component from '@glimmer/component';
 
-export default class Label extends Component {
-  get required() {
-    return this.args.isRequired === true;
-  }
-}
+export default class Label extends Component {}
