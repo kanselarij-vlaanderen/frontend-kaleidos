3362f1986906e8c411464a9f0c847b049cba1f2c
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-04 14:39:57 +0200

    merge label-error component into label

--- app/components/auk/label.js
+++ app/components/auk/label.js
@@ -1,3 +1,10 @@
 import Component from '@glimmer/component';
 
-export default class Label extends Component {}
+export default class Label extends Component {
+  get class() {
+    if (this.args.error) {
+      return 'auk-label-error';
+    }
+    return 'auk-label';
+  }
+}
