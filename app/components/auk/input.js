e422977651c35b7c6e8f3f9bc65f38eab3a73cb8
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-04 12:41:13 +0200

    make the block argument a boolean

--- app/components/auk/input.js
+++ app/components/auk/input.js
@@ -4,7 +4,7 @@ import Component from '@glimmer/component';
  * Simple wrapper for Ember's <Input> component. Takes all arguments that <Input> takes.
  *
  * @argument {String} icon
- * @argument {String} block
+ * @argument {Boolean} block
  * @argument {String} error
  */
 export default class AuInput extends Component {
