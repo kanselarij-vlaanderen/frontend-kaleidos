d6543dd82317e8d6554e5484746c02e30120a7b2
Erika Pauwels <erika.pauwels@gmail.com>
2021-06-05 21:23:01 +0200

    Remove unnecessary async from hooks in route

--- app/pods/publications/publication/translations/route.js
+++ app/pods/publications/publication/translations/route.js
@@ -1,15 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default class PublicationTranslationRoute extends Route {
-  async model() {
+  model() {
     return this.modelFor('publications.publication').translationSubcase;
   }
 
-  async afterModel() {
+  afterModel() {
     this.publicationFlow = this.modelFor('publications.publication');
   }
 
-  async setupController(controller) {
+  setupController(controller) {
     super.setupController(...arguments);
     controller.publicationFlow = this.publicationFlow;
   }
