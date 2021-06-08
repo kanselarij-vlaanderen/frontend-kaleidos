ce1e1a32db5b5181a6cbdd692e18de9b92e8b6ce
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-08 10:25:19 +0200

    remove unnecessary extra sizing argument

--- app/components/auk/modal.js
+++ app/components/auk/modal.js
@@ -2,15 +2,10 @@ import Component from '@glimmer/component';
 
 /**
  *
- * @argument {Boolean} resized show the modal fullscreen
  * @argument {String} size: Size can be: "full-screen", "full-screen-padded", "large (default)", "medium", "small"
  */
 export default class Modal extends Component {
   get size() {
-    if (this.args.resized) {
-      return 'auk-modal--full-screen';
-    }
-    // if not resized, use the size given
     if (this.args.size) {
       if (this.args.size === 'full-screen-padded') {
         return 'auk-modal--full-screen auk-u-p-4';
