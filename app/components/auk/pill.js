e1436a13592eacf7e3b498826bc06350ff7e184f
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-08 17:33:37 +0200

    document arguments

--- app/components/auk/pill.js
+++ app/components/auk/pill.js
@@ -1,25 +1,19 @@
 import Component from '@glimmer/component';
 
+/**
+ *
+ * @argument {String} skin: possible values are (empty, default), "success", "danger", "warning"
+ * @argumemt {String} layout: possible values are (empty, default is "icon-left"), "icon-only", "icon-right"
+ * @argument {String} icon: name of the icon to render
+ */
 export default class Pill extends Component {
-  /**
-   * skin can be:
-   * (empty, default)
-   * success
-   * danger
-   * warning
-   */
   get skin() {
     if (this.args.skin) {
       return `auk-pill--${this.args.skin}`;
     }
     return 'auk-pill--default';
   }
-  /**
-   * layout can be:
-   * (empty, default is icon-left)
-   * icon-only
-   * icon-right
-   */
+
   get layout() {
     if (this.args.layout) {
       return `auk-pill--${this.args.layout}`;
