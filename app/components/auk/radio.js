4d64b76bc2d84f27441bfb40e87ced3ce18ec36a
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-09 16:34:16 +0200

    remove unnecessary sublabel existence test getter

--- app/components/auk/radio.js
+++ app/components/auk/radio.js
@@ -1,13 +1,6 @@
 import Component from '@glimmer/component';
 
 export default class Radio extends Component {
-  get sublabel() {
-    if (this.args.sublabel) {
-      return this.args.sublabel;
-    }
-    return false;
-  }
-
   get name() {
     if (!this.args.name) {
       console.error('radio items should have names');
