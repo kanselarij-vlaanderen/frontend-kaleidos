8a9e517f00c21e3acad0afa50d6bb5d27702647a
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-09 16:46:17 +0200

    Use ember warn instead of console.error for name arg validation

--- app/components/auk/radio.js
+++ app/components/auk/radio.js
@@ -1,9 +1,17 @@
 import Component from '@glimmer/component';
+import { isEmpty } from '@ember/utils';
+import { warn } from '@ember/debug';
 
 export default class Radio extends Component {
+  constructor() {
+    super(...arguments);
+    warn(`Using ${this.constructor.name} without providing @name is most likely to be an oversight`, isEmpty(this.args.name), {
+      id: 'auk.name-argument',
+    });
+  }
+
   get name() {
-    if (!this.args.name) {
-      console.error('radio items should have names');
+    if (isEmpty(this.args.name)) {
       return 'radio-buttons';
     }
     return this.args.name;
