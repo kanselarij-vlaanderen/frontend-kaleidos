6d9e1628a206dffde747781b5a751ba8265962f3
Erika Pauwels <erika.pauwels@gmail.com>
2021-06-09 08:34:01 +0200

    Move translation request email message to a util

--- app/components/publications/publication/translations/translation-request-modal.js
+++ app/components/publications/publication/translations/translation-request-modal.js
@@ -3,7 +3,7 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency-decorators';
-import moment from 'moment';
+import { translationRequestEmail } from 'frontend-kaleidos/utils/publication-email';
 import { add } from 'ember-math-helpers/helpers/add';
 
 export default class PublicationsTranslationRequestModalComponent extends Component {
@@ -14,22 +14,13 @@ export default class PublicationsTranslationRequestModalComponent extends Compon
   @service store;
 
   @tracked translationDueDate = this.args.translationSubcase.dueDate ? this.args.translationSubcase.dueDate : new Date();
-  @tracked subject = `Vertalingsaanvraag VO-dossier: ${this.args.identification.idName}`;
-  @tracked message = ' Collega,\n'
-    + '\n'
-    + 'In bijlage document(en) die wij indienen voor vertaling.\n'
-    + '\n'
-    + 'Het betreft:\n'
-    + '\n'
-    + `VO-dossier: ${this.args.identification.idName}\n`
-    + `Titel: ${this.args.publicationFlow.shortTitle}\n`
-    + `Uiterste vertaaldatum: ${moment(this.translationDueDate).format('DD-MM-YYYY')}\n`
-    + `Aantal pagina’s: ${this.totalPages}\n`
-    + `Aantal woorden: ${this.totalWords}\n`
-    + `Aantal documenten: ${this.totalDocuments}\n`
-    + '\n'
-    + 'Vragen bij dit dossier kunnen met vermelding van publicatienummer gericht worden aan onderstaande email adres.';
+  @tracked subject;
+  @tracked message;
 
+  constructor() {
+    super(...arguments);
+    this.setEmailFields.perform();
+  }
 
   get totalPages() {
     const pages = this.args.selectedPieces.mapBy('pages').filter((page) => page >= 0);
@@ -45,18 +36,35 @@ export default class PublicationsTranslationRequestModalComponent extends Compon
     return this.args.selectedPieces.length;
   }
 
-
-  @action
-  setTranslationDueDate(selectedDates) {
-    this.translationDueDate = selectedDates[0];
-  }
-
   get saveIsDisabled() {
     return this.translationDueDate === null
       || this.subject === null
       || this.message === null;
   }
 
+  @task
+  *setEmailFields() {
+    const publicationFlow = yield this.args.translationSubcase.publicationFlow;
+    const identification = yield publicationFlow.identification;
+
+    const mailParams = {
+      identifier: identification.idName,
+      title: publicationFlow.shortTitle,
+      dueDate: this.translationDueDate,
+      totalPages: this.totalPages,
+      totalWords: this.totalWords,
+      totalDocuments: this.totalDocuments,
+    };
+
+    this.message = translationRequestEmail(mailParams);
+    this.subject = `Vertalingsaanvraag VO-dossier: ${identification.idName}`;
+  }
+
+  @action
+  setTranslationDueDate(selectedDates) {
+    this.translationDueDate = selectedDates[0];
+    this.setEmailFields.perform();
+  }
 
   @action
   cancelRequest() {
