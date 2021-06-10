44f5c916ac6b68a5c92d9b08a7a9023b3cf907cd
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-10 11:22:40 +0200

    document component & argument

--- app/components/auk/radio-list.js
+++ app/components/auk/radio-list.js
@@ -1,13 +1,14 @@
 import Component from '@glimmer/component';
 import { guidFor } from '@ember/object/internals';
 
+/**
+ * radio-list component. Yields a radio-button component as "Radio", thereby taking care of the group "name".
+ *
+ * @argument{String} layout: Layout can be (empty, default = "block") or "inline"
+ */
 export default class RadioList extends Component {
   groupName = `radio-${guidFor(this)}`;
-  /**
-   * layout can be:
-   * (empty, default = block)
-   * inline
-   */
+
   get layout() {
     if (this.args.layout) {
       return `auk-radio-list--${this.args.layout}`;
