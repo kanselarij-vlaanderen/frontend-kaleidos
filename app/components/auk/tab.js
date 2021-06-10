ad65ae7a7e87e4a60573bc7880c5c9ebc250c15a
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-10 15:18:23 +0200

    add proper support for all link params in tab

--- app/components/auk/tab.js
+++ app/components/auk/tab.js
@@ -1,12 +1,29 @@
 import Component from '@glimmer/component';
+import { computed } from '@ember/object';
+import { assert } from '@ember/debug';
+
 
 export default class Tab extends Component {
-  get route() {
-    if (this.args.route) {
-      return this.args.route;
+  // Workaround for linkTo not accepting @model and @models parameter, regardless if one is null
+  // https://github.com/emberjs/ember.js/issues/18265
+  @computed('args.{model,models}')
+  get models() {
+    const hasModel = 'model' in this.args;
+    const hasModels = 'models' in this.args;
+
+    assert(
+      'You cannot provide both the `@model` and `@models` arguments.',
+      !hasModel || !hasModels
+    );
+
+    if (hasModel) {
+      return [this.args.model];
+    } else if (hasModels) {
+      return this.args.models;
     }
-    return null;
+    return [];
   }
+
   get icon() {
     if (this.args.icon) {
       return this.args.icon;
