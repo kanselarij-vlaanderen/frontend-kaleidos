4bc182c5d084f4b568d1d1907c135cf4bfcfb87a
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-11 10:04:02 +0200

    document item arguments

--- app/components/auk/dropdown/item.js
+++ app/components/auk/dropdown/item.js
@@ -1,5 +1,13 @@
 import Component from '@glimmer/component';
 
+/**
+ * Dropdown item. Takes most arguments that LinkTo takes for creating a dropdown element that links somewhere
+ *
+ * @argument {String} class: Sets the css class of the wrapping <li>
+ * @argument {String} skin
+ * @argument {String} href: Set an (external) href link instead of composing one with LinkTo
+ * @argument {Boolean} textOnly: textual-only dropdown element. No <LinkTo> nor <a>
+ */
 export default class Item extends Component {
   get skin() {
     if (this.args.skin) {
