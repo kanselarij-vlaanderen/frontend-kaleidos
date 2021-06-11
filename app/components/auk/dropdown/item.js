a2cd225007612e6161bfdfd999755b1da554b53d
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-11 10:42:22 +0200

    provide all available LinkTo arguments

--- app/components/auk/dropdown/item.js
+++ app/components/auk/dropdown/item.js
@@ -1,4 +1,6 @@
 import Component from '@glimmer/component';
+import { computed } from '@ember/object';
+import { assert } from '@ember/debug';
 
 /**
  * Dropdown item. Takes most arguments that LinkTo takes for creating a dropdown element that links somewhere
@@ -9,6 +11,26 @@ import Component from '@glimmer/component';
  * @argument {Boolean} textOnly: textual-only dropdown element. No <LinkTo> nor <a>
  */
 export default class Item extends Component {
+  // Workaround for linkTo not accepting @model and @models parameter, regardless if one is null
+  // https://github.com/emberjs/ember.js/issues/18265
+  @computed('args.{model,models}')
+  get models() {
+    const hasModel = 'model' in this.args;
+    const hasModels = 'models' in this.args;
+
+    assert(
+      'You cannot provide both the `@model` and `@models` arguments.',
+      !hasModel || !hasModels
+    );
+
+    if (hasModel) {
+      return [this.args.model];
+    } else if (hasModels) {
+      return this.args.models;
+    }
+    return [];
+  }
+
   get skin() {
     if (this.args.skin) {
       return `auk-menu__item--${this.args.skin}`;
