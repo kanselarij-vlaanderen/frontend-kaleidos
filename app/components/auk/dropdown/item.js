ff111fd9519e6388a7d340aa5a53832dd9700d35
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-11 09:55:50 +0200

    remove unnecessary getter

--- app/components/auk/dropdown/item.js
+++ app/components/auk/dropdown/item.js
@@ -1,12 +1,6 @@
 import Component from '@glimmer/component';
 
 export default class Item extends Component {
-  get class() {
-    if (this.args.class) {
-      return this.args.class;
-    }
-    return null;
-  }
   get skin() {
     if (this.args.skin) {
       return `auk-menu__item--${this.args.skin}`;
