41ec51623446f1dde7b0ee5fa2832b1ab5227531
Flor Sanders <me@florsanders.be>
2021-06-12 13:24:11 +0200

    [refactor] Restored edit, unarchive, archive and show archived functionality

--- app/pods/cases/index/controller.js
+++ app/pods/cases/index/controller.js
@@ -39,71 +39,65 @@ export default class CasesIndexController extends Controller {
   @tracked isEditingRow = false;
   @tracked isNotArchived = false;
   @tracked isArchivingCase = false;
-  caseToEdit = null;
 
   @action
   editCase(_case) {
-    console.warn('Not implemented: Editing case');
-    console.log(_case);
-    // this.caseToEdit = _case;
-    // this.isEditingRow = !this.isEditingRow;
+    this.selectedCase = _case;
+    this.isEditingRow = true;
+  }
+
+  @action
+  cancelEditing() {
+    this.selectedCase = null;
+    this.isEditingRow = false;
   }
 
   @action
   async archiveCase() {
-    console.warn('Not implemented: archive case');
-    // const _case = await this.store.findRecord('case', this.get('selectedCase.id'));
-    // _case.isArchived = true;
-    // const subcases = await _case.subcases;
-    // await Promise.all(subcases.map(async(subcase) => {
-    //   subcase.isArchived = true;
-    //   const savedSubcase = await subcase.save();
-    //   return savedSubcase;
-    // }));
-    // _case.save().then(() => {
-    //   this.selectedCase = null;
-    //   this.isArchivingCase = false;
-    //   this.send('refreshModel');
-    // });
+    const _case = await this.store.findRecord('case', this.get('selectedCase.id'));
+    _case.isArchived = true;
+    const subcases = await _case.subcases;
+    await Promise.all(subcases.map(async(subcase) => {
+      subcase.isArchived = true;
+      const savedSubcase = await subcase.save();
+      return savedSubcase;
+    }));
+    await _case.save();
+    this.selectedCase = null;
+    this.isArchivingCase = false;
+    this.send('refreshModel');
   }
 
   @action
   async unarchiveCase(_case) {
-    console.warn('Not implemented: unarchive case');
-    console.log(_case);
-    // _case.isArchived = false;
-    // const subcases = await _case.subcases;
-    // await Promise.all(subcases.map(async(subcase) => {
-    //   subcase.isArchived = false;
-    //   const savedSubcase = await subcase.save();
-    //   return savedSubcase;
-    // }));
-    // await _case.save();
+    _case.isArchived = false;
+    const subcases = await _case.subcases;
+    await Promise.all(subcases.map(async(subcase) => {
+      subcase.isArchived = false;
+      const savedSubcase = await subcase.save();
+      return savedSubcase;
+    }));
+    await _case.save();
   }
 
   @action
   requestArchiveCase(_case) {
-    console.warn('Not implemented: Requesting archive case');
-    console.log(_case);
-    // this.selectedCase = _case;
-    // this.isArchivingCase = true;
+    this.selectedCase = _case;
+    this.isArchivingCase = true;
   }
 
   @action
   cancelArchiveCase() {
-    console.warn('Not implemented: Cancel archive case');
-    // this.selectedCase = null;
-    // this.isArchivingCase = false;
+    this.selectedCase = null;
+    this.isArchivingCase = false;
   }
 
   @action
   close(_case) {
-    console.warn('Not implemented: Closing case');
-    console.log(_case);
-    // if (!_case) {
-    //   return;
-    // }
-    // this.navigateToCase(_case);
+    if (!_case) {
+      return;
+    }
+    this.navigateToCase(_case);
   }
 
   @action
@@ -114,6 +108,12 @@ export default class CasesIndexController extends Controller {
   @action
   toggleColumnDisplayOptions() {
     this.showTableDisplayOptions = !this.showTableDisplayOptions;
+  }
+
+  @action
+  toggleShowArchived() {
+    // TODO: setter instead of @tracked on qp's before updating to Ember 3.22+ (https://github.com/emberjs/ember.js/issues/18715)
+    this.set('showArchived', !this.showArchived);
   }
 
   @action
