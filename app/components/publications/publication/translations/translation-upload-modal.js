4d2d885c3441bd085f53c7f19c8de39bc9aa1ba2
Tanee Grade <tanee.grade@cereus.be>
2021-06-14 15:57:32 +0200

    KAS-2481 Added Modal + save + added pieces to requests

---
+++ app/components/publications/publication/translations/translation-upload-modal.js
@@ -0,0 +1,97 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency-decorators';
+import { guidFor } from '@ember/object/internals';
+
+export default class PublicationsTranslationTranslationUploadModalComponent extends Component {
+  /**
+   * @argument onSave: should take arguments (pieces)
+   * @argument onCancel
+   */
+  @service store;
+  @service('file-queue') fileQueueService;
+
+  @tracked translationDocument = null;
+  @tracked newPieces = [];
+  @tracked name = null;
+  @tracked pagesAmount = null;
+  @tracked wordsAmount = null;
+  @tracked receivedAtDate = null;
+
+
+  constructor() {
+    super(...arguments);
+    if (this.fileQueueService.find(this.fileQueueName)) {
+      this.fileQueueService.create(this.fileQueueName);
+    }
+  }
+
+  get fileQueueName() {
+    return `${guidFor(this)}-file-queue`;
+  }
+
+  get fileQueue() {
+    return this.fileQueueService.find(this.fileQueueName);
+  }
+
+  get saveIsDisabled() {
+    return this.translationDocument === null || this.receivedAtDate === null;
+  }
+
+  @action
+  uploadPiece(file) {
+    const now = new Date();
+    const documentContainer = this.store.createRecord('document-container', {
+      created: now,
+    });
+    this.translationDocument = this.store.createRecord('piece', {
+      created: now,
+      modified: now,
+      file: file,
+      confidential: false,
+      name: file.filenameWithoutExtension,
+      documentContainer: documentContainer,
+    });
+    this.name = file.filenameWithoutExtension;
+    this.newPieces.pushObject(this.translatedDocument);
+  }
+
+  @task
+  *cancelTranslation() {
+    if (this.translationDocument) {
+      yield this.deleteUploadedPiece.perform(this.translationDocument);
+    }
+    this.args.onCancel();
+  }
+
+  @task
+  *deleteUploadedPiece(piece) {
+    const file = yield piece.file;
+    yield file.destroyRecord();
+    this.newPieces.removeObject(piece);
+    const documentContainer = yield piece.documentContainer;
+    yield documentContainer.destroyRecord();
+    yield piece.destroyRecord();
+  }
+
+  @task
+  *saveTranslation() {
+    if (this.args.onSave) {
+      yield this.args.onSave({
+        piece: this.translationDocument,
+        name: this.name,
+        receivedAtDate: this.receivedAtDate,
+        pagesAmount: this.pagesAmount,
+        wordsAmount: this.wordsAmount,
+        activity: this.args.activity,
+      });
+    }
+  }
+
+  @action
+  setReceivedAtDate(selectedDates) {
+    this.receivedAtDate = selectedDates[0];
+  }
+}
--- app/pods/publications/publication/translations/requests/controller.js
+++ app/pods/publications/publication/translations/requests/controller.js
@@ -1,7 +1,47 @@
 import Controller from '@ember/controller';
+import { task } from 'ember-concurrency-decorators';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class PublicationsPublicationTranslationsRequestController extends Controller {
+  @service store;
+
   @tracked translationSubcase;
   @tracked publicationFlow;
+
+  @tracked showTranslationUploadModal = false;
+
+  @task
+  *saveTranslationUpload(translationUpload) {
+    const piece = translationUpload.piece;
+    const activity = translationUpload.activity;
+
+    const documentContainer = yield piece.documentContainer;
+    yield documentContainer.save();
+
+    piece.translationSubcase = this.translationSubcase;
+    piece.pages = translationUpload.pagesAmount;
+    piece.words = translationUpload.wordsAmount;
+    piece.name = translationUpload.name;
+    piece.language = activity.language;
+    yield piece.save();
+
+    activity.endDate = translationUpload.receivedAtDate;
+    activity.generatedPieces !== undefined ? activity.generatedPieces.push(piece) : activity.generatedPieces = [piece];
+    yield activity.save();
+
+    this.showTranslationUploadModal = false;
+    this.send('refresh');
+  }
+
+  @action
+  openTranslationUploadModal() {
+    this.showTranslationUploadModal = true;
+  }
+
+  @action
+  closeTranslationUploadModal() {
+    this.showTranslationUploadModal = false;
+  }
 }
