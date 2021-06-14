2da89938dfc85fd447c3376a9d9dab4baaa0d9a3
mdv-professional <mdv.werk@outlook.com>
2021-06-14 16:23:02 +0200

    KAS-2581 (busy) | documents tab [pub proof]

--- app/pods/publications/publication/proofs/documents/controller.js
+++ app/pods/publications/publication/proofs/documents/controller.js
@@ -1,6 +1,79 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class PublicationsPublicationProofsDocumentsController extends Controller {
-  @tracked sort;
+  @inject intl;
+
+  @tracked pieces;
+
+  queryParams = [{
+    qpSorting: {
+      // no intl: language not set yet.
+      as: 'sorteer',
+    },
+  }]
+
+  qpSorting = '';
+
+  columnMap = {
+    receiptDate: {
+      translationKey: 'received',
+      // ['translation'] added dynamically
+      // TODO: correct properties!
+      propertyPath: 'created',
+    },
+    uploadDate: {
+      translationKey: 'uploaded',
+      // TODO: correct properties!
+      propertyPath: 'modified',
+    },
+  }
+
+  constructor() {
+    super(...arguments);
+
+    for (const key in this.columnMap) {
+      const column = this.columnMap[key];
+      column.translation = this.intl.t(column.translationKey);
+    }
+  }
+
+  toQPSorting(sorting) {
+    const qpValue = this.columnMap[sorting].translation;
+    this.set('qpSorting', qpValue);
+  }
+
+  fromQPSorting() {
+    const qpValue = this.qpSorting;
+    const isDesc = qpValue.startsWith('-');
+    const translatedSortKey = qpValue.substr(isDesc);
+
+    const sortField = Object.values(this.columnMap).find((column) => {
+      const translation = column.translation;
+      return translatedSortKey === translation;
+    });
+
+    return [isDesc, sortField];
+  }
+
+  @action
+  changeSortOrder(sorting) {
+    this.toQPSorting(sorting);
+  }
+
+  initSort() {
+    this.model.sortBy('created');
+    // this.sort();
+  }
+
+  sort(sortField) {
+    this.model.sortBy(sortField.propertyPath);
+  }
+
+  @action
+  openPieceUploadModal() {
+
+  }
 }
--- app/pods/publications/publication/proofs/documents/route.js
+++ app/pods/publications/publication/proofs/documents/route.js
@@ -2,13 +2,25 @@ import Route from '@ember/routing/route';
 
 
 export default class PublicationsPublicationProofsDocumentsRoute extends Route {
-  model() {
-    /** @type {import('app/models/_model-types').PublicationSubcase} */
-    const pubSubcase = this.modelFor('publications.publication.proofing');
-    // todo: add brondocumenten
-    const publicationActivity = this.store.findRecord('publication-activity', pubSubcase.id, {
-      include: 'proofing-activities,proofing-activities.used-pieces,proofing-activities.generated-pieces',
+  async model() {
+    // /** @type {import('app/models/_model-types').PublicationSubcase} */
+    // const pubSubcase = this.modelFor('publications.publication.proofs');
+    // // todo: add brondocumenten
+    // const publicationActivity = this.store.findRecord('publication-activity', pubSubcase.id, {
+    //   include: 'proofing-activities,proofing-activities.used-pieces,proofing-activities.generated-pieces',
+    // });
+    // return publicationActivity;
+    const parentParams = this.paramsFor('publications.publication');
+    const pieces = await this.store.query('piece', {
+      'filter[publication-flow][:id:]': parentParams.publication_id,
+      // TODO: paginatie uitklaren in design
+      'page[size]': 200,
     });
-    return publicationActivity;
+    return pieces;
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    controller.initSort();
   }
 }
