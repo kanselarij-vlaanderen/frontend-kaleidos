ce2d52cb8c2ef44aa5ed5d273a4c94672dbb8262
Erika Pauwels <erika.pauwels@gmail.com>
2021-06-15 14:34:33 +0200

    Replace promise.then() with async/await

--- app/pods/publications/publication/case/route.js
+++ app/pods/publications/publication/case/route.js
@@ -6,25 +6,24 @@ export default class CaseRoute extends Route {
   }
 
   async afterModel(model) {
-    const isViaCouncilOfMinisters = this.store.queryOne('subcase', {
+    const subcase = await this.store.queryOne('subcase', {
       filter: {
         case: {
           [':id:']: model.case.get('id'),
         },
         ':has:agenda-activities': 'yes',
       },
-    }).then((subcase) => !!subcase);
+    });
+    this.isViaCouncilOfMinisters = !!subcase;
 
     // TODO This is not ideal, there are currently +- 60 organizations that come from ACM-IDM, they don't have a name
     // TODO need a better filter, add a boolean to model maybe ?
-    const organizationsPromise = this.store.query('organization', {
+    const organizations = await this.store.query('organization', {
       page: {
         size: 200,
       },
-    }).then((organizations) => organizations.filter((org) => org.name));
-
-    this.isViaCouncilOfMinisters = await isViaCouncilOfMinisters;
-    this.organizations = await organizationsPromise;
+    });
+    this.organizations = organizations.filter((org) => org.name);
   }
 
   setupController(controller) {
