1617f69d6e4e570b9946770bf57c5caa1b7fb33b
Karel Persoons <karel@brenner.company>
2021-06-16 22:12:19 +0200

    Improve modifier styling on styleguide samples

--- app/components/styleguide-sample.js
+++ app/components/styleguide-sample.js
@@ -1,13 +1,17 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { isPresent } from '@ember/utils';
 
 export default class StyleguideSample extends Component {
   @tracked visibility = true;
   // code open or closed on the sample level
 
-  get getModalCssClass() {
-    return this.args.modal;
+  get getModifierClass() {
+    if (isPresent(this.args.modifier)) {
+      return `br-styleguide-sample__sample--${this.args.modifier}`;
+    }
+    return '';
   }
 
   @action
