217205ef09ae8cd5fa1afe219d7175ab152fac5e
MathiasZabot <mathias.zabot@gmail.com>
2021-06-18 11:29:25 +0200

    KAS-2654 cleanup system alerts

--- cypress/selectors/settings.selectors.js
+++ cypress/selectors/settings.selectors.js
@@ -43,6 +43,26 @@ const selectors = {
     },
   },
 
+  // route settings/system-alerts/index/template
+  systemAlertsIndex: {
+    add: '[data-test-route-settings-system-alerts-index-add]',
+    remove: '[data-test-route-settings-system-alerts-index-remove]',
+    // TODO unused selector (remove?)
+    edit: '[data-test-route-settings-system-alerts-index-edit]',
+  },
+
+  // component system-alert-form
+  systemAlertForm: {
+    title: '[data-test-system-alert-form-title] input',
+    message: '[data-test-system-alert-form-message] textarea',
+    // TODO unused selectors (remove?)
+    fromDate: '[data-test-system-alert-form-from-date] input',
+    toDate: '[data-test-system-alert-form-to-date] input',
+  },
+
+  // component system-alert
+  systemAlert: '[data-test-system-alert]',
+
   // component vl-delete-user
   vlDeleteUser: {
     delete: '[data-test-vl-delete-user]',
