72ae8089ba2f4bec912e2549310c52d7a4c4dc6a
Flor Sanders <me@florsanders.be>
2021-06-21 12:27:44 +0200

    [fix] Case edit shortTitle changes property upon save

--- app/components/cases/edit-case.js
+++ app/components/cases/edit-case.js
@@ -1,9 +1,20 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 import { task } from 'ember-concurrency-decorators';
 
 export default class EditCaseComponent extends Component {
+  @tracked shortTitle;
+
+  @action
+  onChange() {
+    console.log('Update');
+    this.shortTitle = this.args.caseToEdit.shortTitle;
+  }
+
   @task
   *editCaseTask() {
+    this.args.caseToEdit.shortTitle = this.shortTitle;
     yield this.args.caseToEdit.save();
     this.args.onClose();
   }
