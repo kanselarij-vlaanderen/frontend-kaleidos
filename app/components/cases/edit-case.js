9c6537ad6da381076f86dc76f46b52fc1f3eccb0
Flor Sanders <me@florsanders.be>
2021-06-21 12:08:43 +0200

    [refactor] Edit case action to concurrency task

--- app/components/cases/edit-case.js
+++ app/components/cases/edit-case.js
@@ -1,15 +1,10 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
+import { task } from 'ember-concurrency-decorators';
 
 export default class EditCaseComponent extends Component {
-  @tracked isLoading = false;
-
-  @action
-  async saveChanges() {
-    this.isLoading = true;
-    await this.args.caseToEdit.save();
+  @task
+  *editCaseTask() {
+    yield this.args.caseToEdit.save();
     this.args.onClose();
-    this.isLoading = false;
   }
 }
