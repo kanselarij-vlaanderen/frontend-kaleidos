978d406d0045844d16f27535045cae29d26f2e70
ValenberghsSven <sven.valenberghs1@gmail.com>
2021-06-21 13:08:48 +0200

    KAS-2681 meeting-actions part 1

--- cypress/integration/unit/meeting-actions.spec.js
+++ cypress/integration/unit/meeting-actions.spec.js
@@ -1,6 +1,8 @@
 /* global context, it, cy,beforeEach, afterEach */
 // / <reference types="Cypress" />
+
 import agenda from '../../selectors/agenda.selectors';
+import cases from '../../selectors/case.selectors';
 import utils from '../../selectors/utils.selectors';
 
 context('meeting actions tests', () => {
@@ -12,51 +14,50 @@ context('meeting actions tests', () => {
     cy.logout();
   });
 
+  // TODO-agendaHeader
+
   it('should perform action delete agenda with agendaitems on designagenda', () => {
     cy.login('Admin');
-    cy.visit('/vergadering/5EB287CDF359DD0009000008/agenda/c7b045ce-3976-459c-aeaa-d0e8597b96d8/agendapunten');
-    cy.deleteAgenda('5EB287CDF359DD0009000008');
-
-    // Verify subcase is still ok on agenda A after delete designagenda
+    cy.visitAgendaWithLink('/vergadering/5EB287CDF359DD0009000008/agenda/c7b045ce-3976-459c-aeaa-d0e8597b96d8/agendapunten');
+    // verify agenda B has agendaitem
     cy.openDetailOfAgendaitem('Cypress test: delete agenda - 1588758436');
-    cy.contains('Naar procedurestap', {
-      timeout: 12000,
-    });
-
-    // Verify delete agenda A works
+    cy.deleteAgenda('5EB287CDF359DD0009000008');
+    // verify agendaitem is still ok on agenda A after delete designagenda
+    cy.openDetailOfAgendaitem('Cypress test: delete agenda - 1588758436');
+    // verify delete agenda A works
     cy.deleteAgenda('5EB287CDF359DD0009000008', true);
+    // verify subcase can be proposed for different agenda
+    cy.visit('/dossiers/5EB287A9F359DD0009000005/deeldossiers/5EB287BBF359DD0009000007/overzicht');
+    cy.get(cases.subcaseHeader.actions.proposeForAgenda);
   });
 
   it('should perform action close agenda with agendaitems on designagenda', () => {
     cy.login('Admin');
-    const SubcaseTitleShort = 'Cypress test: close agenda - 1588775338';
+    const subcaseTitleShort = 'Cypress test: close agenda - 1588775338';
     cy.visitAgendaWithLink('/vergadering/5EB2C9CBF5E1260009000005/agenda/74e87383-80a4-4616-8814-5883cafa6da0/agendapunten');
-    cy.openDetailOfAgendaitem(SubcaseTitleShort);
-    cy.contains('Naar procedurestap', {
-      timeout: 12000,
-    });
-
+    // verify agenda B has agendaitem
+    cy.openDetailOfAgendaitem(subcaseTitleShort);
+    // verify agenda A has same agendaitem
     cy.changeSelectedAgenda('Agenda A');
-    cy.openDetailOfAgendaitem(SubcaseTitleShort);
-    cy.contains('Naar procedurestap', {
-      timeout: 12000,
-    });
-    cy.wait(1000);
+    cy.openDetailOfAgendaitem(subcaseTitleShort);
+    // close agenda
     cy.changeSelectedAgenda('Ontwerpagenda');
     cy.closeAgenda();
-
-    cy.openDetailOfAgendaitem(SubcaseTitleShort);
-    cy.contains('Naar procedurestap', {
-      timeout: 12000,
-    });
+    // verify agenda A still has agendaitem (no links broken)
+    cy.openDetailOfAgendaitem(subcaseTitleShort);
+    // go to subcase and back to agenda, verify links are ok
+    cy.get(agenda.agendaitemTitlesView.linkToSubcase).click();
+    cy.get(cases.subcaseDescription.agendaLink).click();
+    // checking url would be better, but it changes xx seconds after clicking the link and can fail because of it
+    cy.get(agenda.agendaitemTitlesView.linkToSubcase);
   });
 
   it('should not be able to delete approved agendaitem from designagenda with profile: Kanselarij', () => {
-    const SubcaseTitleShort = 'Cypress test: delete approve agenda Kanselarij - 1588775768';
+    const subcaseTitleShort = 'Cypress test: delete approve agenda Kanselarij - 1588775768';
     cy.login('Kanselarij');
     cy.visitAgendaWithLink('/vergadering/5EB2CB8FF5E126000900000D/agenda/cd6b8ae7-0f00-451c-b4ad-fa236d5e6a20/agendapunten');
-
-    cy.openDetailOfAgendaitem(SubcaseTitleShort);
+    cy.openDetailOfAgendaitem(subcaseTitleShort);
+    // verify this profile does not have the option to delete approved agendaitems
     cy.get(agenda.agendaitemControls.actions).click();
     cy.get(agenda.agendaitemControls.action.delete).should('not.exist');
   });
@@ -65,7 +66,7 @@ context('meeting actions tests', () => {
     cy.login('Admin');
     const SubcaseTitleShort = 'Cypress test: delete approve agenda Admin - 1588776224';
     cy.visitAgendaWithLink('/vergadering/5EB2CD4EF5E1260009000015/agenda/9da67561-a827-47a2-8f58-8b3fd5739df4/agendapunten');
-    cy.agendaitemExists(SubcaseTitleShort); // this makes sure the page is reloaded after approving the agenda
+    cy.agendaitemExists(SubcaseTitleShort);
     // Verify agendaitem exists and has subcase on design agenda and agenda A
     cy.changeSelectedAgenda('Agenda A');
     cy.openDetailOfAgendaitem(SubcaseTitleShort);
