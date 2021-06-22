9f89fa921cfcb2a99459658e4e8bc17982c3e33a
Karel Persoons <karel@brenner.company>
2021-06-22 21:38:34 +0200

    Refactor static buttons to octane components (part 1)

--- app/components/auk/button.js
+++ app/components/auk/button.js
@@ -1,4 +1,6 @@
 import Component from '@glimmer/component';
+import { computed } from '@ember/object';
+import { assert } from '@ember/debug';
 
 /**
  *
@@ -10,6 +12,26 @@ import Component from '@glimmer/component';
  * @argument block {Boolean}
  */
 export default class Button extends Component {
+  // Workaround for linkTo not accepting @model and @models parameter, regardless if one is null
+  // https://github.com/emberjs/ember.js/issues/18265
+  @computed('args.{model,models}')
+  get models() {
+    const hasModel = 'model' in this.args;
+    const hasModels = 'models' in this.args;
+
+    assert(
+      'You cannot provide both the `@model` and `@models` arguments.',
+      !hasModel || !hasModels
+    );
+
+    if (hasModel) {
+      return [this.args.model];
+    } else if (hasModels) {
+      return this.args.models;
+    }
+    return [];
+  }
+
   get skin() {
     if (this.args.skin) {
       return `auk-button--${this.args.skin}`;
