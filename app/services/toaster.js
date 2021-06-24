06d3ea8ca38c710e53959ad6878912a76c0d9d2c
Michaël Dierick <michael.dierick@redpencil.io>
2021-06-24 12:06:21 +0200

    document the reason for faster timeout than expected

--- app/services/toaster.js
+++ app/services/toaster.js
@@ -13,6 +13,7 @@ export default class ToasterService extends Service {
   @(task(function *(toast) {
     toast.options.onClose = toast.options.onClose || (() => this.toasts.removeObject(toast));
     this.toasts.pushObject(toast);
+    // TODO: At first glance one might think the below timeout doesn't work properly after 5 seconds. This is caused by following CSS-animation: https://github.com/kanselarij-vlaanderen/au-kaleidos-css/blob/766b9b410fae626988fc7bf0542437889c4e94b7/_auk-alert-stack.scss#L24
     yield timeout(toast.options.timeOut);
     if (this.toasts.includes(toast)) {
       this.toasts.removeObject(toast);
