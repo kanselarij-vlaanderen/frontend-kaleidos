78802c13b74be59fd165374c8b1de7f01c112767
MathiasZabot <mathias.zabot@gmail.com>
2021-06-24 16:45:18 +0200

    KAS-2693 cleanup CSS checkbox and route kort bestek

--- cypress/integration/unit/NewsletterInfo/shouldSwitchTheFinishedSwitch.spec.js
+++ cypress/integration/unit/NewsletterInfo/shouldSwitchTheFinishedSwitch.spec.js
@@ -1,6 +1,9 @@
 /* global context, it, cy, before, beforeEach */
 // / <reference types="Cypress" />
 
+import route from '../../../selectors/route.selectors';
+import utils from '../../../selectors/utils.selectors';
+
 context('NewsletterInfo: Switching the finished switch', () => {
   before(() => {
     cy.resetCache();
@@ -15,7 +18,7 @@ context('NewsletterInfo: Switching the finished switch', () => {
   const goToKortBestek = () => {
     cy.log('going to kort bestek');
     cy.visit('/vergadering/5EBA9588751CF70008000012/kort-bestek');
-    cy.get('table > tbody', {
+    cy.get(route.newsletter.dataTable, {
       timeout: 20000,
     });
     cy.log('went to kort bestek');
@@ -24,25 +27,22 @@ context('NewsletterInfo: Switching the finished switch', () => {
   it('Should check the box "in kort bestek"', () => {
     cy.route('PATCH', '/newsletter-infos/*').as('patchNewsletterInfo');
     goToKortBestek();
-    cy.get('.auk-checkbox').last()
-      .scrollIntoView();
-    cy.get('.auk-checkbox').last()
-      .click()
-      .wait('@patchNewsletterInfo');
+    cy.get(utils.vlCheckbox.label).eq(0)
+      .scrollIntoView()
+      .click();
+    cy.get('@patchNewsletterInfo');
   });
 
   it('Should check the box "in kort bestek" and uncheck it afterward', () => {
     cy.route('PATCH', '/newsletter-infos/*').as('patchNewsletterInfo');
     goToKortBestek();
-    cy.get('.auk-checkbox').last()
-      .scrollIntoView();
-    cy.get('.auk-checkbox').last()
-      .click()
-      .wait('@patchNewsletterInfo');
-    cy.get('.auk-checkbox').last()
-      .scrollIntoView();
-    cy.get('.auk-checkbox').last()
-      .click()
-      .wait('@patchNewsletterInfo');
+    cy.get(utils.vlCheckbox.label).eq(0)
+      .scrollIntoView()
+      .click();
+    cy.get('@patchNewsletterInfo');
+    cy.get(utils.vlCheckbox.label).eq(0)
+      .scrollIntoView()
+      .click();
+    cy.get('@patchNewsletterInfo');
   });
 });
