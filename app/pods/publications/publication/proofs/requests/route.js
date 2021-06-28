bc4866267266e35c5424ab3d930a8cdd2f9195e7
mdv-professional <mdv.werk@outlook.com>
2021-06-28 10:07:35 +0200

    KAS-2477 (busy) model | requests tab [pub proof]

--- app/pods/publications/publication/proofs/requests/route.js
+++ app/pods/publications/publication/proofs/requests/route.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default class PublicationsPublicationProofsRequestsRoute extends Route {
+  model() {
+    const publicationSubcase = this.modelFor('publications.publication.proofs');
+    this.store.query('request-activity', {
+      'filter[publication-subcase][:id:]': publicationSubcase.id,
+      include: 'email',
+    });
+  }
 }
