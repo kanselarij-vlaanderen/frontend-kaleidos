ab845568f4ad33dd86ccd06779760d4dd5650e64
mdv-professional <mdv.werk@outlook.com>
2021-06-28 14:26:16 +0200

    KAS-2477 fine-tuning | requests tab [pub proof]

--- app/pods/publications/publication/proofs/requests/controller.js
+++ app/pods/publications/publication/proofs/requests/controller.js
@@ -2,38 +2,45 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import moment from 'moment';
+import ProofingActivity from 'frontend-kaleidos/models/proofing-activity';
+import PublicationActivity from 'frontend-kaleidos/models/publication-activity';
 
 class Row {
   intl;
-  @tracked requestActivity;
+
+  requestActivity;
+  // resolved relationships to prevent await in getter (await requestActivity.proofingActivity)
+  proofingActivity;
+  publicationActivity;
+
   @tracked pieces;
 
   constructor(params) {
     Object.assign(this, params);
   }
 
-
   get targetActivityType() {
-    if (this.requestActivity.proofingActivity) {
-      return 'proofing-activity';
-    } else if (this.requestActivity.publicationActivity) {
-      return 'publication-activity';
+    if (this.proofingActivity) {
+      return ProofingActivity.modelName;
+    } else if (this.publicationActivity) {
+      return this.publicationActivity.modelName;
     }
     throw new Error('unknown request');
   }
 
   get panelTitle() {
-    let requestType;
+    let requestTypeTranslationKey;
+    // testing with `instanceof` resolves true for every Model
     switch (this.targetActivityType) {
-      case 'proofing-activity':
-        requestType = 'proofing-request';
+      case ProofingActivity.modelName:
+        requestTypeTranslationKey = 'proofing-request';
         break;
-      case 'publication-activity':
-        requestType = 'publication-request';
+      case PublicationActivity.modelName:
+        requestTypeTranslationKey = 'publication-request';
         break;
       default: throw new Error('unknown request');
     }
-    const requestName = this.intl.t(requestType);
+    const requestName = this.intl.t(requestTypeTranslationKey);
 
     const dateString = moment(this.requestActivity.startDate).format('D MMMM');
     const timeString = moment(this.requestActivity.startDate).format('H:mm');
@@ -54,24 +61,27 @@ export default class PublicationsPublicationProofsRequestsController extends Con
 
   async initRows(model) {
     this.rows = await Promise.all(model.map(async(requestActivity) => {
+      const [proofingActivity, publicationActivity] = await Promise.all([
+        requestActivity.proofingActivity,
+        requestActivity.publicationActivity
+      ]);
+
+      let pieces = [
+        ...requestActivity.usedPieces.toArray(),
+        ...(proofingActivity?.generatedPieces.toArray() ?? []),
+        ...(publicationActivity?.generatedPieces.toArray() ?? [])
+      ];
+      pieces = pieces.flat(Number.POSITIVE_INFINITY);
+      pieces.sortBy('created');
+
       const row = new Row({
         intl: this.intl,
         requestActivity: requestActivity,
-        pieces: await this.#concatPieces(requestActivity),
+        proofingActivity: proofingActivity,
+        publicationActivity: publicationActivity,
+        pieces: pieces,
       });
       return row;
     }));
-  }
-
-  async #concatPieces(requestActivity) {
-    let pieces = [
-      ...requestActivity.usedPieces.toArray(),
-      ...((await requestActivity.proofingActivity)?.generatedPieces.toArray() ?? []),
-      ...((await requestActivity.publicationActivity)?.generatedPieces.toArray() ?? [])
-    ];
-    pieces = pieces.flat(Number.POSITIVE_INFINITY);
-    console.log('pieces', pieces);
-    pieces.sortBy('created');
-    return pieces;
   }
 }
