3047caabf929f058d594c39bfe7eeb0cca6131bd
Tanee Grade <tanee.grade@cereus.be>
2021-06-29 14:13:46 +0200

    KAS-2497 remove legacy code + add default from

--- app/models/email-notification-setting.js
+++ app/models/email-notification-setting.js
@@ -3,4 +3,5 @@ import Model, { attr } from '@ember-data/model';
 export default class EmailNotificationSetting extends Model {
   @attr('string') translationRequestToEmail;
   @attr('string') proofRequestToEmail;
+  @attr('string') defaultFromEmail;
 }
--- app/pods/publications/publication/translations/documents/controller.js
+++ app/pods/publications/publication/translations/documents/controller.js
@@ -4,7 +4,6 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { PUBLICATION_EMAIL } from 'frontend-kaleidos/config/config';
 import CONSTANTS from 'frontend-kaleidos/config/constants';
-import CONFIG from 'frontend-kaleidos/utils/config';
 import { inject as service } from '@ember/service';
 
 export default class PublicationsPublicationTranslationsDocumentController extends Controller {
@@ -101,12 +100,12 @@ export default class PublicationsPublicationTranslationsDocumentController exten
     const files = yield Promise.all(filePromises);
 
     const folder = yield this.store.findRecordByUri('mail-folder', PUBLICATION_EMAIL.OUTBOX);
-    const mailTo =  this.store.queryOne('email-notification-setting', {
+    const mailSettings =  this.store.queryOne('email-notification-setting', {
       'page[size]': 1,
     });
     const mail = yield this.store.createRecord('email', {
-      to: mailTo.translationRequestToEmail,
-      from: CONFIG.EMAIL.DEFAULT_FROM,
+      to: mailSettings.translationRequestToEmail,
+      from: mailSettings.defaultFromEmail,
       folder: folder,
       attachments: files,
       requestActivity: requestActivity,
