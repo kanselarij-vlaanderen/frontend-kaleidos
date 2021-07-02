f4dc6722ad89b15130c850744b4eab2cdeec2062
mdv-professional <mdv.werk@outlook.com>
2021-07-02 13:26:28 +0200

    KAS-2477 simplify                                 | requests tab [pub proof]

--- app/pods/publications/publication/proofs/requests/controller.js
+++ app/pods/publications/publication/proofs/requests/controller.js
@@ -1,73 +1,60 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import ProofingActivity from 'frontend-kaleidos/models/proofing-activity';
-import PublicationActivity from 'frontend-kaleidos/models/publication-activity';
 
 class Row {
-  intl;
-
   requestActivity;
   // resolved relationships to prevent await in getter (await requestActivity.proofingActivity)
   proofingActivity;
   publicationActivity;
 
-  @tracked pieces;
-
   constructor(params) {
     Object.assign(this, params);
   }
 
-  get targetActivityType() {
+  get requestTypeTranslationKey() {
     if (this.proofingActivity) {
-      return ProofingActivity.modelName;
+      return 'proofing-request';
     } else if (this.publicationActivity) {
-      return PublicationActivity.modelName;
+      return 'publication-request';
     }
     throw new Error('unknown request');
   }
 
-  get requestType() {
-    // testing with `instanceof` returns true for every Model type
-    switch (this.targetActivityType) {
-      case ProofingActivity.modelName:
-        return 'proofing-request';
-      case PublicationActivity.modelName:
-        return 'publication-request';
-      default: throw new Error('unknown request');
+  get attachments() {
+    return this.requestActivity.usedPieces.sortBy('created');
+  }
+
+  get resultActivity() {
+    if (this.proofingActivity) {
+      return this.proofingActivity;
+    } else if (this.publicationActivity) {
+      return this.publicationActivity;
     }
+    throw new Error('unknown activity');
+  }
+
+  get receivedPieces() {
+    return this.resultActivity.generatedPieces.sortBy('created');
   }
 }
 
 export default class PublicationsPublicationProofsRequestsController extends Controller {
-  @service intl;
-
   @tracked rows;
 
   async initRows(model) {
-    this.rows = await Promise.all(model.map(async(requestActivity) => {
-      console.log('req', requestActivity);
-      const [proofingActivity, publicationActivity] = await Promise.all([
-        requestActivity.proofingActivity,
-        requestActivity.publicationActivity
-      ]);
+    this.rows = await Promise.all(model.map(this.#createRow));
+  }
 
-      let pieces = [
-        ...requestActivity.usedPieces.toArray(),
-        ...(proofingActivity?.generatedPieces.toArray() ?? []),
-        ...(publicationActivity?.generatedPieces.toArray() ?? [])
-      ];
-      pieces = pieces.flat(Number.POSITIVE_INFINITY);
-      pieces.sortBy('created');
+  async #createRow(requestActivity) {
+    const [proofingActivity, publicationActivity] = await Promise.all([
+      requestActivity.proofingActivity,
+      requestActivity.publicationActivity
+    ]);
 
-      const row = new Row({
-        intl: this.intl,
-        requestActivity: requestActivity,
-        proofingActivity: proofingActivity,
-        publicationActivity: publicationActivity,
-        pieces: pieces,
-      });
-      return row;
-    }));
+    return new Row({
+      requestActivity: requestActivity,
+      proofingActivity: proofingActivity,
+      publicationActivity: publicationActivity,
+    });
   }
 }
