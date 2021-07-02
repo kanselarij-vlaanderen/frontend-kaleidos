9a1be7095df7ac82b7963e664b4e4e27499b2a70
mdv-professional <mdv.werk@outlook.com>
2021-07-02 14:56:07 +0200

    KAS-2604 (busy)                                    | correction upload modal [pub proof]

---
+++ app/helpers/auth.js
@@ -0,0 +1,10 @@
+import Helper from '@ember/component/helper';
+import { inject as service } from '@ember/service';
+
+export default class AuthHelper extends Helper {
+  @service currentSession;
+
+  // compute(groups) {
+
+  // }
+}
