06da4f855fa2efafbfbeffa1496bc12b127cd4ab
mdv-professional <mdv.werk@outlook.com>
2021-07-05 17:24:52 +0200

    KAS-2475/PR-971 fix sort                          | new request [pub proof]

--- app/pods/publications/publication/translations/documents/route.js
+++ app/pods/publications/publication/translations/documents/route.js
@@ -30,8 +30,8 @@ export default class PublicationsPublicationTranslationsDocumentRoute extends Ro
     pieces = pieces.flatMap((pieces) => pieces.toArray());
     pieces = new Set(pieces); // using set to ensure a collection of unique pieces
     pieces = A([...pieces]);
-    // this.model.sortBy(property) does not work properly
-    // reverse sort on created
+    // this.model.sortBy('created') does not work properly
+    // descending sort on created
     pieces.sort((piece1, piece2) => piece2.created - piece1.created);
     return pieces;
   }
