2684edc1d39b39930e659cd2651d680a246632f8
Karel Persoons <karel@brenner.company>
2021-07-06 18:05:45 +0200

    Refactor to glimmer component

--- app/components/cases/cases-header.js
+++ app/components/cases/cases-header.js
@@ -1,20 +1,20 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class CasesHeader extends Component {
   @service currentSession;
-
-  isAddingCase = false;
+  @tracked isAddingCase = false;
 
   @action
   toggleAddingCase() {
-    this.toggleProperty('isAddingCase');
+    this.isAddingCase = !this.isAddingCase;
   }
 
   @action
   closeAction(caze) {
-    this.toggleProperty('isAddingCase');
-    this.close(caze);
+    this.isAddingCase = !this.isAddingCase;
+    this.args.close(caze);
   }
 }
