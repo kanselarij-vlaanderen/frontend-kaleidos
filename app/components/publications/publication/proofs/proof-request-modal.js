fe58cf332051af608298786f0bd25aa304edc626
mdv-professional <mdv.werk@outlook.com>
2021-07-06 13:07:32 +0200

    KAS-2484/PR-976 fix lint                          | upload modal [pub proofs]

--- app/components/publications/publication/proofs/proof-request-modal.js
+++ app/components/publications/publication/proofs/proof-request-modal.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 import { isBlank } from '@ember/utils';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency-decorators';
