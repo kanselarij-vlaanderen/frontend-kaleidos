d0901ce11bfd71b5a5fc45e751ab7f8e626ff9f7
ValenberghsSven <sven.valenberghs1@gmail.com>
2021-07-06 16:22:33 +0200

    KAS-2694 finalize reopen and agenda spec, add some selectors and a new command

--- cypress/integration/unit/agenda-reopen-previous.spec.js
+++ cypress/integration/unit/agenda-reopen-previous.spec.js
@@ -17,44 +17,57 @@ context('Agenda reopen previous tests', () => {
     cy.logout();
   });
 
-  const designAgendaBTitle = 'Ontwerpagenda B';
+  const designAgendaB = 'Ontwerpagenda B';
+  const approvedagendaA = 'Agenda A';
   const reopenPreviousVersion = 'Vorige versie heropenen';
-  const designAgendaATitle = 'Ontwerpagenda A';
-  const designAgendaCTitle = 'Ontwerpagenda C';
 
   it('should delete current design agenda and reopen previous accepted agenda', () => {
     cy.route('GET', '/agendas/**').as('getAgendas');
-    cy.createAgenda('Elektronische procedure', dateToCreateAgenda, 'Daar');
+    cy.createAgenda('Elektronische procedure', dateToCreateAgenda, 'Reopen previous test');
     cy.openAgendaForDate(dateToCreateAgenda);
     cy.setFormalOkOnItemWithIndex(0);
-    // TODO test existance of the action on design agenda A (not.exist)
+    cy.get(agenda.agendaSideNav.agenda).should('have.length', 1);
+    cy.agendaNameExists('A');
+    // check if action does not exist on design agenda A
+    cy.get(agenda.agendaHeader.showAgendaOptions).click();
+    cy.get(agenda.agendaHeader.agendaActions.reopenPreviousVersion).should('not.exist');
+    cy.get(agenda.agendaHeader.showAgendaOptions).click();
     cy.approveDesignAgenda();
-    cy.contains(designAgendaBTitle).should('exist');
-    cy.contains(designAgendaATitle).should('not.exist');
+    // verify we have 2 agendas, A(approved) and B(design)
+    cy.get(agenda.agendaSideNav.agenda).should('have.length', 2);
+    cy.agendaNameExists('B');
+    cy.agendaNameExists('A', false);
     cy.get(agenda.agendaHeader.showAgendaOptions).click();
     cy.get(agenda.agendaHeader.agendaActions.reopenPreviousVersion).click();
+    // Check the message in the confirm modal
     cy.get(auk.modal.header.title).contains(reopenPreviousVersion);
-    cy.get(auk.alert.message).contains(designAgendaBTitle);
-    cy.get(auk.alert.message).contains('Agenda A');
+    cy.get(auk.alert.message).contains(designAgendaB);
+    cy.get(auk.alert.message).contains(approvedagendaA);
     cy.get(auk.loader).should('not.exist'); // data loading task might be running, disabling the next button
     cy.get(agenda.agendaHeader.confirm.reopenPreviousVersion).contains(reopenPreviousVersion)
       .click();
     cy.wait('@getAgendas');
-    cy.contains(designAgendaBTitle).should('not.exist');
-    cy.contains(designAgendaATitle).should('exist');
-    // TODO test if action does not exist on ontwerpagenda A
-    cy.approveDesignAgenda();
+    cy.get(agenda.agendaSideNav.agenda).should('have.length', 1);
+    cy.agendaNameExists('A');
   });
 
+  // TODO-agendaHeader test existance of all the actions in different agenda status in 1 aggregated test
   it('should not show action menu item Vorige versie heropenen in action menu when not design agenda', () => {
     cy.openAgendaForDate(dateToCreateAgenda);
-    cy.route('GET', '/agendas/**').as('getAgendas');
-    cy.contains(designAgendaBTitle).should('exist');
-    cy.contains(designAgendaATitle).should('not.exist');
     cy.approveDesignAgenda();
-    cy.contains(designAgendaCTitle).should('exist');
+    cy.get(agenda.agendaSideNav.agenda).should('have.length', 2);
+    cy.agendaNameExists('B');
+    cy.agendaNameExists('A', false);
+    cy.approveDesignAgenda();
+    cy.get(agenda.agendaSideNav.agenda).should('have.length', 3);
+    cy.agendaNameExists('C');
+    cy.agendaNameExists('B', false);
+    cy.agendaNameExists('A', false);
     cy.deleteAgenda();
-    cy.contains(designAgendaCTitle).should('not.exist');
+    cy.get(agenda.agendaSideNav.agenda).should('have.length', 2);
+    cy.agendaNameExists('B', false);
+    cy.agendaNameExists('A', false);
+    // Check if action does not exist when there are multiple agendas but no design agenda
     cy.get(agenda.agendaHeader.showAgendaOptions).click();
     cy.get(agenda.agendaHeader.agendaActions.reopenPreviousVersion).should('not.exist');
   });
