3371169722bd8683502fda332b89078afe562c83
mdv-professional <mdv.werk@outlook.com>
2021-07-07 14:53:44 +0200

    KAS-2609 set end date                             | finish [pub proof]

--- app/pods/publications/publication/proofs/controller.js
+++ app/pods/publications/publication/proofs/controller.js
@@ -1,4 +1,19 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
-export default class PublicationPublishPreviewController extends Controller {
+export default class PublicationsPublicationProofController extends Controller {
+  @action
+  async toggleFinishProof(event) {
+    const translationIsFinished = event.target.checked;
+    if (translationIsFinished) {
+      const now = new Date();
+      this.model.endDate = now;
+      this.model.save();
+      this.publicationFlow.closingDate = now;
+      this.model.save();
+    } else {
+      this.model.endDate = null;
+      this.model.save();
+    }
+  }
 }
--- app/pods/publications/publication/proofs/route.js
+++ app/pods/publications/publication/proofs/route.js
@@ -5,4 +5,12 @@ export default class PublicationProofsRoute extends Route {
   model() {
     return this.modelFor('publications.publication').publicationSubcase;
   }
+
+  afterModel() {
+    this.publicationFlow = this.modelFor('publications.publication');
+  }
+
+  setupController(controller) {
+    controller.publicationFlow = this.publicationFlow;
+  }
 }
