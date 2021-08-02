451a408dc760b787401d4e3ee923c0ee9c49396a
Michaël Dierick <michael.dierick@redpencil.io>
2021-08-02 13:59:52 +0200

    add icon-only layout support to dropdown-menu

--- app/components/dropdown-menu.js
+++ app/components/dropdown-menu.js
@@ -7,6 +7,7 @@ import { action } from '@ember/object';
  * @argument {String} placement. See EmberAttacher doc for options
  * @argument {String} skin. See Auk::Button for options
  * @argument {Boolean} disabled. See Auk::Button
+ * @argument {String} icon. Name of the kaleidos-icon in case you want to provide a custom one (mostly used in icon-only settings where no label is provided)
  * @argument {String} label. Label for the trigger button
  */
 export default class DropdownMenuComponent extends Component {
@@ -17,6 +18,9 @@ export default class DropdownMenuComponent extends Component {
   }
 
   get icon() {
+    if (this.args.icon) {
+      return this.args.icon;
+    }
     if (this.placement.startsWith('top')) {
       return 'chevron-up';
     }
