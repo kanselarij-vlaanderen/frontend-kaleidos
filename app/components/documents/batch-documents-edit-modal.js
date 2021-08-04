d7e32a10573ffe1d5c94d054857116f072ab94e3
mdv-professional <mdv.werk@outlook.com>
2021-08-04 11:35:43 +0200

    KAS-2707 batch edit modal (busy)                  | batch modal [doc view]

--- app/components/documents/batch-documents-edit-modal.js
+++ app/components/documents/batch-documents-edit-modal.js
@@ -7,6 +7,8 @@ import { task } from 'ember-concurrency-decorators';
 import { PAGE_SIZE } from 'frontend-kaleidos/config/config';
 
 class Row {
+  piece;
+
   @tracked name;
   @tracked documentType;
   @tracked accessLevel;
@@ -36,10 +38,16 @@ export default class BatchDocumentsModal extends Component {
     return this.initRows.isRunning || this.loadData.isRunning;
   }
 
+  get isSaveDisabled() {
+    return this.isLoading || this.save.isRunning;
+  }
+
   @task
   *initRows() {
     this.rows = yield Promise.all(this.args.pieces.map(async(piece) => {
       const row = new Row();
+      row.piece = piece;
+
       row.name = piece.name;
       const docContainer = await piece.documentContainer;
       row.documentType = docContainer.type;
@@ -57,6 +65,7 @@ export default class BatchDocumentsModal extends Component {
     ]);
   }
 
+  // TODO: delete
   @task
   *loadDocumentTypes() {
     return yield this.queryDocumentTypes.perform();
@@ -97,24 +106,6 @@ export default class BatchDocumentsModal extends Component {
     return yield this.store.query('access-level', query);
   }
 
-  @task
-  *searchDocumentTypes(searchTerm) {
-    yield timeout(300);
-    if (!searchTerm) {
-      return this.loadDocumentTypes.lastValue;
-    }
-    return yield this.queryDocumentTypes.perform(searchTerm);
-  }
-
-  @task
-  *searchAccessLevels(searchTerm) {
-    yield timeout(300);
-    if (!searchTerm) {
-      return this.loadAccessLevels.lastValue;
-    }
-    return yield this.queryAccessLevels.perform(searchTerm);
-  }
-
   get areAllSelected() {
     return this.rows.length === this.selectedRows.length;
   }
@@ -138,13 +129,118 @@ export default class BatchDocumentsModal extends Component {
     }
   }
 
+  get batchDocumentType() {
+    return this.getBatchSelectedValue((row) => row.documentType);
+  }
+
+  get batchAccessLevel() {
+    return this.getBatchSelectedValue((row) => row.accessLevel);
+  }
+
+  getBatchSelectedValue(getProperty) {
+    if (this.selectedRows.length === 0) {
+      return undefined;
+    }
+    const [firstRow, ...otherRows] = this.selectedRows;
+    const firstProperty = getProperty(firstRow);
+    const areRowsEqual = otherRows.every((row) => getProperty(row) === firstProperty);
+    if (!areRowsEqual) {
+      return undefined;
+    }
+    return firstProperty;
+  }
+
+  get batchIsToBeDeleted() {
+    return this.selectedRows.some((row) => row.isToBeDeleted);
+  }
+
+  @task
+  *searchDocumentTypes(searchTerm) {
+    yield timeout(300);
+    if (!searchTerm) {
+      return this.documentTypes;
+    }
+    return yield this.queryDocumentTypes.perform(searchTerm);
+  }
+
   @action
   setDocumentType(row, documentType) {
     row.documentType = documentType;
   }
 
+  @task
+  *searchAccessLevels(searchTerm) {
+    yield timeout(300);
+    if (!searchTerm) {
+      return this.accessLevels;
+    }
+    return yield this.queryAccessLevels.perform(searchTerm);
+  }
+
   @action
-  setAccessLevel(row, documentType) {
-    row.accessLevel = documentType;
+  setAccessLevel(rows, accessLevel) {
+    // // allow call with row as parameter
+    // if (!Array.isArray(rows)) {
+    //   rows = [rows];
+    // }
+
+    for (const row of rows) {
+      row.accessLevel = accessLevel;
+    }
+  }
+
+  @action
+  setConfidential(rows, event) {
+    // // allow call with row as parameter
+    // if (!Array.isArray(rows)) {
+    //   rows = [rows];
+    // }
+
+    const checked = event.target.checked;
+    for (const row of rows) {
+      row.confidential = checked;
+    }
+  }
+
+  @action
+  setToBeDeleted(rows, isToBeDeleted) {
+    // // allow call with row as parameter
+    // if (!Array.isArray(rows)) {
+    //   rows = [rows];
+    // }
+
+    for (const row of rows) {
+      row.isToBeDeleted = isToBeDeleted;
+    }
+  }
+
+  @task
+  *save() {
+    const saves = this.rows.map(async(row) => {
+      const piece = row.piece;
+      if (row.isToBeDeleted) {
+        piece.destroyRecord();
+        piece.file.destroyRecord();
+        // TODO: documentContainer destroy
+        // and use fileService
+      } else {
+        piece.name = row.name;
+        piece.confidential = row.confidential;
+        // does not check for relationship changes
+        let hasChanged = piece.dirtyType === 'updated';
+        if (piece.documentType !== row.documentType) {
+          hasChanged = true;
+          piece.documentType = row.documentType;
+        }
+        if (piece.accessLevel !== row.accessLevel) {
+          hasChanged = true;
+          piece.accessLevel = row.accessLevel;
+        }
+        if (hasChanged) {
+          await piece.save();
+        }
+      }
+    });
+    yield Promise.all(saves);
   }
 }
