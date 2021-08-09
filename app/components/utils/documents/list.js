5df43f826cec0acb77a94d876f47776178e4175e
Michaël Dierick <michael.dierick@redpencil.io>
2021-08-09 13:55:46 +0200

    resolve no-empty-backing-classes for glimmer components

--- app/components/utils/documents/list.js
+++ app/components/utils/documents/list.js
@@ -1,5 +1,8 @@
+/* eslint-disable ember/no-empty-glimmer-component-classes */
 import Component from '@glimmer/component';
 
-/** @argument {Piece[]} pieces 'piece,piece.file' */
+/**
+ * @argument {Piece[]} pieces 'piece,piece.file'
+ */
 export default class UtilsDocumentsListComponent extends Component {
 }
