a46a7f5b60282925bc4a8f2cdc00785ae5fb83a1
Michaël Dierick <michael.dierick@redpencil.io>
2021-08-10 08:42:24 +0200

    remove unnecessary get/set + instant refresh + whitespace format

--- app/pods/settings/users/index/controller.js
+++ app/pods/settings/users/index/controller.js
@@ -1,29 +1,24 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { later } from '@ember/runloop';
 import { isEmpty } from '@ember/utils';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class UsersSettingsController extends Controller {
-  @service intl;
+  queryParams = ['filter'];
 
+  @service intl;
   @service toaster;
 
   sizeOptions = Object.freeze([5, 10, 20, 50, 100, 200]);
-
   isUploadingFile = null;
 
   @tracked size = 10;
-
   @tracked page = 0;
-
-  queryParams = ['filter'];
-
   @tracked filterText = '';
 
   get filter() {
-    const searchText = this.get('filterText');
+    const searchText = this.filterText;
     if (isEmpty(searchText)) {
       return null;
     }
@@ -42,17 +37,14 @@ export default class UsersSettingsController extends Controller {
 
   @action
   showFileUploader() {
-    this.toggleProperty('isUploadingFile');
+    this.isUploadingFile = !this.isUploadingFile;
   }
 
   @action
   uploaded(response) {
     if (response && response.status === 200) {
       this.toaster.success(this.intl.t('import-users-success'), this.intl.t('successfully-created-title'));
-      later(() => {
-        this.send('refresh');
-        this.set('shouldRefreshTableModel', true);
-      }, 2000);
+      this.send('refresh');
     } else {
       this.toaster.error(this.intl.t('error'), this.intl.t('warning-title'));
     }
