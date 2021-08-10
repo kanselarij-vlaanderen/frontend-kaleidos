20a3075872e901177313884402e668a18abb8016
Tanee Grade <tanee.grade@cereus.be>
2021-08-10 11:40:04 +0200

    KAS-2805 Added empty state & configurable emptymessage

--- app/components/utils/documents/list.js
+++ app/components/utils/documents/list.js
@@ -1,5 +1,11 @@
 import Component from '@glimmer/component';
-
+import { inject as service } from '@ember/service';
+import { isBlank } from '@ember/utils';
 /** @argument {Piece[]} pieces 'piece,piece.file' */
 export default class UtilsDocumentsListComponent extends Component {
+  @service intl;
+
+  get emptyMessage() {
+    return !isBlank(this.args.emptyMessage) ? this.args.emptyMessage : this.intl.t('no-documents-available');
+  }
 }
