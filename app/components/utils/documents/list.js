ef98f6a7d6dcbad9e349e7d1ba86d443ddad1b52
ValenberghsSven <sven.valenberghs1@gmail.com>
2021-08-11 14:49:04 +0200

    KAS-2805 add argument

--- app/components/utils/documents/list.js
+++ app/components/utils/documents/list.js
@@ -1,7 +1,10 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { isBlank } from '@ember/utils';
-/** @argument {Piece[]} pieces 'piece,piece.file' */
+/**
+ * @argument {Piece[]} pieces 'piece,piece.file'
+ * @argument {String} emptyMessage message to show when there are no pieces, defaults to no documents message
+ * */
 export default class UtilsDocumentsListComponent extends Component {
   @service intl;
 
