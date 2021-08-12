88010be5d15fe1e69e29497f13bb7e958db5df39
Michaël Dierick <michael.dierick@redpencil.io>
2021-08-12 15:18:30 +0200

    disable mandatee-editing features since they break data

--- app/pods/settings/ministers/controller.js
+++ app/pods/settings/ministers/controller.js
@@ -1,11 +1,19 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { task } from 'ember-concurrency';
-import { isPresent } from '@ember/utils';
+// import { task } from 'ember-concurrency';
+// import { isPresent } from '@ember/utils';
 
 export default Controller.extend({
   currentSession: service(),
 
+  /*
+  This route is currently a read-only-feature.
+  The original minister-management feature wasn't designed with the full
+  complexity of the mandatees/government-body model in mind. Since the edit-features
+  as-is would break data, they are currently disabled awaiting re-design.
+  For more info on the mandatee-model, see https://themis-test.vlaanderen.be/docs/catalogs
+  */
+  /*
   isEditingMandatee: false,
   isAddingMandatee: false,
   isResigningMandatee: false,
@@ -57,4 +65,5 @@ export default Controller.extend({
       this.send('refreshRoute');
     },
   },
+  */
 });
