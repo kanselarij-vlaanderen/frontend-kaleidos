0f38403015b9d5dadaf79986c75e54cbfe678e3d
Tanee Grade <tanee.grade@cereus.be>
2021-08-20 09:00:27 +0200

    KAS-2834 transition in before model + asyncs

--- app/pods/document/route.js
+++ app/pods/document/route.js
@@ -8,7 +8,7 @@ export default class DocumentRoute extends Route {
     this.simpleAuthSession.requireAuthentication(transition, 'login');
   }
 
-  async model(params) {
+  model(params) {
     return this.store.queryOne('piece', {
       'filter[:id:]': params.piece_id,
       include: 'file',
