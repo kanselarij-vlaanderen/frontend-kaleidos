27c2fe51f3d5f7ae7c1c34909ba18f8edd6bafe6
Michaël Dierick <michael.dierick@redpencil.io>
2021-08-20 16:26:34 +0200

    Fix helper wrongfully interpreted as component

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -6,6 +6,9 @@ module.exports = {
   rules: {
     // We like to handle the most common branch in the first clause, which is
     // why the first clause has a negated condition some times.
-    'no-negated-condition': 'off'
-  }
+    'no-negated-condition': 'off',
+    'no-curly-component-invocation': {
+      allow: ['subcase-timeline-item-text'], // a helper
+    },
+  },
 };
