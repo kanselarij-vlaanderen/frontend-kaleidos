0f2c48f67bd659c1c19b156cc6342afc708361d3
ValenberghsSven <sven.valenberghs1@gmail.com>
2021-08-25 17:01:50 +0200

    KAS-2839 comment possible values, remove default type=text

--- app/components/auk/input.js
+++ app/components/auk/input.js
@@ -4,7 +4,9 @@ import Component from '@glimmer/component';
  * Simple wrapper for Ember's <Input> component. Takes all arguments that <Input> takes.
  *
  * @argument {String} icon
+ * @argument {String} type: Possible values are ("text", default), "number". Consider other components for "radio", "checkbox", etc..
  * @argument {Boolean} block
+ * @argument {Boolean} width: Possible values are "small, "medium", "large"
  * @argument {Boolean} error
  */
 export default class Input extends Component {
@@ -12,7 +14,6 @@ export default class Input extends Component {
     if (this.args.block) {
       return 'auk-input--block';
     }
-
     return null;
   }
 
@@ -20,7 +21,6 @@ export default class Input extends Component {
     if (this.args.width) {
       return `auk-input--w-${this.args.width}`;
     }
-
     return null;
   }
 
@@ -28,7 +28,6 @@ export default class Input extends Component {
     if (this.args.error) {
       return 'auk-input--error';
     }
-
     return null;
   }
 }
