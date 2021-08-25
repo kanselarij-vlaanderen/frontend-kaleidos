0d387f6b98693770a572052ce1d385a8fbb84175
Tanee Grade <tanee.grade@cereus.be>
2021-08-25 09:54:39 +0200

    KAS-2705 delete version

--- app/components/documents/document-preview-sidebar.js
+++ app/components/documents/document-preview-sidebar.js
@@ -2,6 +2,8 @@ import Component from '@glimmer/component';
 import { task } from 'ember-concurrency-decorators';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
 
 /**
  *
@@ -11,12 +13,16 @@ import { action } from '@ember/object';
  * - "versions"
  */
 export default class DocumentsDocumentPreviewSidebar extends Component {
+  @service fileService;
+
   @tracked documentType;
   @tracked docContainer;
   @tracked accessLevel;
   @tracked lastPiece;
+  @tracked versionPieces;
 
   @tracked activeTab = 'details';
+  @tracked isOpenUploadVersionModal = false;
 
   constructor() {
     super(...arguments);
@@ -26,14 +32,47 @@ export default class DocumentsDocumentPreviewSidebar extends Component {
   @task
   *loadData() {
     this.docContainer = yield this.args.piece.documentContainer;
-    yield this.docContainer.reverseSortedPieces;
-    this.lastPiece = yield this.docContainer.lastPiece;
+    this.loadVersionHistory.perform();
     this.documentType = yield this.docContainer.type;
     this.accessLevel = yield this.args.piece.accessLevel;
+  }
+
+  @task
+  *loadVersionHistory() {
+    this.versionPieces = yield this.docContainer.reverseSortedPieces;
+    this.lastPiece = yield this.docContainer.lastPiece;
   }
 
   @action
   setActiveTab(tabName) {
     this.activeTab = tabName;
+  }
+
+
+  @action
+  openUploadVersionModal() {
+    this.isOpenUploadVersionModal = true;
+  }
+
+
+  resetTabs() {
+    this.showDetails = false;
+    this.showSignatures = false;
+    this.showVersions = false;
+  }
+
+  @action
+  async deleteVersion(versionPiece) {
+    await this.fileService.deletePiece(versionPiece);
+    // delete orphan container if last piece is deleted
+    if (this.versionPieces.size <= 1){
+      await this.fileService.deleteDocumentContainer(this.docContainer)
+      this.args.transitionBack();
+    }
+    //if you deleted current file also go back
+    if (versionPiece.id === this.args.piece.id){
+      this.args.transitionBack();
+    }
+    this.loadVersionHistory.perform();
   }
 }
--- app/components/documents/version-card.js
+++ app/components/documents/version-card.js
@@ -1,9 +1,14 @@
 import Component from '@glimmer/component';
 import { task } from 'ember-concurrency-decorators';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class DocumentsVersionCardComponent extends Component {
+  @service currentSession;
   @tracked accessLevel;
+
+  @tracked isVerifyingDelete;
 
   constructor() {
     super(...arguments);
@@ -22,4 +27,21 @@ export default class DocumentsVersionCardComponent extends Component {
   get isLastPiece(){
     return this.args.lastPiece.id === this.args.piece.id
   }
+
+  @action
+  openVerify() {
+    this.isVerifyingDelete = true;
+  }
+
+  @action
+  confirmDelete() {
+    this.args.onDeletePiece();
+    this.isVerifyingDelete = false;
+  }
+
+  @action
+  cancelDelete() {
+    this.isVerifyingDelete = false;
+  }
+
 }
