47a61de8db831d4b22d60ea86e4e82eb45fd6364
Tanee Grade <tanee.grade@cereus.be>
2021-09-02 09:41:28 +0200

    KAS-2702 undo deeper nesting

--- app/components/documents/document-preview/version-card.js
+++ app/components/documents/document-preview/version-card.js
@@ -4,7 +4,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-export default class  DocumentsDocumentPreviewVersionsVersionCardComponent extends Component {
+export default class  DocumentsDocumentPreviewVersionCardComponent extends Component {
   @service currentSession;
   @tracked accessLevel;
 
--- app/components/documents/document-preview/versions-tab.js
+++ app/components/documents/document-preview/versions-tab.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
-export default class DocumentsDocumentPreviewVersionsVersionsTabComponent extends Component {
+export default class DocumentsDocumentPreviewVersionsTabComponent extends Component {
   @action
   openDeletePiece(piece) {
     this.args.openDeletePiece(piece);
