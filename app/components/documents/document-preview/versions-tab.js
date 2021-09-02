e2ae80c0c33dafb1188d74df203b885ae549b355
Erika Pauwels <erika.pauwels@gmail.com>
2021-09-02 23:26:58 +0200

    Refactor sorting to get-function

--- app/components/documents/document-preview/versions-tab.js
+++ app/components/documents/document-preview/versions-tab.js
@@ -1,6 +1,5 @@
 import Component from '@glimmer/component';
 import { sortPieces } from 'frontend-kaleidos/utils/documents';
-import { A } from '@ember/array';
 import { task } from 'ember-concurrency-decorators';
 import { tracked } from '@glimmer/tracking';
 
@@ -15,7 +14,10 @@ export default class DocumentsDocumentPreviewVersionsTabComponent extends Compon
   @task
   *loadVersionsData() {
     const pieces = yield this.args.documentContainer.hasMany('pieces').reload();
-    const sortedPieces = A(sortPieces(pieces.toArray()));
-    this.versions = sortedPieces.slice(0);
+    this.versions = pieces.toArray();
+  }
+
+  get sortedVersions() {
+    return sortPieces(this.versions);
   }
 }
