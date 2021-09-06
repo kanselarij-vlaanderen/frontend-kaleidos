46bd0f38209cd103f3673dca81be8984fc4d114b
Tanee Grade <tanee.grade@cereus.be>
2021-09-06 15:35:46 +0200

    KAS-2846 reformat prettier

--- app/models/regulation-type.js
+++ app/models/regulation-type.js
@@ -1,6 +1,4 @@
-import Model, {
-  attr, hasMany
-} from '@ember-data/model';
+import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class RegulationType extends Model {
   @attr('string') label;
