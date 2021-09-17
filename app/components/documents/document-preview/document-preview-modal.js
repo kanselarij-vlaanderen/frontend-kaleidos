d2f05cfb507a2e3d1cd97dfb1d29ccc4c004141b
Tanee Grade <tanee.grade@cereus.be>
2021-09-17 13:48:22 +0200

    KAS-2899 open of close sidebar based on size screen && don't show info when closed

--- app/components/documents/document-preview/document-preview-modal.js
+++ app/components/documents/document-preview/document-preview-modal.js
@@ -1,10 +1,13 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class DocumentsDocumentPreviewDocumentPreviewModal extends Component {
+  @service media;
   @tracked selectedVersion;
-  @tracked sidebarIsOpen = true;
+
+  @tracked sidebarIsOpen = this.media.get('isBigScreen');
 
   constructor() {
     super(...arguments);
