7bcd0bb6af4d3fc9590fae867d67d291815ebfbb
Michaël Dierick <michael.dierick@redpencil.io>
2021-10-05 11:53:45 +0200

    disable changing pub status when staatsblad pub exists

--- app/models/decision.js
+++ app/models/decision.js
@@ -1,9 +1,15 @@
 import Model, { belongsTo, attr } from '@ember-data/model';
+import CONSTANTS from 'frontend-kaleidos/config/constants';
 
 export default class DecisionModel extends Model {
+  @attr('string') uri;
   @attr('string') title;
   @attr('date') publicationDate;
   @attr('string') staatsbladUri;
 
   @belongsTo('publication-activity') publicationActivity;
+
+  get isStaatsbladResource() {
+    return this.uri && this.uri.startsWith(CONSTANTS.STAATSBLAD_ELI_DOMAIN);
+  }
 }
