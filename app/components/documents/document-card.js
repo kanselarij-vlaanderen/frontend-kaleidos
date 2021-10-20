7f3e07f0b584bc1a243413b20e2d73db1651b9c4
Tanee Grade <tanee.grade@cereus.be>
2021-10-20 10:52:36 +0200

    Revert "KAS-2947 get case related data & show a case link when file is linked to a case"
    This reverts commit bff2c256caa66264954cf0b3207161e1ba332511.

--- app/components/documents/document-card.js
+++ app/components/documents/document-card.js
@@ -40,7 +40,6 @@ export default class DocumentsDocumentCardComponent extends Component {
   @tracked accessLevel;
   @tracked documentContainer;
   @tracked signMarkingActivity;
-  @tracked case;
 
   @tracked uploadedFile;
   @tracked newPiece;
@@ -53,7 +52,6 @@ export default class DocumentsDocumentCardComponent extends Component {
     this.loadCodelists.perform();
     this.loadPieceRelatedData.perform();
     this.loadSignatureRelatedData.perform();
-    this.loadCaseRelatedData.perform();
   }
 
   get shouldShowPublications() {
@@ -74,15 +72,6 @@ export default class DocumentsDocumentCardComponent extends Component {
   *loadSignatureRelatedData() {
     if (this.args.hasMarkForSignature) {
       this.signMarkingActivity = yield this.piece.signMarkingActivity;
-    }
-  }
-
-  @task
-  *loadCaseRelatedData() {
-    const submissionActivity = yield this.piece.submissionActivity;
-    if (submissionActivity) {
-      const subcase = yield submissionActivity.subcase;
-      this.case = yield subcase.case;
     }
   }
 
