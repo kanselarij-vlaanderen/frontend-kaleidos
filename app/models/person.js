03f1f50777c6c6ace75240a47522fe10ad7cbb14
Michaël Dierick <michael.dierick@redpencil.io>
2021-10-28 14:19:34 +0200

    add todo's regarding deprecatingAlias'es

--- app/models/person.js
+++ app/models/person.js
@@ -1,11 +1,12 @@
 import Model, {
   attr, hasMany, belongsTo
 } from '@ember-data/model';
-import { deprecatingAlias } from '@ember/object/computed';
+import { deprecatingAlias } from '@ember/object/computed'; // eslint-disable-line
 
 export default class Person extends Model {
   @attr('string') firstName;
   @attr('string') lastName;
+  // TODO: factor out this alias (kept in on purpose to keep merge conflicts low)
   @deprecatingAlias('fullName', {
     id: 'minister-migration.deprecate-alternativeName',
     until: 'unknown',
@@ -20,6 +21,7 @@ export default class Person extends Model {
     return `${this.firstName || ''} ${this.lastName || ''}`.trim(); // trim in case one of both is empty
   }
 
+  // TODO: factor out this alias (kept in on purpose to keep merge conflicts low)
   @deprecatingAlias('fullName', {
     id: 'minister-migration.deprecate-nameToDisplay',
     until: 'unknown',
