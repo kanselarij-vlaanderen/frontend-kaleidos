18cd974e50bb70e551bc35b43c50ce0619be5209
ValenberghsSven <sven.valenberghs1@gmail.com>
2021-11-03 13:15:09 +0100

    KAS-2777 no it.only ...

--- cypress/integration/unit/agendaitem-changes.spec.js
+++ cypress/integration/unit/agendaitem-changes.spec.js
@@ -142,7 +142,7 @@ context('Agendaitem changes tests', () => {
     cy.get(agenda.printableAgenda.container).contains(subcaseTitle3);
   });
 
-  it.only('should verify that you can compare agendas', () => {
+  it('should verify that you can compare agendas', () => {
     cy.route('GET', '/agendas/f66c6d79-6ad2-49e2-af55-702df3a936d8/status').as('loadAgendaBStatus');
     cy.visit('/vergadering/5EBA48CF95A2760008000006/agenda/f66c6d79-6ad2-49e2-af55-702df3a936d8/vergelijken');
     cy.wait('@loadAgendaBStatus');
