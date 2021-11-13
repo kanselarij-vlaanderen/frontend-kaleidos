0e0bd689de38a10cd85194619443ddfcfcf81de0
mdv-professional <mdv.werk@outlook.com>
2021-11-13 14:43:07 +0100

    KAS-2817 navigate to previous page on close > signinghub iframe [sign]

--- app/pods/signatures/sign-flow/documents/controller.js
+++ app/pods/signatures/sign-flow/documents/controller.js
@@ -5,50 +5,40 @@ import { inject as service } from '@ember/service'
 
 export default class SignaturesSignFlowDocumentsController extends Controller {
   @service router;
-  previousPath;
-  counter;
+  referringPath;
   @tracked signFlow;
   @tracked signingHubUrl;
   @tracked isShown;
 
   @action
   onLoad(event) {
-    this.isShown = true;
     try {
       // If we can access below property, we didn't bump in to a cors error, which
       // in turn means that the iframe redirected back to our domain, because it was done
       // with the required user-interaction
-      // TODO: verify whether this works when deployed
+      /** @todo Verify whether this works when deployed */
       if (event.target.contentWindow.location.origin) {
         this.isShown = false;
         this.close();
       }
     } catch (err) {
-      this.counter++
       // To some other internal domain. This probably only occurs on initial load.
     }
   }
 
-
   @action
   close() {
-    console.log(this.previousPath)
-    if (this.previousPath) {
-      console.log('history', history.length)
-      console.log(this.counter + 1)
-      history.go(-(this.counter + 1))
+    // Because an iFrame saves entries to history,
+    //  normal navigation is easier to implement than going back (with History API)
+    //  (which seems possible by counting iFrame's "load" events)
+    // The disadvantage of normal navigation is, that it is not possible to go back when a page is refreshed.
+    // It might feel more natural anyway, since this modal has a separate URL.
+    if (this.referringPath) {
+      this.router.transitionTo(this.referringPath)
     } else {
+      // transition to default route
       this.router.transitionTo('')
     }
   }
-  // @action
-  // close() {
-  //   // IFrame prevents us from using the back method of the browser's History API
-  //   //  since it saves entries for the navigation inside the iframe
-  //   if (this.previousPath) {
-  //     this.router.recognizeAndLoad(this.previousPath)
-  //   } else {
-  //     this.router.transitionTo('')
-  //   }
-  // }
 }
+
--- app/pods/signatures/sign-flow/documents/route.js
+++ app/pods/signatures/sign-flow/documents/route.js
@@ -1,37 +1,28 @@
 import Route from '@ember/routing/route';
+import { isNone } from '@ember/utils'
 import { inject as service } from '@ember/service';
 
+// API currently only supports 1 document
+// Known issue: Back button for this page does not work.
 export default class SignaturesSignFlowDocumentsRoute extends Route {
   @service router;
-
-  activate() {
-    this.counter = 0;
-  }
-
-  async beforeModel() {
-    this.indexPrevious = history.length;
-    console.log(history.length)
-    this.previousPath = this.router.currentURL //this.getCurrentPath()
-  }
-
-  getCurrentPath() {
-    let location = window.location;
-    let path = location.pathname + location.hash + location.search;
-    let rootURL = this.router.rootURL;
-    path = path.substring(rootURL.length - 1);
-    return path
+  beforeModel() {
+    // router.currentRoute is not yet set when a fresh app instance is opened.
+    // (router.currentURL is set though)
+    const isFreshWithinKaleidos = isNone(this.router.currentRoute)
+    if (!isFreshWithinKaleidos) {
+      // The property name router.currentURL is misleading: only contains the current path
+      this.referringPath = this.router.currentURL;
+    }
   }
 
   async afterModel() {
-    const parentParams = this.paramsFor('signatures.sign-flow');
-    const signFlowId = parentParams.signflow_id;
-
     const signFlow = this.modelFor('signatures.sign-flow');
     const signSubcase = await signFlow.signSubcase;
     const signMarkingActivity = await signSubcase.signMarkingActivity;
     const piece = await signMarkingActivity.piece;
 
-    const data = await this.loadSigningHubUrl(signFlowId, piece.id);
+    const data = await this.loadSigningHubUrl(signFlow.id, piece.id);
     this.signFlow = signFlow;
     this.signingHubUrl = data.url;
   }
@@ -40,17 +31,7 @@ export default class SignaturesSignFlowDocumentsRoute extends Route {
     controller.signFlow = this.signFlow;
     controller.signingHubUrl = this.signingHubUrl;
     controller.isShown = true;
-    controller.indexPrevious = this.indexPrevious;
-    let currentPath = this.getCurrentPath();
-    console.log(this.router.currentURL)
-    controller.previousPath = this.router.currentURL;
-    // if (currentPath !== this.previousPath) {
-    //   controller.previousPath = this.previousPath;
-    // }
-    if (this.counter !== undefined) {
-      controller.counter = this.counter;
-      delete this.counter;
-    }
+    controller.referringPath = this.referringPath;
   }
 
   /**
