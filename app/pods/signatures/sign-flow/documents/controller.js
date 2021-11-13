f9265a25199484e5d2d936b1806d41f742cb45e0
mdv-professional <mdv.werk@outlook.com>
2021-11-13 11:06:05 +0100

    KAS-2817 explore options for closing modal > signinghub iframe [sign]

--- app/pods/signatures/sign-flow/documents/controller.js
+++ app/pods/signatures/sign-flow/documents/controller.js
@@ -1,17 +1,54 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service'
 
 export default class SignaturesSignFlowDocumentsController extends Controller {
+  @service router;
+  previousPath;
+  counter;
   @tracked signFlow;
+  @tracked signingHubUrl;
+  @tracked isShown;
+
+  @action
+  onLoad(event) {
+    this.isShown = true;
+    try {
+      // If we can access below property, we didn't bump in to a cors error, which
+      // in turn means that the iframe redirected back to our domain, because it was done
+      // with the required user-interaction
+      // TODO: verify whether this works when deployed
+      if (event.target.contentWindow.location.origin) {
+        this.isShown = false;
+        this.close();
+      }
+    } catch (err) {
+      this.counter++
+      // To some other internal domain. This probably only occurs on initial load.
+    }
+  }
+
 
   @action
   close() {
-    // If no route where you returned from go to the home page
-    if (history.length > 1) {
-      history.back();
+    console.log(this.previousPath)
+    if (this.previousPath) {
+      console.log('history', history.length)
+      console.log(this.counter + 1)
+      history.go(-(this.counter + 1))
     } else {
-      this.router.transitionTo('');
+      this.router.transitionTo('')
     }
   }
+  // @action
+  // close() {
+  //   // IFrame prevents us from using the back method of the browser's History API
+  //   //  since it saves entries for the navigation inside the iframe
+  //   if (this.previousPath) {
+  //     this.router.recognizeAndLoad(this.previousPath)
+  //   } else {
+  //     this.router.transitionTo('')
+  //   }
+  // }
 }
--- app/pods/signatures/sign-flow/documents/route.js
+++ app/pods/signatures/sign-flow/documents/route.js
@@ -1,7 +1,28 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class SignaturesSignFlowDocumentsRoute extends Route {
-  async model() {
+  @service router;
+
+  activate() {
+    this.counter = 0;
+  }
+
+  async beforeModel() {
+    this.indexPrevious = history.length;
+    console.log(history.length)
+    this.previousPath = this.router.currentURL //this.getCurrentPath()
+  }
+
+  getCurrentPath() {
+    let location = window.location;
+    let path = location.pathname + location.hash + location.search;
+    let rootURL = this.router.rootURL;
+    path = path.substring(rootURL.length - 1);
+    return path
+  }
+
+  async afterModel() {
     const parentParams = this.paramsFor('signatures.sign-flow');
     const signFlowId = parentParams.signflow_id;
 
@@ -10,13 +31,26 @@ export default class SignaturesSignFlowDocumentsRoute extends Route {
     const signMarkingActivity = await signSubcase.signMarkingActivity;
     const piece = await signMarkingActivity.piece;
 
-    const data = await this.loadSigningHubUrl(signFlowId, piece.id)
+    const data = await this.loadSigningHubUrl(signFlowId, piece.id);
     this.signFlow = signFlow;
-    return data;
+    this.signingHubUrl = data.url;
   }
 
   async setupController(controller) {
     controller.signFlow = this.signFlow;
+    controller.signingHubUrl = this.signingHubUrl;
+    controller.isShown = true;
+    controller.indexPrevious = this.indexPrevious;
+    let currentPath = this.getCurrentPath();
+    console.log(this.router.currentURL)
+    controller.previousPath = this.router.currentURL;
+    // if (currentPath !== this.previousPath) {
+    //   controller.previousPath = this.previousPath;
+    // }
+    if (this.counter !== undefined) {
+      controller.counter = this.counter;
+      delete this.counter;
+    }
   }
 
   /**
