8235969861a8c3c839ce454b4cb93b58cc5c93a5
Tanee Grade <tanee.grade@cereus.be>
2021-11-16 16:03:29 +0100

    KAS-2976 We want to filter the items after get

--- app/pods/newsletter/index/route.js
+++ app/pods/newsletter/index/route.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { PAGE_SIZE } from 'frontend-kaleidos/config/config';
-import { isEmpty } from '@ember/utils';
 
 export default class IndexNewsletterRoute extends Route {
   @service currentSession;
@@ -18,22 +17,22 @@ export default class IndexNewsletterRoute extends Route {
    * that don't have a newsletter-info yet.
    */
   async model(params) {
-    const agenda = this.modelFor('newsletter').agenda; // eslint-disable-line
-
+    const agenda = await this.modelFor('newsletter').agenda;
     const filter = {
       'show-as-remark': false,
       agenda: {
         id: agenda.id,
       },
-      ['is-approval']: false,
     };
-    let agendaitems = await this.loadAgendaItems(params, filter);
+    let agendaitems = await this.store.query('agendaitem', {
+      filter,
+      include: 'treatments.newsletter-info',
+      sort: params.sort,
+      'page[size]': PAGE_SIZE.AGENDAITEMS,
+    });
 
     // KAS-2976 - Old Kaleidos items have undefined is-approval states and need to be queried for this view
-    if (isEmpty(agendaitems)) {
-      filter['is-approval'] = undefined;
-      agendaitems = await this.loadAgendaItems(params, filter);
-    }
+    agendaitems = agendaitems.filter((item) => item.isApproval !== true);
 
     return Promise.all(
       agendaitems.map(async (agendaitem) => {
@@ -46,24 +45,6 @@ export default class IndexNewsletterRoute extends Route {
         };
       })
     );
-  }
-
-  async afterModel() {
-    this.agenda = this.modelFor('newsletter').agenda;
-  }
-
-  async setupController(controller) {
-    super.setupController(...arguments);
-    controller.agenda = this.agenda;
-  }
-
-  async loadAgendaItems(params, filter) {
-    return await this.store.query('agendaitem', {
-      filter,
-      include: 'treatments.newsletter-info',
-      sort: params.sort,
-      'page[size]': PAGE_SIZE.AGENDAITEMS,
-    });
   }
 
   @action
