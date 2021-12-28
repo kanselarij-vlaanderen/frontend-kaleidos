ef60788e187223fb26e817ec8579c86f16d8ac9d
Michaël Dierick <michael.dierick@redpencil.io>
2021-12-28 12:24:56 +0100

    integrate signinghub iframe component

---
+++ app/pods/signatures/signing-flow/documents/controller.js
@@ -0,0 +1,13 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+
+export default class SignaturesSignFlowDocumentsController extends Controller {
+  @service router;
+
+  @action
+  navigateToOverview() {
+    this.router.transitionTo('signatures.index');
+  }
+}
