121a71b76f008499733578f80054327c6e5f8f2e
mdv-professional <mdv.werk@outlook.com>
2022-01-05 12:31:44 +0100

    make publication reports routes static KAS-3066 pub: v3: reports: tabs

--- app/router.js
+++ app/router.js
@@ -75,7 +75,7 @@ Router.map(function() {
   if (!isEmpty(ENV.APP.ENABLE_PUBLICATIONS_TAB)) {
     this.route('publications', { path: '/publicaties', }, function() {
       this.route('overview', { path: '/overzicht' }, function () {
-        this.route('report', { path: '/:report' })
+        this.route('all', { path: '/alle-dossiers' })
       });
       this.route('publication', { path: ':publication_id', }, function() {
         this.route('case', { path: '/dossier', });
