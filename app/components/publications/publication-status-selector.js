1265cd8840831f47e0c90683085554290afd90c1
Tanee Grade <tanee.grade@cereus.be>
2022-01-06 15:12:20 +0100

    KAS-3026 Added statusSelection modal

--- app/components/publications/publication-status-selector.js
+++ app/components/publications/publication-status-selector.js
@@ -1,13 +1,41 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { task } from 'ember-concurrency-decorators';
 
 export default class PublicationStatusSelector extends Component {
   @service store;
   @tracked publicationStatusses;
 
+  @tracked publicationStatus;
+  @tracked publicationStatusChange;
+
+
   constructor() {
     super(...arguments);
     this.publicationStatusses = this.store.peekAll('publication-status').sortBy('position');
+    this.publicationStatus = this.args.publicationStatus;
+    this.publicationStatusChange = this.args.publicationStatusChange.startedAt;
+  }
+
+  @action
+  selectPublicationStatus(status) {
+    this.publicationStatus = status;
+    this.publicationStatusChange = new Date();
+  }
+
+  @action
+  setStatusDate(selectedDates) {
+    this.publicationStatusChange  = selectedDates[0];
+  }
+
+  @task
+  *savePublicationStatus() {
+    const saveInformation = {
+      status: this.publicationStatus,
+      changeDate: this.publicationStatusChange
+    };
+    yield this.args.onSave(saveInformation);
   }
 }
--- app/pods/publications/publication/controller.js
+++ app/pods/publications/publication/controller.js
@@ -2,6 +2,8 @@ import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import CONSTANTS from 'frontend-kaleidos/config/constants';
+import { isEmpty} from '@ember/utils';
 
 export default class PublicationController extends Controller {
   @service media;
@@ -9,6 +11,11 @@ export default class PublicationController extends Controller {
   @service toaster;
 
   @tracked sidebarIsOpen = this.media.get('isBigScreen');
+
+  @tracked decision;
+  @tracked showStatusSelector = false;
+  @tracked showConfirmWithdraw = false;
+
 
   @action
   toggleSidebar() {
@@ -19,5 +26,98 @@ export default class PublicationController extends Controller {
   async saveSidebarProperty(modifiedObject) {
     await modifiedObject.save();
     this.toaster.success(this.intl.t('successfully-saved'));
+  }
+
+  @action
+  openStatusSelector() {
+    this.showStatusSelector = true;
+  }
+
+  @action
+  closeStatusSelector() {
+    this.showStatusSelector = false;
+  }
+
+  @action
+  async withdrawPublicationFlow() {
+    const withdrawn = await this.store.findRecordByUri('publication-status', CONSTANTS.PUBLICATION_STATUSES.WITHDRAWN);
+    await this.setPublicationStatus(withdrawn);
+    this.showConfirmWithdraw = false;
+  }
+
+  @action
+  cancelWithdraw() {
+    this.showConfirmWithdraw = false;
+  }
+
+  @action
+  async setPublicationStatus(status,date) {
+    if (isEmpty(date)) {
+      date = new Date();
+    }
+    const oldStatus = await this.model.status;
+    const publicationSubcase = await this.model.publicationSubcase;
+
+    // create publication when status changed to "published"
+    if (status.isPublished && !this.decision) {
+      const publicationActivities = await publicationSubcase.publicationActivities;
+
+      if (publicationActivities.length) {
+        const publicationActivity = publicationActivities.objectAt(0);
+        this.decision = this.store.createRecord('decision', {
+          publicationActivity: publicationActivity,
+          publicationDate: new Date(),
+        });
+        this.decision.save();
+      }
+    }
+    // remove created decision when "published" status is reverted
+    else if ((oldStatus.isPublished && !status.isPublished)
+      && (this.decision && !this.decision.isStaatsbladResource)
+    ) {
+      // only remove decision when it is not a staatsblad resource
+      this.decision.deleteRecord();
+      this.decision.save();
+    }
+
+    // update status
+    this.model.status = status;
+
+    // update closing dates of auxiliary activities
+    const translationSubcase = await this.model.translationSubcase;
+
+    if (status.isPublished || status.isWithdrawn) {
+      this.model.closingDate = new Date();
+
+      if (!translationSubcase.endDate) {
+        translationSubcase.endDate = new Date();
+        await translationSubcase.save();
+      }
+      if (!publicationSubcase.endDate) {
+        publicationSubcase.endDate = new Date();
+        await publicationSubcase.save();
+      }
+    } else {
+      this.model.closingDate = null;
+    }
+
+    // add a status-change
+    const statusChange = this.store.createRecord('publication-status-change', {
+      startedAt: date,
+      publication: this.model,
+    });
+
+    await this.model.save();
+    await statusChange.save();
+    this.closeStatusSelector();
+  }
+
+  @action
+  async savePublicationStatus(statusInformation) {
+    if (statusInformation.status.isWithdrawn) {
+      this.showConfirmWithdraw = true;
+    } else {
+      await this.setPublicationStatus(statusInformation.status,statusInformation.changeDate);
+    }
   }
 }
