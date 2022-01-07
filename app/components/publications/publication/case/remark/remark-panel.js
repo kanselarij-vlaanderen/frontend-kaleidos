772e2505bbbfd9ce73424135c1b138993260f3d6
mdv-professional <mdv.werk@outlook.com>
2022-01-07 17:06:25 +0100

    add remark panel KAS-3081 pub: v3: details: info + remark

--- app/components/publications/publication/case/remark/remark-panel.js
+++ app/components/publications/publication/case/remark/remark-panel.js
@@ -1,5 +1,36 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency-decorators';
 
 export default class PublicationsPublicationCaseRemarkPanelComponent extends Component {
+  @tracked isInEditMode;
 
+  @tracked remark;
+
+  constructor() {
+    super(...arguments);
+  }
+
+  @action
+  putInEditMode() {
+    var publicationFlow = this.args.publicationFlow;
+    this.isInEditMode = true;
+    this.remark = publicationFlow.remark;
+  }
+
+  @action
+  cancelEdit() {
+    this.showError = false;
+    this.isInEditMode = false;
+  }
+
+  @task
+  *save() {
+    var publicationFlow = this.args.publicationFlow;
+    publicationFlow.remark = this.remark;
+    // no try-catch: don't exit if save didn't work
+    yield publicationFlow.save();
+    this.isInEditMode = false;
+  }
 }
