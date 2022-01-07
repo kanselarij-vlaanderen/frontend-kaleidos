d5e1bd76edfd04a5fc32e873bf0f9cfacc99121d
Erika Pauwels <erika.pauwels@gmail.com>
2022-01-07 18:01:42 +0100

    Add newlines between get-functions

--- app/models/publication-status.js
+++ app/models/publication-status.js
@@ -12,12 +12,15 @@ export default class PublicationStatus extends Model {
   get isStarted() {
     return this.uri === CONSTANTS.PUBLICATION_STATUSES.STARTED;
   }
+
   get isPublished() {
     return this.uri === CONSTANTS.PUBLICATION_STATUSES.PUBLISHED;
   }
+
   get isWithdrawn() {
     return this.uri === CONSTANTS.PUBLICATION_STATUSES.WITHDRAWN;
   }
+
   get isPaused() {
     return this.uri === CONSTANTS.PUBLICATION_STATUSES.PAUSED;
   }
@@ -25,20 +28,24 @@ export default class PublicationStatus extends Model {
   get isToTranslations() {
     return this.uri === CONSTANTS.PUBLICATION_STATUSES.TO_TRANSLATIONS;
   }
+
   get isTranslationIn() {
     return this.uri === CONSTANTS.PUBLICATION_STATUSES.TRANSLATION_IN;
   }
+
   get isProofRequested() {
     return this.uri === CONSTANTS.PUBLICATION_STATUSES.PROOF_REQUESTED;
   }
+
   get isProofRecalled() {
     return this.uri === CONSTANTS.PUBLICATION_STATUSES.PROOF_RECALLED;
   }
+
   get isProofCorrected() {
     return this.uri === CONSTANTS.PUBLICATION_STATUSES.PROOF_CORRECTED;
   }
+
   get isPublicationRequested() {
     return this.uri === CONSTANTS.PUBLICATION_STATUSES.PUBLICATION_REQUESTED;
   }
-
 }
