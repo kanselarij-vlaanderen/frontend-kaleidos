51146e2f75c0d4ff5209ebe7d756c7f7e6e8dce3
Michaël Dierick <michael.dierick@redpencil.io>
2022-01-07 12:14:08 +0100

    remove unused import

--- app/pods/signatures/signing-flow/documents/controller.js
+++ app/pods/signatures/signing-flow/documents/controller.js
@@ -1,6 +1,5 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 
 export default class SignaturesSignFlowDocumentsController extends Controller {
