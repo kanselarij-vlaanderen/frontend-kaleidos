160c28dc36e0290a0dad7d40d5142bad9e1c40d9
Tanee Grade <tanee.grade@cereus.be>
2022-01-17 15:26:38 +0100

    KAS-3095 Added new proofInfo panel

---
+++ app/components/publications/publication/proofs/proof-info-panel.js
@@ -0,0 +1,55 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency-decorators';
+/**
+ * @argument {PublicationSubcase}
+ * @argument {PublicationFlow}
+ * @argument {onSave}
+ */
+export default class PublicationsPublicationCaseContactPersonsPanelComponent extends Component {
+  @service store;
+
+  @tracked isEditing = false;
+  @tracked proofPrintCorrector;
+  @tracked publicationStatus;
+
+  @tracked dueDate;
+
+  constructor() {
+    super(...arguments);
+    this.loadStatus.perform();
+  }
+
+  @task
+  *loadStatus() {
+    this.publicationStatus = yield this.args.publicationFlow.status;
+  }
+
+  @action
+  openEditingPanel() {
+    this.isEditing = true;
+    this.proofPrintCorrector = this.args.publicationSubcase.proofPrintCorrector;
+    this.dueDate = this.args.publicationSubcase.dueDate;
+  }
+
+  @action
+  closeEditingPanel() {
+    this.isEditing = false;
+  }
+
+  @action
+  setPublicationDueDate(selectedDates) {
+    this.dueDate = selectedDates[0];
+  }
+
+  @task
+  *save() {
+    yield this.args.onSave({
+      dueDate: this.dueDate,
+      corrector: this.proofPrintCorrector
+    });
+    this.isEditing = false;
+  }
+}
