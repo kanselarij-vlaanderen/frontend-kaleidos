7d4a32fcd902108f6f99684cf2ccade01832b5e0
Tanee Grade <tanee.grade@cereus.be>
2022-01-20 14:56:13 +0100

    KAS-3095 fix component name

--- app/components/publications/publication/proofs/proof-info-panel.js
+++ app/components/publications/publication/proofs/proof-info-panel.js
@@ -8,7 +8,7 @@ import { task } from 'ember-concurrency-decorators';
  * @argument {PublicationFlow}
  * @argument {onSave}
  */
-export default class PublicationsPublicationCaseContactPersonsPanelComponent extends Component {
+export default class PublicationsPublicationProofsProofInfoPanelComponent extends Component {
   @service store;
 
   @tracked isEditing = false;
