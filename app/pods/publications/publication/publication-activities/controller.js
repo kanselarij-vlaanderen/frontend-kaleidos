a259a92cbcc7ec6165c505f4842b8ccc909aff30
Tanee Grade <tanee.grade@cereus.be>
2022-01-25 16:16:27 +0100

    KAS-3096 save within component

--- app/pods/publications/publication/publication-activities/controller.js
+++ app/pods/publications/publication/publication-activities/controller.js
@@ -1,15 +1,6 @@
 import Controller from '@ember/controller';
 import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 
 export default class PublicationsPublicationPublicationActivitiesController extends Controller {
   @tracked publicationFlow;
-
-  @action
-  async savePublicationInfo(publicationInfo) {
-    this.publicationFlow.mode = publicationInfo.mode;
-    await this.publicationFlow.save();
-    this.model.targetEndDate = publicationInfo.targetEndDate;
-    await this.model.save();
-  }
 }
