8f7a63668024ef7175445ccfdd29ec2b7d891ec3
Erika Pauwels <erika.pauwels@gmail.com>
2022-01-26 19:39:17 +0100

    Save publication-flow and publication-subcase in parallel

--- app/components/publications/publication/publication-activities/publications-info-panel.js
+++ app/components/publications/publication/publication-activities/publications-info-panel.js
@@ -3,6 +3,8 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency-decorators';
+import { all } from 'ember-concurrency';
+
 /**
  * @argument {publicationSubcase}
  * @argument {PublicationFlow}
@@ -43,11 +45,10 @@ export default class PublicationsPublicationPublicationActivitiesPublicationInfo
 
   @task
   *save() {
-    const publicationSubcase = this.args.publicationSubcase;
-    yield publicationSubcase.save();
-
-    const publicationFlow = this.args.publicationFlow;
-    yield publicationFlow.save();
+    yield all([
+      this.args.publicationSubcase.save(),
+      this.args.publicationFlow.save()
+    ]);
     this.isEditing = false;
   }
 }
