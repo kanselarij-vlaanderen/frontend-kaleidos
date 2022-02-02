e9c66a2a69031094d186bc0bf0873696ec474bbf
Erika Pauwels <erika.pauwels@gmail.com>
2022-02-02 12:30:35 +0100

    Move publication-date to publication-info panel
    Should have been done on a previous story. Piggybacking on this one...

--- app/components/publications/publication-sidebar.js
+++ app/components/publications/publication-sidebar.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { lastValue, task } from 'ember-concurrency';
-import { tracked } from '@glimmer/tracking';
 
 export default class PublicationsPublicationSidebarComponent extends Component {
   /**
@@ -14,27 +13,15 @@ export default class PublicationsPublicationSidebarComponent extends Component {
    */
   @service store;
 
-  @tracked decision;
-
   @lastValue('loadTranslationSubcase') translationSubcase;
 
   constructor() {
     super(...arguments);
     this.loadTranslationSubcase.perform();
-    this.loadDecision.perform();
   }
 
   get publicationFlow() {
     return this.args.publicationFlow;
-  }
-
-  @task
-  *loadDecision() {
-    const publicationSubcase = yield this.publicationFlow.publicationSubcase;
-    this.decision = yield this.store.queryOne('decision', {
-      'filter[publication-activity][subcase][:id:]': publicationSubcase.id,
-      sort: 'publication-activity.start-date,publication-date',
-    });
   }
 
   @task
