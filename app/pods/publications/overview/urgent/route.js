dd3e2b2a5daa80c0c0ecffae1b1635eebd8e6eaf
mdv-professional <mdv.werk@outlook.com>
2022-02-04 16:26:16 +0100

    (copy) report routes KAS-3067 pub: v3: reports: tabs

---
+++ app/pods/publications/overview/urgent/route.js
@@ -0,0 +1,52 @@
+import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+
+export default class PublicationsOverviewAllRoute extends Route {
+  queryParams = {
+    page: {
+      refreshModel: true,
+      as: 'pagina',
+    },
+    size: {
+      refreshModel: true,
+      as: 'aantal',
+    },
+    sort: {
+      refreshModel: true,
+      as: 'sorteer',
+    },
+  };
+
+  async model(params) {
+    return this.store.query('publication-flow', {
+      sort: params.sort,
+      page: {
+        number: params.page,
+        size: params.size,
+      },
+      // eslint-disable-next-line prettier/prettier
+      include: [
+        'identification',
+        'status',
+        'publication-subcase',
+        'translation-subcase',
+        'case',
+      ].join(','),
+    });
+  }
+
+  setupController(ctrl) {
+    ctrl.routeName = this.routeName;
+  }
+
+  @action
+  loading(transition) {
+    // see snippet in https://api.emberjs.com/ember/3.27/classes/Route/events/loading?anchor=loading
+    // eslint-disable-next-line ember/no-controller-access-in-routes
+    const controller = this.controllerFor(this.routeName);
+    controller.isLoadingModel = true;
+    transition.promise.finally(() => {
+      controller.isLoadingModel = false;
+    });
+  }
+}
