cc14c41e354f1638573fd30231a1824a289ac590
mdv-professional <mdv.werk@outlook.com>
2022-02-07 16:56:16 +0100

    "Aanvraag drukproef" tab filter KAS-3067 pub: v3: reports: tabs

--- app/pods/publications/overview/proof/route.js
+++ app/pods/publications/overview/proof/route.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
+import CONSTANTS from 'frontend-kaleidos/config/constants';
 
 export default class PublicationsOverviewProofRoute extends Route {
   queryParams = {
@@ -19,6 +20,7 @@ export default class PublicationsOverviewProofRoute extends Route {
 
   async model(params) {
     return this.store.query('publication-flow', {
+      'filter[status][:uri:]': CONSTANTS.PUBLICATION_STATUSES.PROOF_REQUESTED,
       sort: params.sort,
       page: {
         number: params.page,
