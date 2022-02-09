50d852dcc93bc880106875f5f583533e9f5ce440
Erika Pauwels <erika.pauwels@gmail.com>
2022-02-09 15:02:32 +0100

    Use publication document card on publication decisions tab
    Remove complex logic to add new versions. OVRB users only add new
    documents and don't care about versions.

--- app/pods/publications/publication/decisions/controller.js
+++ app/pods/publications/publication/decisions/controller.js
@@ -1,13 +1,48 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
 
 export default class PublicationsPublicationDecisionsController extends Controller {
+  @service store;
+
   @tracked publicationFlow;
   @tracked isViaCouncilOfMinisters;
+  @tracked isOpenReferenceDocumentUploadModal;
 
   @action
-  refreshModel() {
+  openReferenceDocumentUploadModal() {
+    this.isOpenReferenceDocumentUploadModal = true;
+  }
+
+  @action
+  closeReferenceDocumentUploadModal() {
+    this.isOpenReferenceDocumentUploadModal = false;
+  }
+
+  @task
+  *saveReferenceDocument(document) {
+    const now = new Date();
+    const documentContainer = this.store.createRecord('document-container', {
+      created: now,
+    });
+    yield documentContainer.save();
+
+    const piece = this.store.createRecord('piece', {
+      created: now,
+      modified: now,
+      name: document.name,
+      confidential: false,
+      pages: document.pageCount,
+      file: document.file,
+      documentContainer: documentContainer,
+      publicationFlow: this.publicationFlow,
+    });
+
+    yield piece.save();
+
     this.send('refresh');
+    this.closeReferenceDocumentUploadModal();
   }
 }
