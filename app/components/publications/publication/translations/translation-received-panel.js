55df3d946f8203a57af4d171c98c3451e9850b0b
Tanee Grade <tanee.grade@cereus.be>
2022-02-10 15:26:38 +0100

    KAS-3106 Undo unused arguments

--- app/components/publications/publication/translations/translation-received-panel.js
+++ app/components/publications/publication/translations/translation-received-panel.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
 
 /**
- * @argument {translationSubcase}
  * @argument {Piece} Translated Piece
  */
 export default class PublicationsPublicationTranslationTranslationReceivedPanel extends Component {
