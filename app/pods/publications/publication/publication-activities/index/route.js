0b1b63fcbfac2bfb736e83795f56b2540b0dff91
mdv-professional <mdv.werk@outlook.com>
2022-02-10 16:05:17 +0100

    (copy) start from publication/decisions/index route KAS-3108 pub: v3: details: publications

---
+++ app/pods/publications/publication/publication-activities/index/route.js
@@ -0,0 +1,38 @@
+import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { PAGE_SIZE } from 'frontend-kaleidos/config/config';
+
+export default class PublicationsPublicationDecisionsIndexRoute extends Route {
+  @service store;
+  @service publicationService;
+
+  async model() {
+    const parentParams = this.paramsFor('publications.publication');
+    const pieces = await this.store.query('piece', {
+      'filter[publication-flow][:id:]': parentParams.publication_id,
+      // TODO: paginatie uitklaren in design
+      'page[size]': PAGE_SIZE.PUBLICATION_FLOW_PIECES,
+      include: 'document-container',
+    });
+    return pieces.toArray();
+  }
+
+  async afterModel() {
+    this.publicationFlow = this.modelFor('publications.publication.decisions');
+    this.isViaCouncilOfMinisters =
+      await this.publicationService.getIsViaCouncilOfMinisters(
+        this.publicationFlow
+      );
+  }
+
+  setupController(ctrl) {
+    ctrl.publicationFlow = this.publicationFlow;
+    ctrl.isViaCouncilOfMinisters = this.isViaCouncilOfMinisters;
+  }
+
+  @action
+  refresh() {
+    super.refresh();
+  }
+}
