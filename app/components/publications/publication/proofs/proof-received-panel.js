19d8832bcb4ecf6c318eca354a1b75d221e1702d
Tanee Grade <tanee.grade@cereus.be>
2022-02-10 14:36:28 +0100

    KAS-3107 Add the panels

---
+++ app/components/publications/publication/proofs/proof-received-panel.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+/**
+ * @argument {translationSubcase}
+ * @argument {Piece} Translated Piece
+ */
+export default class PublicationsPublicationTranslationTranslationReceivedPanel extends Component {
+  constructor() {
+    super(...arguments);
+  }
+
+}
