23238770127baeca935c3e0f09ed134946ffd083
Michaël Dierick <michael.dierick@redpencil.io>
2022-02-15 21:01:50 +0100

    add publication table config utility class

---
+++ app/utils/publication-table-config.js
@@ -0,0 +1,60 @@
+import tableColumns from 'frontend-kaleidos/config/publications/overview-table-columns';
+import { tracked } from '@glimmer/tracking';
+import { TrackedSet } from 'tracked-built-ins';
+
+export default class PublicationTableConfig {
+  localStorageKey;
+  defaultColumnKeys;
+  allColumnKeys;
+
+  @tracked visibleColumnKeys = new TrackedSet();
+
+  constructor(localStorageKey, defaultColumnKeys=[]) {
+    this.localStorageKey = localStorageKey;
+    this.defaultColumnKeys = defaultColumnKeys;
+    this.allColumnKeys = tableColumns.map(c => c.keyName);
+  }
+
+  get visibleColumns() {
+    return tableColumns
+      .filter((column) => this.visibleColumnKeys.has(column.keyName));
+  }
+
+  loadFromLocalStorage() {
+    const serializedColumnVisibilityConfig = localStorage.getItem(
+      this.localStorageKey
+    );
+    if (serializedColumnVisibilityConfig) {
+      const columnsDisplayConfig = JSON.parse(serializedColumnVisibilityConfig);
+      this.visibleColumnKeys = new TrackedSet(
+        Object.entries(columnsDisplayConfig)
+          .filter(([_, value]) => {
+            return value;
+          })
+          .map(([key, _]) => {
+            return key;
+          })
+      );
+      return this.visibleColumnKeys;
+    }
+  }
+
+  getSerializedVisibilityConfig() {
+    const columnsVisibilityConfig = {};
+    tableColumns.forEach((column) => {
+      columnsVisibilityConfig[column.keyName] =
+        this.visibleColumnKeys.has(column.keyName);
+    });
+    return JSON.stringify(columnsVisibilityConfig);
+  }
+
+  saveToLocalStorage() {
+    const serializedColumnsDisplayConfig = this.getSerializedVisibilityConfig();
+    localStorage.setItem(this.localStorageKey, serializedColumnsDisplayConfig);
+  }
+
+  loadDefault() {
+    this.visibleColumnKeys = new TrackedSet(this.defaultColumnKeys);
+    return this.visibleColumns;
+  }
+}
