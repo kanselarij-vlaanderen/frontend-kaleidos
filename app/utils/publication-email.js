cf82658b9fc77ba8d38df7cdff89836749fe5a64
mdv-professional <mdv.werk@outlook.com>
2022-02-16 15:42:11 +0100

    (refactor) put publicationRequestEmail to new method KAS-3108 pub: v3: details: publications

--- app/utils/publication-email.js
+++ app/utils/publication-email.js
@@ -1,7 +1,15 @@
 import moment from 'moment';
 
-// * NOTE * We use '\t\n' as newline to prevent outlook from removing extra line breaks with just '\n'
- 
+// * NOTES *
+// 1. '\t\n' as newline:
+// => prevent outlook from removing extra line breaks with just '\n'
+// @see {@link https://docs.microsoft.com/en-us/outlook/troubleshoot/message-body/line-breaks-are-removed-in-posts-made-in-plain-text}
+// "By default, the Auto Remove Line Breaks feature in Outlook is enabled.
+// This causes the line breaks to be removed. Any two or more successive
+// line breaks are not removed.""
+// 2. no mulitline string:
+// => ensure exact representation
+
 const footer = 'Met vriendelijke groet,\n'
   + '\n'
   + 'Vlaamse overheid\t\n'
@@ -59,22 +67,6 @@ async function proofRequestEmail(params) {
       + `VO-dossier: ${idName}\n`
       + '\n'
       + 'Vragen bij dit dossier kunnen met vermelding van publicatienummer gericht worden aan onderstaand email adres.\t\n';
-  } else if (params.stage === 'final') {
-    const publicationSubcase = await publicationFlow.publicationSubcase;
-    const targetDate = publicationSubcase.targetEndDate;
-    const targetDateString = targetDate ? moment(targetDate).format('DD/MM/YYYY') : '-';
-
-    subject = `Verbeterde drukproef BS-werknr: ${numacNumber} VO-dossier: ${idName}`;
-    message = 'Beste,\n'
-      + '\n'
-      + 'Hierbij de verbeterde drukproef :\n'
-      + '\n'
-      + `BS-werknummer: ${numacNumber}\n`
-      + `VO-dossier: ${idName}\n`
-      + '\n'
-      + `De gewenste datum van publicatie is: ${targetDateString}\t\n`
-      + '\n'
-      + 'Vragen bij dit dossier kunnen met vermelding van publicatienummer gericht worden aan onderstaand email adres.\t\n';
   }
   return {
     subject: subject,
@@ -82,7 +74,42 @@ async function proofRequestEmail(params) {
   };
 }
 
+async function publicationRequestEmail({ publicationFlow }) {
+  const identification = await publicationFlow.identification;
+  const publicationNumber = identification.idName;
+
+  const numacNumbers = await publicationFlow.numacNumbers;
+  const numacNumbersString = numacNumbers.mapBy('idName').join(', ') || '-';
+
+  const publicationSubcase = await publicationFlow.publicationSubcase;
+  const targetDate = publicationSubcase.targetEndDate;
+  const targetDateString = targetDate
+    ? moment(targetDate).format('DD/MM/YYYY')
+    : '-';
+
+  const subject = `Verbeterde drukproef BS-werknr: ${numacNumbersString} VO-dossier: ${publicationNumber}`;
+  const message =
+    'Beste,\n' +
+    '\n' +
+    'Hierbij de verbeterde drukproef :\n' +
+    '\n' +
+    `BS-werknummer: ${numacNumbersString}\t\n` +
+    `VO-dossier: ${publicationNumber}\n` +
+    '\n' +
+    `De gewenste datum van publicatie is: ${targetDateString}\n` +
+    '\n' +
+    'Vragen bij dit dossier kunnen met vermelding van publicatienummer gericht worden aan onderstaand email adres.\t\n' +
+    '\n' +
+    footer;
+
+  return {
+    subject,
+    message,
+  };
+}
+
 export {
   translationRequestEmail,
-  proofRequestEmail
+  proofRequestEmail,
+  publicationRequestEmail,
 };
