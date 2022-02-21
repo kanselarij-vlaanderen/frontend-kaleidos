6b238efe79a4bd8114bc609b24b4800a0ad73739
mdv-professional <mdv.werk@outlook.com>
2022-02-21 17:10:52 +0100

    (cleanup) remove decisions route leftover / /publications/:id/publication KAS-3108 pub: v3: details: publications

--- app/pods/publications/publication/publication-activities/index/controller.js
+++ app/pods/publications/publication/publication-activities/index/controller.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import { dropTask, task } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
 
 /* eslint-disable no-unused-vars */
 import RequestActivity from '../../../../../models/request-activity';
@@ -46,7 +46,6 @@ export class PublicationPublicationEvent {
 export default class PublicationsPublicationPublicationActivitiesIndexController extends Controller {
   @service store;
   @service publicationService;
-  @service fileService;
 
   @tracked publicationFlow;
   @tracked isOpenRequestModal = false;
@@ -73,12 +72,5 @@ export default class PublicationsPublicationPublicationActivitiesIndexController
 
     this.send('refresh');
     this.isOpenRequestModal = false;
-  }
-
-  @dropTask
-  *deletePiece(piece) {
-    const documentContainer = yield piece.documentContainer;
-    yield this.fileService.deleteDocumentContainer(documentContainer);
-    this.send('refresh');
   }
 }
