6c1e069afa71d0e98350c61d8cd21cb571e04e77
Erika Pauwels <erika.pauwels@gmail.com>
2022-02-26 11:57:31 +0100

    Move edit translation activity modal to separate component
    Since the modal needs to keep track of internal state (to cache input
    value and disable save button) it's more clear to make a separate
    component instead of merging this state in the translation-received-panel component

---
+++ app/components/publications/publication/translations/translation-activity-edit-modal.js
@@ -0,0 +1,33 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { isEmpty } from '@ember/utils';
+import { task } from 'ember-concurrency';
+
+export default class PublicationsPublicationTranslationsTranslationActivityEditModal extends Component {
+  @tracked receivedDate;
+
+  constructor() {
+    super(...arguments);
+    this.receivedDate = this.args.receivedDate;
+  }
+
+  get isSaveDisabled() {
+    return isEmpty(this.receivedDate) || this.save.isRunning;
+  }
+
+  @action
+  setReceivedDate(selectedDates) {
+    if (selectedDates.length) {
+      this.receivedDate = selectedDates[0];
+    } else {
+      // this case occurs when users manually empty the date input-field
+      this.receivedDate = undefined;
+    }
+  }
+
+  @task
+  *save() {
+    yield this.args.onSave();
+  }
+}
--- app/components/publications/publication/translations/translation-received-panel.js
+++ app/components/publications/publication/translations/translation-received-panel.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency-decorators';
-import { isEmpty } from '@ember/utils';
 import { inject as service } from '@ember/service';
 
 /**
@@ -12,19 +11,11 @@ import { inject as service } from '@ember/service';
  * @argument onSaveEditReceivedTranslation
  */
 export default class PublicationsTranslationTranslationReceivedPanelComponent extends Component {
-
   @service router;
   @service publicationService;
 
   @tracked isOpenTranslationEditModal = false;
   @tracked isOpenProofRequestModal = false;
-
-  @tracked newEndDate;
-  @tracked newReceivedDate;
-
-  get isSaveDisabled() {
-    return isEmpty(this.newReceivedDate);
-  }
 
   get piecesOfTranslation() {
     return [
@@ -33,20 +24,8 @@ export default class PublicationsTranslationTranslationReceivedPanelComponent ex
     ];
   }
 
-  @action
-  openTranslationEditModal() {
-    this.isOpenTranslationEditModal = true;
-    // When opening modal, reset possibly stale local variable
-    this.newReceivedDate = this.args.translationActivity.endDate;
-  }
-
-  @action
-  closeTranslationEditModal() {
-    this.isOpenTranslationEditModal = false;
-  }
-
   @task
-  *saveReceivedDate() {
+  *updateTranslationActivity() {
     yield this.args.onSaveEditReceivedTranslation({
       translationActivity: this.args.translationActivity,
       receivedDate: this.newReceivedDate,
@@ -69,13 +48,13 @@ export default class PublicationsTranslationTranslationReceivedPanelComponent ex
   }
 
   @action
-  setNewReceivedDate(selectedDates) {
-    if (selectedDates.length) {
-      this.newReceivedDate = selectedDates[0];
-    } else {
-      // this case occurs when users manually empty the date input-field
-      this.newReceivedDate = undefined;
-    }
+  openTranslationEditModal() {
+    this.isOpenTranslationEditModal = true;
+  }
+
+  @action
+  closeTranslationEditModal() {
+    this.isOpenTranslationEditModal = false;
   }
 
   @action
