968d435a7dd671574790a20066c223227921557d
Sergio Fenoll <sergio@fenoll.be>
2022-03-03 13:04:46 +0100

    Fix typo & use no parens for emtpy @attr

--- app/models/document-type.js
+++ app/models/document-type.js
@@ -1,7 +1,7 @@
 import Model, { hasMany, attr } from '@ember-data/model';
 
 export default class DocumentType extends Model {
-  @attr('string') labe;
+  @attr('string') label;
   @attr('string') scopeNote;
   @attr('number') priority;
   @attr('string') altLabel;
