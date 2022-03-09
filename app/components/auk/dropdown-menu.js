e956c6737b5d921b4262eb91971d5e55c52b2898
Karel Persoons <karel@brenner.company>
2022-03-09 19:39:37 +0100

    KAS-2881 Refactor dropdown-menu component (part 1)

--- app/components/auk/dropdown-menu.js
+++ app/components/auk/dropdown-menu.js
@@ -2,16 +2,24 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
+import Button from './button';
+import ButtonLink from './button-link';
+
 /**
  *
+ * @argument {String} type. Determines the type (Auk::Button & Auk::ButtonLink) of the initiator
  * @argument {String} placement. See EmberAttacher doc for options
- * @argument {String} skin. See Auk::Button for options
+ * @argument {String} skin. See Auk::Button & Auk::ButtonLink for options
  * @argument {Boolean} disabled. See Auk::Button
  * @argument {String} icon. Name of the kaleidos-icon in case you want to provide a custom one (mostly used in icon-only settings where no label is provided)
  * @argument {String} label. Label for the trigger button
  */
 export default class DropdownMenuComponent extends Component {
   @tracked isOpen = false;
+
+  get initiator() {
+    return this.args.type === 'button-link' ? ButtonLink : Button;
+  }
 
   get placement() {
     return this.args.placement || 'bottom-end';
