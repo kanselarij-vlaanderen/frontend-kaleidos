c0ae1cced1096f8f60bd164b96c68f77b7b6c564
mdv-professional <mdv.werk@outlook.com>
2022-03-10 17:10:03 +0100

    update model KAS-3242 pub v3: email addressees

--- app/models/email-notification-setting.js
+++ app/models/email-notification-setting.js
@@ -2,7 +2,11 @@ import Model, { attr } from '@ember-data/model';
 
 export default class EmailNotificationSetting extends Model {
   @attr('string') defaultFromEmail;
+
   @attr('string') translationRequestToEmail;
+  @attr('string') translationRequestCcEmail;
   @attr('string') proofRequestToEmail;
   @attr('string') proofRequestCcEmail;
+  @attr('string') publicationRequestToEmail;
+  @attr('string') publicationRequestCcEmail;
 }
