336d24fcecd2025c1de5aed6fa8e182b85913441
mdv-professional <mdv.werk@outlook.com>
2022-03-10 14:49:56 +0100

    (refactor S) sort email settings KAS-3242 pub v3: email addressees

--- app/models/email-notification-setting.js
+++ app/models/email-notification-setting.js
@@ -1,8 +1,8 @@
 import Model, { attr } from '@ember-data/model';
 
 export default class EmailNotificationSetting extends Model {
+  @attr('string') defaultFromEmail;
   @attr('string') translationRequestToEmail;
   @attr('string') proofRequestToEmail;
   @attr('string') proofRequestCcEmail;
-  @attr('string') defaultFromEmail;
 }
