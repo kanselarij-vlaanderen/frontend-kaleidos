644e7d4401d3eb53f6437fcf81d97556caa66ebc
mdv-professional <mdv.werk@outlook.com>
2022-03-10 17:35:21 +0100

    🚧 edit e-mail settings* at once: - refactor: remove `@tracked` properties - use array with address setting keys to cleanup addresses - show e-mail addresses in different sections and short labels   reason: easily understandable layout
    KAS-3242 pub v3: email addressees

--- app/pods/settings/emails/controller.js
+++ app/pods/settings/emails/controller.js
@@ -1,23 +1,28 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
-import { isPresent, isBlank } from '@ember/utils';
+import { isBlank } from '@ember/utils';
 import { task } from 'ember-concurrency-decorators';
+
+const EMAIL_ADDRESS_KEYS = [
+  'translationRequestToEmail',
+  'translationRequestCcEmail',
+  'proofRequestToEmail',
+  'proofRequestCcEmail',
+  'publicationRequestToEmail',
+  'publicationRequestCcEmail',
+];
 
 export default class SettingsEmailController extends Controller {
   @service store;
 
-  @tracked emailToTranslationRequest = this.model.translationRequestToEmail;
-  @tracked emailToProofRequest = this.model.proofRequestToEmail;
-  @tracked emailCcProofRequest = this.model.proofRequestCcEmail;
-
   @task
   *saveSettings() {
-    this.model.translationRequestToEmail = this.emailToTranslationRequest;
-    this.model.proofRequestToEmail = this.emailToProofRequest;
-    this.model.proofRequestCcEmail = isPresent(this.emailCcProofRequest)
-      ? this.emailCcProofRequest
-      : undefined;
+    for (let addressKey of EMAIL_ADDRESS_KEYS) {
+      const address = this.model[addressKey];
+      const cleanAddress = address?.trim() ? address : undefined;
+      this.model[addressKey] = cleanAddress;
+    }
+
     yield this.model.save();
 
     this.transitionToRoute('settings.overview');
@@ -25,8 +30,9 @@ export default class SettingsEmailController extends Controller {
 
   get isDisabled() {
     return (
-      isBlank(this.emailToProofRequest) ||
-      isBlank(this.emailToTranslationRequest)
+      isBlank(this.model.translationRequestToEmail) ||
+      isBlank(this.model.proofRequestToEmail) ||
+      isBlank(this.model.publicationRequestToEmail)
     );
   }
 }
