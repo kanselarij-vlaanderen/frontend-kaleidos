ce5fb068de62658e2c1c679376ac8ddf62796234
ValenberghsSven <sven.valenberghs1@gmail.com>
2022-03-11 09:09:50 +0100

    remove old relations on theme model

--- app/models/theme.js
+++ app/models/theme.js
@@ -1,10 +1,7 @@
-import Model, { hasMany, attr } from '@ember-data/model';
+import Model, { attr } from '@ember-data/model';
 
 export default class Theme extends Model {
   @attr('string') label;
   @attr('string') scopeNote;
   @attr('boolean') deprecated;
-
-  @hasMany('subcase', { inverse: null }) subcase;
-  @hasMany('subcase', { inverse: null }) agendaitem;
 }
