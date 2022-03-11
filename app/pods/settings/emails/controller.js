cb8b127e58c7a03276d6ce58bfb8b4f6c58bd5c4
mdv-professional <mdv.werk@outlook.com>
2022-03-11 12:48:08 +0100

    no data cleaning for email settings /settings/email reason: - simpler and email delivery still works KAS-3242 pub v3: email addressees

--- app/pods/settings/emails/controller.js
+++ app/pods/settings/emails/controller.js
@@ -4,29 +4,13 @@ import { inject as service } from '@ember/service';
 import { isBlank } from '@ember/utils';
 import { task } from 'ember-concurrency-decorators';
 
-const EMAIL_ADDRESS_KEYS = [
-  'translationRequestToEmail',
-  'translationRequestCcEmail',
-  'proofRequestToEmail',
-  'proofRequestCcEmail',
-  'publicationRequestToEmail',
-  'publicationRequestCcEmail',
-];
-
 export default class SettingsEmailController extends Controller {
   @service router;
   @service store;
 
   @task
   *save() {
-    for (let addressKey of EMAIL_ADDRESS_KEYS) {
-      const address = this.model[addressKey];
-      const cleanAddress = address?.trim() ? address : undefined;
-      this.model[addressKey] = cleanAddress;
-    }
-
     yield this.model.save();
-
     this.router.transitionTo('settings.overview');
   }
 
