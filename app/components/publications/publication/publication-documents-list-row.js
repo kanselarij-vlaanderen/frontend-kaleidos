4986448d8c749498355935d86878f55ec7507f02
mdv-professional <mdv.werk@outlook.com>
2022-03-17 16:40:16 +0100

    split up Publications::Publication::PublicationDocumentsList in list and row component KAS-3255 pub: v3.1: display document properties

---
+++ app/components/publications/publication/publication-documents-list-row.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+
+// eslint-disable-next-line ember/no-empty-glimmer-component-classes,prettier/prettier
+export default class PublicationsPublicationPublicationDocumentsListRowComponent extends Component {
+
+}
