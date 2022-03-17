257f706a6975ed91e78c3da071aeaeb8b425b073
mdv-professional <mdv.werk@outlook.com>
2022-03-17 16:44:17 +0100

    show piece name / Publications::Publication::PublicationDocumentsListRow KAS-3255 pub: v3.1: display document properties

--- app/components/publications/publication/publication-documents-list-row.js
+++ app/components/publications/publication/publication-documents-list-row.js
@@ -1,6 +1,19 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { getPieceDisplayName } from 'frontend-kaleidos/utils/documents';
 
-// eslint-disable-next-line ember/no-empty-glimmer-component-classes,prettier/prettier
 export default class PublicationsPublicationPublicationDocumentsListRowComponent extends Component {
+  @tracked name;
 
+  constructor() {
+    super(...arguments);
+
+    this.initFields();
+  }
+
+  @action
+  async initFields() {
+    this.name = await getPieceDisplayName(this.args.piece);
+  }
 }
