d8d687d98f3286900c9260a0a728b97c1ad16cc1
mdv-professional <mdv.werk@outlook.com>
2022-03-17 16:13:34 +0100

    (lint-fix) KAS-3255 pub: v3.1: display document properties

--- app/utils/documents.js
+++ app/utils/documents.js
@@ -1,12 +1,19 @@
 import { A } from '@ember/array';
-import VRDocumentName, { compareFunction } from 'frontend-kaleidos/utils/vr-document-name';
+import VRDocumentName, {
+  compareFunction,
+} from 'frontend-kaleidos/utils/vr-document-name';
 import fetch from 'fetch';
 
 export const sortDocumentContainers = (pieces, containers) => {
   // Sorting is done in the frontend to work around a Virtuoso issue, where
   // FROM-statements for multiple graphs, combined with GROUP BY, ORDER BY results in
   // some items not being returned. By not having a sort parameter, this doesn't occur.
-  const sortedPieces = A(pieces.toArray()).sort((pieceA, pieceB) => compareFunction(new VRDocumentName(pieceA.get('name')), new VRDocumentName(pieceB.get('name'))));
+  const sortedPieces = A(pieces.toArray()).sort((pieceA, pieceB) =>
+    compareFunction(
+      new VRDocumentName(pieceA.get('name')),
+      new VRDocumentName(pieceB.get('name'))
+    )
+  );
   /*
     Code below for compatibility towards mixin consumers.
     Since names are now on each piece
@@ -17,41 +24,54 @@ export const sortDocumentContainers = (pieces, containers) => {
     let matchingPieceB = null;
     for (let index = 0; index < containerA.get('pieces.length'); index++) {
       const piece = containerA.get('pieces').objectAt(index);
-      matchingPieceA = sortedPieces.filterBy('id', piece.id).sortBy('created').lastObject;
+      matchingPieceA = sortedPieces
+        .filterBy('id', piece.id)
+        .sortBy('created').lastObject;
       if (matchingPieceA) {
         break;
       }
     }
     for (let index = 0; index < containerB.get('pieces.length'); index++) {
       const piece = containerB.get('pieces').objectAt(index);
-      matchingPieceB = sortedPieces.filterBy('id', piece.id).sortBy('created').lastObject;
+      matchingPieceB = sortedPieces
+        .filterBy('id', piece.id)
+        .sortBy('created').lastObject;
       if (matchingPieceB) {
         break;
       }
     }
-    return sortedPieces.indexOf(matchingPieceA) - sortedPieces.indexOf(matchingPieceB);
+    return (
+      sortedPieces.indexOf(matchingPieceA) -
+      sortedPieces.indexOf(matchingPieceB)
+    );
   });
 };
 
-export const sortPieces = (pieces, NameClass = VRDocumentName, sortingFunc = compareFunction) => {
+export const sortPieces = (
+  pieces,
+  NameClass = VRDocumentName,
+  sortingFunc = compareFunction
+) => {
   const validNamedPieces = [];
   let invalidNamedPieces = A();
   for (const piece of pieces) {
     try {
-      (new NameClass(piece.name)).parseMeta();
+      new NameClass(piece.name).parseMeta();
       validNamedPieces.push(piece);
     } catch {
       invalidNamedPieces.push(piece);
     }
   }
-  validNamedPieces.sort((docA, docB) => sortingFunc(new NameClass(docA.name), new NameClass(docB.name)));
+  validNamedPieces.sort((docA, docB) =>
+    sortingFunc(new NameClass(docA.name), new NameClass(docB.name))
+  );
   invalidNamedPieces = invalidNamedPieces.sortBy('created').toArray();
   invalidNamedPieces.reverse();
 
   return [...validNamedPieces, ...invalidNamedPieces];
 };
 
-export const addPieceToAgendaitem = async function(agendaitem, piece) {
+export const addPieceToAgendaitem = async function (agendaitem, piece) {
   const endpoint = `/agendaitems/${agendaitem.get('id')}/pieces`;
   const body = {
     data: {
@@ -67,11 +87,18 @@ export const addPieceToAgendaitem = async function(agendaitem, piece) {
     body: JSON.stringify(body),
   });
   if (!response.ok) {
-    throw new Error(`Failed to add document ${piece.get('id')} to agendaitem ${agendaitem.get('id')}`);
+    throw new Error(
+      `Failed to add document ${piece.get('id')} to agendaitem ${agendaitem.get(
+        'id'
+      )}`
+    );
   }
 };
 
-export const restorePiecesFromPreviousAgendaitem = async function(agendaitem, documentContainer) {
+export const restorePiecesFromPreviousAgendaitem = async function (
+  agendaitem,
+  documentContainer
+) {
   const endpoint = `/agendaitems/${agendaitem.get('id')}/pieces/restore`;
   const body = {
     data: {
@@ -87,6 +114,10 @@ export const restorePiecesFromPreviousAgendaitem = async function(agendaitem, do
     body: JSON.stringify(body),
   });
   if (!response.ok) {
-    throw new Error(`Failed to restore pieces from container ${documentContainer.get('id')} to agendaitem ${agendaitem.get('id')}`);
+    throw new Error(
+      `Failed to restore pieces from container ${documentContainer.get(
+        'id'
+      )} to agendaitem ${agendaitem.get('id')}`
+    );
   }
 };
