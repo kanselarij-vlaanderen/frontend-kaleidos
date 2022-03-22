e0a11eaeaa9707f4c0aa85adcc90d23c362d4593
Sergio Fenoll <sergio@fenoll.be>
2022-03-22 09:43:02 +0100

    Allow DocumentPreviewModal to switch between files of same piece

--- app/components/documents/document-preview/document-preview-modal.js
+++ app/components/documents/document-preview/document-preview-modal.js
@@ -1,26 +1,42 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { canViewDocument } from '../../../utils/can-view-document';
 
 export default class DocumentsDocumentPreviewDocumentPreviewModal extends Component {
   @tracked selectedVersion;
+  @tracked selectedFile;
 
   @tracked sidebarIsOpen = true;
 
   constructor() {
     super(...arguments);
     this.selectedVersion = this.args.piece;
-    this.sidebarIsOpen = JSON.parse(localStorage.getItem('documentViewerSidebar'))
+    [this.selectedFile] = this.selectedVersion.files.filter((file) =>
+      canViewDocument(file)
+    ) ?? [this.setSelectedVersion.file];
+    this.sidebarIsOpen = JSON.parse(
+      localStorage.getItem('documentViewerSidebar')
+    );
   }
 
   @action
   setSelectedVersion(piece) {
     this.selectedVersion = piece;
+    this.selectedFile = this.selectedVersion.file;
+  }
+
+  @action
+  setSelectedFile(file) {
+    this.selectedFile = file;
   }
 
   @action
   toggleSidebar() {
     this.sidebarIsOpen = !this.sidebarIsOpen;
-    localStorage.setItem('documentViewerSidebar',JSON.stringify(this.sidebarIsOpen));
+    localStorage.setItem(
+      'documentViewerSidebar',
+      JSON.stringify(this.sidebarIsOpen)
+    );
   }
 }
