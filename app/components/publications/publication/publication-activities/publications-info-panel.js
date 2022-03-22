8303443e91aa51a9046db01260527cad4a07b6f3
Tanee.Grade <tanee.grade@gmail.com>
2022-03-22 11:28:47 +0100

    KAS-3253 fix lint

--- app/components/publications/publication/publication-activities/publications-info-panel.js
+++ app/components/publications/publication/publication-activities/publications-info-panel.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { all, task } from 'ember-concurrency';
+import { task } from 'ember-concurrency';
 
 /**
  * @argument {publicationSubcase}
