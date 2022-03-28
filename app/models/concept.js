ea58d959868a663f1941c1e65d4b3f726dcfd3f1
mdv-professional <mdv.werk@outlook.com>
2022-03-28 10:30:40 +0200

    add models/concept.deprecated KAS-3278 pub: v3.1: government domains update

--- app/models/concept.js
+++ app/models/concept.js
@@ -3,6 +3,8 @@ export default class Concept extends Model {
   @attr('string') label;
   @attr('string') scopeNote;
   @attr('string') altLabel;
+  @attr('boolean') deprecated;
+
   @hasMany('concept', { inverse: 'broader'}) narrower;
   // The idea behind also using broader is because ember was automatically connecting the broader parent under the narrower relation
   // Resulting in all concepts with 1 inverse narrower to break length == 0 checks on narrower
