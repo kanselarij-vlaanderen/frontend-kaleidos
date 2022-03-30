67a7de9a28b103b71be6643a00da7117b0562a9d
Sergio Fenoll <sergio@fenoll.be>
2022-03-30 23:16:43 +0200

    Make the Utils::KindSelector default choice clearer

--- app/components/utils/kind-selector.js
+++ app/components/utils/kind-selector.js
@@ -5,21 +5,22 @@ import { inject as service } from '@ember/service';
 
 /**
  * @argument {boolean} hideLabel Whether to hide the label of the dropdown
- * @argument {boolean} initializeEmptyKind Whether to initialize the dropdown to the default meeting kind of no meeting kind was provided
+ * @argument {boolean} initializeEmptyKind Whether to initialize the dropdown to the default meeting kind, "Ministerraad", if no meeting kind was provided
  * @argument {function} setAction Action to perform once a meeting kind has been selected
  * @argument {MeetingKindModel} kind (Optional) The meeting kind to set the dropdown to
  */
 export default class UtilsKindSelector extends Component {
   @service store;
   @tracked kind = null;
-  @tracked options = this.store.peekAll('meeting-kind'); // Meeting kinds get loaded in the agendas route, so we can just peek them here.
+  @tracked options = this.store.peekAll('meeting-kind').sortBy('position'); // Meeting kinds get loaded in the agendas route, so we can just peek them here.
+  @tracked defaultKind = this.options.firstObject;
 
   constructor() {
     super(...arguments);
 
     this.kind = this.args.kind;
     if (this.args.initializeEmptyKind && !this.args.kind) {
-      this.setAction(this.options.firstObject);
+      this.setAction(this.defaultKind);
     }
   }
 
