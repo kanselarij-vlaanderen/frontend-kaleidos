b500f367ad9d1ad242236f0c72e493526ff33d80
mdv-professional <mdv.werk@outlook.com>
2022-04-08 10:37:54 +0200

    create row models KAS-3178 pub: v3: reports: basic: view

--- app/pods/publications/overview/reports/controller.js
+++ app/pods/publications/overview/reports/controller.js
@@ -1,5 +1,33 @@
 import Controller from '@ember/controller';
 
+class BaseRow {
+  /** @abstract */
+  key;
+
+  get titleKey() {
+    return `publication-reports--type--${this.key}`;
+  }
+}
+
+class GovernmentDomainRow extends BaseRow {
+  key = 'government-domain';
+}
+
+class RegulationTypeRow extends BaseRow {
+  key = 'regulation-type';
+}
+
+// TODO: will be split up in "van BVR per minister" and "van decreet per minister"
+class MandateeRow extends BaseRow {
+  key = 'mandatee';
+}
+
+export const ReportTypeRows = [
+  GovernmentDomainRow,
+  RegulationTypeRow,
+  MandateeRow,
+];
+
 export default class PublicationsOverviewReportsController extends Controller {
 
 }
--- app/pods/publications/overview/reports/route.js
+++ app/pods/publications/overview/reports/route.js
@@ -1,5 +1,10 @@
 import Route from '@ember/routing/route';
+import { ReportTypeRows } from './controller';
 
 export default class PublicationsOverviewReportsRoute extends Route {
-
+  model() {
+    return ReportTypeRows.map((Row) => {
+      return new Row();
+    });
+  }
 }
