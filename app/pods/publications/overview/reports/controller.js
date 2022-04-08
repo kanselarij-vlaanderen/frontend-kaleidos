d595d5609618c98c93907f150fc807913ebec2bb
mdv-professional <mdv.werk@outlook.com>
2022-04-08 12:27:49 +0200

    trigger generate report | /publications/overview/reports KAS-3178 pub: v3: reports: basic: view

--- app/pods/publications/overview/reports/controller.js
+++ app/pods/publications/overview/reports/controller.js
@@ -1,25 +1,83 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { dasherize } from '@ember/string';
+import moment from 'moment';
+import { task } from 'ember-concurrency';
 
 class BaseRow {
-  /** @abstract */
-  key;
+  /**
+   * @abstract
+   * @member {String} key
+   */
+
+  /**
+   * @abstract
+   * @method getReportQueryParams
+   * @returns {{ [key: string]: any }}
+   */
+
+  store;
+  intl;
+  currentSession;
 
   get titleKey() {
     return `publication-reports--type--${this.key}`;
+  }
+
+  @task
+  *triggerGenerateReport(params) {
+    const now = new Date();
+
+    const reportNameDatePrefix = moment(now).format('YYYYMMDDhhmmss');
+    const reportNameType = dasherize(this.intl.t(this.titleKey));
+    const reportName = `${reportNameDatePrefix}-${reportNameType}`;
+
+    const query = this.getReportQueryParams(params);
+
+    const user = yield this.currentSession.user;
+
+    const job = this.store.createRecord('publication-metrics-export-job', {
+      created: now,
+      timeStarted: now,
+      generatedBy: user,
+      config: {
+        name: reportName,
+        query: query,
+      },
+    });
+    yield job.save();
   }
 }
 
 class GovernmentDomainRow extends BaseRow {
   key = 'government-domain';
+
+  getReportQueryParams() {
+    return {
+      group: this.key,
+    };
+  }
 }
 
 class RegulationTypeRow extends BaseRow {
   key = 'regulation-type';
+
+  getReportQueryParams() {
+    return {
+      group: this.key,
+    };
+  }
 }
 
 // TODO: will be split up in "van BVR per minister" and "van decreet per minister"
 class MandateeRow extends BaseRow {
   key = 'mandatee';
+
+  getReportQueryParams() {
+    return {
+      group: this.key,
+    };
+  }
 }
 
 export const ReportTypeRows = [
@@ -29,5 +87,5 @@ export const ReportTypeRows = [
 ];
 
 export default class PublicationsOverviewReportsController extends Controller {
-
+  @service store;
 }
--- app/pods/publications/overview/reports/route.js
+++ app/pods/publications/overview/reports/route.js
@@ -1,10 +1,20 @@
 import Route from '@ember/routing/route';
 import { ReportTypeRows } from './controller';
+import { inject as service } from '@ember/service';
 
 export default class PublicationsOverviewReportsRoute extends Route {
+  @service store;
+  @service intl;
+  @service currentSession;
+
   model() {
     return ReportTypeRows.map((Row) => {
-      return new Row();
+      const row = new Row();
+      return Object.assign(row, {
+        store: this.store,
+        intl: this.intl,
+        currentSession: this.currentSession,
+      });
     });
   }
 }
