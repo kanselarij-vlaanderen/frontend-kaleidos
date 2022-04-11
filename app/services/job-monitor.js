ca9f23a27f80958c388c2da760dcec522634ef19
mdv-professional <mdv.werk@outlook.com>
2022-04-11 14:17:08 +0200

    (deprecation-fix) JobMonitorService KAS-3178 pub: v3: reports: basic: view

--- app/services/job-monitor.js
+++ app/services/job-monitor.js
@@ -1,29 +1,20 @@
 import Service from '@ember/service';
-import { A } from '@ember/array';
-import {
-  task, timeout
-} from 'ember-concurrency';
+import { task, timeout } from 'ember-concurrency';
 
-// TODO: octane-refactor
-// eslint-disable-next-line ember/no-classic-classes
-export default Service.extend({
-  jobs: null,
-
-  init() {
-    this._super(...arguments);
-    this.set('jobs', A([]));
-  },
+export default class JobMonitorService extends Service {
+  jobs = [];
 
   register(job) {
     this.jobs.pushObject(job);
     this.monitorJobProgress.perform(job);
-  },
+  }
 
-  monitorJobProgress: task(function *(job) {
+  @task
+  *monitorJobProgress(job) {
     while (!job.hasEnded) {
       yield timeout(1000);
       yield job.reload();
     }
     return job;
-  }),
-});
+  }
+}
