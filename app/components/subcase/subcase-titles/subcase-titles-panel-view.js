6a13e276361f37f03f380bc2caf47ab7b0de9317
Tanee.Grade <tanee.grade@gmail.com>
2022-04-21 14:00:17 +0200

    KAS-3312 Octane refactor

--- app/components/subcase/subcase-titles/subcase-titles-panel-view.js
+++ app/components/subcase/subcase-titles/subcase-titles-panel-view.js
@@ -1,34 +1,10 @@
-// TODO: octane-refactor
-// eslint-disable-next-line ember/no-classic-components
-import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-// eslint-disable-next-line ember/no-computed-properties-in-native-classes
-import {
-  action, computed
-} from '@ember/object';
-
-// TODO: octane-refactor
-// eslint-disable-next-line ember/require-tagless-components
+import Component from '@glimmer/component';
 export default class SubcaseTitlesPanelView extends Component {
-  classNames = ['auk-u-mb-8'];
-  @service currentSession;
-  subcase = null;
-
-  @computed('subcase.approved')
-  get pillSkin() {
-    return this.getPillSkin();
-  }
-
   async getPillSkin() {
-    const approved = await this.subcase.get('approved');
+    const approved = await this.args.subcase.approved;
     if (approved) {
       return 'success';
     }
     return 'default';
-  }
-
-  @action
-  toggleIsEditingAction() {
-    this.toggleIsEditing();
   }
 }
