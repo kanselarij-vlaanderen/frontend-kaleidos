ebb98c3eb95e485167372e288960e4d9a18a255b
Sergio Fenoll <sergio@fenoll.be>
2022-04-21 16:36:03 +0200

    Refactor agendaitem-nav to agendaitem template
    Side load all the necessary data for the nav in the route itself

--- app/pods/agenda/agendaitems/agendaitem/controller.js
+++ app/pods/agenda/agendaitems/agendaitem/controller.js
@@ -1,6 +1,13 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class AgendaAgendaitemsAgendaitemController extends Controller {
+  @service currentSession;
+
   @tracked meeting;
+  @tracked subcaseExists;
+  @tracked decisionsExist;
+  @tracked newsItemExists;
+  @tracked pressAgendaitemExists;
 }
--- app/pods/agenda/agendaitems/agendaitem/route.js
+++ app/pods/agenda/agendaitems/agendaitem/route.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { isPresent } from '@ember/utils';
 
 export default class AgendaitemAgendaitemsAgendaRoute extends Route {
   @service store;
@@ -14,17 +15,26 @@ export default class AgendaitemAgendaitemsAgendaRoute extends Route {
     });
   }
 
-  afterModel(model, transition) {
+  async afterModel(model, transition) {
     const arrayToSearch = model.showAsRemark ? this.modelFor('agenda.agendaitems').announcements : this.modelFor('agenda.agendaitems').notas;
     if (!arrayToSearch.includes(model) && arrayToSearch.length) { // This can happen when the selected item no longer is visible in the sidebar after filtering
       this.transitionTo('agenda.agendaitems.agendaitem', arrayToSearch[0]);
     }
     this.transition = transition; // set on the route for use in setupController, since the provided "transition" argument there always comes back "undefined"
+
+    const agendaActivity = await model.agendaActivity;
+    this.subcase = await agendaActivity?.subcase;
+    this.treatments = await model.treatments;
+    this.newsletterInfo = await this.treatments.firstObject.newsletterInfo;
   }
 
   setupController(controller, model) {
     super.setupController(...arguments);
     controller.meeting = this.modelFor('agenda').meeting;
+    controller.subcaseExists = !(model.isApproval) && isPresent(this.subcase);
+    controller.decisionsExist = isPresent(this.treatments);
+    controller.newsItemExists = isPresent(this.newsletterInfo);
+    controller.pressAgendaitemExists = model.titlePress && model.textPress;
 
     // eslint-disable-next-line ember/no-controller-access-in-routes
     const parentController = this.controllerFor('agenda.agendaitems');
