9e34a98504f98f5afaf387e1a90e317bd7a233b0
Sergio Fenoll <sergio@fenoll.be>
2022-04-22 16:33:05 +0200

    Get currentAgendaitem from anchor
    When we're rendering agenda tabs from agenda.agendaitems we should try
    and determine the currentAgendaitem based on the queryParam ancho, if available.

--- app/components/agenda/agenda-header/agenda-tabs.js
+++ app/components/agenda/agenda-header/agenda-tabs.js
@@ -41,11 +41,16 @@ export default class AgendaAgendaHeaderAgendaTabsComponent extends Component {
   get currentAgendaItemId() {
     const currentRoute = this.router.currentRoute;
     let agendaItemsRoute = currentRoute;
-    if (currentRoute && currentRoute.name.startsWith('agenda.agendaitems.agendaitem')) {
+    if (currentRoute?.name.startsWith('agenda.agendaitems.agendaitem')) {
       while (agendaItemsRoute.name !== 'agenda.agendaitems.agendaitem') {
         agendaItemsRoute = agendaItemsRoute.parent;
       }
       return agendaItemsRoute.params.agendaitem_id;
+    } else if (currentRoute?.name.startsWith('agenda.agendaitems')) {
+      while (agendaItemsRoute.name !== 'agenda.agendaitems') {
+        agendaItemsRoute = agendaItemsRoute.parent;
+      }
+      return agendaItemsRoute.queryParams.anchor;
     }
     return null;
   }
