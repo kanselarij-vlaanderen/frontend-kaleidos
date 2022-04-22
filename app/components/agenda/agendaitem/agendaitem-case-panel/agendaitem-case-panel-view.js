adef2fd4fd152cac78d85b01531b5c5f4d64c9fe
Tanee.Grade <tanee.grade@gmail.com>
2022-04-22 09:01:33 +0200

    KAS-3312 Cleanup refactor

--- app/components/agenda/agendaitem/agendaitem-case-panel/agendaitem-case-panel-view.js
+++ app/components/agenda/agendaitem/agendaitem-case-panel/agendaitem-case-panel-view.js
@@ -1,16 +1,11 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
-import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import {
   lastValue, task
 } from 'ember-concurrency';
 
 export default class AgendaitemCasePanelView extends Component {
-  @service currentSession;
-
   @lastValue('loadCase') case;
-
   @tracked showLoader = false;
 
   constructor() {
