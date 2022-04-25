be4c657e929a5a84701332d8cd20e6c4a294ed2f
mdv-professional <mdv.werk@outlook.com>
2022-04-25 09:45:42 +0200

    rf: remove publicationYear Field class | /publications/overview/reports KAS-1670 pub: v3: reports: extended

--- app/components/publications/overview/reports/generate-report-modal.js
+++ app/components/publications/overview/reports/generate-report-modal.js
@@ -5,27 +5,6 @@ import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency';
 
-class PublicationYearField extends EmberObject {
-  @tracked value;
-
-  get valueAsNumber() {
-    return Number.parseInt(this.value);
-  }
-
-  constructor() {
-    super(...arguments);
-    this.value = new Date(Date.now()).getFullYear();
-  }
-
-  setQueryFilter(filterParams) {
-    const year = this.valueAsNumber;
-    const yearStartDate = new Date(year, 0, 1, 0, 0, 0, 0);
-    const nextYearStartDate = new Date(year + 1, 0, 1, 0, 0, 0, 0);
-
-    filterParams.publicationDate = [yearStartDate, nextYearStartDate];
-  }
-}
-
 class MandateeField extends EmberObject {
   @service store;
 
@@ -75,7 +54,6 @@ class MandateeField extends EmberObject {
 }
 
 const FIELDS = {
-  publicationYear: PublicationYearField,
   mandatee: MandateeField,
 };
 
@@ -90,9 +68,11 @@ export default class GenerateReportModalComponent extends Component {
   constructor() {
     super(...arguments);
 
-    const year = new Date(Date.now()).getFullYear();
-    this.decisionDateRangeStart = new Date(year, 0, 1, 0, 0, 0, 0);
-    this.decisionDateRangeEnd = new Date(year, 11, 31, 0, 0, 0, 0); // we only use date part in frontend, so we can leave hour parts === 0
+    const currentYear = new Date(Date.now()).getFullYear();
+    this.publicationYear = currentYear;
+
+    this.decisionDateRangeStart = new Date(currentYear, 0, 1, 0, 0, 0, 0);
+    this.decisionDateRangeEnd = new Date(currentYear, 11, 31, 0, 0, 0, 0); // we only use date part in frontend, so we can leave hour parts === 0
 
     let owner = getOwner(this).ownerInjection();
     const fields = this.args.fields;
@@ -106,9 +86,21 @@ export default class GenerateReportModalComponent extends Component {
     }
   }
 
+  get publicationYearAsNumber() {
+    return Number.parseInt(this.publicationYear);
+  }
+
   @task
   *triggerGenerateReport() {
     const queryFilter = {};
+
+    const publicationYear = this.publicationYearAsNumber;
+    const publicationDateRangeStart = new Date(publicationYear, 0, 1, 0, 0, 0, 0); /* eslint-disable-line prettier/prettier */ // (no new line for each number)
+    const publicationDateRangeEnd = new Date(publicationYear + 1, 0, 1, 0, 0, 0, 0); /* eslint-disable-line prettier/prettier */
+    queryFilter.publicationDate = [
+      publicationDateRangeStart,
+      publicationDateRangeEnd,
+    ];
 
     let decisionDateRangeEnd = this.decisionDateRangeEnd;
     if (decisionDateRangeEnd) {
