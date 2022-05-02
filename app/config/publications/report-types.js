1dac26d78920f21e2ff6d87019cd2e26abc31cfd
mdv-professional <mdv.werk@outlook.com>
2022-05-02 17:14:37 +0200

    KAS-1670 add config for report types > pub: v3: reports: extended

--- app/config/publications/report-types.js
+++ app/config/publications/report-types.js
@@ -1,14 +1,78 @@
+import CONSTANTS from 'frontend-kaleidos/config/constants';
+
 export default [
   {
-    translationKey: 'publication-reports--type--government-domain',
-    metricsTypeUri: 'government-domain', // TODO: these aren't URI's. that should change
+    translationKey: 'publication-reports--type--by-mandatee--on-decision-date',
+    metricsTypeUri: 'by-mandatee--on-decision-date', // TODO: these aren't URI's. that should change
+    fixedQueryParameters: {
+      group: 'mandatee',
+      // fixed query filters for this report type
+      filter: {},
+      // user input fields for query filters
+    },
+    userInputFieldsForQuery: {
+      decisionDateRange: true,
+    },
   },
   {
-    translationKey: 'publication-reports--type--regulation-type',
-    metricsTypeUri: 'regulation-type',
+    translationKey: 'publication-reports--type--by-government-domain',
+    metricsTypeUri: 'by-government-domain',
+    fixedQueryParameters: {
+      group: 'government-domain',
+      filter: {},
+    },
+    userInputFieldsForQuery: {
+      publicationYear: true,
+      governmentDomain: true,  
+    },
   },
   {
-    translationKey: 'publication-reports--type--mandatee',
-    metricsTypeUri: 'mandatee',
+    translationKey: 'publication-reports--type--by-mandatee--only-bvr',
+    metricsTypeUri: 'by-mandatee--only-bvr',
+    fixedQueryParameters: {
+      group: 'mandatee',
+      filter: {
+        regulationType: [CONSTANTS.REGULATION_TYPES.BVR],
+      }
+    },
+    userInputFieldsForQuery: {
+      publicationYear: true,
+      mandatee: true,
+    },
   },
+  {
+    translationKey: 'publication-reports--type--by-regulation-type--only-not-via-council-of-ministers',
+    metricsTypeUri: 'by-regulation-type--only-not-via-council-of-ministers',
+    fixedQueryParameters: {
+      group: 'regulation-type',
+      filter: {
+        isViaCouncilOfMinisters: false,
+      }
+    },
+    userInputFieldsForQuery: {
+      publicationYear: true,
+    },
+  },
+  {
+    translationKey: 'publication-reports--type--by-regulation-type',
+    metricsTypeUri: 'by-regulation-type',
+    fixedQueryParameters: {
+      group: 'regulation-type',
+    },
+    userInputFieldsForQuery: {
+      publicationYear: true,
+      regulationType: true,  
+    },
+  },
+  {
+    translationKey: 'publication-reports--type--by-mandatee--only-decree',
+    metricsTypeUri: 'by-mandatee--only-decree',
+    fixedQueryParameters: {
+      group: 'mandatee',
+    },
+    userInputFieldsForQuery: {
+      publicationYear: true,
+      mandatee: true,
+    },
+  }
 ];
