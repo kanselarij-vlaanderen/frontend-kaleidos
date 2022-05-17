7c130057685e0765a3ad24dcb8e5faf984164090
Sergio Fenoll <sergio@fenoll.be>
2022-05-17 14:23:27 +0200

    Move show access level logic to document-badge-list

--- app/components/documents/document-badge-list.js
+++ app/components/documents/document-badge-list.js
@@ -1,7 +1,12 @@
-import templateOnly from '@ember/component/template-only';
+import Component from '@glimmer/component';
+import CONSTANTS from 'frontend-kaleidos/config/constants';
 
 /**
  * @argument pieces:
  * @argument highlightedPieces:
  */
-export default templateOnly();
+export default class DcoumentsDocumentBadgeListComponent extends Component {
+  shouldShowAccessLevel(accessLevel) {
+    return (accessLevel?.uri === CONSTANTS.ACCESS_LEVELS.MINISTERRAAD) || (accessLevel?.uri === CONSTANTS.ACCESS_LEVELS.INTERN_SECRETARIE);
+  }
+}
--- app/components/documents/document-badge.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import CONSTANTS from 'frontend-kaleidos/config/constants';
-
-export default class DocumentsDocumentBadgeComponent extends Component {
-
-  get showAccessLevel() {
-    return (this.args.accessLevel?.uri === CONSTANTS.ACCESS_LEVELS.MINISTERRAAD) || (this.args.accessLevel?.uri === CONSTANTS.ACCESS_LEVELS.INTERN_SECRETARIE);
-  }
-}
