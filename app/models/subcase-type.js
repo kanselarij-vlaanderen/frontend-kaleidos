827df6a1e6b5727e0719a833868aa849c19b0f00
Sergio Fenoll <sergio@fenoll.be>
2022-05-18 16:48:23 +0200

    Octane refactor subcase-type model

--- app/models/subcase-type.js
+++ app/models/subcase-type.js
@@ -1,12 +1,9 @@
 import Model, { hasMany, attr } from '@ember-data/model';
 
-// TODO: octane-refactor
-// eslint-disable-next-line ember/no-classic-classes
-export default Model.extend({
-  label: attr('string'),
-  altLabel: attr('string'),
-  scopeNote: attr('string'),
-  subcases: hasMany('subcase', {
-    inverse: null,
-  }),
-});
+export default class Subcase extends Model {
+  @attr label;
+  @attr altLabel;
+  @attr scopeNote;
+
+  @hasMany('subcase', { inverse: null }) subcases;
+}
