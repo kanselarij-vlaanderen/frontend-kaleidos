4489a022f95b4ba03f80863d3344b5b798754a12
Sven Valenberghs <sven.valenberghs@faros.be>
2022-05-18 19:08:34 +0200

    KAS-3386 filter on all other statuses

--- app/pods/publications/overview/urgent/route.js
+++ app/pods/publications/overview/urgent/route.js
@@ -1,6 +1,19 @@
 import PublicationsOverviewBaseRoute from '../_base/route';
 import CONSTANTS from 'frontend-kaleidos/config/constants';
 
+const NOT_PUBLISHED_STATUSES_URIS = [
+  CONSTANTS.PUBLICATION_STATUSES.STARTED,
+  CONSTANTS.PUBLICATION_STATUSES.TRANSLATION_REQUESTED,
+  CONSTANTS.PUBLICATION_STATUSES.TRANSLATION_RECEIVED,
+  CONSTANTS.PUBLICATION_STATUSES.PROOF_REQUESTED,
+  CONSTANTS.PUBLICATION_STATUSES.PROOF_RECEIVED,
+  CONSTANTS.PUBLICATION_STATUSES.PROOF_RECALLED,
+  CONSTANTS.PUBLICATION_STATUSES.PROOF_CORRECTED,
+  CONSTANTS.PUBLICATION_STATUSES.PUBLICATION_REQUESTED,
+  CONSTANTS.PUBLICATION_STATUSES.WITHDRAWN,
+  CONSTANTS.PUBLICATION_STATUSES.PAUSED,
+];
+
 export default class PublicationsOverviewUrgentRoute extends PublicationsOverviewBaseRoute {
   defaultColumns = [
     'publicationNumber',
@@ -9,20 +22,28 @@ export default class PublicationsOverviewUrgentRoute extends PublicationsOvervie
     'numberOfPages',
     'publicationDueDate',
   ];
-  tableConfigStorageKey = "publication-table.urgent";
+  tableConfigStorageKey = 'publication-table.urgent';
 
   beforeModel() {
-    super.beforeModel(...arguments)
+    super.beforeModel(...arguments);
+    const notPublishedStatuses = this.store
+      .peekAll('publication-status')
+      .filter((status) => {
+        return NOT_PUBLISHED_STATUSES_URIS.includes(status.uri);
+      });
     this.filter = {
       'urgency-level': {
         ':uri:': CONSTANTS.URGENCY_LEVELS.SPEEDPROCEDURE,
+      },
+      status: {
+        ':id:': notPublishedStatuses.mapBy('id').join(','),
       },
     };
   }
 
   renderTemplate(controller) {
     this.render('publications.overview.all', {
-      controller: controller
+      controller: controller,
     });
   }
 }
