6799488e28018cfe9ba5f475bbe4c3c940853943
Sergio Fenoll <sergio@fenoll.be>
2022-05-18 16:46:38 +0200

    Add subcase type constant for "bekrachtiging VR"

--- app/config/constants.js
+++ app/config/constants.js
@@ -43,6 +43,9 @@ export default {
     NOTA: 'http://kanselarij.vo.data.gift/id/dossier-type-codes/1b6a6975-28e7-46b5-83fe-da37bb967db2',
     REMARK: 'http://kanselarij.vo.data.gift/id/dossier-type-codes/305E9678-8106-4C14-9BD6-60AE2032D794',
   },
+  SUBCASE_TYPES: {
+    BEKRACHTIGING: 'http://example.com/step/bdba2bbc-7af6-490b-98a8-433955cfe869',
+  },
   // AGENDA
   ACCEPTANCE_STATUSSES: {
     NOT_YET_OK: 'http://kanselarij.vo.data.gift/id/concept/goedkeurings-statussen/B72D1561-8172-466B-B3B6-FCC372C287D0',
