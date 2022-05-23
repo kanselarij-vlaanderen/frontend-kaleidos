2d17eb04769d99d57673b47778b324244b5af766
mdv-professional <mdv.werk@outlook.com>
2022-05-23 16:29:34 +0200

    KAS-3384 make sort helper more generic > Pub: v3: Numacnummer in zoekresultaatoverzicht

--- app/helpers/sort.js
+++ app/helpers/sort.js
@@ -1,4 +1,4 @@
 import { helper } from '@ember/component/helper';
 
 // ember-composable-helpers seems to miss a simple sort helper
-export default helper(([array] /*, hash */) => array.slice().sort() );
+export default helper(([array] /*, hash */) => array?.slice().sort((a, b) => a - b /* custom sort fn: handle dates correctly (not on toString() value) */));
