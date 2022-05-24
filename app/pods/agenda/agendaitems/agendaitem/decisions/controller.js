4f690a9dd6de039fa9a84dd5dd77e93a4535667a
Sergio Fenoll <sergio@fenoll.be>
2022-05-24 10:49:57 +0200

    Calculate access level only when uploading file

--- app/pods/agenda/agendaitems/agendaitem/decisions/controller.js
+++ app/pods/agenda/agendaitems/agendaitem/decisions/controller.js
@@ -9,7 +9,6 @@ export default class DecisionAgendaitemAgendaitemsAgendaController extends Contr
 
   @tracked agendaitem;
   @tracked meeting;
-  @tracked subcase;
 
   get treatments() {
     return this.model;
