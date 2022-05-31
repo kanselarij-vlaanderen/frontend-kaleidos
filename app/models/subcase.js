4e6e9c2cc57808ccc0231dcc56bafc76b6435254
Sergio Fenoll <sergio@fenoll.be>
2022-05-31 13:54:45 +0200

    Refactor subcase model to Octane syntax

--- app/models/subcase.js
+++ app/models/subcase.js
@@ -1,42 +1,32 @@
 import { belongsTo, hasMany, attr } from '@ember-data/model';
-import { inject } from '@ember/service';
-import { alias } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 import ModelWithModifier from 'frontend-kaleidos/models/model-with-modifier';
 
-// TODO: octane-refactor
-/* eslint-disable ember/no-get */
-export default ModelWithModifier.extend({
-  modelName: alias('constructor.modelName'),
-  store: inject(),
-  intl: inject(),
+export default class Subcase extends ModelWithModifier {
+  @service store;
+  @service intl;
 
-  created: attr('datetime'),
-  modified: attr('datetime'),
-  shortTitle: attr('string'),
-  title: attr('string'),
-  showAsRemark: attr('boolean'),
-  confidential: attr('boolean'),
-  isArchived: attr('boolean'),
-  subcaseName: attr('string'),
+  @attr('datetime') created;
+  @attr('datetime') modified;
+  @attr shortTitle;
+  @attr title;
+  @attr('boolean') showAsRemark;
+  @attr('boolean') confidential;
+  @attr('boolean') isArchived;
+  @attr subcaseName;
 
-  agendaActivities: hasMany('agenda-activity', {
-    inverse: null,
-  }),
-  submissionActivities: hasMany('submission-activity', {
-    serialize: false,
-  }),
-  linkedPieces: hasMany('piece'),
-  mandatees: hasMany('mandatee'),
-  treatments: hasMany('agenda-item-treatment', {
-    inverse: null,
-  }),
+  get modelName() {
+    return this.constructor.modelName;
+  }
 
-  type: belongsTo('subcase-type'),
-  case: belongsTo('case'),
-  requestedForMeeting: belongsTo('meeting', {
-    inverse: null,
-  }),
-  requestedBy: belongsTo('mandatee', {
-    inverse: null,
-  }),
-});
+  @belongsTo('subcase-type') type;
+  @belongsTo('case') case;
+  @belongsTo('meeting', { inverse: null }) requestedForMeeting;
+  @belongsTo('mandatee', { inverse: null }) requestedBy;
+
+  @hasMany('agenda-activity', { inverse: null }) agendaActivities;
+  @hasMany('submission-activity', { serialize: false }) submissionActivities;
+  @hasMany('piece') linkedPieces;
+  @hasMany('mandatee') mandatees;
+  @hasMany('agenda-item-treatment', { inverse: null }) treatments;
+}
