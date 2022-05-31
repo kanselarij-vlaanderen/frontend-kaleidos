4b7e004102d94dcd282ee39bc6bb86f5118fcc77
Sergio Fenoll <sergio@fenoll.be>
2022-05-31 19:31:11 +0200

    Document usages of computed in agendaitem

--- app/models/agendaitem.js
+++ app/models/agendaitem.js
@@ -59,12 +59,18 @@ export default ModelWithModifier.extend({
   pieces: hasMany('piece'),
   linkedPieces: hasMany('piece'),
 
-  // TODO only used in print agenda function
+  // TODO this computed property is used in:
+  // - Agenda::PrintableAgenda::ListSection::ItemGroup
+  // - Agenda::PrintableAgenda::ListSection::ItemGroup::Item
+  // Refactor these uses and remove this computed property
   sortedPieces: computed('pieces.@each.name', function() {
     return A(this.get('pieces').toArray()).sort((pieceA, pieceB) => compareFunction(new VRDocumentName(pieceA.get('name')), new VRDocumentName(pieceB.get('name'))));
   }),
 
-  // TODO KAS-2975 only used for compare function
+  // KAS-2975 only used for compare function
+  // TODO this computed property is used in:
+  // - agendaitem#hasAddedPieces
+  // Refactor this use and remove this computed property
   documentContainers: computed('pieces.@each.name', 'id', function() {
     return PromiseArray.create({
       promise: this.get('pieces').then((pieces) => {
@@ -89,6 +95,7 @@ export default ModelWithModifier.extend({
 
   isDesignAgenda: reads('agenda.isDesignAgenda'),
 
+
   nota: computed('id', function() {
     return PromiseObject.create({
       promise: this.store.queryOne('document-container', {
@@ -107,6 +114,10 @@ export default ModelWithModifier.extend({
     });
   }),
 
+  // TODO this computed property is used in:
+  // - NewsItem::EditItem
+  // - NewsletterTable::ButtonToolbar
+  // Refactor these usages and remove this computed property
   notaOrVisienota: computed('id', 'nota', function() {
     return PromiseObject.create({
       promise: this.nota.then((nota) => {
@@ -130,22 +141,42 @@ export default ModelWithModifier.extend({
     });
   }),
 
+  // TODO this computed property is used in:
+  // - agenda.print controller#notaGroups
+  // - Agenda::AgendaDetail::Sidebar
+  // - Agenda::AgendaOverview
+  // - Agenda::PrintableAgenda::ListSection::ItemGroup
+  // - agenda-service#groupAgendaitemsOnGroupName
+  // Refactor these usages and remove this computed property
   sortedMandatees: computed('mandatees.[]', function() {
     return this.get('mandatees').sortBy('priority');
   }),
 
+  // TODO this computed property is used in:
+  // - Agenda::AgendaOverview::AgendaOverviewItem
+  // - Agenda::AgendaDetail::SidebarItem
+  // - Agenda::Agendaheader::AgendaActionPopupAgendaitems
+  // - Agenda::Agendaitem::AgendaitemCasePanel::AgendaitemCasePanelView
+  // Refactor these usages and remove this computed property
   formallyOkToShow: computed('formallyOk', function() {
     const options = CONFIG.formallyOkOptions;
     const foundOption = options.find((formallyOkOption) => formallyOkOption.uri === this.formallyOk);
     return EmberObject.create(foundOption);
   }),
 
+  // TODO this computed property is used in:
+  // - Agenda::AGendaHeader::AgendaACtionPOpupAgendaitems
+  // - agendaitem#hasChanges
+  // Refactor these usages and remove this computed property
   checkAdded: computed('id', 'addedAgendaitems.[]', 'agenda.createdFor.agendas.[]', async function() {
     const wasAdded = (this.addedAgendaitems && this.addedAgendaitems.includes(this.id));
     return wasAdded;
   }),
 
   // TODO KAS-2975 only used for compare function
+  // TODO this computed property is used in:
+  // - Agenda::CompareAgendaList
+  // Refactor this usage and remove this computed property
   hasChanges: computed('checkAdded', 'hasAddedPieces', async function() {
     const hasAddedPieces = await this.hasAddedPieces;
     const checkAdded = await this.checkAdded;
@@ -153,11 +184,17 @@ export default ModelWithModifier.extend({
   }),
 
   // TODO KAS-2975 only used for compare function
+  // TODO this computed property is used in:
+  // - agendaitem#hasChanges
+  // Refactor this usage and remove this computed property
   hasAddedPieces: computed('documentContainers.[]', 'addedPieces.[]', async function() {
     const documentContainers = await this.get('documentContainers');
     return documentContainers && documentContainers.some((documentContainers) => documentContainers.checkAdded);
   }),
 
+  // TODO this computed property is used in:
+  // - Agenda::AgendaHeader::AgendaACtionPopupAgendaitems
+  // Refactor this usage and remove this computed property
   newsletterInfo: computed('treatments.@each.newsletterInfo', 'treatments', 'id', async function() {
     const newsletterInfos = await this.store.query('newsletter-info', {
       'filter[agenda-item-treatment][agendaitem][:id:]': this.get('id'),
