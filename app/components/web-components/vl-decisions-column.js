81142c6d6e603210588bd560cf38d41ff3245e0a
Sergio Fenoll <sergio@fenoll.be>
2022-05-31 15:29:26 +0200

    Replace usage of subcase.approved in VlDecisionsColumn

--- app/components/web-components/vl-decisions-column.js
+++ app/components/web-components/vl-decisions-column.js
@@ -1,17 +1,39 @@
 // TODO: octane-refactor
 // eslint-disable-next-line ember/no-classic-components
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
+import CONSTANTS from 'frontend-kaleidos/config/constants';
 
 // TODO: octane-refactor
 // eslint-disable-next-line ember/no-classic-classes, ember/require-tagless-components
 export default Component.extend({
+  store: service(),
 
-  textToShow: computed('row', 'value', 'row.agendaActivity.subcase.decisions.[]', async function() {
+  textToShow: computed('row', 'value', 'row.agendaActivity.subcase', async function() {
     const agendaitem = await this.row;
-    const agendaActivity = await agendaitem.get('agendaActivity');
-    const subcase = await agendaActivity.get('subcase');
-    const approved = await subcase.get('approved');
+    const agendaActivity = await agendaitem?.agendaActivity;
+    const subcase = await agendaActivity?.subcase;
+    let approved = false;
+
+    const meeting = await subcase?.requestedForMeeting;
+    if (meeting?.isFinal) {
+      const approvedDecisionResultCode = await this.store.findRecordByUri(
+        'decision-result-code',
+        CONSTANTS.DECISION_RESULT_CODE_URIS.GOEDGEKEURD
+      );
+      const acknowledgedDecisionResultCode = await this.store.findRecordByUri(
+        'decision-result-code',
+        CONSTANTS.DECISION_RESULT_CODE_URIS.KENNISNAME
+      );
+      approved = !!(await this.store.queryOne('agenda-item-treatment', {
+        'filter[subcase][id]': subcase.id,
+        'filter[decision-result-code][:id:]': [
+          approvedDecisionResultCode.id,
+          acknowledgedDecisionResultCode.id,
+        ].join(','),
+      }));
+    }
 
     if (approved) {
       return 'Beslist';
