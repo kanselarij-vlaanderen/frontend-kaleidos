aba7a410cb3452b29aab0f9ec904b0fd9d9c217c
Sergio Fenoll <sergio@fenoll.be>
2022-06-01 16:33:04 +0200

    Octane refactor agenda

--- app/models/agenda.js
+++ app/models/agenda.js
@@ -1,45 +1,41 @@
 import Model, { hasMany, belongsTo, attr } from '@ember-data/model';
-import { computed } from '@ember/object';
+// eslint-disable-next-line ember/no-computed-properties-in-native-classes
 import { reads } from '@ember/object/computed';
 // LoadableModel is still depended upon here and there. Should refactor out in the more general direction the codebase handles these load operations.
 // eslint-disable-next-line ember/no-mixins
 import LoadableModel from 'ember-data-storefront/mixins/loadable-model';
 
-// TODO: octane-refactor
-/* eslint-disable ember/no-get */
-// eslint-disable-next-line ember/no-classic-classes
-export default Model.extend(LoadableModel, {
-  name: computed.alias('serialnumber'),
-  title: attr('string'),
-  serialnumber: attr('string'),
-  issued: attr('datetime'),
-  createdFor: belongsTo('meeting'),
-  status: belongsTo('agendastatus', {
-    inverse: null,
-  }),
-  agendaitems: hasMany('agendaitem', {
-    inverse: null,
-    serialize: false,
-  }),
-  created: attr('datetime'),
-  modified: attr('datetime'),
+export default class Agenda extends Model.extend(LoadableModel) {
+  @attr title;
+  @attr serialnumber;
+  @attr('datetime') issued;
+  @attr('datetime') created;
+  @attr('datetime') modified;
 
+  @belongsTo('meeting') createdFor;
+  @belongsTo('agendastatus', { inverse: null }) status;
   // the next and previous version of agenda is set in agenda-approve-service, read-only in frontend
-  previousVersion: belongsTo('agenda', {
+  @belongsTo('agenda', {
     inverse: 'nextVersion',
     serialize: false,
-  }),
-  nextVersion: belongsTo('agenda', {
+  }) previousVersion;
+  @belongsTo('agenda', {
     inverse: 'previousVersion',
     serialize: false,
-  }),
-  isDesignAgenda: reads('status.isDesignAgenda'),
+  }) nextVersion;
 
-  isApproved: reads('status.isApproved'),
+  @hasMany('agendaitem', {
+    inverse: null,
+    serialize: false,
+  }) agendaitems;
+
+
+  // TODO: Either refactor these two or figure out how to make them accessible using @tracked ?
+  @reads('status.isDesignAgenda') isDesignAgenda;
+  @reads('status.isApproved') isApproved;
 
   async asyncCheckIfDesignAgenda() {
-    await this.get('status');
-
-    return this.get('isDesignAgenda');
-  },
-});
+    await this.status;
+    return this.isDesignAgenda;
+  }
+}
