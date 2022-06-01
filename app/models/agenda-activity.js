40e28fcb68545a8d7d2ccf0f2b6980e88450f9b7
Sergio Fenoll <sergio@fenoll.be>
2022-06-01 14:52:42 +0200

    Octane refactor agenda-activity

--- app/models/agenda-activity.js
+++ app/models/agenda-activity.js
@@ -1,11 +1,8 @@
 import Model, { hasMany, belongsTo, attr } from '@ember-data/model';
 
-// TODO: octane-refactor
-/* eslint-disable ember/no-get */
-// eslint-disable-next-line ember/no-classic-classes
-export default Model.extend({
-  startDate: attr('datetime'),
-  subcase: belongsTo('subcase'),
-  agendaitems: hasMany('agendaitems'), // TODO ember-data model name should be singular?
-  submissionActivities: hasMany('submission-activity'),
-});
+export default class AgendaActivity extends Model {
+  @attr('datetime') startDate;
+  @belongsTo('subcase') subcase;
+  @hasMany('agendaitem') agendaitems;
+  @hasMany('submission-activity') submissionActivities;
+}
