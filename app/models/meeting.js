af17a0ad4017629a750fdf184617059808a0808e
Sergio Fenoll <sergio@fenoll.be>
2022-06-02 13:44:58 +0200

    Remove unused documentContainers computed property

--- app/models/meeting.js
+++ app/models/meeting.js
@@ -1,11 +1,7 @@
 import Model, { belongsTo, hasMany, attr } from '@ember-data/model';
-import { PromiseArray } from '@ember-data/store/-private';
 import { computed } from '@ember/object';
 import { inject } from '@ember/service';
 import { KALEIDOS_START_DATE } from 'frontend-kaleidos/config/config';
-import {
-  sortDocumentContainers
-} from 'frontend-kaleidos/utils/documents';
 
 // TODO: octane-refactor
 /* eslint-disable ember/no-get */
@@ -43,31 +39,6 @@ export default Model.extend({
   }),
 
   themisPublicationActivities: hasMany('themis-publication-activity'),
-
-  // TODO this computed property is used in:
-  // - agendaitem#hasAddedPieces
-  // Refactor that usage and remove this computed property
-  documentContainers: computed('pieces.@each.name', 'id', 'store', function() {
-    return PromiseArray.create({
-      promise: this.get('pieces').then((pieces) => {
-        if (pieces && pieces.get('length') > 0) {
-          return this.store.query('document-container', {
-            filter: {
-              pieces: {
-                meeting: {
-                  id: this.get('id'),
-                },
-              },
-            },
-            page: {
-              size: pieces.get('length'), // # documentContainers will always be <= # pieces
-            },
-            include: 'type,pieces,pieces.access-level,pieces.next-piece,pieces.previous-piece',
-          }).then((containers) => sortDocumentContainers(this.get('pieces'), containers));
-        }
-      }),
-    });
-  }),
 
   // TODO this computed property is used in:
   // - agenda template
