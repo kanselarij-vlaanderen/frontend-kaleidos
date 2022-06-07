3a53c2d232fc291c7b0058e83d7c214789db9dcb
Karel Persoons <karel@brenner.company>
2022-06-07 13:48:32 +0200

    KAS-3446 Adjust test selector

--- cypress/selectors/document.selectors.js
+++ cypress/selectors/document.selectors.js
@@ -85,7 +85,7 @@ const selectors = {
     // Clicking the pill in this component toggles edit mode
     pill: '[data-test-access-level-pill]',
     selector: '[data-test-access-level-selector]',
-    edit: '[data-test-editable-access-level-pill] + button',
+    edit: '[data-test-access-level-pill] + button',
     save: '[data-test-access-level-pill-save]',
     cancel: '[data-test-access-level-pill-cancel]',
   },
