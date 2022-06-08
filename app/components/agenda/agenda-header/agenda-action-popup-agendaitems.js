d097da2d99d129959a02aa213836717244d3403c
Sergio Fenoll <sergio@fenoll.be>
2022-06-08 23:07:05 +0200

    Remove unneeded newsletter-info in agenda action popup

--- app/components/agenda/agenda-header/agenda-action-popup-agendaitems.js
+++ app/components/agenda/agenda-header/agenda-action-popup-agendaitems.js
@@ -3,15 +3,7 @@ import { inject as service } from '@ember/service';
 import bind from 'frontend-kaleidos/utils/bind';
 
 export default class AgendaAgendaHeaderAgendaActionPopupAgendaitemsComponenet extends Component {
-  @service store;
   @service agendaService;
-
-  @bind
-  async newsletterInfo(agendaitem) {
-    return await this.store.queryOne('newsletter-info', {
-      'filter[agenda-item-treatment][agendaitem][:id:]': agendaitem.id,
-    });
-  }
 
   @bind
   async checkAdded(agendaitem) {
