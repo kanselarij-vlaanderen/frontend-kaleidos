ce21407bf1d9924b1887a94c6957b38646a6dd05
mdv-professional <mdv.werk@outlook.com>
2022-06-16 14:05:38 +0200

    KAS-3431 Release decisions: Automanual: model changes Release decisions: Automanual

--- app/models/themis-publication-activity.js
+++ app/models/themis-publication-activity.js
@@ -2,6 +2,7 @@ import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class ThemisPublicationActivity extends Model {
   @attr('datetime') startDate;
+  @attr('datetime') plannedStart;
   @attr('stringSet') scope;
 
   @belongsTo('meeting') meeting;
