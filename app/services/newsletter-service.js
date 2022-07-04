84ea11ff00dddc546fdbb38e9a575e32a2224e3e
mdv-professional <mdv.werk@outlook.com>
2022-07-04 09:47:09 +0200

    KAS-3431 handle legacy data / NewsletterService.createNewsItemForMeeting Release decisions: Automanual

--- app/services/newsletter-service.js
+++ app/services/newsletter-service.js
@@ -213,9 +213,9 @@ export default class NewsletterService extends Service {
         meeting.themisPublicationActivities,
       ]);
       themisPublicationActivities = themisPublicationActivities.toArray();
-      const internalDocumentPublicationDate = internalDocumentPublicationActivity.plannedPublicationTime ?? internalDocumentPublicationActivity.unconfirmedPublicationTime;
+      const internalDocumentPublicationDate = internalDocumentPublicationActivity?.plannedPublicationTime ?? internalDocumentPublicationActivity?.unconfirmedPublicationTime;
       const plannedThemisPublicationActivity = AgendaPublicationUtils.getPlannedThemisPublicationActivity(themisPublicationActivities);
-      const plannedThemisPublicationDate = plannedThemisPublicationActivity.plannedPublicationTime ?? plannedThemisPublicationActivity.unconfirmedPublicationTime;
+      const plannedThemisPublicationDate = plannedThemisPublicationActivity?.plannedPublicationTime ?? plannedThemisPublicationActivity?.unconfirmedPublicationTime;
       const newsletter = this.store.createRecord('newsletter-info', {
         meeting,
         finished: false,
