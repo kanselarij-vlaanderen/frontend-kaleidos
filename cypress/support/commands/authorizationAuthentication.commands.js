3b9213e2483c3cbe4bec57bae7e05b59c255934d
Sven Valenberghs <sven.valenberghs@faros.be>
2022-07-12 13:17:33 +0200

    implement small delay right after test login to ensure data is loaded

--- cypress/support/commands/authorizationAuthentication.commands.js
+++ cypress/support/commands/authorizationAuthentication.commands.js
@@ -39,6 +39,8 @@ function login(name, retries = 0) {
   });
   cy.intercept('GET', '/accounts/*').as('getAccount');
   cy.intercept('GET', '/accounts/*/user').as('getAccountUser');
+  cy.intercept('GET', '/concepts?filter**').as('loadConcepts');
+  cy.intercept('GET', '/agendas?filter**').as('loadAgendasRoute');
   cy.visit('/overzicht?size=2').wait('@getCurrentSession')
     .then((responseBody) => {
       console.log(responseBody);
@@ -52,6 +54,7 @@ function login(name, retries = 0) {
       }
     });
   cy.wait('@getAccount').wait('@getAccountUser');
+  cy.wait('@loadConcepts').wait('@loadAgendasRoute');
   cy.log('/login');
 }
 
