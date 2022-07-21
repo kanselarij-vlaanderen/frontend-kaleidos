7619051f44d79e1ef5ed5926998ae80ac7d2e239
Sergio Fenoll <sergio@fenoll.be>
2022-07-21 09:30:34 +0200

    refactor: only load access level data when handling said access level

--- app/helpers/is-access-level-in-effect.js
+++ app/helpers/is-access-level-in-effect.js
@@ -6,11 +6,15 @@ import { isPresent } from '@ember/utils';
 export default class IsAccessLevelInEffect extends Helper {
   @service agendaService;
 
-  async compute([piece]) {
+  async compute([accessLevel, piece]) {
+    if (accessLevel.uri === CONSTANTS.ACCESS_LEVELS.INTERN_SECRETARIE) {
+      // Always in effect
+      return true;
+    }
+
     let inEffect = true;
     if (piece) {
       // Always assume that we're dealing with the latest agenda
-      const accessLevel = await piece.accessLevel;
       const agendaitems = (await piece.agendaitems).toArray();
       let agendaitem;
       for (const ag of agendaitems) {
@@ -26,45 +30,40 @@ export default class IsAccessLevelInEffect extends Helper {
       const agenda = await agendaitem.agenda;
       const meeting = await agenda.createdFor;
 
-      const isDesignAgenda = (await agenda.status).isDesignAgenda;
-      const previousAgenda = await agenda.previousAgenda;
-      let pieceChanged = false;
-      if (previousAgenda) {
-        pieceChanged = (
-          await this.agendaService.changedPieces(
-            agenda.id,
-            previousAgenda.id,
-            agendaitem.id
-          )
-        ).includes(piece);
-      }
-      const decisionsReleased = isPresent(meeting.releasedDecisions);
-      const documentsReleased = isPresent(meeting.releasedDocuments);
-      const documentsReleasedToThemis = (
-        await meeting.themisPublicationActivities
-      ).any((activity) => activity.scopes.includes('documents'));
-
       switch (accessLevel.uri) {
-        case CONSTANTS.ACCESS_LEVELS.INTERN_SECRETARIE:
-          // Always in effect
-          inEffect = true;
-          break;
         case CONSTANTS.ACCESS_LEVELS.MINISTERRAAD:
+          const isDesignAgenda = (await agenda.status).isDesignAgenda;
+          const previousAgenda = await agenda.previousAgenda;
+          let pieceChanged = false;
+          if (previousAgenda) {
+            pieceChanged = (
+              await this.agendaService.changedPieces(
+                agenda.id,
+                previousAgenda.id,
+                agendaitem.id
+              )
+            ).includes(piece);
+          }
           if (isDesignAgenda && (!isPresent(previousAgenda) | pieceChanged)) {
             inEffect = false;
           }
           break;
         case CONSTANTS.ACCESS_LEVELS.INTERN_REGERING:
+          const decisionsReleased = isPresent(meeting.releasedDecisions);
           if (!decisionsReleased) {
             inEffect = false;
           }
           break;
         case CONSTANTS.ACCESS_LEVELS.INTERN_OVERHEID:
+          const documentsReleased = isPresent(meeting.releasedDocuments);
           if (!documentsReleased) {
             inEffect = false;
           }
           break;
         case CONSTANTS.ACCESS_LEVELS.PUBLIEK:
+          const documentsReleasedToThemis = (
+            await meeting.themisPublicationActivities
+          ).any((activity) => activity.scope.includes('documents'));
           if (!documentsReleasedToThemis) {
             inEffect = false;
           }
