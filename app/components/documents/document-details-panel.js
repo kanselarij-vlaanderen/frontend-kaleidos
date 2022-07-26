92a2d06dadd0f139e23cb0a81436482505927d25
Michaël Dierick <michael.dierick@redpencil.io>
2022-07-26 17:15:01 +0200

    prettier linting

--- app/components/documents/document-details-panel.js
+++ app/components/documents/document-details-panel.js
@@ -3,7 +3,6 @@ import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { task } from 'ember-concurrency';
-import { isPresent } from '@ember/utils';
 
 export default class DocumentsDocumentDetailsPanel extends Component {
   /**
--- app/components/documents/document-preview/details-tab.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
-import { inject as service } from '@ember/service';
-import { task } from 'ember-concurrency';
-
-export default class DocumentsDocumentPreviewDetailsDetailsTabComponent extends Component {
-
-}
