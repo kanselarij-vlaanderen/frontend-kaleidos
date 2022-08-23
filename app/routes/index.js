ace3c93f7dfa17c7567166426b54c425c488657b
Erika Pauwels <erika.pauwels@gmail.com>
2022-08-23 09:35:14 +0200

    Add automatic transition to publications route for OVRB

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,8 @@ export default class IndexRoute extends Route {
   beforeModel() {
     if (this.currentSession.isKortBestek) {
       this.router.transitionTo('newsletters');
+    } else if (this.currentSession.isOvrb) {
+      this.router.transitionTo('publications');
     } else {
       this.router.transitionTo('agendas');
     }
