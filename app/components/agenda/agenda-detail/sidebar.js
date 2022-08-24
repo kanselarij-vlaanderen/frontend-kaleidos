cbbec679c89ee88538a1415979622b0f9ec4cd0d
Erika Pauwels <erika.pauwels@gmail.com>
2022-08-24 11:46:26 +0200

    Update remark to announcement in inline documentation

--- app/components/agenda/agenda-detail/sidebar.js
+++ app/components/agenda/agenda-detail/sidebar.js
@@ -3,7 +3,7 @@ import templateOnly from '@ember/component/template-only';
 /**
   * @argument notaGroups: Array of AgendaitemGroup-objects
   * @argument isLoadingNotaGroups: boolean indicating whether to show the loading state for nota's
-  * @argument announcements: Array of agendaitems with type remark
+  * @argument announcements: Array of agendaitems with type announcement
   * @argument newItems: items to be marked as "new on this agenda"
   * @argument currentAgenda: the agenda that is currently open
   * @argument showModifiedOnly: if we should filter only on modified agendaitems
