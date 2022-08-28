2775a1b49bceb98340841e715d4d21cf278d9313
Sergio Fenoll <sergio@fenoll.be>
2022-08-28 12:57:50 +0200

    feat: make is-nota and is-announcement helpers compatible with mu-search

--- app/helpers/is-announcement.js
+++ app/helpers/is-announcement.js
@@ -2,7 +2,11 @@ import { helper } from '@ember/component/helper';
 import CONSTANTS from 'frontend-kaleidos/config/constants';
 
 function isAnnouncement([type]) {
-  return type.get('uri') === CONSTANTS.AGENDA_ITEM_TYPES.ANNOUNCEMENT;
+  let uri = type;
+  if (typeof type?.get === 'function') {
+    uri = type.get('uri');
+  }
+  return uri === CONSTANTS.AGENDA_ITEM_TYPES.ANNOUNCEMENT;
 }
 
 export default helper(isAnnouncement);
--- app/helpers/is-nota.js
+++ app/helpers/is-nota.js
@@ -2,7 +2,11 @@ import { helper } from '@ember/component/helper';
 import CONSTANTS from 'frontend-kaleidos/config/constants';
 
 function isNota([type]) {
-  return type.get('uri') === CONSTANTS.AGENDA_ITEM_TYPES.NOTA;
+  let uri = type;
+  if (typeof type?.get === 'function') {
+    uri = type.get('uri');
+  }
+  return uri === CONSTANTS.AGENDA_ITEM_TYPES.NOTA;
 }
 
 export default helper(isNota);
