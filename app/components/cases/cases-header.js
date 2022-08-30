ecac5c98fa7b10c298e10e081677d2cc43df8942
Michaël Dierick <michael.dierick@redpencil.io>
2022-08-30 16:40:01 +0200

    added observations and todo to case header component

--- app/components/cases/cases-header.js
+++ app/components/cases/cases-header.js
@@ -3,6 +3,10 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
+/**
+ * TODO: rename action to something more self-explanatory
+ * @argument close: action passing down a newly created decisionmaking-flow.
+ */
 export default class CasesHeader extends Component {
   @service currentSession;
   @tracked isAddingCase = false;
