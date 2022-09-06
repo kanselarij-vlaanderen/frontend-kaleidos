5713c351ecce954360b52c696864511d471db149
Dieter Peirs <github@dieterpeirs.com>
2022-09-06 13:56:16 +0200

    settings user page update

--- app/pods/settings/users/user/controller.js
+++ app/pods/settings/users/user/controller.js
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class SettingsUsersUserController extends Controller {
   @service intl;
@@ -12,4 +13,44 @@ export default class SettingsUsersUserController extends Controller {
     await this.model.save();
     this.toaster.success(this.intl.t('successfully-saved'));
   }
+
+  @tracked showBlockRelation = false;
+
+  @action
+  blockRelation() {
+    this.showBlockRelation = !this.showBlockRelation;
+  }
+
+  @action
+  addBlockRelation() {
+    this.showBlockRelation = !this.showBlockRelation;
+    this.toaster.success("De werkrelatie is geblokkeerd.");
+  }
+
+  @tracked showUnblockRelation = false;
+
+  @action
+  unblockRelation() {
+    this.showUnblockRelation = !this.showUnblockRelation;
+  }
+
+  @action
+  addUnblockRelation() {
+    this.showUnblockRelation = !this.showUnblockRelation;
+    this.toaster.success("De werkrelatie is geblokkeerd.");
+  }
+
+  @tracked showBlockPerson = false;
+
+  @action
+  blockPerson() {
+    this.showBlockPerson = !this.showBlockPerson;
+  }
+
+  @action
+  addBlockPerson() {
+    this.showBlockPerson = !this.showBlockPerson;
+    this.toaster.success("De persoon is geblokkeerd.");
+  }
+
 }
