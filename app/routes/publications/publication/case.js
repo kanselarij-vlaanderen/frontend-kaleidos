7fbb0ef8860735098083fc695009d57e927c2509
Erika Pauwels <erika.pauwels@gmail.com>
2022-09-09 13:04:29 +0200

    Remove unused service injection

--- app/routes/publications/publication/case.js
+++ app/routes/publications/publication/case.js
@@ -1,9 +1,6 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default class CaseRoute extends Route {
-  @service publicationService;
-
   model() {
     return this.modelFor('publications.publication');
   }
