d3d9dc788afd6d1ca25257cf506b7a853efa2cb9
Erika Pauwels <erika.pauwels@gmail.com>
2022-09-24 20:56:40 +0200

    Refactor Utils::BackButton to Glimmer component

--- app/components/utils/back-button.js
+++ app/components/utils/back-button.js
@@ -1,21 +1,15 @@
-// TODO: octane-refactor
-// eslint-disable-next-line ember/no-classic-components
-import Component from '@ember/component';
-import { inject } from '@ember/service';
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
-// TODO: octane-refactor
-// eslint-disable-next-line ember/no-classic-classes, ember/require-tagless-components
-export default Component.extend({
-  router: inject(),
-  defaultRoute: 'agendas',
-  // TODO: octane-refactor
-  // eslint-disable-next-line ember/no-actions-hash
-  actions: {
-    goToParentRoute() {
-      const parentRoute = this.router.currentRoute.parent;
-      if (parentRoute && parentRoute.name !== 'application') {
-        this.router.transitionTo(parentRoute.name);
-      }
-    },
-  },
-});
+export default class UtilsBackButtonComponent extends Component {
+  @service router;
+
+  @action
+  goToParentRoute() {
+    const parentRoute = this.router.currentRoute.parent;
+    if (parentRoute && parentRoute.name !== 'application') {
+      this.router.transitionTo(parentRoute.name);
+    }
+  }
+}
