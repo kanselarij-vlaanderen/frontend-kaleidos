9d6d7a5bf34c80dbb32e5ab7b8767b84aa330aad
Sergio Fenoll <sergio@fenoll.be>
2022-09-30 16:44:25 +0200

    feat: perform checks before trying to send mail campaign
    And show a red toast if the checks fail

--- app/components/newsletter/newsletter-header-overview.js
+++ app/components/newsletter/newsletter-header-overview.js
@@ -3,6 +3,7 @@ import { action } from '@ember/object';
 import moment from 'moment';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import { isPresent } from '@ember/utils';
 import { task } from 'ember-concurrency';
 import CONSTANTS from 'frontend-kaleidos/config/constants';
 
@@ -77,25 +78,32 @@ export default class NewsletterHeaderOverviewComponent extends Component {
   @task
   *publishToMail() {
     try {
-      yield this.ensureMailCampaign();
+      if (yield this.canSendMailCampaign()) {
+        yield this.ensureMailCampaign();
 
-      if (this.mailCampaign?.isSent) {
-        this.toaster.error(this.intl.t('error-already-sent-newsletter'));
-      } else {
-        if (yield this.validateMailCampaign()) {
-          try {
-            yield this.newsletterService.sendMailCampaign(this.mailCampaign.campaignId);
-            this.mailCampaign.sentAt = new Date();
-            yield this.mailCampaign.save();
-            this.toaster.success(this.intl.t('success-publish-newsletter-to-mail'));
-          } catch(e) {
-            console.log("error sending newsletter", e);
-            this.toaster.error(
-              this.intl.t('error-send-newsletter'),
-              this.intl.t('warning-title')
-            );
+        if (this.mailCampaign?.isSent) {
+          this.toaster.error(this.intl.t('error-already-sent-newsletter'));
+        } else {
+          if (yield this.validateMailCampaign()) {
+            try {
+              yield this.newsletterService.sendMailCampaign(this.mailCampaign.campaignId);
+              this.mailCampaign.sentAt = new Date();
+              yield this.mailCampaign.save();
+              this.toaster.success(this.intl.t('success-publish-newsletter-to-mail'));
+            } catch(e) {
+              console.log("error sending newsletter", e);
+              this.toaster.error(
+                this.intl.t('error-send-newsletter'),
+                this.intl.t('warning-title')
+              );
+            }
           }
         }
+      } else {
+        this.toaster.error(
+          this.intl.t('error-cannot-send-newsletter'),
+          this.intl.t('warning-title')
+        );
       }
       this.showConfirmPublishMail = false;
     } catch (error) {
@@ -179,6 +187,34 @@ export default class NewsletterHeaderOverviewComponent extends Component {
     }
 
     this.showConfirmPublishAll = false;
+  }
+
+  async canSendMailCampaign() {
+    const documentPublicationActivity = await this.args.meeting.internalDocumentPublicationActivity;
+
+    let hasDocumentPublicationDate = isPresent(documentPublicationActivity.plannedDate);
+    let hasNotas = false;
+    let hasThemes = false;
+
+    const agendaitems = await this.args.agenda.agendaitems;
+    for (const agendaitem of agendaitems.toArray()) {
+      const agendaitemTreatment = await agendaitem.treatment;
+      const newsletterItem = await agendaitemTreatment.newsletterInfo;
+      const themes = await newsletterItem?.themes;
+      if (themes?.length) {
+        hasThemes = true;
+      } else {
+        hasThemes = false;
+        break
+      }
+
+      const type = await agendaitem.type;
+      if (type.uri === CONSTANTS.AGENDA_ITEM_TYPES.NOTA) {
+        hasNotas = true;
+      }
+    }
+
+    return hasDocumentPublicationDate && hasNotas && hasThemes;
   }
 
   async validateMailCampaign() {
