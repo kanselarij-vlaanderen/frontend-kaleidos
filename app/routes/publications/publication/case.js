b626aa803161907e8f9994c855f47410364791a0
Sergio Fenoll <sergio@fenoll.be>
2022-09-30 13:15:55 +0200

    chore: remove publication.case controller and unused loading in route

--- app/routes/publications/publication/case.js
+++ app/routes/publications/publication/case.js
@@ -1,29 +1,7 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default class CaseRoute extends Route {
-  @service store;
-  @service publicationService;
-
   model() {
     return this.modelFor('publications.publication');
-  }
-
-  async afterModel(model) {
-    this.governmentAreas = await model.governmentAreas;
-    const isViaCouncilOfMinisters = await this.publicationService.getIsViaCouncilOfMinisters(model);
-    if (isViaCouncilOfMinisters) {
-      const decisionActivity = await model.decisionActivity;
-      const [meetingId, agendaId, ] = await this.publicationService.getModelsForAgendaitemFromDecisionActivity(decisionActivity);
-      this.meeting = await this.store.findRecord('meeting', meetingId);
-      this.agenda = await this.store.findRecord('agenda', agendaId);
-    }
-  }
-
-  setupController(controller) {
-    super.setupController(...arguments);
-    controller.governmentAreas = this.governmentAreas;
-    controller.meeting = this.meeting;
-    controller.agenda = this.agenda;
   }
 }
