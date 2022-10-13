3dd9d08c0558a80641c0a3404e82255614c7cbf0
Sergio Fenoll <sergio@fenoll.be>
2022-10-13 09:42:12 +0200

    feat: unblock users and memberships from overview

--- app/controllers/settings/users/index.js
+++ app/controllers/settings/users/index.js
@@ -20,6 +20,8 @@ export default class UsersSettingsController extends Controller {
 
   @tracked showBlockMembership = false;
   @tracked showBlockUser = false;
+  @tracked showUnblockMembership = false;
+  @tracked showUnblockUser = false;
 
   @action
   search(e) {
@@ -30,6 +32,12 @@ export default class UsersSettingsController extends Controller {
   @action
   toggleShowBlockMembership() {
     this.showBlockMembership = !this.showBlockMembership;
+    this.membershipsBeingBlocked = [];
+  }
+
+  @action
+  toggleShowUnblockMembership() {
+    this.showUnblockMembership = !this.showUnblockMembership;
     this.membershipsBeingBlocked = [];
   }
 
@@ -50,10 +58,26 @@ export default class UsersSettingsController extends Controller {
   }
 
   @action
+  async unblockUser() {
+    const allowed = await this.store.findRecordByUri('concept', CONSTANTS.USER_ACCESS_STATUSES.ALLOWED);
+    this.userBeingBlocked.status = allowed;
+    await this.userBeingBlocked.save();
+  }
+
+  @action
   async blockMemberships() {
     const blocked = await this.store.findRecordByUri('concept', CONSTANTS.USER_ACCESS_STATUSES.BLOCKED);
     for (const membership of this.membershipsBeingBlocked) {
       membership.status = blocked;
+    }
+    await Promise.all(this.membershipsBeingBlocked.map((membership) => membership.save()));
+  }
+
+  @action
+  async unblockMemberships() {
+    const allowed = await this.store.findRecordByUri('concept', CONSTANTS.USER_ACCESS_STATUSES.ALLOWED);
+    for (const membership of this.membershipsBeingBlocked) {
+      membership.status = allowed;
     }
     await Promise.all(this.membershipsBeingBlocked.map((membership) => membership.save()));
   }
