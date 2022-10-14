3a0a10bb267fa2a0d389af6b32eafbad3d1fbe2c
Sergio Fenoll <sergio@fenoll.be>
2022-10-14 10:10:06 +0200

    feat: show last known login time in user overview

--- app/routes/settings/users/index.js
+++ app/routes/settings/users/index.js
@@ -42,7 +42,7 @@ export default class SettingsUsersIndexRoute extends Route {
         number: params.page,
         size: params.size,
       },
-      include: 'memberships.organization,memberships.role',
+      include: 'status,memberships.organization,memberships.role,memberships.status,login-activities',
     };
 
     if (isPresent(params.filter)) {
