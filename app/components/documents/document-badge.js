a8d9bd01ce61eed746df5addfdca826bb63a9865
Erika Pauwels <erika.pauwels@gmail.com>
2022-11-02 13:21:46 +0100

    Add isDraftAccessLevel method to access-level-service

---
+++ app/components/documents/document-badge.js
@@ -0,0 +1,28 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { keepLatestTask } from 'ember-concurrency';
+
+/**
+ * @argument doc Piece to show in the badge:
+ * @argument isShowingAccessLevel Whether access level pill must be shown
+ * @argument agendaContext Meeting/agenda/agendaitem the badge is shown for
+ * @argument isHighlighted Whether badge must be highlighted
+ */
+export default class DcoumentsDocumentBadgeComponent extends Component {
+  @service pieceAccessLevelService;
+
+  @tracked isDraftAccessLevel;
+
+  constructor() {
+    super(...arguments);
+    this.loadData.perform();
+  }
+
+  @keepLatestTask
+  *loadData() {
+    const accessLevel = yield this.args.doc.accessLevel;
+    const context = Object.assign(this.args.agendaContext || {}, { piece: this.args.doc });
+    this.isDraftAccessLevel = yield this.pieceAccessLevelService.isDraftAccessLevel(accessLevel, context);
+  }
+}
