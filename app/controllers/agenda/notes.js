d5bf92be27ba7c143ac2c0b9bb93d35909d6fa96
Dieter Peirs <github@dieterpeirs.com>
2022-11-21 16:02:31 +0100

    add save and edit to notulen

--- app/controllers/agenda/notes.js
+++ app/controllers/agenda/notes.js
@@ -6,6 +6,18 @@ export default class AgendaNotesController extends Controller {
   editorInstance;
 
   @tracked isFullscreen = false;
+  @tracked isEditing = false;
+
+  @action
+  toggleEdit() {
+    this.isEditing = !this.isEditing;
+  }
+
+  @action
+  toggleAll() {
+    this.isFullscreen = false;
+    this.isEditing = false;
+  }
 
   @action
   toggleFullscreen() {
