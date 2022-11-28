ed31499fef3c173001f1c8bbcb149403f1f26731
Sergio Fenoll <sergio@fenoll.be>
2022-11-28 10:01:56 +0100

    fix: export the right function name and escape 'at' in datetime-at

--- app/helpers/datetime-at.js
+++ app/helpers/datetime-at.js
@@ -1,6 +1,6 @@
 import { helper } from '@ember/component/helper';
 import { dateFormat } from 'frontend-kaleidos/utils/date-format';
 
-export default helper(function datetimeAt([datetimeOrString, at="om"]) {
-  return dateFormat(datetimeOrString, `dd-MM-yyyy ${at} HH:mm`);
+export default helper(function datetimeAt([datetimeOrString, at = "om"]) {
+  return dateFormat(datetimeOrString, `dd-MM-yyyy '${at}' HH:mm`);
 });
--- app/utils/date-format.js
+++ app/utils/date-format.js
@@ -1,6 +1,6 @@
 import { format } from 'date-fns';
 
-export function dateHelper(dateOrString, formatString) {
+export function dateFormat(dateOrString, formatString) {
   if (formatString) {
     // new Date(null) returns a date representing the Unix epoch
     // This is undesired, we actually want an invalid date in that case
