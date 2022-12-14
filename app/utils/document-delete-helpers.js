a346dfc2353a70696513d47d6aebdae5e2954d0e
Sergio Fenoll <sergio@fenoll.be>
2022-12-14 16:54:10 +0100

    docs: adds some documentation to the document delete helpers

--- app/utils/document-delete-helpers.js
+++ app/utils/document-delete-helpers.js
@@ -1,3 +1,9 @@
+/**
+ * Deletes the provided document container's pieces, and also itself.
+ *
+ * @param documentContainerOrPromise {DocumentContainer | Promise<DocumentContainer>}
+ * @returns {Promise}
+ */
 export async function deleteDocumentContainer(documentContainerOrPromise) {
   const documentContainer = await documentContainerOrPromise;
   if (documentContainer) {
@@ -5,20 +11,36 @@ export async function deleteDocumentContainer(documentContainerOrPromise) {
     for (let piece of pieces) {
       await deletePiece(piece);
     }
+  }
+}
+
+/**
+ * Deletes the provided piece and its file(s), and its document container if the
+ * container would otherwise be orphaned.
+ *
+ * @param pieceOrPromise {Piece | Promise<Piece>}
+ * @returns {Promise}
+ */
+export async function deletePiece(pieceOrPromise) {
+  const piece = await pieceOrPromise;
+  const documentContainer = await piece.documentContainer;
+  if (piece) {
+    const file = await piece.file;
+    await deleteFile(file);
+    await piece.destroyRecord();
+  }
+  const allPieces = await documentContainer.pieces;
+  if (allPieces.length === 0) {
     return documentContainer.destroyRecord();
   }
 }
 
-export async function deletePiece(pieceOrPromise) {
-  const piece = await pieceOrPromise;
-  if (piece) {
-    const file = await piece.file;
-    await deleteFile(file);
-    return piece.destroyRecord();
-  }
-  // TODO: delete container in case we just orphaned it
-}
-
+/**
+ * Deletes the provided file and its derived file if it exists.
+ *
+ * @param fileOrPromise {File | Promise<File>}
+ * @returns {Promise}
+ */
 export async function deleteFile(fileOrPromise) {
   const file = await fileOrPromise;
   if (file) {
