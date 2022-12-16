84b35de1c13a80bfe7af9e9897398c6806e97aad
Sven Valenberghs <sven.valenberghs@faros.be>
2022-12-16 14:10:53 +0100

    KAS-3807 change profile tests to check dragging availability

--- cypress/integration/unit/profile-secretarie.spec.js
+++ cypress/integration/unit/profile-secretarie.spec.js
@@ -124,6 +124,7 @@ context('Testing the application as Kanselarij user', () => {
       cy.get(agenda.agendaActions.planReleaseDocuments).should('not.exist');
       cy.get(agenda.agendaActions.publishToWeb).should('not.exist');
       cy.get(agenda.agendaActions.unpublishFromWeb).should('not.exist');
+      cy.clickReverseTab('Overzicht'); // close dropdown
 
       // Main view - Search
       cy.get(agenda.agendaitemSearch.input);
@@ -131,6 +132,10 @@ context('Testing the application as Kanselarij user', () => {
       // Overview Tab - General actions
       cy.get(agenda.agendaOverview.showChanges);
       cy.get(agenda.agendaOverview.formallyOkEdit);
+
+      // Overview Tab - General action - Dragging
+      cy.get(agenda.agendaOverviewItem.dragging).should('not.exist');
+      cy.get(agenda.agendaOverview.formallyOkEdit).click();
       cy.get(agenda.agendaOverviewItem.dragging);
 
       // Detail Tab - tabs
