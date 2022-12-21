a11fc58ef09f32213eaf8b5966b72ddb670fd931
Erika Pauwels <erika.pauwels@gmail.com>
2022-12-21 13:53:00 +0100

    Update manual

--- app/routes/manual.js
+++ app/routes/manual.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class ManualRoute extends Route {
   beforeModel() {
-    window.location.replace('/Kaleidos_Raadplegen_20200629.pdf');
+    window.location.replace('/handleiding.pdf');
   }
 }
