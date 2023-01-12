da2eb62c22edb20d7c67882a6821128a21b8b27a
stuycru <stuycru@cronos.be>
2023-01-12 10:37:24 +0100

    added reload on relation belongsto agenda

--- app/components/agenda/agenda-header/agenda-version-actions.js
+++ app/components/agenda/agenda-header/agenda-version-actions.js
@@ -544,6 +544,7 @@ export default class AgendaAgendaHeaderAgendaVersionActions extends Component {
   async reloadMeeting() {
     // This is a workaround for route not reloading attributes and agendas on refresh model
     await this.args.meeting.reload();
+    await this.args.meeting.belongsTo('agenda').reload();
     await this.args.meeting.hasMany('agendas').reload();
   }
 
