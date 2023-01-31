e5aac001467896b5db4682947b01f6b15a63345c
Kobe Mertens <kobe.mertens@hotmail.com>
2023-01-31 12:03:58 +0100

    added search.documents route

---
+++ app/controllers/search/documents.js
@@ -0,0 +1,70 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import { isEmpty } from '@ember/utils';
+
+export default class CasesSearchController extends Controller {
+  @service router;
+  @service intl;
+
+  queryParams = [
+    {
+      confidentialOnly: {
+        type: 'boolean',
+      },
+      page: {
+        type: 'number',
+      },
+      size: {
+        type: 'number',
+      },
+      sort: {
+        type: 'string',
+      },
+    },
+  ];
+
+  sizeOptions = [5, 10, 20, 50, 100, 200];
+  sortOptions = [
+    { value: '-session-dates', label: this.intl.t('meeting-date') },
+    { value: '', label: this.intl.t('relevance-score') }, // empty string as value because null is not handled correctly by select-element
+  ];
+
+  @tracked page;
+  @tracked size;
+  @tracked sort;
+  @tracked confidentialOnly;
+  @tracked searchText;
+
+  constructor() {
+    super(...arguments);
+    this.page = 0;
+    this.size = this.sizeOptions[2];
+    this.sort = this.sortOptions[1].value;
+    this.confidentialOnly = false;
+  }
+
+  get emptySearch() {
+    return isEmpty(this.searchText);
+  }
+
+  @action
+  selectSize(size) {
+    this.size = size;
+  }
+
+  @action
+  selectSort(event) {
+    this.sort = event.target.value;
+  }
+
+  @action
+  navigateToCase(decisionmakingFlow) {
+    this.router.transitionTo('cases.case.subcases', decisionmakingFlow.id);
+  }
+
+  get customFiltersElement() {
+    return document.getElementById('search-subroute-filters-area')
+  }
+}
---
+++ app/routes/search/documents.js
@@ -0,0 +1,148 @@
+import Route from '@ember/routing/route';
+import { isEmpty } from '@ember/utils';
+import { action } from '@ember/object';
+import { startOfDay, endOfDay, parse } from 'date-fns';
+import search from 'frontend-kaleidos/utils/mu-search';
+import Snapshot from 'frontend-kaleidos/utils/snapshot';
+
+export default class CasesSearchRoute extends Route {
+  queryParams = {
+    confidentialOnly: {
+      refreshModel: true,
+      as: 'enkel_vertrouwelijk',
+    },
+    page: {
+      refreshModel: true,
+      as: 'pagina',
+    },
+    size: {
+      refreshModel: true,
+      as: 'aantal',
+    },
+    sort: {
+      refreshModel: true,
+      as: 'sorteer',
+    },
+  };
+
+  postProcessDates(_case) {
+    const { sessionDates } = _case.attributes;
+    if (sessionDates) {
+      if (Array.isArray(sessionDates)) {
+        const sorted = sessionDates.sort();
+        _case.attributes.sessionDates = sorted[sorted.length - 1];
+      } else {
+        _case.attributes.sessionDates = sessionDates;
+      }
+    }
+  }
+
+  constructor() {
+    super(...arguments);
+    this.lastParams = new Snapshot();
+  }
+
+  model(filterParams) {
+    const searchParams = this.paramsFor('search');
+    const params = { ...searchParams, ...filterParams };
+
+    this.lastParams.stageLive(params);
+
+    if (
+      this.lastParams.anyFieldChanged(
+        Object.keys(params).filter((key) => key !== 'page')
+      )
+    ) {
+      params.page = 0;
+    }
+
+    const textSearchFields = ['title^3', 'fileName^2'];
+
+    const searchModifier = ':sqs:';
+    const textSearchKey = textSearchFields.join(',');
+
+    const filter = {};
+
+    if (!isEmpty(params.searchText)) {
+      filter[searchModifier + textSearchKey] = params.searchText;
+    }
+
+    if (!isEmpty(params.mandatees)) {
+      filter['mandateeFirstNames,mandateeFamilyNames'] = params.mandatees;
+    }
+
+    /* A closed range is treated as something different than 2 open ranges because
+     * mu-search(/elastic?) (semtech/mu-search:0.6.0-beta.11, semtech/mu-search-elastic-backend:1.0.0)
+     * returns an off-by-one result (1 to many) in case of two open ranges combined.
+     */
+    if (!isEmpty(params.dateFrom) && !isEmpty(params.dateTo)) {
+      const from = startOfDay(parse(params.dateFrom, 'dd-MM-yyyy', new Date()));
+      const to = endOfDay(parse(params.dateTo, 'dd-MM-yyyy', new Date())); // "To" interpreted as inclusive
+      filter[':lte,gte:created'] = [
+        to.toISOString(),
+        from.toISOString(),
+      ].join(',');
+    } else if (!isEmpty(params.dateFrom)) {
+      const date = startOfDay(parse(params.dateFrom, 'dd-MM-yyyy', new Date()));
+      filter[':gte:created'] = date.toISOString();
+    } else if (!isEmpty(params.dateTo)) {
+      const date = endOfDay(parse(params.dateTo, 'dd-MM-yyyy', new Date())); // "To" interpreted as inclusive
+      filter[':lte:created'] = date.toISOString();
+    }
+
+    if (params.confidentialOnly) {
+      filter[':lte:confidentialityLevel'] = 2;
+    }
+
+    this.lastParams.commit();
+
+    if (isEmpty(params.searchText)) {
+      return [];
+    }
+
+    // session-dates can contain multiple values.
+    // Depending on the sort order (desc, asc) we need to aggregrate the values using min/max
+    let sort = params.sort;
+    if (params.sort === 'session-dates') {
+      sort = ':min:session-dates';
+    } else if (params.sort === '-session-dates') {
+      sort = '-:max:session-dates'; // correctly converted to mu-search syntax by the mu-search util
+    }
+
+    const { postProcessDates } = this;
+    return search(
+      'pieces',
+      params.page,
+      params.size,
+      sort,
+      filter,
+      (searchData) => {
+        const entry = searchData.attributes;
+        entry.id = searchData.id;
+        postProcessDates(searchData);
+        return entry;
+      }
+    );
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+
+    if (controller.page !== this.lastParams.committed.page) {
+      controller.page = this.lastParams.committed.page;
+    }
+
+    controller.searchText = this.paramsFor('search').searchText;
+  }
+
+  @action
+  loading(transition) {
+    // eslint-disable-next-line ember/no-controller-access-in-routes
+    const controller = this.controllerFor(this.routeName);
+    controller.isLoadingModel = true;
+    transition.promise.finally(() => {
+      controller.isLoadingModel = false;
+    });
+    return true;
+  }
+}
