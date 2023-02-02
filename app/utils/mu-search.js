251993f9574c88420f896f418c1afefe98161621
Kobe Mertens <kobe.mertens@hotmail.com>
2023-02-02 12:02:03 +0100

    autoformat

--- app/utils/mu-search.js
+++ app/utils/mu-search.js
@@ -6,7 +6,8 @@ import getPaginationMetadata from './get-pagination-metadata';
 function sortOrder(sort) {
   if (sort.startsWith('-')) {
     return 'desc';
-  } if (sort.length > 0) {
+  }
+  if (sort.length > 0) {
     return 'asc';
   }
   return null;
@@ -20,14 +21,23 @@ function snakeToCamel(text) {
   return text.replace(/(-\w)/g, (entry) => entry[1].toUpperCase());
 }
 
-async function muSearch(index, page, size, sort, filter, dataMapping) {
+async function muSearch(
+  index,
+  page,
+  size,
+  sort,
+  filter,
+  dataMapping,
+) {
   const endpoint = new URL(`/${index}/search`, window.location.origin);
-  const params = new URLSearchParams(Object.entries({
-    'page[size]': size,
-    'page[number]': page,
-    // eslint-disable-next-line camelcase
-    collapse_uuids: 't',
-  }));
+  const params = new URLSearchParams(
+    Object.entries({
+      'page[size]': size,
+      'page[number]': page,
+      // eslint-disable-next-line camelcase
+      collapse_uuids: 't',
+    })
+  );
 
   for (const field in filter) {
     params.append(`filter[${field}]`, filter[field]);
@@ -39,16 +49,15 @@ async function muSearch(index, page, size, sort, filter, dataMapping) {
 
   endpoint.search = params.toString();
 
-  const {
-    count, data,
-  } = await (await fetch(endpoint)).json();
+  const { count, data } = await (await fetch(endpoint)).json();
   const pagination = getPaginationMetadata(page, size, count);
   const entries = A(await Promise.all(data.map(dataMapping)));
 
   return ArrayProxy.create({
     content: entries,
     meta: {
-      count, pagination,
+      count,
+      pagination,
     },
   });
 }
