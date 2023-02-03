af6fbf90ab91415df4b43c46e8d1328e4fe46d3f
Sergio Fenoll <sergio@fenoll.be>
2023-02-03 11:35:17 +0100

    fix: import sanitizeHtml from the correct pacakge in news items search

--- app/controllers/search/news-items.js
+++ app/controllers/search/news-items.js
@@ -3,7 +3,7 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { isEmpty } from '@ember/utils';
-import sanitizeHtml from 'ember-cli-sanitize-html';
+import sanitizeHtml from 'sanitize-html';
 
 export default class SearchNewsItemsControllers extends Controller {
   @service router;
