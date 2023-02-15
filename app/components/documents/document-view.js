2c5f17fb333e3800672d8d216482ac775088ef25
stuycru <stuycru@cronos.be>
2023-02-15 12:20:05 +0100

    fix warning message shows when bronbestand was docx file before converting I added a workaround using a timeout that shows the warning after 0.5s if there's a docx file

--- app/components/documents/document-view.js
+++ app/components/documents/document-view.js
@@ -1,9 +1,19 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 const PDF_MIME = 'application/pdf';
 const PDF_EXTENSION = 'pdf';
 
 export default class DocumentsDocumentView extends Component {
+  @tracked showWarning = false;
+
+  constructor() {
+    super(...arguments);
+    //this is a workaround to delay the showing of the warning message for incompatible file types
+    setTimeout(() => {
+      this.showWarning = true
+    }, 500)
+  }
   get isPdfDocument() {
     if (this.args.file) {
       return this.args.file.get('format').toLowerCase().includes(PDF_MIME)
@@ -12,4 +22,5 @@ export default class DocumentsDocumentView extends Component {
       return false;
     }
   }
+
 }
