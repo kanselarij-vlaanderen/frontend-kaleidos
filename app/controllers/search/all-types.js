6a0b0b2e7449b29de3e5296aff5b6e9288865f64
Kobe Mertens <kobe.mertens@hotmail.com>
2023-02-28 14:44:25 +0100

    feat: add all-types search route

---
+++ app/controllers/search/all-types.js
@@ -0,0 +1,53 @@
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import { isEmpty } from '@ember/utils';
+
+export default class AllTypesController extends Controller {
+  @service router;
+  @service intl;
+
+  queryParams = [
+    {
+      sort: {
+        type: 'string',
+      },
+    },
+  ];
+
+  sortOptions = [
+    { value: '-session-dates', label: this.intl.t('meeting-date') },
+    { value: '', label: this.intl.t('relevance-score') }, // empty string as value because null is not handled correctly by select-element
+  ];
+
+  @tracked sort;
+
+  constructor() {
+    super(...arguments);
+    this.sort = this.sortOptions[1].value;
+  }
+
+  get emptySearch() {
+    return isEmpty(this.searchText);
+  }
+
+  @action
+  selectSize(size) {
+    this.size = size;
+  }
+
+  @action
+  selectSort(event) {
+    this.sort = event.target.value;
+  }
+
+  @action
+  navigateToCase(decisionmakingFlow) {
+    this.router.transitionTo('cases.case.subcases', decisionmakingFlow.id);
+  }
+
+  get customFiltersElement() {
+    return document.getElementById('search-subroute-filters-area');
+  }
+}
---
+++ app/routes/search/all-types.js
@@ -0,0 +1,156 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { isEmpty } from '@ember/utils';
+import { action } from '@ember/object';
+import search from 'frontend-kaleidos/utils/mu-search';
+import Snapshot from 'frontend-kaleidos/utils/snapshot';
+import AgendaItemsSearchRoute from './agendaitems';
+import CasesSearchRoute from './cases';
+import SearchDocumentsRoute from './documents';
+import SearchNewsItemsRoute from './news-items';
+import SearchDecisionsRoute from './decisions';
+
+export default class AllTypes extends Route {
+  @service store;
+
+  queryParams = {
+    sort: {
+      refreshModel: true,
+      as: 'sorteer',
+    },
+  };
+
+  CONTENT_TYPES = [
+    {
+      name: 'decisionmaking-flows',
+      searchFields: CasesSearchRoute.textSearchFields,
+      highlightFields: CasesSearchRoute.highlightFields,
+      dataMapping: CasesSearchRoute.postProcessData,
+      createFilter: CasesSearchRoute.createFilter,
+    },
+    {
+      name: 'agendaitems',
+      searchFields: AgendaItemsSearchRoute.textSearchFields,
+      highlightFields: AgendaItemsSearchRoute.highlightFields,
+      dataMapping: AgendaItemsSearchRoute.postProcessData,
+      createFilter: AgendaItemsSearchRoute.createFilter,
+    },
+    {
+      name: 'pieces',
+      searchFields: SearchDocumentsRoute.textSearchFields,
+      highlightFields: SearchDocumentsRoute.highlightFields,
+      dataMapping: SearchDocumentsRoute.postProcessData,
+      createFilter: SearchDocumentsRoute.createFilter,
+    },
+    {
+      name: 'agendaitems',
+      searchFields: SearchDecisionsRoute.textSearchFields,
+      highlightFields: SearchDecisionsRoute.highlightFields,
+      dataMapping: SearchDecisionsRoute.postProcessData,
+      createFilter: SearchDecisionsRoute.createFilter,
+    },
+    {
+      name: 'news-items',
+      searchFields: SearchNewsItemsRoute.textSearchFields,
+      highlightFields: SearchNewsItemsRoute.highlightFields,
+      dataMapping: SearchNewsItemsRoute.postProcessData,
+      createFilter: SearchNewsItemsRoute.createFilter,
+    },
+  ];
+
+  constructor() {
+    super(...arguments);
+    this.lastParams = new Snapshot();
+  }
+
+  async model(filterParams) {
+    const searchParams = this.paramsFor('search');
+    const params = { ...searchParams, ...filterParams };
+
+    this.lastParams.stageLive(params);
+
+    const results = await Promise.all(
+      this.CONTENT_TYPES.map((type) => {
+        const searchModifier = ':sqs:';
+        const textSearchKey = type.searchFields.join(',');
+
+        const filter = type.createFilter(searchModifier, textSearchKey, params);
+
+        this.lastParams.commit();
+
+        if (isEmpty(params.searchText)) {
+          return [];
+        }
+
+        // session-dates can contain multiple values.
+        // Depending on the sort order (desc, asc) we need to aggregrate the values using min/max
+        let sort = params.sort;
+        if (params.sort === 'session-dates') {
+          sort = ':min:session-dates';
+        } else if (params.sort === '-session-dates') {
+          sort = '-:max:session-dates'; // correctly converted to mu-search syntax by the mu-search util
+        }
+
+        return search(
+          type.name,
+          0,
+          10,
+          sort,
+          filter,
+          (searchData) => type.dataMapping(searchData, this.store),
+          {
+            fields: type.highlightFields,
+          }
+        );
+
+        //   return {
+        //     type: type.name,
+        //     data: results,
+        //   };
+
+        // return async () => {
+        //   const results = await search(
+        //     type.name,
+        //     0,
+        //     10,
+        //     sort,
+        //     filter,
+        //     (searchData) => type.dataMapping(searchData, this.store),
+        //     {
+        //       fields: type.highlightFields,
+        //     }
+        //   );
+
+        //   return {
+        //     type: type.name,
+        //     data: results,
+        //   };
+        // };
+      })
+    );
+
+    return results;
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+    const searchText = this.paramsFor('search').searchText;
+
+    if (controller.page !== this.lastParams.committed.page) {
+      controller.page = this.lastParams.committed.page;
+    }
+
+    controller.searchText = searchText;
+  }
+
+  @action
+  loading(transition) {
+    // eslint-disable-next-line ember/no-controller-access-in-routes
+    const controller = this.controllerFor(this.routeName);
+    controller.isLoadingModel = true;
+    transition.promise.finally(() => {
+      controller.isLoadingModel = false;
+    });
+    return true;
+  }
+}
