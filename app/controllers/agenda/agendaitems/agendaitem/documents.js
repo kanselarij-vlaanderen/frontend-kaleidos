df7fddc2656614a81093d2e3aafcd49d862313e1
Sven Valenberghs <sven.valenberghs@faros.be>
2023-03-01 14:40:04 +0100

    KAS-3845 fix rollback of document to get previousPiece before delete, no double delete of doc container

--- app/controllers/agenda/agendaitems/agendaitem/documents.js
+++ app/controllers/agenda/agendaitems/agendaitem/documents.js
@@ -214,9 +214,8 @@ export default class DocumentsAgendaitemsAgendaController extends Controller {
   }
 
   @action
-  async setPreviousPiecesFromAgendaitem(deletedPiece) {
-    // TODO: Assess if we need to go over container. `previousVersion` (if existant) might suffice?
-    const documentContainer = await deletedPiece.documentContainer;
+  async setPreviousPiecesFromAgendaitem(previousPiece) {
+    const documentContainer = await previousPiece?.documentContainer;
     if (documentContainer) {
       const pieces = documentContainer.pieces;
       if (this.agendaitem && pieces.length > 0) {
