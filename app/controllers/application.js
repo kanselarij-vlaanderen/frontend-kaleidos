00efb52a2afdedd943c222e0b0496ce35991d7a0
Sven Valenberghs <sven.valenberghs@faros.be>
2023-03-13 17:29:35 +0100

    KAS-3845 revert removing services from application

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,11 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
+  @service currentSession;
+  @service systemAlert;
+  @service toaster;
+
   constructor() {
     super(...arguments);
     document.addEventListener('wheel', () => {
