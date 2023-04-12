0283e4a011bd44f93b0112b9b87127ce571a1f18
Sergio Fenoll <sergio@fenoll.be>
2023-04-12 13:02:37 +0200

    feat: use agenda status activity in all of side-nav component

--- app/components/agenda/side-nav.js
+++ app/components/agenda/side-nav.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import { task } from 'ember-concurrency';
 
 export default class AgendaSideNavComponent extends Component {
   /**
@@ -13,25 +12,12 @@ export default class AgendaSideNavComponent extends Component {
   @service store;
 
   @tracked isCollapsedSidebar = false;
-  @tracked agendaHasStatusActivities = false;
-  @tracked mostRecentStatusActivity = undefined;
-  @tracked mostRecentStatusActivityIsApproved = false
 
-  constructor() {
-    super(...arguments);
-    this.loadData.perform();
-  }
-
-  @task
-  *loadData() {
-    const agendaStatusActivities = this.args.currentAgenda.get('agendaStatusActivities');
-    if (agendaStatusActivities.length > 0) {
-      this.agendaHasStatusActivities = true;
-      const sortedAgendaStatusActivities = yield agendaStatusActivities.sort((a,b) => a.startDate - b.startDate);
-      this.mostRecentStatusActivity = sortedAgendaStatusActivities[sortedAgendaStatusActivities.length -1];
-      if (this.mostRecentStatusActivity.statusSet.isApproved) {
-        this.mostRecentStatusActivityIsApproved = true;
-      }
+  getLatestAgendaStatusActivity = async (agenda) => {
+    const activities = await agenda.agendaStatusActivities.toArray();
+    const sorted = activities.sort((a, b) => b.startDate - a.startDate);
+    if (sorted.length) {
+      return sorted[0];
     }
   }
 
