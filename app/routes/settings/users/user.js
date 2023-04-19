5aa887d8c7108362cb82d224473ff2ce077d32ce
stuycru <stuycru@cronos.be>
2023-04-19 11:25:42 +0200

    remove unused button on organizatinos index template add default padding in the user template on the mandatee selector add pre-loading of the selectedPerson in the user route

--- app/routes/settings/users/user.js
+++ app/routes/settings/users/user.js
@@ -4,9 +4,20 @@ import { inject as service } from '@ember/service';
 export default class SettingsUsersUserRoute extends Route {
   @service store;
 
+  person
+
   model(params) {
     return this.store.findRecord('user', params.id, {
       include: 'memberships.organization,memberships.role,person'
     });
+  }
+
+  async afterModel(model) {
+    this.person = await model.person
+  }
+
+  setupController(controller, model) {
+    super.setupController(controller, model);
+    controller.selectedPerson = this.person;
   }
 }
