eff3681f12163488893507fac9cfc21b8025f6ef
Sergio Fenoll <sergio@fenoll.be>
2023-04-20 12:20:41 +0200

    feat: add extra states to signature pill

--- app/components/signature-pill.js
+++ app/components/signature-pill.js
@@ -8,7 +8,10 @@ import { task } from 'ember-concurrency';
 export default class SignaturePillComponent extends Component {
   @tracked isMarked = false;
   @tracked isPrepared = false;
+  @tracked hasToBeApproved = false; // TODO: The necessary activities for this do not yet exist, so this is always false
+  @tracked hasToBeSigned = false;
   @tracked isSigned = false;
+  @tracked isCancelled = false;
 
   constructor() {
     super(...arguments);
@@ -21,11 +24,15 @@ export default class SignaturePillComponent extends Component {
     if (signMarkingActivity) {
       const signSubcase = await signMarkingActivity.signSubcase;
       const signPreparationActivity = await signSubcase.signPreparationActivity;
+      const signSigningActivities = await signSubcase.signSigningActivites.toArray();
       const signCompletionActivity = await signSubcase.signCompletionActivity;
+      const signCancellationActivity = await signSubcase.signCancellationActivity;
 
       this.isMarked = !!signMarkingActivity;
       this.isPrepared = !!signPreparationActivity;
+      this.hasToBeSigned = signSigningActivities.length && !signCompletionActivity;
       this.isSigned = !!signCompletionActivity;
+      this.isCancelled = !!signCancellationActivity;
     }
   });
 }
