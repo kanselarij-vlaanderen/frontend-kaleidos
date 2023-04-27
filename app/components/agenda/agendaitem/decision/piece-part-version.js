2170df004b362a6fc4cc95ab8f4e52372b22a6a6
Kobe Mertens <kobe.mertens@hotmail.com>
2023-04-27 16:06:29 +0200

    feat: piece part version labels are updating title

--- app/components/agenda/agendaitem/decision/piece-part-version.js
+++ app/components/agenda/agendaitem/decision/piece-part-version.js
@@ -1,9 +1,30 @@
 import Component from '@glimmer/component';
 import { trackedFunction } from 'ember-resources/util/function';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { dateFormat } from 'frontend-kaleidos/utils/date-format';
+import { action } from '@ember/object';
 
 export default class PiecePartVersionComponent extends Component {
-  constructor() {
-    super(...arguments);
+  @service intl;
+
+  @tracked selectedVersion = this.args.piecePart;
+
+  labelForVersion = (piecePart) => {
+    if (piecePart === this.args.piecePart) {
+      return this.intl.t('current-version');
+    } else {
+      return this.intl.t('version-of-date-at-time', {
+        date: dateFormat(piecePart.created, 'dd-MM-yyyy'),
+        time: dateFormat(piecePart.created, 'HH:mm'),
+      });
+    }
+  };
+
+  @action
+  onVersionClicked(piecePart) {
+    this.selectedVersion = piecePart;
+    this.args.onVersionClicked(piecePart.value);
   }
 
   previousVersions = trackedFunction(this, async () => {
