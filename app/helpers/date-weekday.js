5dbecf7cb44370e6f771d6cff79e0ab931be8377
Kobe Mertens <kobe.mertens@hotmail.com>
2023-05-11 15:18:11 +0200

    feat: use date-fns for weekday formatting

--- app/helpers/date-weekday.js
+++ app/helpers/date-weekday.js
@@ -1,19 +1,11 @@
 import Helper from '@ember/component/helper';
 import { inject as service } from '@ember/service';
+import { format } from 'date-fns';
 
 export default class DateWeekday extends Helper {
   @service intl;
 
   compute([date]) {
-    const weekDays = [
-      this.intl.t('sunday'),
-      this.intl.t('monday'),
-      this.intl.t('tuesday'),
-      this.intl.t('wednesday'),
-      this.intl.t('thursday'),
-      this.intl.t('friday'),
-      this.intl.t('saturday'),
-    ];
-    return weekDays[date.getDay()];
+    return format(date, 'EEEE');
   }
 }
