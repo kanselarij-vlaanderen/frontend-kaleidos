add901082bda1523366910f339e9d853399059c2
stuycru <stuycru@cronos.be>
2023-05-12 10:23:59 +0200

    add pill on publications if there is a signing flow

--- app/components/publications/documents/document-card-step.js
+++ app/components/publications/documents/document-card-step.js
@@ -1,9 +1,23 @@
 import Component from '@glimmer/component';
 import { task } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
 
 export default class PublicationsDocumentsDocumentCardStepComponent extends Component {
+
+  @tracked signMarkingActivity;
+
+  constructor() {
+    super(...arguments);
+    this.loadSignatureRelatedData.perform();
+  }
+
   @task
   *deletePiece() {
     yield this.args.onDelete();
+  }
+
+  @task
+  *loadSignatureRelatedData() {
+    this.signMarkingActivity = yield this.args.piece.signMarkingActivity;
   }
 }
