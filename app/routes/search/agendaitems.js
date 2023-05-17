1bababbd9d7a181ea8f225ca40d084b97047d74e
stuycru <stuycru@cronos.be>
2023-05-17 14:20:36 +0200

    add fix where highlighting didn't highlight every word in the shorttitle

--- app/routes/search/agendaitems.js
+++ app/routes/search/agendaitems.js
@@ -63,8 +63,11 @@ export default class AgendaitemSearchRoute extends Route {
       entry.title = entry.title.join(' ');
     }
     if ((agendaitem.highlight && agendaitem.highlight.shortTitle) && agendaitem.highlight.shortTitle[0]?.split(" ").length < agendaitem.attributes.shortTitle?.split(" ").length) {
-      const highlightShortTitleEmsRemoved = agendaitem.highlight.shortTitle[0].replaceAll(/<em>|<\/em>/g, "");
-      entry.shortTitle = agendaitem.attributes.shortTitle.replace(highlightShortTitleEmsRemoved, agendaitem.highlight.shortTitle[0]);
+      entry.shortTitle = agendaitem.attributes.shortTitle;
+      for (let i = 0; i < agendaitem.highlight.shortTitle.length; i++) {
+        const highlightShortTitleEmsRemoved = agendaitem.highlight.shortTitle[i].replace(/<em>|<\/em>/g, "");
+        entry.shortTitle = entry.shortTitle.replaceAll(highlightShortTitleEmsRemoved, agendaitem.highlight.shortTitle[i]);
+      }
     } 
     return entry;
   };
