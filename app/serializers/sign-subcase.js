aaffbc9480c7494ce5a702376eb50dd797e85a6b
Sven Valenberghs <sven.valenberghs@faros.be>
2023-05-17 14:34:22 +0200

    KAS-4099 revert serializer changes, hasmany seems needed when deleting sign-subcase after deleting hasmany models

--- app/serializers/sign-subcase.js
+++ app/serializers/sign-subcase.js
@@ -5,6 +5,9 @@ const SKIP_SERIALIZED = [
   'signPreparationActivity',
   'signCancellationActivity',
   'signCompletionActivity',
+  'signSigningActivities',
+  'signApprovalActivities',
+  'signRefusalActivities'
 ];
 
 export default class PieceSerializer extends ApplicationSerializer {
@@ -12,6 +15,13 @@ export default class PieceSerializer extends ApplicationSerializer {
     const key = relationship.key;
     if (!SKIP_SERIALIZED.includes(key)) {
       super.serializeBelongsTo(snapshot, json, relationship);
+    }
+  }
+
+  serializeHasMany(snapshot, json, relationship) {
+    const key = relationship.key;
+    if (!SKIP_SERIALIZED.includes(key)) {
+      super.serializeHasMany(snapshot, json, relationship);
     }
   }
 }
