01b23f5cf75dd4afcc7c46ec261e2d4aff728cf5
Sven Valenberghs <sven.valenberghs@faros.be>
2023-05-23 17:10:46 +0200

    KAS-4042 fix test: rare case where the active detail tab was not loaded yet

--- cypress/support/commands/agenda-commands.js
+++ cypress/support/commands/agenda-commands.js
@@ -511,7 +511,10 @@ function agendaitemExists(agendaitemName) {
   cy.get(auk.loader, {
     timeout: 20000,
   }).should('not.exist');
+  // Detail tab is only shown after loading data (first or anchor item), but no loader is showing during the process
+  // We need to ensure Detail tab exists before looking through the child components
   cy.get(agenda.agendaTabs.tabs).children()
+    .should('contain', 'Detail')
     .find(auk.tab.activeHref)
     .find(auk.tab.label)
     .invoke('text')
