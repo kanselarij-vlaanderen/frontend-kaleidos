38c8e0cf6bac0960870e7ea6ff6fe9985bd7efa4
Kobe Mertens <kobe.mertens@hotmail.com>
2023-05-30 09:22:53 +0200

    chore: change property name to avoid type confusion

--- app/models/minutes.js
+++ app/models/minutes.js
@@ -4,5 +4,5 @@ import { attr, belongsTo } from '@ember-data/model';
 export default class Minutes extends Piece {
   @attr value;
   @belongsTo('meeting', { inverse: 'minutes', async: true })
-  isMinutesForMeeting;
+  minutesForMeeting;
 }
