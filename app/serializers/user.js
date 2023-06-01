16fbe99265e4d82c34263dd7abb3b1223403ab3e
Sven Valenberghs <sven.valenberghs@faros.be>
2023-06-01 10:44:38 +0200

    KAS-4120 add serializer rule for user.person, fix test and add comments

--- app/serializers/user.js
+++ app/serializers/user.js
@@ -1,6 +1,6 @@
 import ApplicationSerializer from './application';
 
-const SKIP_SERIALIZED = ['loginActivity', 'account'];
+const SKIP_SERIALIZED = ['loginActivity', 'account', 'person'];
 
 export default class AgendaSerializer extends ApplicationSerializer {
   serializeBelongsTo(snapshot, json, relationship) {
