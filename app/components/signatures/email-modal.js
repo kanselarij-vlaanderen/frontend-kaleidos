a59b4be7f5c942dec0ca02680b544bd455bfed69
Sergio Fenoll <sergio@fenoll.be>
2023-06-14 15:59:26 +0200

    feat: stop using inputmask for email validation
    Just use a simple regex and max length check instead.
    
    Max length of 254 characters taken from:
    https://www.rfc-editor.org/errata/eid1690

--- app/components/signatures/email-modal.js
+++ app/components/signatures/email-modal.js
@@ -1,23 +1,9 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import Inputmask from 'inputmask';
 
 export default class SignaturesEmailModalComponent extends Component {
-  inputmask;
   @tracked emailBuffer;
-
-  constructor() {
-    super(...arguments);
-
-    this.inputmask = new Inputmask(this.inputmaskOptions);
-  }
-
-  get inputmaskOptions() {
-    return {
-      alias: 'email',
-    };
-  }
 
   @action
   onInputEvent(event) {
@@ -25,6 +11,7 @@ export default class SignaturesEmailModalComponent extends Component {
   }
 
   get isValid() {
-    return this.inputmask.isValid(this.emailBuffer);
+    return this.emailBuffer?.length <= 254
+      && /.+\@.+\..+/.test(this.emailBuffer);
   }
 }
