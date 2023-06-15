dc63aeed35b17f2393eff7eb0c2a958ea4e848e8
Karel Persoons <karel@brenner.company>
2023-06-15 17:17:23 +0200

    KAS-4141 Remove alert component from styleguide docs

--- app/router.js
+++ app/router.js
@@ -111,8 +111,6 @@ Router.map(function() {
   this.route('styleguide', function() {
     this.route('accordion');
     this.route('accordion-panel');
-    this.route('alert-skins');
-    this.route('alert-types');
     this.route('alert-stack');
     this.route('avatar');
     this.route('brand');
