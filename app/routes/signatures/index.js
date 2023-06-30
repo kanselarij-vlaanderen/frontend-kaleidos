4a07995b22a4973180602ab197c9f99ccca9cf8a
Sven Valenberghs <sven.valenberghs@faros.be>
2023-06-30 11:49:54 +0200

    use new params names in route mode query

--- app/routes/signatures/index.js
+++ app/routes/signatures/index.js
@@ -76,10 +76,10 @@ export default class SignaturesIndexRoute extends Route {
         'sign-subcase.sign-marking-activity.piece.document-container.type'
       ].join(','),
       page: {
-        number: params.page,
-        size: params.size,
+        number: params.pageSignaturesIndex,
+        size: params.sizeSignaturesIndex,
       },
-      sort: params.sort,
+      sort: params.sortSignaturesIndex,
     });
   }
 
