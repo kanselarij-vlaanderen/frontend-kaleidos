8d08d9c8feeb3fe5f26febd3d7e11e728bd8ab9a
Sergio Fenoll <sergio@fenoll.be>
2023-06-30 08:47:05 +0200

    feat: make the signature pills poll their status regularly

--- app/components/signature-pill.js
+++ app/components/signature-pill.js
@@ -1,9 +1,14 @@
 import Component from '@glimmer/component';
+import { later, cancel } from '@ember/runloop';
+import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import fetch from 'fetch';
 import constants from 'frontend-kaleidos/config/constants';
 import { task as trackedTask } from 'ember-resources/util/ember-concurrency';
 import { task } from 'ember-concurrency';
+import { SIGN_FLOW_STATUS_REFRESH_INTERVAL_MS } from 'frontend-kaleidos/config/config';
+
+const { SIGNED, REFUSED, CANCELED, MARKED } = constants.SIGNFLOW_STATUSES;
 
 /**
  * @param signMarkingActivity {SignMarkingActivityModel|Promise<SignMarkingActivityModel>}
@@ -12,8 +17,30 @@ export default class SignaturePillComponent extends Component {
   @service intl;
   @service currentSession;
 
+  scheduledRefresh;
+  @tracked triggerTask;
+
+  willDestroy() {
+    super.willDestroy(...arguments);
+    cancel(this.scheduledRefresh);
+  }
+
+  schedulePublicationActivitiesRefresh() {
+    if (![REFUSED, SIGNED, CANCELED].includes(this.data?.value?.status?.uri)) {
+      this.scheduledRefresh = later(this, async () => {
+        this.args.signMarkingActivity.reload();
+        const signSubcase = await this.args.signMarkingActivity.signSubcase.reload();
+        const signFlow = await signSubcase.signFlow.reload();
+        await signFlow.status.reload();
+        this.triggerTask = new Date();
+      }, SIGN_FLOW_STATUS_REFRESH_INTERVAL_MS);
+    }
+  }
+
   loadData = task(async () => {
-    const { SIGNED, REFUSED, MARKED } = constants.SIGNFLOW_STATUSES;
+    // Cancel the scheduled refresh, in case we're retriggering because
+    // a dependency (marking activity) changed
+    cancel(this.scheduledRefresh);
     const signMarkingActivity = await this.args.signMarkingActivity;
     if (!signMarkingActivity) return;
     const signSubcase = await signMarkingActivity.signSubcase;
@@ -42,13 +69,15 @@ export default class SignaturePillComponent extends Component {
       }
     }
 
+    this.schedulePublicationActivitiesRefresh();
+
     return {
       signingHubUrl,
       status,
     };
   });
 
-  data = trackedTask(this, this.loadData);
+  data = trackedTask(this, this.loadData, () => [this.triggerTask]); // Make the resource dependant on this.triggerTask
 
   get skin() {
     const { REFUSED, CANCELED } = constants.SIGNFLOW_STATUSES;
