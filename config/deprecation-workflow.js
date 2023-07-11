a830a9d0ec37d6324eebae97002b126c18a69689
Sergio Fenoll <sergio@fenoll.be>
2023-07-11 10:37:40 +0200

    test: commit deprecation workflow
    This way if we still encounter one of the listed deprecations Ember will
    throw an exception.

---
+++ config/deprecation-workflow.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+  workflow: [
+    { handler: "throw", matchId: "remove-owner-inject" },
+    { handler: "throw", matchId: "ember-data:deprecate-array-like" },
+    { handler: "throw", matchId: "ember-data:non-explicit-relationships" },
+    { handler: "throw", matchId: "ember-data:deprecate-promise-proxies" },
+    { handler: "throw", matchId: "ember-data:deprecate-promise-many-array-behaviors" },
+    { handler: "throw", matchId: "ember-data:model-save-promise" },
+    { handler: "throw", matchId: "ember-data:no-a-with-array-like" }
+  ]
+};
