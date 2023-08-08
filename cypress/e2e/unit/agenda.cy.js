fa5e25bea99fcd88c11dd2c7cf3c47605ee56993
MathiasZabot <mathias.zabot@gmail.com>
2023-08-08 16:27:05 +0200

    KAS-4194 add check for false positives

--- cypress/e2e/unit/agenda.cy.js
+++ cypress/e2e/unit/agenda.cy.js
@@ -545,7 +545,8 @@ context('Agenda tests', () => {
     downloadDocs(false);
     cy.readFile('cypress/downloads/VR_zitting_27_10_2023_ontwerpagenda_alle_punten.zip', {
       timeout: 25000,
-    }).should('contain', 'VR PV 2023_1 - punt 0003');
+    }).should('contain', 'VR PV 2023_1 - punt 0003')
+      .should('not.contain', 'VR PV 2023_1 - punt 0002');
   });
 
   it('Should download all decisions for Crevits', () => {
@@ -563,7 +564,8 @@ context('Agenda tests', () => {
     downloadDocs(false);
     cy.readFile('cypress/downloads/VR_zitting_27_10_2023_ontwerpagenda_alle_punten.zip', {
       timeout: 25000,
-    }).should('contain', 'VR PV 2023_1 - punt 0002');
+    }).should('contain', 'VR PV 2023_1 - punt 0002')
+      .should('not.contain', 'VR PV 2023_1 - punt 0003');
   });
 
   it('Should download all decisions', () => {
