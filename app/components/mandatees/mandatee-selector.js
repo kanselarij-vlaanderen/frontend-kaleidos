8dfc03cf827f40e062cff1d2e97ceb2080ba38e5
Kobe Mertens <kobe.mertens@hotmail.com>
2023-08-22 15:17:27 +0200

    chore: fix loading message

--- app/components/mandatees/mandatee-selector.js
+++ app/components/mandatees/mandatee-selector.js
@@ -1,9 +1,8 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { isEmpty } from '@ember/utils';
 import { tracked } from '@glimmer/tracking';
-import { restartableTask, task, timeout } from 'ember-concurrency';
+import { restartableTask, timeout } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 /**
  * @argument multiple
@@ -16,53 +15,46 @@ import { inject as service } from '@ember/service';
 export default class MandateeSelector extends Component {
   @service mandatees;
 
-  @tracked mandateeOptions = [];
-  @tracked referenceDate;
+  @tracked openSearchMandateeOptions = [];
 
-  constructor() {
-    super(...arguments);
-    this.referenceDate = this.args.referenceDate || new Date();
-    this.loadMandatees.perform();
+  get referenceDate() {
+    return this.args.referenceDate || new Date();
   }
 
-  @task
-  *loadMandatees() {
-    if (this.args.openSearch) {
-      this.mandateeOptions = [];
+  get mandateeOptions() {
+    if (!this.args.openSearch) {
+      return this.loadMandatees();
     } else {
-      this.mandateeOptions = yield this.mandatees.getMandateesActiveOn.perform(
-        this.referenceDate
-      );
-      this.mandateeOptions = this.mandateeOptions.filter(
-        (mandatee) => !this.args.excludeMandatees?.includes(mandatee)
-      );
+      return undefined;
     }
-  }
-
-  @restartableTask
-  *filterMandatees(searchTerm) {
-    yield timeout(100);
-    return this.mandateeOptions.filter((mandatee) => {
-      const lastName = mandatee.belongsTo('person').value().lastName;
-      return lastName.toLowerCase().startsWith(searchTerm.toLowerCase());
-    });
   }
 
   @action
-  resetMandateeOptionsIfEmpty(param) {
-    if (isEmpty(param)) {
-      this.mandateeOptions = this.loadMandatees.perform();
-    }
+  openSearchChanged() {
+    this.openSearchMandateeOptions = [];
+  }
+
+  async loadMandatees() {
+    const mandateeOptions = await this.mandatees.getMandateesActiveOn.perform(
+      this.referenceDate
+    );
+    mandateeOptions.filter(
+      (mandatee) => !this.args.excludeMandatees?.includes(mandatee)
+    );
+
+    return mandateeOptions;
   }
 
   @restartableTask
   *searchMandatee(searchTerm) {
+    this.openSearchMandateeOptions = [];
     yield timeout(300);
-    this.mandateeOptions = yield this.mandatees.fetchMandateesByName.perform(
+    const mandateeOptions = yield this.mandatees.fetchMandateesByName.perform(
       searchTerm,
       this.referenceDate
     );
-    this.mandateeOptions = this.mandateeOptions.filter(
+
+    this.openSearchMandateeOptions = mandateeOptions.filter(
       (mandatee) => !this.args.excludeMandatees?.includes(mandatee)
     );
   }
