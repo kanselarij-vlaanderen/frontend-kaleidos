aa34b71af1b8811927c60eae1e02b5ddba98c617
Sven Valenberghs <sven.valenberghs@faros.be>
2023-09-18 14:53:43 +0200

    add locale to timezoned formatting

--- app/utils/date-format.js
+++ app/utils/date-format.js
@@ -1,5 +1,6 @@
 // import { format } from 'date-fns';
 import { formatInTimeZone as format } from 'date-fns-tz';
+import { nlBE } from 'date-fns/locale';
 
 const timezone = 'Europe/Brussels';
 
@@ -9,7 +10,9 @@ export function dateFormat(dateOrString, formatString) {
     // This is undesired, we actually want an invalid date in that case
     const dateObject = new Date(dateOrString ?? NaN);
     if (!isNaN(dateObject.valueOf())) {
-      return format(dateObject, timezone, formatString);
+      return format(dateObject, timezone, formatString, {
+        locale: nlBE,
+      });
     }
   }
 }
