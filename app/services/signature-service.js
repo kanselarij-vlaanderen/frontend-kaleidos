53c451663d2aa94f920b8bd7fcc3049e3e9e2e58
Sven Valenberghs <sven.valenberghs@faros.be>
2023-09-25 16:54:06 +0200

    KAS-4279 better error handling in both cases

--- app/services/signature-service.js
+++ app/services/signature-service.js
@@ -70,13 +70,14 @@ export default class SignatureService extends Service {
       for (let signFlow of signFlows) {
         await this.removeSignFlow(signFlow, true);
       }
+      let stringifiedJson;
       try {
         const json = await response?.json();
-        throw new Error(JSON.stringify(json));
+        stringifiedJson = JSON.stringify(json);
       } catch (error) {
-        // digital-signing might be down in this case
-        throw new Error(response.statusText);
+        // cannot stringify could mean digital-signing is down
       }
+      throw new Error(stringifiedJson ?? response.statusText);
     }
   }
 
