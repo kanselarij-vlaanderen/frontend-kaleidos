6bec613ba1908666e2b8bc51f0561e196bcb1b5f
stuycru <stuycru@cronos.be>
2023-10-18 13:57:12 +0200

    removed unnecessary checks and replaced with agendaitem.isApproval

--- app/components/agenda/agendaitem/agendaitem-decision.js
+++ app/components/agenda/agendaitem/agendaitem-decision.js
@@ -325,23 +325,12 @@ export default class AgendaitemDecisionComponent extends Component {
 
   @action
   async updateBetreftContent() {
-    const meetingKind = await this.args.agendaContext.meeting.kind;
-    const isMinisterraad = meetingKind.uri === CONSTANTS.MEETING_KINDS.MINISTERRAAD;
-    const { shortTitle, title, number } = this.args.agendaContext.agendaitem;
+    const { shortTitle, title } = this.args.agendaContext.agendaitem;
     const documents = this.pieces;
     const agendaActivity = await this.args.agendaitem.agendaActivity;
     const subcase = await agendaActivity?.subcase;
-    const titleStartsWithApproval = 
-    this.args.agendaitem.shortTitle.startsWith("Goedkeuring") 
-    || this.args.agendaitem.title.startsWith("Goedkeuring");
-    const isApproval = 
-    this.args.agendaitem.isApproval 
-    && number === 1 
-    && isMinisterraad 
-    && !subcase 
-    && titleStartsWithApproval
       this.setBetreftEditorContent(
-        `<p>${generateBetreft(shortTitle, title, isApproval, documents, subcase?.subcaseName)}</p>`
+        `<p>${generateBetreft(shortTitle, title, this.args.agendaitem.isApproval, documents, subcase?.subcaseName)}</p>`
       );
   }
 
