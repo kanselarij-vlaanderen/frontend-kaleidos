6faf68a3166f9a391063acaffd9dc19027cd2e88
Sven Valenberghs <sven.valenberghs@faros.be>
2023-10-18 14:00:00 +0200

    KAS-4358 remove maybeMeeting param

--- app/utils/generate-report-name.js
+++ app/utils/generate-report-name.js
@@ -7,17 +7,10 @@ import CONSTANTS from 'frontend-kaleidos/config/constants';
  *
  * @param {Agendaitem} agendaitem The agendaitem whose number and type will be
  * used to generate the report name.
- * @param {?Meeting} [maybeMeeting=undefined] The meeting whose number will be
- * used to generate the report name. Optional, if not provided the meeting will
- * be fetched via the agendaitem.
+ * @param {Meeting} [meeting] The meeting whose number will be
+ * used to generate the report name.
  */
-export default async function generateReportName(agendaitem, maybeMeeting = undefined) {
-  let meeting = maybeMeeting;
-  if (meeting === undefined) {
-    const agenda = await agendaitem.agenda;
-    meeting = await agenda.createdFor;
-  }
-
+export default async function generateReportName(agendaitem, meeting) {
   const meetingNumber = meeting.numberRepresentation;
   const type = await agendaitem.type;
   const agendaitemType = type.uri === CONSTANTS.AGENDA_ITEM_TYPES.ANNOUNCEMENT
