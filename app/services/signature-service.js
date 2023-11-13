36467fba38e697d0165b78e83b809b2a8cc6c65e
Sergio Fenoll <sergio@fenoll.be>
2023-11-13 20:54:01 +0100

    feat: cleanup signed piece copy when deleting sign flow

--- app/services/signature-service.js
+++ app/services/signature-service.js
@@ -186,6 +186,8 @@ export default class SignatureService extends Service {
       const piece = await signMarkingActivity.piece;
       const signedPiece = await piece.signedPiece;
       const signedFile = await signedPiece?.file;
+      const signedPieceCopy = await piece.signedPieceCopy;
+      const signedPieceCopyFile = await signedPieceCopy?.file;
       const signPreparationActivity = await signSubcase
         ?.belongsTo('signPreparationActivity')
         .reload();
@@ -208,6 +210,8 @@ export default class SignatureService extends Service {
       // delete in reverse order of creation
       await signedFile?.destroyRecord();
       await signedPiece?.destroyRecord();
+      await signedPieceCopyFile?.destroyRecord();
+      await signedPieceCopy?.destroyRecord();
       await signPreparationActivity?.destroyRecord();
       await signCompletionActivity?.destroyRecord();
       await signCancellationActivity?.destroyRecord();
