899f517f2856a2e815fe3612ba31d71c44a19b0b
Sergio Fenoll <sergio@fenoll.be>
2023-12-09 00:45:57 +0100

    chore: cleanup submission alert a little bit

--- app/components/vp/submissions-alert.js
+++ app/components/vp/submissions-alert.js
@@ -1,9 +1,30 @@
 import Component from '@glimmer/component'; 
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
 export default class SubmissionsAlertComponent extends Component {
+  @service intl;
+
   @tracked showActivity = null;
+
+  get message() {
+    if (this.args.parliamentFlow.isIncomplete) {
+      return this.intl.t('this-case-was-incompletely-submitted-to-VP-on');
+    } else {
+      return this.intl.t('this-case-was-completely-submitted-to-VP-on');
+    }
+  }
+
+  get skin() {
+    if (this.args.parliamentFlow.isIncomplete) {
+      return "warning";
+    } else {
+      return "success";
+    }
+  }
+
+  g
 
   @action
   showModalFor(submissionActivity) {
