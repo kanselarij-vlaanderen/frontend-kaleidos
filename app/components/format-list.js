9fd12044b854848b8f32f1cd8ac24fec194345d1
Sergio Fenoll <sergio@fenoll.be>
2023-12-11 14:38:45 +0100

    docs: document strange behaviour in format-list component

--- app/components/format-list.js
+++ app/components/format-list.js
@@ -9,6 +9,9 @@ export default class FormatListComponent extends Component {
     const parts = this.args.list?.slice();
     if (parts) {
       const formatter = new Intl.ListFormat(this.args.locale);
+      // Intl.ListFormat#formatToParts (and #format) only accepts strings, if
+      // you pass in an object it'll throw a TypeError. This hack with passing
+      // in an array of 'thing' strings is there to circumvent this behaviour.
       const formattedParts = formatter.formatToParts(parts.map((_part) => 'thing'));
 
       let index = 0;
