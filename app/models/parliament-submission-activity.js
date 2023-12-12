d9a29b23ef17ca978b46e00bf88e651a828b6703
Tom De Nies <tom@firststage.be>
2023-12-12 16:48:30 +0100

    added comment to model

--- app/models/parliament-submission-activity.js
+++ app/models/parliament-submission-activity.js
@@ -3,6 +3,7 @@ import Model, { attr, hasMany, belongsTo } from '@ember-data/model';
 export default class ParliamentSubmissionActivity extends Model {
   @attr('date') startDate;
   @attr('date') endDate;
+  @attr('string') comment;
 
   @belongsTo('user', { inverse: null, async: true }) submitter;
 
