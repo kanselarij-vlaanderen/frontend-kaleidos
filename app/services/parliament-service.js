f7e9296a6c4b4ab24a8f1f343cb27e0940732e1a
Kobe Mertens <kobe.mertens@hotmail.com>
2024-01-22 16:51:00 +0100

    update parliament service to use job approach

--- app/services/parliament-service.js
+++ app/services/parliament-service.js
@@ -1,6 +1,7 @@
 import Service, { inject as service } from '@ember/service';
 import fetch from 'fetch';
 import CopyErrorToClipboardToast from 'frontend-kaleidos/components/utils/toaster/copy-error-to-clipboard-toast';
+import CONSTANTS from 'frontend-kaleidos/config/constants';
 
 export default class ParliamentService extends Service {
   @service toaster;
@@ -17,7 +18,7 @@ export default class ParliamentService extends Service {
       const body = await resp.json();
       return body.isReady;
     }
-  };
+  }
 
   async getPiecesReadyToBeSent(agendaitem) {
     const resp = await fetch(
@@ -30,27 +31,30 @@ export default class ParliamentService extends Service {
       const body = await resp.json();
       return body.data;
     }
-  };
+  }
 
   async sendToVP(agendaitem, pieces, comment, isComplete) {
-    const params = {
-      agendaitem: agendaitem.uri,
-      pieces: pieces.map(piece => piece.uri),
-      isComplete: isComplete,
-      ...(comment ? { comment: comment } : null),
-    };
-    const response = await fetch(
-      `/vlaams-parlement-sync/`,
+    const sendingToast = this.toaster.loading(
+      this.intl.t('the-files-are-being-sent-to-parliament'),
+      null,
       {
-        headers: {
-          'Content-Type': 'application/vnd.api+json',
-          Accept: 'application/vnd.api+json'
-        },
-        method: 'POST',
-        body: JSON.stringify(params)
+        timeOut: 10 * 60 * 1000,
       }
     );
-
+    const params = {
+      agendaitem: agendaitem.uri,
+      pieces: pieces.map((piece) => piece.uri),
+      isComplete,
+      ...(comment ? { comment } : null),
+    };
+    const response = await fetch(`/vlaams-parlement-sync/`, {
+      headers: {
+        'Content-Type': 'application/vnd.api+json',
+        Accept: 'application/vnd.api+json',
+      },
+      method: 'POST',
+      body: JSON.stringify(params),
+    });
     if (!response.ok) {
       let errorMessage = '';
       try {
@@ -67,6 +71,7 @@ export default class ParliamentService extends Service {
           errorMessage = `Something went wrong while reading response: ${error}`;
         }
       }
+      this.toaster.close(sendingToast);
       this.toaster.show(CopyErrorToClipboardToast, {
         title: this.intl.t('warning-title'),
         message: this.intl.t('error-while-sending-to-VP-message'),
@@ -74,10 +79,77 @@ export default class ParliamentService extends Service {
         showDatetime: true,
         options: {
           timeOut: 60 * 10 * 1000,
-        }
+        },
       });
     } else {
-      this.toaster.success(this.intl.t('case-was-sent-to-VP'));
+      const job = await response.json();
+      this.delayedPoll(job, sendingToast);
+    }
+  }
+
+  async delayedPoll(job, toast) {
+    // Many files will only take about half a second to send
+    // This avoids having to wait 2 seconds until the next poll
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    this.pollSendToVpJob(job, toast);
+  }
+
+  async pollSendToVpJob(job, toast) {
+    const {
+      data: { attributes: jobResult },
+    } = await this.getJob(job, 'send-to-vp-job');
+    if (!jobResult) {
+      return;
+    }
+
+    if (jobResult.status === CONSTANTS.VP_JOB_STATUSES.SUCCESS) {
+      this.toaster.close(toast);
+      this.toaster.success(
+        this.intl.t('the-files-are-sent-to-parliament'),
+        null,
+        {
+          closable: true,
+          timeOut: 5 * 1000,
+        }
+      );
+    } else if (jobResult.status === CONSTANTS.VP_JOB_STATUSES.FAILED) {
+      this.toaster.close(toast);
+      this.toaster.error(
+        this.intl.t('error-while-sending-to-VP-message')
+      );
+    } else {
+      setTimeout(() => {
+        this.pollSendToVpJob(job, toast);
+      }, 2000);
+    }
+  }
+
+  async getJob(job) {
+    let response;
+    try {
+      response = await fetch(`/vlaams-parlement-sync/send-to-vp-jobs/${job.id}`);
+      const data = await response.json();
+      if (!response.ok) {
+        throw new Error(
+          `Backend response contained an error (status: ${
+            response.status
+          }): ${JSON.stringify(data)}`
+        );
+      }
+      return data;
+    } catch (error) {
+      // Errors returned from services *should* still
+      // be valid JSON(:API), but we could encounter
+      // non-JSON if e.g. a service is down. If so,
+      // throw a nice error that only contains the
+      // response status.
+      if (error instanceof SyntaxError) {
+        throw new Error(
+          `Backend response contained an error (status: ${response.status})`
+        );
+      } else {
+        throw error;
+      }
     }
   }
 }
