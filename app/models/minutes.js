3a1ca215e76c22fdeba6dca7aa22f06da4437c42
Sven Valenberghs <sven.valenberghs@faros.be>
2024-02-07 15:49:36 +0100

    KAS-4104 explicit polymorphic relations, fix 1 typo

--- app/models/minutes.js
+++ app/models/minutes.js
@@ -2,7 +2,7 @@ import Piece from './piece';
 import { belongsTo, hasMany } from '@ember-data/model';
 
 export default class Minutes extends Piece {
-  @belongsTo('meeting', { inverse: 'minutes', async: true })
+  @belongsTo('meeting', { inverse: 'minutes', async: true, as: 'minutes' })
   minutesForMeeting;
   @hasMany('piece-part', { inverse: 'minutes', async: true }) pieceParts;
 }
