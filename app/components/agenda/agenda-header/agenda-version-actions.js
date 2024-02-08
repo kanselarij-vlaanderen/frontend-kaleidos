875bc7b6b62432f5e026ea3c723756879cc624b5
Sven Valenberghs <sven.valenberghs@faros.be>
2024-02-08 16:43:45 +0100

    KAS-4104 replace firstObject, lastObject, objectAt and findBy

--- app/components/agenda/agenda-header/agenda-version-actions.js
+++ app/components/agenda/agenda-header/agenda-version-actions.js
@@ -84,7 +84,7 @@ export default class AgendaAgendaHeaderAgendaVersionActions extends Component {
   }
 
   get latestAgenda() {
-    return this.args.reverseSortedAgendas.firstObject;
+    return this.args.reverseSortedAgendas.at(0);
   }
 
   get isMeetingClosable() {
