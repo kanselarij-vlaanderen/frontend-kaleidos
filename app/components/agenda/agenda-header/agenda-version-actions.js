13d21fd23ac53a2c31d1111374817b8d5f6eb040
Sven Valenberghs <sven.valenberghs@faros.be>
2024-02-09 16:14:20 +0100

    KAS-4104 add slice to access proxyArray

--- app/components/agenda/agenda-header/agenda-version-actions.js
+++ app/components/agenda/agenda-header/agenda-version-actions.js
@@ -84,7 +84,7 @@ export default class AgendaAgendaHeaderAgendaVersionActions extends Component {
   }
 
   get latestAgenda() {
-    return this.args.reverseSortedAgendas.at(0);
+    return this.args.reverseSortedAgendas.slice().at(0);
   }
 
   get isMeetingClosable() {
