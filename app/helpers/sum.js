b432c4b81bc003eeed3dbd0bb12cda7eb6f05a51
Sergio Fenoll <sergio@fenoll.be>
2024-02-16 14:28:10 +0100

    chore: replacing sum helper with add from dependency
    ember-math-helpers already includes a sum helper which does the same
    thing as the one implemented here. It's also just an alias of the add
    helper (also defined in that package) which we already use in the
    codebase. Replacing it here with add for consistency.

--- app/helpers/sum.js
+++
@@ -1,9 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function sum(params) {
-  return params.reduce((a, b) => {
-    return Number(a) + Number(b);
-  });
-};
-
-export default helper(sum);
