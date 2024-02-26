89cc89f3ce84e6c1c23b4fed308137d72f0daeda
Kobe Mertens <kobe.mertens@hotmail.com>
2024-02-26 11:21:12 +0100

    update dossier page

--- app/routes/cases/index.js
+++ app/routes/cases/index.js
@@ -1,6 +1,9 @@
-import Route from '@ember/routing/route';
-import { action } from "@ember/object";
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { isPresent } from '@ember/utils';
+import { startOfDay, endOfDay } from 'date-fns';
+import parseDate from 'frontend-kaleidos/utils/parse-date-search-param';
+import Route from '@ember/routing/route';
 
 export default class CasesIndexRoute extends Route {
   @service store;
@@ -21,24 +24,52 @@ export default class CasesIndexRoute extends Route {
       refreshModel: true,
       as: 'toon_enkel_gearchiveerd',
     },
+    dateFrom: {
+      refreshModel: true,
+      as: 'van',
+    },
+    dateTo: {
+      refreshModel: true,
+      as: 'tot',
+    },
+    nameSearchText: {
+      refreshModel: true,
+      as: 'dossier_naam',
+    },
   };
 
   model(params) {
     const options = {
-      include: 'case',
+      include: 'decisionmaking-flow',
       sort: params.sort,
       page: {
         number: params.page,
         size: params.size,
       },
     };
-    if (params.showArchivedOnly) {
-      options['filter[:has:closed]'] = true;
-    } else {
-      options['filter[:has-no:closed]'] = true;
+
+    if (params.nameSearchText) {
+      options['filter[short-title]'] = params.nameSearchText;
     }
 
-    return this.store.query('decisionmaking-flow', options);
+    options['filter[:has:decisionmaking-flow]'] = true;
+
+    if (isPresent(params.dateFrom)) {
+      const date = startOfDay(parseDate(params.dateFrom));
+      options['filter[:gte:created]'] = date.toISOString();
+    }
+    if (isPresent(params.dateTo)) {
+      const date = endOfDay(parseDate(params.dateTo));
+      options['filter[:lte:created]'] = date.toISOString();
+    }
+
+    // if (this.personFilter) {
+    //   options[
+    //     'filter[decisionmaking-flow][subcases][requestedBy][person][:id:]'
+    //   ] = null; // [lijst met ids]
+    // }
+
+    return this.store.query('case', options);
   }
 
   @action
