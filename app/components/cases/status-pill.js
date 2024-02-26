4ea5e536d0fe3505aceac549a0b2ee50fee2e050
Kobe Mertens <kobe.mertens@hotmail.com>
2024-02-26 11:17:22 +0100

    add cases status pill

---
+++ app/components/cases/status-pill.js
@@ -0,0 +1,39 @@
+import { service } from '@ember/service';
+import Component from '@glimmer/component';
+import { task } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
+
+export default class CasesStatusPill extends Component {
+  @service store;
+  @service intl;
+
+  @tracked statusText;
+
+  constructor() {
+    super(...arguments);
+    this.loadStatusText.perform();
+  }
+
+  loadStatusText = task(async () => {
+    if (!this.args.case) {
+      throw new Error('@case argument is required');
+    }
+
+    const decisionmakingFlow = await this.args.case.decisionmakingFlow;
+    // Note: probably better to include subcase type here but did not work
+    const subcase = await this.store.queryOne('subcase', {
+      'filter[decisionmaking-flow][:id:]': decisionmakingFlow.id,
+      sort: '-created',
+    });
+    if (subcase) {
+      const subcaseType = await subcase.type;
+      if (subcaseType) {
+        this.statusText = subcaseType.label;
+      } else {
+        this.statusText = this.intl.t('no-status-yet-pill');
+      }
+    } else {
+      this.statusText = this.intl.t('no-subcases-yet-pill');
+    }
+  });
+}
