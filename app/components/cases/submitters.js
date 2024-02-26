c5557fb624ebcdd480f4d3cafdcc8dfce96ddfba
Kobe Mertens <kobe.mertens@hotmail.com>
2024-02-26 14:44:26 +0100

    add submitters component

---
+++ app/components/cases/submitters.js
@@ -0,0 +1,35 @@
+import { service } from '@ember/service';
+import Component from '@glimmer/component';
+import { task } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
+
+export default class CasesSubmitters extends Component {
+  @service store;
+
+  @tracked submittersText = null;
+
+  constructor() {
+    super(...arguments);
+    this.loadSubmittersText.perform();
+  }
+
+  loadSubmittersText = task(async () => {
+    if (!this.args.case) {
+      throw new Error('@case argument is required');
+    }
+
+    const subcases = await this.store.queryAll('subcase', {
+      'filter[decisionmaking-flow][case][:id:]': this.args.case.id,
+      include: 'requested-by.person',
+    });
+
+    const persons = new Set();
+    subcases.forEach((subcase) => {
+      const submitter = subcase.belongsTo('requestedBy').value();
+      if (submitter) {
+        persons.add(submitter.belongsTo('person').value().fullName);
+      }
+    });
+    this.submittersText = [...persons].join(',');
+  });
+}
