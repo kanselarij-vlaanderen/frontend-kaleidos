8ac938a470890f6dc8f6a79be90282b9ad2f5797
Sergio Fenoll <sergio@fenoll.be>
2024-03-08 09:57:15 +0100

    chore: simplify add-subcase route logic
    Instead of setting values on the controller we can just return the
    decisionmakingFlow and latestSubcase from our model.

--- app/controllers/cases/case/subcases/add-subcase.js
+++ app/controllers/cases/case/subcases/add-subcase.js
@@ -1,11 +1,8 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class CasesCaseSubcasesAddSubcaseController extends Controller {
-  @tracked decisionmakingFlow;
-  @tracked latestSubcase;
   @service router;
 
   @action
