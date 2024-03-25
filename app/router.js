21b21cacdaaf7117b3730dd03f762e4d247b8d89
Kobe Mertens <kobe.mertens@hotmail.com>
2024-03-25 17:50:20 +0100

    add parliament route

--- app/router.js
+++ app/router.js
@@ -31,6 +31,7 @@ Router.map(function() {
 
   this.route('cases', { path: '/dossiers', }, function() {
     this.route('case', { path: ':id', }, function() {
+      this.route('parliament-flow', { path: '/parlement'});
       this.route('subcases', { path: '/deeldossiers', }, function() {
         this.route('subcase', { path: ':subcase_id', }, function() {});
         this.route('add-subcase', { path: '/procedurestap-toevoegen',});
