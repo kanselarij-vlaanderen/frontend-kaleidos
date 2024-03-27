0f5fe331030834d4f09f874ef2a52f8d54d194d2
Kobe Mertens <kobe.mertens@hotmail.com>
2024-03-27 20:36:15 +0100

    update this.modelFor('cases.case') to new model

--- app/routes/cases/case/subcases.js
+++ app/routes/cases/case/subcases.js
@@ -9,7 +9,7 @@ export default class CasesCaseSubcasesRoute extends Route {
   model() {
     //  We want to sort descending on date the subcase was concluded.
     //  In practice, reverse sorting on created will be close
-    const decisionmakingFlow = this.modelFor('cases.case');
+    const { decisionmakingFlow } = this.modelFor('cases.case');
     const queryParams = {
       'filter[decisionmaking-flow][:id:]': decisionmakingFlow.id,
       sort: 'created',
