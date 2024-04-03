7aa0c8892b84c966874db3dd92f4f6f019e5a490
Kobe Mertens <kobe.mertens@hotmail.com>
2024-04-03 15:52:19 +0200

    fix cases route refresh

--- app/controllers/cases/case/subcases/subcase.js
+++ app/controllers/cases/case/subcases/subcase.js
@@ -7,6 +7,6 @@ export default class CasesCaseSubcasesSubcaseController extends Controller {
 
   @action
   refreshSubcases() {
-    this.router.refresh('cases.case.index');
+    this.router.refresh('cases.case');
   }
 }
