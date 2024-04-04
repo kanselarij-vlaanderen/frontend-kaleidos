8b4479e40c8836f532a70f934d2df34aa583c165
stuycru <stuycru@cronos.be>
2024-04-04 13:47:21 +0200

    remove import

--- app/utils/hash-util.js
+++ app/utils/hash-util.js
@@ -1,5 +1,3 @@
-import VRDocumentName from 'frontend-kaleidos/utils/vr-document-name';
-
 export function setHash(pieceName) {
   const hash = pieceName.replaceAll(" ", "-");
   window.location.hash = hash;
