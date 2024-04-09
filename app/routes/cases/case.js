32b8d653f117f9d1b6afb803167af7ace9003168
Kobe Mertens <kobe.mertens@hotmail.com>
2024-04-09 10:46:03 +0200

    run queries in parallel

--- app/routes/cases/case.js
+++ app/routes/cases/case.js
@@ -6,45 +6,40 @@ export default class CasesCaseRoute extends Route {
   @service router;
 
   async model(params) {
-    const decisionmakingFlow = await this.store.findRecord(
-      'decisionmaking-flow',
-      params.id
-    );
-    const parliamentFlow = await this.store.queryOne('parliament-flow', {
-      'filter[case][decisionmaking-flow][:id:]': params.id,
-      include: 'parliament-subcase',
-    });
+    const [decisionmakingFlow, parliamentFlow, subcases] = await Promise.all([
+      this.store.findRecord('decisionmaking-flow', params.id),
+      this.store.queryOne('parliament-flow', {
+        'filter[case][decisionmaking-flow][:id:]': params.id,
+        include: 'parliament-subcase',
+      }),
+      this.store.queryAll('subcase', {
+        'filter[decisionmaking-flow][:id:]': params.id,
+        sort: 'created',
+        include: 'type',
+      }),
+    ]);
 
     let parliamentSubmissionActivities;
     let latestParliamentSubmissionActivity;
     let latestParliamentRetrievalActivity;
 
     if (parliamentFlow) {
-      parliamentSubmissionActivities = await this.store.queryAll(
-        'parliament-submission-activity',
-        {
-          'filter[parliament-subcase][parliament-flow][:id:]':
-            parliamentFlow.id,
-          sort: '-start-date',
-        }
-      );
+      [parliamentSubmissionActivities, latestParliamentRetrievalActivity] =
+        await Promise.all([
+          this.store.queryAll('parliament-submission-activity', {
+            'filter[parliament-subcase][parliament-flow][:id:]':
+              parliamentFlow.id,
+            sort: '-start-date',
+          }),
+          this.store.queryOne('parliament-retrieval-activity', {
+            'filter[parliament-subcase][parliament-flow][:id:]':
+              parliamentFlow.id,
+            sort: '-start-date',
+          }),
+        ]);
       parliamentSubmissionActivities = parliamentSubmissionActivities?.slice();
       latestParliamentSubmissionActivity = parliamentSubmissionActivities?.at(0);
-      latestParliamentRetrievalActivity = await this.store.queryOne(
-        'parliament-retrieval-activity',
-        {
-          'filter[parliament-subcase][parliament-flow][:id:]':
-          parliamentFlow.id,
-          sort: '-start-date',
-        }
-      );
     }
-
-    const subcases = await this.store.queryAll('subcase', {
-      'filter[decisionmaking-flow][:id:]': params.id,
-      sort: 'created',
-      include: 'type',
-    });
 
     return {
       decisionmakingFlow,
