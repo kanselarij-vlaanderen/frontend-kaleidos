c5942ce475033642a57b008e1700be9de3fa4ff5
Sergio Fenoll <sergio@fenoll.be>
2024-04-10 20:47:05 +0200

    chore: add authorityDomain property

--- app/models/parliament-retrieval-activity.js
+++ app/models/parliament-retrieval-activity.js
@@ -3,6 +3,7 @@ import Model, { attr, hasMany, belongsTo } from '@ember-data/model';
 export default class ParliamentRetrievalActivity extends Model {
   @attr('date') startDate;
   @attr('date') endDate;
+  @attr authorityDomain;
   @attr('string-set', {
     defaultValue: () => [],
   })
