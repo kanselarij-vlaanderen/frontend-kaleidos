094a653faa8fe4c20555d67cfc2dd284963f944d
Dieter Peirs <github@dieterpeirs.com>
2024-04-23 14:05:45 +0200

    collapse agenda sidenav

--- app/controllers/agenda.js
+++ app/controllers/agenda.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { debounce } from '@ember/runloop';
 import CONSTANTS from 'frontend-kaleidos/config/constants';
 
 export default class AgendaController extends Controller {
@@ -9,6 +10,12 @@ export default class AgendaController extends Controller {
 
   @tracked isLoading = false;
   @tracked isOpenSideNav = true;
+
+  constructor() {
+    super(...arguments);
+    window.addEventListener('resize', () => debounce(this, this.updateSideNavVisibility, 150));
+    this.updateSideNavVisibility();
+  }
 
   get meetingKindPrefix() {
     return this.model.meeting.kind.get('uri') == CONSTANTS.MEETING_KINDS.PVV ? 'MR VV' : 'MR';
@@ -36,6 +43,24 @@ export default class AgendaController extends Controller {
 
   @action
   collapseSideNav() {
-    this.isOpenSideNav = false;
+    if (window.innerWidth >= 768) {
+      this.isOpenSideNav = false;
+    }
+  }
+
+  willDestroy() {
+    super.willDestroy(...arguments);
+    window.removeEventListener('resize', this.updateSideNavVisibility);
+  }
+
+  @action
+  updateSideNavVisibility() {
+    if (window.innerWidth < 768) {
+      this.isOpenSideNav = true;
+    } else if (window.innerWidth >= 768 && window.innerWidth <= 1024) {
+      this.isOpenSideNav = false;
+    } else {
+      this.isOpenSideNav = true;
+    }
   }
 }
