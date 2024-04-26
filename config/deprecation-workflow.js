dfbdaa9201e93e1ee64ad36a9c8a8a53e612f1d9
Sven Valenberghs <sven.valenberghs@faros.be>
2024-04-26 17:02:34 +0200

    KAS-4104 enforce known deprecations throw errors

---
+++ config/deprecation-workflow.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+  throwOnUnhandled: false, // throw on any deprecation
+  workflow: [
+    { handler: "throw", matchId: "remove-owner-inject" },
+    { handler: "throw", matchId: "ember-data:deprecate-array-like" },
+    { handler: "throw", matchId: "ember-data:non-explicit-relationships" },
+    { handler: "throw", matchId: "ember-data:deprecate-promise-proxies" },
+    { handler: "throw", matchId: "ember-data:deprecate-promise-many-array-behaviors" },
+    { handler: "throw", matchId: "ember-data:model-save-promise" },
+    { handler: "throw", matchId: "ember-data:no-a-with-array-like" }
+  ]
+};
