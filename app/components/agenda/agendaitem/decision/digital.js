f19b93865dcf813839a64f21fd3c473c09e68cf2
Sergio Fenoll <sergio@fenoll.be>
2024-05-27 15:32:21 +0200

    style: remove extraneous whitespace

--- app/components/agenda/agendaitem/decision/digital.js
+++ app/components/agenda/agendaitem/decision/digital.js
@@ -339,7 +339,7 @@ export default class AgendaAgendaitemDecisionDigitalComponent extends Component 
     } else {
       this.setBetreftEditorContent('');
     }
-  } 
+  }
 
   @action
   handleRdfaEditorInitBeslissing(editorInterface) {
