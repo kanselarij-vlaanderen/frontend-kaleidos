a43b4a8d4d49d5e88823abb6c204715b690511e9
stuycru <stuycru@cronos.be>
2024-06-17 16:10:07 +0200

    KAS-4694: create formallyOkEdit and formallyOkPill components to make editing in agenda overview & detail views possible

---
+++ app/components/agenda/formally-ok-edit.js
@@ -0,0 +1,40 @@
+import Component from '@glimmer/component';
+import CONFIG from 'frontend-kaleidos/utils/config';
+import CONSTANTS from 'frontend-kaleidos/config/constants';
+import { tracked } from '@glimmer/tracking';
+import { action  } from '@ember/object';
+import { task } from 'ember-concurrency';
+
+export default class AgendaFormallyOkEdit extends Component {
+  /**
+   * @argument formallyOkStatusUri: TODO: change to ember object
+   * @argument onChange
+   */
+
+  formallyOkOptions;
+  @tracked defaultFormallyOkOption;
+  @tracked selectedFormallyOkOption;
+
+  constructor() {
+    super(...arguments);
+    this.formallyOkOptions = CONFIG.formallyOkOptions;
+    this.selectedFormallyOkOption = this.formallyOkStatus || this.formallyOkOptions.find((formallyOkOption) => formallyOkOption.uri === CONSTANTS.ACCEPTANCE_STATUSSES.NOT_YET_OK);
+  }
+
+  get formallyOkStatus() {
+    return this.formallyOkOptions.find((formallyOkOption) => formallyOkOption.uri === this.args.selectedFormallyOk.uri);
+  }
+
+  @action
+  onChange(formallyOkOption) {
+    this.selectedFormallyOkOption = formallyOkOption;
+  }
+
+  @task
+  *onSave() {
+    if (this.args.onSave) {
+      yield this.args.onSave(this.selectedFormallyOkOption.uri);
+      }
+    this.args.cancelEdit();
+  }
+}
