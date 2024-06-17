db5452a66a5863ebc032180bd7e317f7a22c0bd0
Karel Persoons <karel@brenner.company>
2024-06-17 18:02:28 +0200

    KAS-4678 Changes to editor itself & styling (part 2)

--- app/components/sanitize-html.js
+++ app/components/sanitize-html.js
@@ -6,7 +6,8 @@ import { isPresent } from '@ember/utils';
 const additionalAllowedTags = ['del'];
 const additionalAllowedAttributes = {
   '*': ['data-indentation-level'],
-  'ol': ['data-list-style'],
+  'ol': ['style', 'data-hierarchical', 'data-list-style'],
+  'li': ['data-list-marker'],
   'table': [
     {
       name: 'id',
@@ -20,6 +21,11 @@ const additionalAllowedAttributes = {
     },
   ],
 };
+const additionalAllowedStyles = {
+  'ol': {
+    'list-style-type': [/.*/]
+  }
+}
 
 export default class SanitizeHtmlComponent extends Component {
   constructor() {
@@ -31,6 +37,7 @@ export default class SanitizeHtmlComponent extends Component {
     const options = this.args.options || {};
     options.allowedTags = isPresent(options.allowedTags) ? options.allowedTags : sanitizeHtml.defaults.allowedTags.concat(additionalAllowedTags); // add more tags to the default allowed tags
     options.allowedAttributes = isPresent(options.allowedAttributes) ? options.allowedAttributes : assign(sanitizeHtml.defaults.allowedAttributes, additionalAllowedAttributes); // add more attributes to the default allowed attributes
+    options.allowedStyles = isPresent(options.allowedStyles) ? options.allowedStyles : assign(sanitizeHtml.defaults.allowedStyles, additionalAllowedStyles); // add more styles to the default allowed styles
     const value = Array.isArray(this.args.value) ? this.args.value.join(' - ') : this.args.value || '';
     return sanitizeHtml(value, options) || '';
   }
