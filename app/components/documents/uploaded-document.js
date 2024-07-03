f516438c5d7b42faf58b2256e362e22478ca6f98
Kobe Mertens <kobe.mertens@hotmail.com>
2024-07-03 15:05:56 +0200

    add access level to uploaded-document component

--- app/components/documents/uploaded-document.js
+++ app/components/documents/uploaded-document.js
@@ -11,15 +11,26 @@ export default class UploadedDocument extends Component {
   @tracked documentTypes = [];
   @tracked documentContainer;
   @tracked selectedDocumentType;
+  @tracked selectedAccessLevel;
 
   constructor() {
     super(...arguments);
     this.loadData.perform();
   }
 
+  async setDefaultSelectedAccessLevel() {
+    const accessLevels = await this.conceptStore.queryAllByConceptScheme(
+      CONSTANTS.CONCEPT_SCHEMES.ACCESS_LEVELS
+    );
+    this.selectedAccessLevel = accessLevels.find(
+      (concept) => concept.uri === CONSTANTS.ACCESS_LEVELS.INTERN_REGERING
+    );
+  }
+
   @task
   *loadData() {
     if (this.args.allowDocumentContainerEdit) {
+      yield this.setDefaultSelectedAccessLevel();
       this.documentTypes = yield this.conceptStore.queryAllByConceptScheme(CONSTANTS.CONCEPT_SCHEMES.DOCUMENT_TYPES);
       this.documentContainer = yield this.args.piece.documentContainer;
       this.selectedDocumentType = yield this.documentContainer.type;
@@ -30,6 +41,12 @@ export default class UploadedDocument extends Component {
     return this.documentTypes
       .slice()
       .sort((d1, d2) => d1.position - d2.position);
+  }
+
+  @action
+  setPieceAccessLevel(accessLevel) {
+    this.args.piece.accessLevel = accessLevel;
+    this.selectedAccessLevel = accessLevel;
   }
 
   @action
