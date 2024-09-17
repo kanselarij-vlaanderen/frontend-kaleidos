25d28a7cbcac296a8a8e8cb71b49940e1b6cbd67
Sven Valenberghs <sven.valenberghs@faros.be>
2024-09-17 14:26:49 +0200

    KAS-4815 fix old inactive submitter not updated on new subcase

--- cypress/e2e/unit/test-new-command.cy.js
+++ cypress/e2e/unit/test-new-command.cy.js
@@ -8,6 +8,7 @@
 // import utils from '../../selectors/utils.selectors';
 // import auk from '../../selectors/auk.selectors';
 // import appuniversum from '../../selectors/appuniversum.selectors';
+import mandatee from '../../selectors/mandatee.selectors';
 import mandateeNames from '../../selectors/mandatee-names.selectors';
 
 context('testing new add subcase command', () => {
@@ -76,5 +77,61 @@ context('testing new add subcase command', () => {
     cy.visitCaseWithLink('dossiers/655B933CD500B784623C3EAA/deeldossiers');
 
     cy.addSubcaseViaModal(subcase1);
+  });
+
+  it('should map to active mandatee as submitter without manual edit', () => {
+    // reason: old subcase with old mandatees > new subcase should map to new mandatees and submitter.
+    // both mandatees and submitter have been wrong before with this automation (old mandatee)
+    const isCI = Cypress.env('CI');
+
+    const subcase1 = {
+      newShortTitle: 'submitter is correctly mapped to active mandatee',
+    };
+
+    let dateRange;
+    if (isCI) {
+      const date09052021 = Cypress.dayjs('2021-05-09').format('DD-MM-YYYY');
+      const date15052022 = Cypress.dayjs('2022-05-15').format('DD-MM-YYYY');
+      dateRange = `${date09052021} tot ${date15052022}`;
+    } else {
+      const date10052021 = Cypress.dayjs('2021-05-10').format('DD-MM-YYYY');
+      const date16052022 = Cypress.dayjs('2022-05-16').format('DD-MM-YYYY');
+      dateRange = `${date10052021} tot ${date16052022}`;
+    }
+
+    cy.visitCaseWithLink('dossiers/6374F30CD9A98BD0A2288557/deeldossiers/6374F312D9A98BD0A2288558');
+    cy.get(mandatee.mandateePanelView.rows).as('listItems');
+    cy.get('@listItems').should('have.length', 1, {
+      timeout: 5000,
+    });
+    cy.get('@listItems').eq(0)
+      .as('row');
+    cy.get('@row').find(mandatee.mandateePanelView.row.name)
+      .should('contain', mandateeNames['10052021-16052022'].second.fullName); // crevits old mandatee
+    cy.get('@row').find(mandatee.mandateePanelView.row.name)
+      .contains(dateRange);
+    cy.get('@row').find(mandatee.mandateePanelView.row.submitter)
+      .children()
+      .should('exist');
+
+    // new subcase
+    cy.addSubcaseViaModal(subcase1);
+
+    cy.get(mandatee.mandateePanelView.rows).as('listItems');
+    cy.get('@listItems').should('have.length', 1, {
+      timeout: 5000,
+    });
+    cy.get('@listItems').eq(0)
+      .as('row');
+    cy.get('@row').find(mandatee.mandateePanelView.row.name)
+      .should('contain', mandateeNames.current.second.fullName); // crevits new mandatee
+    cy.get('@row').find(mandatee.mandateePanelView.row.name)
+      .contains('tot heden'); // crevits new mandatee
+    cy.get('@row').find(mandatee.mandateePanelView.row.submitter) // crevits correct submitter selected
+      .children()
+      .should('exist');
+
+    // cleanup
+    cy.deleteSubcase();
   });
 });
