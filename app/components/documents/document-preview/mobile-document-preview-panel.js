8ca5c51f5a7a92a2138a48617733c2103f47021d
Tom De Nies <tom@firststage.be>
2024-10-02 18:22:14 +0200

    KAS-4838 added new document-preview for mobile devices

---
+++ app/components/documents/document-preview/mobile-document-preview-panel.js
@@ -0,0 +1,44 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { task } from 'ember-concurrency';
+
+/**
+ *
+ * Contains tabs:
+ * - "details"
+ * - "signatures" (translated to dutch for display)
+ * - "versions" (translated to dutch for display)
+ */
+export default class DocumentsDocumentPreviewMobileDocumentPreviewPanel extends Component {
+  @service currentSession;
+
+  @tracked documentContainer;
+  @tracked tab = 'details';
+
+  constructor() {
+    super(...arguments);
+    this.loadPieceData.perform();
+    if (this.args.tab) {
+      this.tab = this.args.tab;
+    }
+  }
+
+  get isShownSignatureTab() {
+    const hasPermission = this.currentSession.may('manage-signatures');
+    const isPiece = this.args.piece.constructor.modelName === 'piece';
+    return hasPermission && isPiece;
+  }
+
+  @task
+  *loadPieceData() {
+    this.documentContainer = yield this.args.piece.documentContainer;
+  }
+
+  @action
+  setTab(tabName) {
+    this.tab = tabName;
+    this.args.onTabChanged(tabName);
+  }
+}
---
+++ app/components/documents/document-preview/mobile-document-preview.js
@@ -0,0 +1,54 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
+import { inject as service } from '@ember/service';
+import sanitize from 'sanitize-filename';
+
+export default class DocumentsDocumentPreviewMobileDocumentPreview extends Component {
+  /**
+   * @argument piece
+   */
+
+  @service pieceAccessLevelService;
+
+  @tracked selectedVersion;
+  @tracked file;
+
+  constructor() {
+    super(...arguments);
+    this.selectedVersion = this.args.piece;
+    this.loadFile.perform();
+  }
+
+  // piece or file downloadlinks for not suitable for switch between source and derived
+  // we want the piece name + the correct file download
+  get downloadLink() {
+    if (this.file?.extension) {
+      const filename = `${this.selectedVersion.name}.${this.file.extension}`;
+      const downloadFilename = sanitize(filename, {
+        replacement: '_',
+      });
+      return `${this.file.downloadLink}?name=${encodeURIComponent(
+        downloadFilename
+      )}`;
+    }
+    return 'route-not-found';
+  }
+
+  get inlineViewLink() {
+    // only shown in pdf reader if extension is pdf, but always calculated.
+    return `${this.downloadLink}&content-disposition=inline`;
+  }
+
+  @task
+  *loadFile() {
+    this.file = null;
+    const file = yield this.selectedVersion.file;
+    const derivedFile = yield file?.derived;
+    this.file = derivedFile || file;
+  }
+
+  canViewConfidentialPiece = async () => {
+    return await this.pieceAccessLevelService.canViewConfidentialPiece(this.selectedVersion);
+  }
+}
