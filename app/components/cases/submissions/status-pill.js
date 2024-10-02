24de2bf8248d9ac22bc99d08eba342c23a89e9a7
Sven Valenberghs <sven.valenberghs@faros.be>
2024-10-02 16:55:05 +0200

    KAS-4842 move submissions page, change model to show not on closed agenda, pill label and color change

---
+++ app/components/cases/submissions/status-pill.js
@@ -0,0 +1,46 @@
+import { service } from '@ember/service';
+import Component from '@glimmer/component';
+import { task } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
+import CONSTANTS from 'frontend-kaleidos/config/constants';
+
+/**
+ * @argument status submission status concept
+ * @argument meeting optional, what meeting the submission is connected to
+ */
+export default class CasesSubmissionsStatusPillComponent extends Component {
+  @service intl;
+  @service store;
+
+  @tracked label;
+  @tracked skin = 'ongoing';
+
+  constructor() {
+    super(...arguments);
+    this.loadStatusText.perform();
+  }
+
+  loadStatusText = task(async () => {
+    // await promises
+    const status = await this.args.status;
+    const meeting = await this.args.meeting;
+    // show status already
+    this.label = status.label || status.altLabel;
+
+    // check if submission is on a visible agenda
+    if (meeting?.id){
+      const agenda = await this.store.queryOne('agenda', {
+        'filter[created-for][:id:]':meeting.id,
+        sort: '-created', // serialnumber
+      });
+      if (agenda?.id) {
+        // different label and skin when submission is treated on an active agenda
+        if (status.uri === CONSTANTS.SUBMISSION_STATUSES.BEHANDELD) {
+          this.label = this.intl.t('on-agenda-status-pill');
+          this.skin = "success";
+          return;
+        }
+      }
+    }
+  });
+}
---
+++ app/routes/submissions.js
@@ -0,0 +1,105 @@
+import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { isPresent } from '@ember/utils';
+import { startOfDay, endOfDay } from 'date-fns';
+// import CONSTANTS from 'frontend-kaleidos/config/constants';
+import parseDate from 'frontend-kaleidos/utils/parse-date-search-param';
+
+export default class SubmissionsRoute extends Route {
+  @service currentSession;
+  @service conceptStore;
+  @service store;
+  @service router;
+  @service('session') simpleAuthSession;
+
+  queryParams = {
+    page: {
+      refreshModel: true,
+      as: 'pagina',
+    },
+    size: {
+      refreshModel: true,
+      as: 'aantal',
+    },
+    sortSubmissions: {
+      refreshModel: true,
+      as: 'sorteer',
+    },
+    dateFrom: {
+      refreshModel: true,
+      as: 'van',
+    },
+    dateTo: {
+      refreshModel: true,
+      as: 'tot',
+    },
+    submitters: {
+      refreshModel: true,
+      as: 'indieners',
+    },
+  };
+
+  async beforeModel(transition) {
+    this.simpleAuthSession.requireAuthentication(transition, this.simpleAuthSession.unauthenticatedRouteName);
+
+    if (!this.currentSession.may('view-submissions')) {
+      this.router.transitionTo('cases.index');
+    }
+  }
+
+  async model(params) {
+    // *note: the cache busting delays the loading a bit, even locally with only 2 submissions it take half a second
+    const options = {
+      'filter[:has:created]': `date-added-for-cache-busting-${new Date().toISOString()}`,
+      'filter[pieces][accepted-piece][agendaitems][agenda][created-for][:has-no:agenda]': 't', // filter out submissions on closed meetings
+      include: 'type,status,requested-by,mandatees.person,submission-activities,decisionmaking-flow,meeting',
+      sort: params.sortSubmissions + (params.sortSubmissions ? ',' : '') + '-modified',
+      page: {
+        number: params.page,
+        size: params.size,
+      },
+    };
+
+    if (isPresent(params.dateFrom)) {
+      const date = startOfDay(parseDate(params.dateFrom));
+      options['filter[:gte:planned-start]'] = date.toISOString();
+    }
+    if (isPresent(params.dateTo)) {
+      const date = endOfDay(parseDate(params.dateTo));
+      options['filter[:lte:planned-start]'] = date.toISOString();
+    }
+
+    if (isPresent(params.submitters)) {
+      const submitters = Array.isArray(params.submitters)
+        ? params.submitters.join(',')
+        : params.submitters || '';
+      options['filter[requested-by][person][:id:]'] = submitters;
+    }
+
+    if (!this.currentSession.may('view-all-submissions')) {
+      options['filter[mandatees][user-organizations][:id:]'] =
+        this.currentSession.organization.id;
+    }
+
+    return this.store.query('submission', options);
+  }
+
+  // when filtering on date, show a loader
+  @action
+  loading(transition) {
+    // eslint-disable-next-line ember/no-controller-access-in-routes
+    const controller = this.controllerFor(this.routeName);
+    controller.isLoadingModel = true;
+    transition.promise.finally(() => {
+      controller.isLoadingModel = false;
+    });
+    // false so we don't transition to the loading route when searching
+    if (transition.from && transition.to) {
+      return transition.from.name != transition.to.name;
+    } else {
+      return false;
+    }
+  }
+}
+
