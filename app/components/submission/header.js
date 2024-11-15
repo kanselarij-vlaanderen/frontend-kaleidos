9c82accf065ecbdd4a12779ed5d8ea086cdaef0c
Sergio Fenoll <sergio@fenoll.be>
2024-11-15 12:29:13 +0100

    fix: only show proposable agenda modal when submission is not an update
    When the submission is an update (i.e. changes to a BIS submission) we don't want to show the proposable agenda modal as changing the meeting doesn't make sense in those cases.

--- app/components/submission/header.js
+++ app/components/submission/header.js
@@ -187,13 +187,21 @@ export default class SubmissionHeaderComponent extends Component {
     await this.draftSubmissionService.updateSubmissionStatus(this.args.submission, statusUri, comment);
   };
 
+  // This method is passed to the modal
+  // In case of an update, the modal passes no parameters through,
+  // so we just have to use the members of the component.
   resubmitSubmission = task(async (meeting, remarks) => {
+    this.toggleResubmitModal();
+    const selectedMeeting = meeting?.id ? meeting : this.selectedMeeting;
+    const comment = meeting?.id ? remarks : this.comment;
+
     await this._updateSubmission(
       CONSTANTS.SUBMISSION_STATUSES.OPNIEUW_INGEDIEND,
-      remarks,
+      comment,
     );
-    await this.agendaService.putDraftSubmissionOnAgenda(meeting, this.args.submission);
-    await this.cabinetMail.sendResubmissionMails(this.args.submission, remarks, meeting);
+    if (meeting?.id)
+      await this.agendaService.putDraftSubmissionOnAgenda(meeting, this.args.submission);
+    await this.cabinetMail.sendResubmissionMails(this.args.submission, comment, selectedMeeting);
     if (isPresent(this.args.onStatusUpdated)) {
       this.args.onStatusUpdated();
     }
