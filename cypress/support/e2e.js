c5fe5d3a795224b06298d65cb216278ae4c7b159
Sven Valenberghs <sven.valenberghs@faros.be>
2024-11-25 14:21:13 +0100

    KAS-4908 downgrade mock-login, comment override in cypress

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -63,9 +63,9 @@ Cypress.Commands.overwrite("type", (originalFn, subject, text, options) => {
 });
 
 // rapid succession of visits to multiple routes with "this.simpleAuthSession.requireAuthentication" seems to be failing unless we add a delay
-Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
-  return setTimeout(() => originalFn(url, options), 100)
-});
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => {
+//   return setTimeout(() => originalFn(url, options), 100)
+// });
 
 
 
