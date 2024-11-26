260ce24f35671eb52d64a4d9f64c153f1be6451f
Sven Valenberghs <sven.valenberghs@faros.be>
2024-11-26 09:52:47 +0100

    KAS-4919 include embroiders/macros to dependencies

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -42,6 +42,7 @@ module.exports = function (defaults) {
     'ember-test-selectors': {
       strip: false
     },
+    //polyfill for insecure context (like cypress on jenkins) https://github.com/emberjs/data/tree/v5.0.0?tab=readme-ov-file#randomuuid-polyfill
     '@embroider/macros': {
       setConfig: {
         '@ember-data/store': {
