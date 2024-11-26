49eb49b19c78375b17f9a955346e2751a89d27ac
Sven Valenberghs <sven.valenberghs@faros.be>
2024-11-26 15:57:00 +0100

    KAS-4922 fix ember-inflector usage deprecation

--- app/services/agenda-service.js
+++ app/services/agenda-service.js
@@ -1,6 +1,6 @@
 import Service, { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import { singularize } from 'ember-inflector';
+import { singularize } from '@ember-data/request-utils/string'
 import fetch from 'fetch';
 import { isEnabledCabinetSubmissions } from 'frontend-kaleidos/utils/feature-flag';
 import CONSTANTS from 'frontend-kaleidos/config/constants';
