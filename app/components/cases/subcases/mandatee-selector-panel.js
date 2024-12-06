c1a7709a37d8ffe8ff1fc4cc52fc58fa8b0ef3e9
Sven Valenberghs <sven.valenberghs@faros.be>
2024-12-06 10:46:28 +0100

    remove console logs, change dockerfile

--- app/components/cases/subcases/mandatee-selector-panel.js
+++ app/components/cases/subcases/mandatee-selector-panel.js
@@ -115,14 +115,12 @@ export default class MandateeSelectorPanel extends Component {
     if (this.args.mandatees?.length) {
       let selectedMandatees = [];
       // Only select args mandatees if they are still active
-      console.log(selectedMandatees);
       for (const oldMandatee of this.args.mandatees) {
         const mandatee = this.currentMandatees.find(
           (currentMandatee) =>
             currentMandatee.get('id') === oldMandatee.id
         );
         if (mandatee) {
-          console.log('mandatee', mandatee);
           // the mandatee is a current one
           selectedMandatees.push(mandatee);
         } else {
@@ -147,15 +145,13 @@ export default class MandateeSelectorPanel extends Component {
           }
         }
       }
-      console.log('selectedMandatees', selectedMandatees);
-      
       let ministersToSelect = [];
+      // yield Promise.all(selectedMandatees?.map ... starting throwing "void(0) is not a function
       if (selectedMandatees) {
         ministersToSelect = yield Promise.all(
           selectedMandatees.map((m) => m.person)
         );
       }
-      console.log('ministersToSelect', ministersToSelect);
       // Try to match the args.submitter to an entry from the mandatees list
       if (this.showSubmitter) {
         // First try to select the active mandatee of the submitter person
