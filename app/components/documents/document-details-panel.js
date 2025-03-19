b7c154efb075bc10c0f174747c562cb73f98f3c0
Sven Valenberghs <sven.valenberghs@faros.be>
2025-03-19 15:43:28 +0100

    KAS-5026 fix incorrect deletion

--- app/components/documents/document-details-panel.js
+++ app/components/documents/document-details-panel.js
@@ -179,7 +179,7 @@ export default class DocumentsDocumentDetailsPanel extends Component {
     if (this.uploadedSourceFile) {
       // use-case: we have a pdf and we want to add docx but keep our pdf
       // derived file does not exist yet in this case
-      const oldFile = yield this.args.piece;
+      const oldFile = yield this.args.piece.file;
       this.uploadedSourceFile.derived = oldFile;
       this.args.piece.file = this.uploadedSourceFile;
       yield Promise.all([oldFile.save(), this.uploadedSourceFile.save()]);
