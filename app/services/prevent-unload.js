772e486e2a98bbdb53d241ac84eb3907c3bc47b2
Sven Valenberghs <sven.valenberghs@faros.be>
2025-06-25 15:24:29 +0200

    KAS-5068 add preventunload and decision report generation to agendaitem panel edit

--- app/services/prevent-unload.js
+++ app/services/prevent-unload.js
@@ -29,9 +29,12 @@ export default class PreventUnloadService extends Service {
   __emberListener = (transition) => {
     if (
       !transition.to.find(route => route.name === this.router.currentRouteName) &&
-      transition.to.localName !== 'loading' &&
-      !confirm('Als u de pagina nu verlaat bent u alle aanpassingen kwijt. Bent u zeker dat u weg wilt navigeren?')) {
-      transition.abort();
+      transition.to.localName !== 'loading') {
+      if(!confirm('Als u de pagina nu verlaat bent u alle aanpassingen kwijt. Bent u zeker dat u weg wilt navigeren?')) {
+        transition.abort();
+      } else {
+        this.disable();
+      }
     }
   }
 }
