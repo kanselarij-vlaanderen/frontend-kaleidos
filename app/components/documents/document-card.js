d274aa69bfe50e19b67e963d6fd3967b25e6b904
Sven Valenberghs <sven.valenberghs@faros.be>
2025-09-11 14:44:39 +0200

    KAS-4425 add and implement automation to ensure signedPiece(Copy) have the correct accesslevel based on main piece

--- app/components/documents/document-card.js
+++ app/components/documents/document-card.js
@@ -476,6 +476,7 @@ export default class DocumentsDocumentCardComponent extends Component {
   async saveAccessLevel() {
     await this.piece.belongsTo('file').reload(); // concurrent edits of file are possible like when signatures are stripped
     await this.piece.save();
+    await this.pieceAccessLevelService.updateSignedPieceAccessLevels(this.piece);
     await this.pieceAccessLevelService.updatePreviousAccessLevels(this.piece);
     if (this.hasConfidentialityChanged && this.args.onChangeConfidentiality) {
       await this.args?.onChangeConfidentiality();
@@ -493,6 +494,7 @@ export default class DocumentsDocumentCardComponent extends Component {
   @action
   async saveAccessLevelOfPiece(piece) {
     await piece.save();
+    await this.pieceAccessLevelService.updateSignedPieceAccessLevels(piece);
     await this.pieceAccessLevelService.updatePreviousAccessLevels(piece);
   }
 
