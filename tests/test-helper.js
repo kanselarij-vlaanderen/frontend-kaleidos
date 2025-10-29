019e7a6e71e1c74346b13743470ecf8a0a9e6ceb
Sven Valenberghs <sven.valenberghs@faros.be>
2025-10-29 12:19:46 +0100

    KAS-5097 bare minimum for ember v6 with overrides

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,12 +1,14 @@
 import Application from '../app';
 import config from '../config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
 import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-
-setup(QUnit.assert);
+import { loadTests } from 'ember-qunit/test-loader';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
 
+setup(QUnit.assert);
+setupEmberOnerrorValidation();
+loadTests();
 start();
