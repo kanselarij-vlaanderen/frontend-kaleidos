decb5b2d0b3f41874f4d99cb4d3382878dfc643e
Sven Valenberghs <sven.valenberghs@faros.be>
2025-10-30 13:08:18 +0100

    KAS-5097 fix deprecation-workflow config

--- app/deprecation-workflow.js
+++ app/deprecation-workflow.js
@@ -20,5 +20,18 @@ setupDeprecationWorkflow({
      */
     /* example: */
     /* { handler: 'silence', matchId: 'template-action' }, */
+    { handler: "throw", matchId: "remove-owner-inject" },
+    { handler: "throw", matchId: "ember-data:deprecate-array-like" },
+    { handler: "throw", matchId: "ember-data:non-explicit-relationships" },
+    { handler: "throw", matchId: "ember-data:deprecate-non-strict-relationships" },
+    { handler: "throw", matchId: "ember-data:deprecate-promise-proxies" },
+    { handler: "throw", matchId: "ember-data:deprecate-promise-many-array-behaviors" },
+    { handler: "throw", matchId: "ember-data:model-save-promise" },
+    { handler: "throw", matchId: "ember-data:no-a-with-array-like" },
+    // newer ones
+    // TODO set this to throw after merging relevant PR
+    { handler: "silence", matchId: "importing-inject-from-ember-service" },
+    // TODO set this to throw after fixing and upgrading ember-concurrency
+    { handler: "silence", matchId: "ember-concurrency.deprecate-decorator-task" },
   ],
 });
