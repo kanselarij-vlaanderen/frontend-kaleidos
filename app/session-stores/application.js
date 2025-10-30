92a5f27dafcb822fbf2881db4e054ce55081cf4a
Sven Valenberghs <sven.valenberghs@faros.be>
2025-10-30 11:27:31 +0100

    KAS-5097 upgrade ember-simple-auth

---
+++ app/session-stores/application.js
@@ -0,0 +1,4 @@
+import AdaptiveStore from 'ember-simple-auth/session-stores/adaptive';
+
+// this is needed from ember-simple-auth v7, this no longer happens automatically
+export default class SessionStore extends AdaptiveStore {}
