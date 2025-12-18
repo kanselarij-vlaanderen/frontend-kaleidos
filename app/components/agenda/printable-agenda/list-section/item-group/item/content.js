1ff918e4843f98b3e2213ae633fc57dea5177bc4
Sven Valenberghs <sven.valenberghs@faros.be>
2025-12-18 14:59:24 +0100

    KAS-5073 make incoming array filter optional if undefined

--- app/components/agenda/printable-agenda/list-section/item-group/item/content.js
+++ app/components/agenda/printable-agenda/list-section/item-group/item/content.js
@@ -4,7 +4,7 @@ export default class AgendaPrintableAgendaListSectionItemGroupItemContentCompone
   get agendaitemNumber() {
     // to fill gaps in numbering, we check if the mapping has a new number for @item
     const agendaitemMapping = this.args.newNumberMappings
-      .filter((mapping) => mapping.agendaitem.id == this.args.item.id)
+      ?.filter((mapping) => mapping.agendaitem.id == this.args.item.id)
       .at(0);
     return agendaitemMapping?.newNumber || this.args.item.number;
   }
